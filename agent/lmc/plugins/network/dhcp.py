40165696592cd44d338afab74a70afbff93ee02a
Cédric Delfosse <cdelfosse@mandriva.com>
2007-06-06 08:52:32 +0000

    * Add GPL licence and SVN keywords property

--- agent/lmc/plugins/network/dhcp.py
+++ agent/lmc/plugins/network/dhcp.py
@@ -1,3 +1,24 @@
+#
+# (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+#
+# $Id$
+#
+# This file is part of LMC.
+#
+# LMC is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# LMC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with LMC; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 import ldap
 from lmc.plugins.base import ldapUserGroupControl, LogView
 from tools import *
