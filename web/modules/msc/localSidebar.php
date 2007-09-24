f4982a7fc1556d508e1e644490e26bcf9236db00
Olivier Roussy <oroussy@mandriva.com>
2007-09-24 10:17:52 +0000

    . remove the explorer .

--- web/modules/msc/localSidebar.php
+++ web/modules/msc/localSidebar.php
@@ -4,7 +4,7 @@ $sidemenu= new SideMenu();
 $sidemenu->setClass("msc");
 $sidemenu->addSideMenuItem(new SideMenuItem(_T("General"), "msc", "msc", "general"));
 $sidemenu->addSideMenuItem(new SideMenuItem(_T("Repository"), "msc", "msc", "repository"));
-$sidemenu->addSideMenuItem(new SideMenuItem(_T("Explorer"), "msc", "msc", "explorer"));
+//$sidemenu->addSideMenuItem(new SideMenuItem(_T("Explorer"), "msc", "msc", "explorer"));
 $sidemenu->addSideMenuItem(new SideMenuItem(_T("Commands states"), "msc", "msc", "cmd_state"));
 $sidemenu->addSideMenuItem(new SideMenuItem(_T("All commands states"), "msc", "msc", "all_cmd_state"));
 
