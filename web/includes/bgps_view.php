d3cf35eaeded9a3b98a4f4ffa525451256f108ba
Nicolas Rueff <nrueff@mandriva.com>
2007-10-10 08:08:05 +0000

    changed code indentation

--- web/includes/bgps_view.php
+++ web/includes/bgps_view.php
@@ -31,48 +31,46 @@ require("session.inc.php");
 require("PageGenerator.php");
 
 
-function
-print_mem_bar($title, $max, $used, $cache = 0, $width = 320)
-{
-  $wused = ($used / $max) * $width;
+function print_mem_bar($title, $max, $used, $cache = 0, $width = 320) {
+    $wused = ($used / $max) * $width;
 
-  if ($title != "")
-    {
-      echo $title." :";
+    if ($title != "") {
+        echo $title." :";
     }
-  echo "<div class=\"membarfree\" style=\"width: ".$width."px\">";
-  if ($cache > 0)
-    {
-      printf("<div class=\"membarcache\" style=\"width: %.0fpx\">", $wused);
-      $wused = (($used - $cache) / $max) * $width;
+    
+    echo "<div class=\"membarfree\" style=\"width: ".$width."px\">";
+    
+    if ($cache > 0) {
+        printf("<div class=\"membarcache\" style=\"width: %.0fpx\">", $wused);
+        $wused = (($used - $cache) / $max) * $width;
     }
-  printf("<div class=\"membarused\" style=\"width: %.0fpx\"></div>", $wused);
+    
+    printf("<div class=\"membarused\" style=\"width: %.0fpx\"></div>", $wused);
 
-  if ($cache > 0)
-    {
-      echo "</div>";
+    if ($cache > 0) {
+        echo "</div>";
     }
-  echo "</div>\n";
+    
+    echo "</div>\n";
 }
 
-    function get_process()
-    {
-        return xmlCall("base.listProcess",null);
-    }
+function get_process() {
+    return xmlCall("base.listProcess",null);
+}
 
-    $arr = get_process();
+$arr = get_process();
 
-    if (count($arr) == 0) { //if no job in background
-        print '<div style="text-align: center;">'._("No job.").'</div>';
-        return;
-    }
+if (count($arr) == 0) { //if no job in background
+    print '<div style="text-align: center;">'._("No job.").'</div>';
+    return;
+}
 
-    foreach ($arr as $ps) {
-        echo $ps[0]."<br/>";
-        echo $ps[2]."<br/>";
-        if ($ps[1] != "-1") {
-            print_mem_bar("progress",100,$ps[1]);
-        }
+foreach ($arr as $ps) {
+    echo $ps[0]."<br/>";
+    echo $ps[2]."<br/>";
+    if ($ps[1] != "-1") {
+        print_mem_bar("progress",100,$ps[1]);
     }
+}
 
 ?>
