9d3c7a2a1bfd55fbe0c726986b36482d19083352
Cédric Delfosse <cdelfosse@mandriva.com>
2007-10-26 13:16:29 +0000

    * Dump a more comprehensible traceback on error

--- web/includes/ErrorHandling.php
+++ web/includes/ErrorHandling.php
@@ -21,11 +21,9 @@
  * along with MMC; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
-?>
-<?php
 
 /**
- * class who handle error message
+ * class which handle error message
  */
 class ErrorHandlingItem {
     var $msg;
@@ -60,38 +58,41 @@ class ErrorHandlingItem {
         $this->registerNotify($xmlResponse);
     }
 
+    /**
+     * Prepare error message to display
+     */
     function registerNotify($xmlResponse) {
+        if ($this->_level !=0 ) {
+            $str= '<div id="errorCode">';
+        } else {
+            $str = '<div>';
+        }
 
-                if ($this->_level !=0 ) {
-                    $str= '<div id="errorCode">';
-                } else {
-                    $str = '<div>';
-                }
+        $str .= "<h3>" . $this->getMsg() . "</h3>";
+        $str .= "<p>" . $this->getAdvice() . "</p>";;
 
-                $str.= "<h3>".$this->getMsg()."</h3>";
-                $str.= "<p>".$this->getAdvice()."</p>";;
-
-                if ($this->_showTraceBack) {
-                    $str.= '<a href="#" onClick="Effect.toggle(\'errorTraceback\',\'slide\');">'._("Show complete trackback").'</a>';
-                    $str.= '<div id="errorTraceback" style="display:none;"><h1 style="margin-top: 1em; margin-bottom: 0.2em;">'._("Complete Traceback").'</h1><pre>';
-                    $str.= $xmlResponse["faultTraceback"]."\n".htmlentities($xmlResponse["faultCode"]);
-                    $str.= '</pre></div>';
-                }
-
-                $str.='</div>';
-
-		$n = new NotifyWidget();
-                $n->setSize($this->_size);
-                $n->setLevel($this->_level);
-		$n->add($str);
+        if ($this->_showTraceBack) {
+            $str .= '<a href="#" onClick="Effect.toggle(\'errorTraceback\',\'slide\');">'._("Show complete trackback").'</a>';
+            $str .= '<div id="errorTraceback" style="display:none;"><h1 style="margin-top: 1em; margin-bottom: 0.2em;">'._("Complete Traceback").'</h1><pre>';
+            $str .= "PHP XMLRPC call: " . $xmlResponse["faultString"] . "\n\n";
+            $str .= "Python Server traceback:\n";
+            $str .= $xmlResponse["faultTraceback"]."\n";
+            $str .= '</pre></div>';
+        }        
+        $str .= '</div>';
+        
+        $n = new NotifyWidget();
+        $n->setSize($this->_size);
+        $n->setLevel($this->_level);
+        $n->add($str);
     }
 
     function setMsg($msg) {
-    $this->msg = $msg;
+        $this->msg = $msg;
     }
 
     function setAdvice($adv) {
-    $this->advice = $adv;
+        $this->advice = $adv;
     }
 
     function match($errorMsg) {
