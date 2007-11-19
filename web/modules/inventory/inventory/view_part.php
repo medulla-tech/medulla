8b6eed0e1dd3a94986bc350262a0b6a1696b287c
Olivier Roussy <oroussy@mandriva.com>
2007-11-19 15:43:54 +0000

    . in some case, it has already been required .

--- web/modules/inventory/inventory/view_part.php
+++ web/modules/inventory/inventory/view_part.php
@@ -22,7 +22,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-require("modules/inventory/includes/xmlrpc.php");
+require_once("modules/inventory/includes/xmlrpc.php");
 
 ?>
 
