3012ef2409fac79a71a2e5bd92544f21676615a6
Olivier Roussy <oroussy@mandriva.com>
2007-12-18 13:08:11 +0000

    . put good names for i18n files .

--- web/modules/msc/tabs.php
+++ web/modules/msc/tabs.php
@@ -6,9 +6,9 @@ require("graph/navbar.inc.php");
 $p = new TabbedPageGenerator();
 $p->setSideMenu($sidemenu);
 $p->addTop(sprintf(_T("%s's secure control"), $_GET['name']), "modules/msc/msc/header.php");
-$p->addTab("tablaunch", "Launch Actions", "", "modules/msc/msc/launch.php", array('name'=>$_GET['name']));
-$p->addTab("tablogs", "Logs", "", "modules/msc/msc/logs.php", array('name'=>$_GET['name']));
-$p->addTab("tabhistory", "History", "", "modules/msc/msc/history.php", array('name'=>$_GET['name']));
+$p->addTab("tablaunch", _T("Launch Actions"), "", "modules/msc/msc/launch.php", array('name'=>$_GET['name']));
+$p->addTab("tablogs", _T("Logs"), "", "modules/msc/msc/logs.php", array('name'=>$_GET['name']));
+$p->addTab("tabhistory", _T("History"), "", "modules/msc/msc/history.php", array('name'=>$_GET['name']));
 $p->display();
 
 ?>
