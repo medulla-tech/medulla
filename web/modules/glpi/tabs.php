61a981b37fa1ce08a75eea39dae01e9d14e7e8f2
Olivier Roussy <oroussy@mandriva.com>
2007-12-18 13:35:55 +0000

    . fix i18n labels .

--- web/modules/glpi/tabs.php
+++ web/modules/glpi/tabs.php
@@ -5,13 +5,13 @@ require("graph/navbar.inc.php");
 
 $p = new TabbedPageGenerator();
 $p->setSideMenu($sidemenu);
-$p->addTop(sprintf(_T("%s's inventory (GLPI)"), $_GET['name']), "modules/glpi/glpi/header.php");
-$p->addTab("tab0", "Hardware", "", "modules/glpi/glpi/view_hardware.php", array('name'=>$_GET['name']));
+$p->addTop(sprintf(_T("%s's inventory (GLPI)", "glpi"), $_GET['name']), "modules/glpi/glpi/header.php");
+$p->addTab("tab0", _T("Hardware", "glpi"), "", "modules/glpi/glpi/view_hardware.php", array('name'=>$_GET['name']));
 
 $i = 1;
 // TODO get the list with trads from agent (conf file...)
-foreach (array('Network', 'Controller') as $tab) {
-    $p->addTab("tab$i", $tab, "", "modules/glpi/glpi/view_part.php", array('name'=>$_GET['name'], 'part' => $tab));
+foreach (array('Network'=>_T('Network', "glpi"), 'Controller'=>_T('Controller', "glpi")) as $tab=>$str) {
+    $p->addTab("tab$i", $str, "", "modules/glpi/glpi/view_part.php", array('name'=>$_GET['name'], 'part' => $tab));
     $i++;
 }
 
