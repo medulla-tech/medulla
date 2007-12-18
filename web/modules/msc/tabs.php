dbae6ae4ab9592055502744b77e3056791416906
Olivier Roussy <oroussy@mandriva.com>
2007-12-18 14:05:11 +0000

    . new i18n labels .

--- web/modules/msc/tabs.php
+++ web/modules/msc/tabs.php
@@ -5,10 +5,10 @@ require("graph/navbar.inc.php");
 
 $p = new TabbedPageGenerator();
 $p->setSideMenu($sidemenu);
-$p->addTop(sprintf(_T("%s's secure control"), $_GET['name']), "modules/msc/msc/header.php");
-$p->addTab("tablaunch", _T("Launch Actions"), "", "modules/msc/msc/launch.php", array('name'=>$_GET['name']));
-$p->addTab("tablogs", _T("Logs"), "", "modules/msc/msc/logs.php", array('name'=>$_GET['name']));
-$p->addTab("tabhistory", _T("History"), "", "modules/msc/msc/history.php", array('name'=>$_GET['name']));
+$p->addTop(sprintf(_T("%s's secure control", 'msc'), $_GET['name']), "modules/msc/msc/header.php");
+$p->addTab("tablaunch", _T("Launch Actions", 'msc'), "", "modules/msc/msc/launch.php", array('name'=>$_GET['name']));
+$p->addTab("tablogs", _T("Logs", 'msc'), "", "modules/msc/msc/logs.php", array('name'=>$_GET['name']));
+$p->addTab("tabhistory", _T("History", 'msc'), "", "modules/msc/msc/history.php", array('name'=>$_GET['name']));
 $p->display();
 
 ?>
