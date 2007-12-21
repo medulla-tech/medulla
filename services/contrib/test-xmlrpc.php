0ed79d016680116b016dbc8365f93cdadb87d2b9
Nicolas Rueff <nrueff@mandriva.com>
2007-12-21 09:37:07 +0000

    Initial commit

---
+++ services/contrib/test-xmlrpc.php
@@ -0,0 +1,151 @@
+#!/usr/bin/php
+<?
+
+function decode_entities($text) {
+    $text = html_entity_decode($text,ENT_QUOTES,"ISO-8859-1"); /* NOTE: UTF-8 does not work! */
+    $text= preg_replace('/&#(\d+);/me',"chr(\\1)",$text); /* decimal notation */
+    $text= preg_replace('/&#x([a-f0-9]+);/mei',"chr(0x\\1)",$text);  /* hex notation */
+    return $text;
+}
+
+if (!isset($conf["global"]["login"])) {
+    $conf["login"] = "";
+    $conf["password"] = "";
+    $conf["host"] = "127.0.0.1";
+    $conf["port"] = "8001";
+    $conf["scheme"] = "http";
+    $conf["debug"]["level"]="0";
+}
+
+$params = $_SERVER["argv"];
+array_shift($params);
+$method = array_shift($params);
+
+$output_options = array( "output_type" => "xml", "verbosity" => "pretty", "escaping" => array("markup", "non-ascii", "non-print"), "version" => "xmlrpc", "encoding" => "UTF-8" );
+
+if ($params == null) {
+    $request = xmlrpc_encode_request($method, null, $output_options);
+} else {
+    $request = xmlrpc_encode_request($method, $params, $output_options);
+    $request = decode_entities($request, ENT_QUOTES, "UTF-8");
+}
+
+/* We build the HTTP POST that will be sent */
+$host= $conf["host"].":".$conf["port"];
+$url = "/";
+$httpQuery = "POST ". $url ." HTTP/1.0\r\n";
+$httpQuery .= "User-Agent: xmlrpc\r\n";
+$httpQuery .= "Host: ". $host ."\r\n";
+$httpQuery .= "Content-Type: text/xml\r\n";
+$httpQuery .= "Content-Length: ". strlen($request) . "\r\n";
+
+$httpQuery .= "Authorization: Basic ".base64_encode($conf["login"].":".$conf["password"]) . "\r\n\r\n";
+$httpQuery .= $request;
+
+$sock = null;
+
+/* Connect to the XML-RPC server */
+if ($conf["scheme"] == "https") {
+    $prot = "ssl://";
+}
+$errLevel = error_reporting();
+error_reporting(0);
+$sock = fsockopen($prot.$conf["host"], $conf["port"], $errNo, $errString);
+error_reporting($errLevel);
+if (!$sock) {
+    print("Can't connect to the agent\n");
+    exit(-1);
+}
+
+/* Send the HTTP POST */
+if ( !fwrite($sock, $httpQuery, strlen($httpQuery)) ) {
+    print("Can't send XML-RPC request to MMC agent");
+    exit(-1);
+}
+fflush($sock);
+
+/* Get the response from the server */
+while (!feof($sock)) {
+    $xmlResponse .= fgets($sock);
+}
+fclose($sock);
+
+/* Process the response */
+if (!strlen($xmlResponse)) {
+    print("MMC agent communication problem");
+    exit(-1);
+}
+
+/* Process the received HTTP header */
+$pos = strpos($xmlResponse, "\r\n\r\n");
+$httpHeader = substr($xmlResponse, 0, $pos);
+if ($method == "base.ldapAuth") {
+    /* The RPC server must send us a session cookie */
+    if (preg_match("/(TWISTED_SESSION=[0-9a-f]+);/", $httpHeader, $match) > 0) {
+        $_SESSION["RPCSESSION"] = $match[1];
+    } else {
+        /* Can't get a session from the Twisted XML-RPC server */
+        print("MMC agent communication problem");
+        exit(-1);
+    }
+}
+
+/* Process the XML response */
+$xmlResponse = substr($xmlResponse, $pos + 4);
+/*
+   Test if the XMLRPC result is a boolean value set to False.
+   If it is the case, xmlrpc_decode will return an empty string.
+   So we need to test this special case.
+*/
+$booleanFalse = "<?xml version='1.0'?>\n<methodResponse>\n<params>\n<param>\n<value><boolean>0</boolean></value>\n</param>\n</params>\n</methodResponse>\n";
+if ($xmlResponse == $booleanFalse) $xmlResponse = "0";
+else {
+    $xmlResponseTmp = xmlrpc_decode($xmlResponse, "UTF-8");
+    /* if we cannot decode in UTF-8 */
+    if (!$xmlResponseTmp) {
+        /* Maybe we received data encoded in ISO latin 1, so convert them
+           to UTF8 first*/
+        $xmlResponse = iconv("ISO-8859-1", "UTF-8", $xmlResponse);
+        $xmlResponse = xmlrpc_decode($xmlResponse, "UTF-8");
+    } else {
+        $xmlResponse = $xmlResponseTmp;
+    }
+}
+
+/* If debug is on, print the XML-RPC call and result */
+if ($conf["debug"]["level"]!=0) {
+    $str = '<div id="debugCode">';
+    $str .= "&nbsp;&nbsp;debuginfo";
+    $str.= '<div id="debugCodeHeader">';
+    $str .= "XML RPC CALL FUNCTION: $method(";
+    if (!$params) {
+        $params = "null";
+    } else if (is_array($params)) {
+        $str .= implode (',',$params);
+    } else {
+        $str .= $params;
+    }
+    $str .=')';
+    $str .= "</div>";
+    if (is_array($xmlResponse)) {
+        $str .= "<pre>";
+        $str .= "result : ";
+        $str .= var_export($xmlResponse);
+        $str .= "</pre>";
+    } else {
+        $str .= "result : ".$xmlResponse;
+    }
+    $str .= '</div>';
+    echo $str;
+}
+
+/* If the XML-RPC server sent a fault, display an error */
+if (($xmlResponse["faultCode"])&&(is_array($xmlResponse))) {
+    print($xmlResponse["faultCode"].":".$xmlResponse["faultString"]."\n");
+    exit -1;
+}
+
+/* Return the result of the remote procedure call */
+print_r($xmlResponse);
+echo "\n";
+?>
---
+++ services/pulse2/launcher/remote_exec.py
@@ -0,0 +1,170 @@
+#!/usr/bin/python
+# -*- coding: utf-8; -*-
+#
+# (c) 2007 Mandriva, http://www.mandriva.com/
+#
+# $Id: agent.py 298 2007-10-30 14:46:30Z cedric $
+#
+# This file is part of Pulse 2.
+#
+# Pulse 2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pulse 2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pulse 2; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import logging
+import os
+import sys
+import twisted.internet.reactor
+
+def set_default_client_options(client):
+    """
+        client i a simple dict, which should contain required connexion infos, for now:
+            protocol: which one to use for connexion, default to 'ssh' which is the only one supported for now
+            host: where to connect, default to 127.0.0.1, fqdn and ipv4/v6 authorized
+            port: default depends on chosen protocol
+            user: when auth is needed, default is root
+            passwd: when auth is needed, default is "" (empty string)
+            cert: when auth is needed, default depends on chosen protocol
+            options: array of strings to pass to the connexion initiator
+    """
+
+    if not client:
+        client = {
+            'protocol': None,
+            'host': None,
+            'port': None,
+            'user': None,
+            'passwd': None,
+            'cert': None,
+            'options': []
+        }
+
+    if not client['protocol']: client['protocol'] = 'ssh'
+    if not client['host']: client['host'] = 'pulse2-win2k'
+
+    if client['protocol'] == 'ssh':
+        if not client['port']: client['port'] = 22
+        if not client['user']: client['user'] = 'root'
+        if not client['passwd']: client['passwd'] = '' # keeped unset as we should use RSA/DSA keys
+        if not client['cert']: client['passwd'] = '/root/.ssh/id_dsa'
+        if not client['options']: client['options'] = [
+            '-T',
+            '-R30080:127.0.0.1:80',
+            '-o StrictHostKeyChecking=no',
+            '-o Batchmode=yes',
+            '-o PasswordAuthentication=no'
+            ]
+    return client
+
+def remote_exec(id, command, client, wrapper, sync = True):
+    if sync:
+        return sync_remote_exec(id, command, client, wrapper)
+    else:
+        return async_remote_exec(id, command, client, wrapper)
+    return None
+
+def sync_remote_exec(id, command, client, wrapper):
+    """
+        command is exactly the command line we want to perform on the other side
+        client is the method used to connect to the client (see set_default_client_options)
+    """
+
+    def cb(shprocess):
+        # The callback just return the process outputs
+        return shprocess.exitCode, shprocess.out, shprocess.err
+
+    client = set_default_client_options(client)
+    if client['protocol'] == "ssh":
+        # FIXME: should use annotate_output and get_keychain
+#        command = "%s %s ssh %s %s@%s \"%s\"" % (mmc.plugins.msc.MscConfig("msc").annotatepath, mmc.plugins.msc.config.get_keychain(), opts, user, host, command)
+        real_command = '%s %s ssh %s %s@%s "%s"' % (wrapper, '', ' '.join(client['options']), client['user'], client['host'], command)
+        d = mmc.support.mmctools.shLaunchDeferred(real_command)
+        d.addCallback(cb)
+        return d
+    return None
+
+def async_remote_exec(id, command, client, wrapper):
+
+    def _progress_cb(self, data):
+        try:
+            self.output
+        except: # if first loop
+            self.output = ""
+        self.output += data;
+        # FIXME: handle log flood
+
+    def _end_cb(self, reason):
+        self.done = True
+        self.exitCode = reason.value.exitCode
+        if self.exitCode == 0:
+            self.status = "job successfully finished"
+        else:
+            self.status = "Error: exited with code " + str(self.exitCode) + "\n" + self.stdall
+        self.progress = -1;
+
+    # do not re-inject already running processes
+    if (do_background_process_exists(id)):
+        return False
+
+    client = set_default_client_options(client)
+    if client['protocol'] == "ssh":
+        # FIXME: should use annotate_output and get_keychain
+#        command = "%s %s ssh %s %s@%s \"%s\"" % (mmc.plugins.msc.MscConfig("msc").annotatepath, mmc.plugins.msc.config.get_keychain(), opts, user, host, command)
+        real_command = '%s %s ssh %s %s@%s "%s"' % (wrapper, '', ' '.join(client['options']), client['user'], client['host'], command)
+        mmc.support.mmctools.shlaunchBackground(real_command, id, _progress_cb, _end_cb)
+    return True
+
+def get_background_process_count():
+    return len(mmc.support.mmctools.ProcessScheduler().listProcess())
+
+def get_background_process_list():
+    return mmc.support.mmctools.ProcessScheduler().listProcess().keys()
+
+def get_background_running_process_count():
+    count = 0
+    for i in mmc.support.mmctools.ProcessScheduler().listProcess().values():
+        if not i.done:
+            count += 1
+    return count
+
+def get_background_running_process_list():
+    ret = []
+    for i in mmc.support.mmctools.ProcessScheduler().listProcess().values():
+        if not i.done:
+            ret.append(i.desc)
+    return ret
+
+def do_background_process_exists(id):
+    return id in mmc.support.mmctools.ProcessScheduler().listProcess().keys()
+
+def is_background_process_done(id):
+    return mmc.support.mmctools.ProcessScheduler().listProcess()[id].done
+
+def purge_background_process(id):
+    try:
+        if is_background_process_done(id):
+            process = mmc.support.mmctools.ProcessScheduler().getProcess(id)
+            ret = {
+                'out': process.out,
+                'err': process.err,
+                'exitcode' : process.getExitCode()
+            }
+            clean_background_process(id)
+            return ret
+    except KeyError:
+        return False
+    return True;
+
+def clean_background_process(id):
+    mmc.support.mmctools.ProcessScheduler().rmProcess(id)
+    return True;
