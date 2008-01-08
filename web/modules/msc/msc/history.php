a32d39da4e8419abb4a51117181f36f00e9a0a8d
Cédric Delfosse <cdelfosse@mandriva.com>
2008-01-08 14:45:12 +0000

    * Fix copyright header * Fix CSS class for an action icon

--- web/modules/msc/msc/history.php
+++ web/modules/msc/msc/history.php
@@ -2,23 +2,24 @@
 
 /**
  * (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+ * (c) 2007 Mandriva, http://www.mandriva.com/
  *
- * $Id: cmd_state.php 24 2007-10-17 08:23:42Z nrueff $
+ * $Id$
  *
- * This file is part of LMC.
+ * This file is part of Mandriva Management Console (MMC).
  *
- * LMC is free software; you can redistribute it and/or modify
+ * MMC is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * LMC is distributed in the hope that it will be useful,
+ * MMC is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with LMC; if not, write to the Free Software
+ * along with MMC; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
@@ -44,7 +45,7 @@ if ($_GET['name']) {
         $params = array('cmd_id'=> $_GET['cmd_id'], 'tab'=>$_GET['tab'], 'gid'=>$_GET['gid']);
         // display the selected command
         $cmd = new Command($_GET['cmd_id']);
-        $cmd->quickDisplay(array(new ActionItem(_T("Details", "msc"),"msctabs","detail","msc", "base", "computers")), $params);
+        $cmd->quickDisplay(array(new ActionItem(_T("Details", "msc"),"msctabs","display","msc", "base", "computers")), $params);
         // display the selected command on host
         $coh = new CommandOnHost($_GET['coh_id']);
         $coh->quickDisplay(); //array(new ActionItem(_T("Details", "msc"),"msctabs","detail","msc", "base", "computers")));
