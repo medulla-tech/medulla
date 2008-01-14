53c029f0988d9ba35f2ea1fd09a8a13a3c4c28b4
Nicolas Rueff <nrueff@mandriva.com>
2008-01-14 16:13:06 +0000

    * New XMLRPCFunc, pulse2.scheduler.management.start_all_commands, which launch commands (yes it does)

---
+++ services/pulse2/scheduler/management.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: utf-8; -*-
+#
+# (c) 2007 Mandriva, http://www.mandriva.com/
+#
+# $Id: pulse2-launcher 22 2008-01-14 14:52:22Z nrueff $
+#
+# This file is part of Pulse2.
+#
+# Pulse 2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pulse 2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pulse 2; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+# Twisted
+import twisted.internet.defer
+
+import mmc.plugins.msc
+import mmc.plugins.msc.database
+
+import logging
+def start_all_commands():
+
+    # open Database connexion
+    mmc.plugins.msc.activate()
+
+    # run relevant commands
+    scheduler = mmc.plugins.msc.database.MscDatabase()
+    scheduler.activate()
+    deffereds = scheduler.startAllCommands()
+
+    # wait for commands to finish
+    scheduler.logger.info("Scheduler: %d tasks to perform" % len(deffereds))
+#    dl = twisted.internet.defer.DeferredList(deffereds)
+#    return dl
+#    dl.addCallback(quit)
+    return len(deffereds)
