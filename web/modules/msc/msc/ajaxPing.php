a9fd24edd43a5f1176ca7a240ad248e37049e45e
Nicolas Rueff <nrueff@mandriva.com>
2008-01-15 16:30:03 +0000

    * OS probing done scheduler-side

--- web/modules/msc/msc/ajaxPing.php
+++ web/modules/msc/msc/ajaxPing.php
@@ -1,10 +1,34 @@
 <?
+/*
+ * (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+ * (c) 2007 Mandriva, http://www.mandriva.com
+ *
+ * $Id: ajaxPackageFilter.php,v 1.1 2008/01/08 14:02:59 root Exp $
+ *
+ * This file is part of Mandriva Management Console (MMC).
+ *
+ * MMC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MMC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MMC; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 require("modules/msc/includes/functions.php");
 require("modules/msc/includes/machines.inc.php");
 require("modules/msc/includes/command_history.php");
 
-$h_param = array("hostname" => array($_GET["hostname"]));
-if (rpcPingMachine($h_param)) {
+require("modules/msc/includes/scheduler_xmlrpc.php");
+
+if (scheduler_ping_client('', $_GET["hostname"])) {
     print '<img style="vertical-align: middle;" alt="'.$coh['deleted'].'" src="modules/msc/graph/images/'.return_icon("DONE").'"/>';
 } else {
     print '<img style="vertical-align: middle;" alt="'.$coh['deleted'].'" src="modules/msc/graph/images/'.return_icon("FAILED").'"/>';
--- web/modules/msc/msc/ajaxPlatform.php
+++ web/modules/msc/msc/ajaxPlatform.php
@@ -1,7 +1,30 @@
 <?
-require_once('modules/msc/includes/machines.inc.php');
+/*
+ * (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+ * (c) 2007 Mandriva, http://www.mandriva.com
+ *
+ * $Id: ajaxPackageFilter.php,v 1.1 2008/01/08 14:02:59 root Exp $
+ *
+ * This file is part of Mandriva Management Console (MMC).
+ *
+ * MMC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MMC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MMC; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
-$h_param = array("hostname" => array($_GET["hostname"]));
-print _T(rpcGetPlatform($h_param), "msc")
+require_once('modules/msc/includes/machines.inc.php');
+require_once('modules/msc/includes/scheduler_xmlrpc.php');
+
+print _T(scheduler_probe_client('', $_GET["hostname"]), "msc")
 
 ?>
