c093ff83acf30dc1f1ab30606a90e9ce310c4bb3
Olivier Roussy <oroussy@mandriva.com>
2008-01-15 15:08:16 +0000

    . put distinct labels for machines and groups .

--- web/modules/msc/msc/tabs.php
+++ web/modules/msc/msc/tabs.php
@@ -6,14 +6,14 @@ require("graph/navbar.inc.php");
 $p = new TabbedPageGenerator();
 $p->setSideMenu($sidemenu);
 if ($_GET['name']) {
-    $p->addTop(sprintf(_T("%s's secure control", 'msc'), $_GET['name']), "modules/msc/msc/header.php");
+    $p->addTop(sprintf(_T("%s's machine secure control", 'msc'), $_GET['name']), "modules/msc/msc/header.php");
     $p->addTab("tablaunch", _T("Launch Actions", 'msc'), "", "modules/msc/msc/launch.php", array('name'=>$_GET['name']));
     $p->addTab("tablogs", _T("Logs", 'msc'), "", "modules/msc/msc/logs.php", array('name'=>$_GET['name']));
     $p->addTab("tabhistory", _T("History", 'msc'), "", "modules/msc/msc/history.php", array('name'=>$_GET['name']));
 } elseif ($_GET['gid']) {
     require("modules/dyngroup/includes/includes.php");
     $group = new Stagroup($_GET['gid']);
-    $p->addTop(sprintf(_T("%s's secure control", 'msc'), $group->getName()), "modules/msc/msc/header.php");
+    $p->addTop(sprintf(_T("%s's group secure control", 'msc'), $group->getName()), "modules/msc/msc/header.php");
     $p->addTab("tablaunch", _T("Launch Actions", 'msc'), "", "modules/msc/msc/launch.php", array('gid'=>$_GET['gid']));
     $p->addTab("tablogs", _T("Logs", 'msc'), "", "modules/msc/msc/logs.php", array('gid'=>$_GET['gid']));
     $p->addTab("tabhistory", _T("History", 'msc'), "", "modules/msc/msc/history.php", array('gid'=>$_GET['gid']));
