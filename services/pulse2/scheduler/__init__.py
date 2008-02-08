2233f4b78639c67ac08e6905d77749b655c7cc72
Nicolas Rueff <nrueff@mandriva.com>
2008-02-08 16:31:54 +0000

    * updated copyright * GPL licence formating * $Id$ handling

--- services/pulse2/scheduler/__init__.py
+++ services/pulse2/scheduler/__init__.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8; -*-
 #
-# (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
-# (c) 2007 Mandriva, http://www.mandriva.com/
+# (c) 2007-2008 Mandriva, http://www.mandriva.com/
 #
-# $Id: scheduler-deamon.py 408 2008-01-04 13:02:20Z nrueff $
+# $Id$
 #
-# This file is part of Mandriva Management Console (MMC).
+# This file is part of Pulse 2, http://pulse2.mandriva.org
 #
-# MMC is free software; you can redistribute it and/or modify
+# Pulse 2 is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# MMC is distributed in the hope that it will be useful,
+# Pulse 2 is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with MMC; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
--- services/pulse2/scheduler/network.py
+++ services/pulse2/scheduler/network.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8; -*-
 #
-# (c) 2007 Mandriva, http://www.mandriva.com/
+# (c) 2007-2008 Mandriva, http://www.mandriva.com/
 #
-# $Id: pulse2-launcher 22 2008-01-14 14:52:22Z nrueff $
+# $Id$
 #
-# This file is part of Pulse2.
+# This file is part of Pulse 2, http://pulse2.mandriva.org
 #
 # Pulse 2 is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,8 +17,9 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Pulse 2; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
 
 # regular modules
 import logging
