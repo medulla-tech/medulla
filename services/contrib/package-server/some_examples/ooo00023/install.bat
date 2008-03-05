0fe7df67dd1663a54f679a26491fbd826b6bd691
Nicolas Rueff <nrueff@mandriva.com>
2008-03-05 08:59:55 +0000

    * OOo 2.3 should now be compatible with the 'no subfolder' limitation

--- services/contrib/package-server/some_examples/ooo00023/install.bat
+++ services/contrib/package-server/some_examples/ooo00023/install.bat
@@ -1,7 +1,11 @@
 @echo off
 echo Debut de l'installation
 chmod ugo+rx *
+mkdir java
+copy jre-6u3-windows-i586-p.exe java
 msiexec.exe /i openofficeorg23.msi /qn /l* OOo2.log.txt
 echo Installation terminee.
 more OOo2.log.txt
+rm -f java\jre-6u3-windows-i586-p.exe
+rmdir java
 rm -f OOo2.log.txt
