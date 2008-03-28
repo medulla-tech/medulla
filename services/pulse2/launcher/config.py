113adc5754248985725d262645bd7abcccf2e9fc
Nicolas Rueff <nrueff@mandriva.com>
2008-03-28 13:28:58 +0000

    * Default config file (thus default values) now "in phase" with documentation * Fixed some invalid default config values in config parser * Bugfixes in config file parsing * Introduced two new keywords in config.ini, section [scheduler]: username and password, preparing HTTP/401 auth scheduler-side * Introduced two new keywords in config.ini, section [launcher_xx]: username and password, preparing HTTP/401 auth launcher-side

--- services/pulse2/launcher/config.py
+++ services/pulse2/launcher/config.py
@@ -40,24 +40,17 @@ class LauncherConfig(mmc.support.mmctools.Singleton):
     name = None
     cp = None
     pid_path = "/var/run/pulse2"
-    launcher_path = "/usr/sbin/pulse2-launcher"
-    wrapper_path = "/usr/sbin/pulse2-output-wrapper"
+    launcher_path = "/usr/local/sbin/pulse2-launcher"
+
+    wrapper_path = "/usr/local/sbin/pulse2-output-wrapper"
     source_path = "/tftpboot/revoboot/msc"
     target_path = "/tmp"
+
     inventory_command = "echo Doing inventory"
-    launchers = {
-        'launcher_01': {
-            'port': 8001,
-            'bind': '127.0.0.1',
-            'slots': 300
-        }
-    }
-    scheduler_host = "127.0.0.1"
-    scheduler_port = "9000"
-    awake_time = 600
-    defer_results = False
+    temp_folder_prefix = "MDVPLS"
+
     ssh_defaultkey = 'default'
-    ssh_keys = { 'default': '/root/.ssh/id_dsa.pub' }
+    ssh_keys = { 'default': '/root/.ssh/id_dsa' }
     ssh_options = [ \
         'StrictHostKeyChecking=no',
         'Batchmode=yes',
@@ -66,15 +59,24 @@ class LauncherConfig(mmc.support.mmctools.Singleton):
         'CheckHostIP=no',
         'ConnectTimeout=10'
     ]
+    scp_options = ssh_options
 
-    scp_options = [ \
-        'StrictHostKeyChecking=no',
-        'Batchmode=yes',
-        'PasswordAuthentication=no',
-        'ServerAliveInterval=10',
-        'CheckHostIP=no',
-        'ConnectTimeout=10'
-    ]
+    scheduler_host = "127.0.0.1"
+    scheduler_port = "8000"
+    scheduler_username = "username"
+    scheduler_password = "password"
+    awake_time = 600
+    defer_results = False
+
+    launchers = {
+        'launcher_01': {
+            'port': 8001,
+            'bind': '127.0.0.1',
+            'username': 'username',
+            'password': 'password',
+            'slots': 300
+        }
+    }
 
     def setoption(self, section, key):
         if self.cp.has_option(section, key):
@@ -90,30 +92,41 @@ class LauncherConfig(mmc.support.mmctools.Singleton):
 
         self.name = name
         for key in ['pid_path', 'launcher_path', 'wrapper_path', 'source_path', 'target_path', 'inventory_command', 'temp_folder_prefix']:
-            self.setoption("launchers", key)
+            self.setoption('launchers', key)
 
-        for key in ['host', 'launcher_path', 'port', 'awake_time', 'defer_results']:
-            self.setoption("scheduler", key)
+        if self.cp.has_section('scheduler'):
+            for key in ['launcher_path', 'awake_time', 'defer_results']:
+                self.setoption('scheduler', key)
+            if self.cp.has_option('scheduler', 'host'):
+                self.scheduler_host = self.cp.get('scheduler', 'host')
+            if self.cp.has_option('scheduler', 'port'):
+                self.scheduler_port = self.cp.get('scheduler', 'port')
+            if self.cp.has_option('scheduler', 'username'):
+                self.scheduler_username = self.cp.get('scheduler', 'username')
+            if self.cp.has_option('scheduler', 'password'):
+                self.scheduler_password = self.cp.get('scheduler', 'password')
 
         for section in self.cp.sections():
-            if re.compile("^launcher_[0-9]+$").match(section):
+            if re.compile('^launcher_[0-9]+$').match(section):
                 self.launchers[section] = {
-                        'port': self.cp.get(section, "port"),
-                        'bind': self.cp.get(section, "bind"),
-                        'slots': self.cp.get(section, "slots")
+                        'port': self.cp.get(section, 'port'),
+                        'bind': self.cp.get(section, 'bind'),
+                        'slots': self.cp.get(section, 'slots'),
+                        'username': self.cp.get(section, 'username'),
+                        'password': self.cp.get(section, 'password')
                     }
 
-        if self.cp.has_option("ssh", "default_key"):
-            self.ssh_defaultkey = self.cp.get("ssh", "default_key")
-        if self.cp.has_option("ssh", "ssh_options"):
-            self.ssh_options = self.cp.get("ssh", "ssh_options").split(' ')
-        if self.cp.has_option("ssh", "scp_options"):
-            self.scp_options = self.cp.get("ssh", "scp_options").split(' ')
-
-        for option in self.cp.options('ssh'):
-            if re.compile("^sshkey_[0-9A-Za-z]+$").match(option):
-                keyname = re.compile("^sshkey_([0-9A-Za-z]+)$").match(option).group(1)
-                self.ssh_keys[keyname] = self.cp.get('ssh', option)
+        if self.cp.has_section('ssh'):
+            if self.cp.has_option('ssh', 'default_key'):
+                self.ssh_defaultkey = self.cp.get('ssh', 'default_key')
+            if self.cp.has_option('ssh', 'ssh_options'):
+                self.ssh_options = self.cp.get('ssh', 'ssh_options').split(' ')
+            if self.cp.has_option('ssh', 'scp_options'):
+                self.scp_options = self.cp.get('ssh', 'scp_options').split(' ')
+            for option in self.cp.options('ssh'):
+                if re.compile('^sshkey_[0-9A-Za-z]+$').match(option):
+                    keyname = re.compile('^sshkey_([0-9A-Za-z]+)$').match(option).group(1)
+                    self.ssh_keys[keyname] = self.cp.get('ssh', option)
 
         self.awake_time=int(self.awake_time)
 
