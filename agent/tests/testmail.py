4ab685ea6eaf9878f02f2f2aa9aec2944c941d6e
Cédric Delfosse <cdelfosse@mandriva.com>
2008-03-28 13:03:11 +0000

    * Add test for the userdefault section of the mail plugin

--- agent/tests/testmail.py
+++ agent/tests/testmail.py
@@ -70,8 +70,10 @@ class TestMailControlVDomain(unittest.TestCase):
         self.m = MailControl(conffile = "tests/vdomaintest.ini", conffilebase = "tests/basetest.ini")
 
     def test_MailControl(self):
-        self.m.addUser("usertest", "userpass", "test", "test")
+        self.m.addUser("usertest", "userpass", "test", "test", "/srv/mail/usertest", False)
         self.m.addMailObjectClass("usertest", "usertestmail")
+        d = self.m.getDetailedUser("usertest")
+        self.assertEqual(d["mailbox"][0], "/srv/mail/usertest/Maildir")
         self.assertEqual(self.m.hasMailObjectClass("usertest"), True)
         self.m.removeUserObjectClass("usertest", "mailAccount")
         self.assertEqual(self.m.hasMailObjectClass("usertest"), False)
