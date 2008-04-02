573557b073029b4d2f85daa21a8aad2316e4f7fa
Olivier Roussy <oroussy@mandriva.com>
2008-04-02 09:23:57 +0000

    . add several new methods to ComputerLocationManager to better manage permissions on locations .

--- services/mmc/plugins/pulse2/location.py
+++ services/mmc/plugins/pulse2/location.py
@@ -49,11 +49,44 @@ class ComputerLocationManager(Singleton):
         klass = self.components[self.main]
         return klass().getUserLocations(userid)
 
+    def getMachinesInLocation(self, location, a_profile = []):
+        klass = self.components[self.main]
+        return klass().getMachinesInLocation(location, a_profile)
+        
+    def getLocationsForMachine(self, machine_uuid, a_profile = []):
+        klass = self.components[self.main]
+        return klass().getLocationsForMachine(machine_uuid, a_profile)
+        
+    def doesUserHaveAccessToMachine(self, userid, machine_uuid):
+        klass = self.components[self.main]
+        return klass().doesUserHaveAccessToMachine(userid, machine_uuid)
+
+    def doesUserHaveAccessToMachines(self, userid, machine_uuid, all = True):
+        klass = self.components[self.main]
+        return klass().doesUserHaveAccessToMachines(userid, machine_uuid, all)
+
 class ComputerLocationI:
     def getUserProfile(self, userid):
+        """ return the linked profiles for one user """
         pass
 
     def getUserLocations(self, userid):
+        """ return the linked locations for one user """
+        pass
+
+    def getMachinesInLocation(self, location, a_profile = []): # TODO implement and use in glpi module
+        """ should return the machines that are in the specified location with the good profiles (or any) """
         pass
     
+    def getLocationsForMachine(self, machine_uuid, a_profile = []): # TODO implement and use in glpi module
+        """ should return the locations in which this machine is """
+        pass
+
+    def doesUserHaveAccessToMachine(self, userid, machine_uuid):
+        """ should return true if the machine is accessible for this user """
+        pass
+
+    def doesUserHaveAccessToMachines(self, userid, machine_uuid, all = True):
+        """ should return true if more than one or all machines are accessible for this user """
+        pass
 
