d953e372e64193c8304dc586f135a1e498e1f9a1
Cédric Delfosse <cdelfosse@mandriva.com>
2008-04-29 15:59:51 +0000

    * Update MDS version and API version for MDS 2.3.1 * Update Changelog * Update debian/changelog

--- web/modules/base/infoPackage.inc.php
+++ web/modules/base/infoPackage.inc.php
@@ -28,9 +28,9 @@ require_once("modules/base/includes/computers.inc.php");
  * module declaration
  */
 $mod = new Module("base");
-$mod->setVersion("2.3.0");
+$mod->setVersion("2.3.1");
 $mod->setRevision("$Rev$");
-$mod->setAPIVersion("5:0:1");
+$mod->setAPIVersion("6:0:2");
 $mod->setDescription(_("User and group"));
 $mod->setPriority(1);
 
