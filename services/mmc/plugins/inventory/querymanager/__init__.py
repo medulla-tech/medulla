a33bffc44a858f2919e76fb7ba9e59b497e2d5c7
Olivier Roussy <oroussy@mandriva.com>
2008-05-05 12:26:19 +0000

    . add a working double search (on two parameters) .

--- services/mmc/plugins/inventory/querymanager/__init__.py
+++ services/mmc/plugins/inventory/querymanager/__init__.py
@@ -1,18 +1,19 @@
 import os
 import re
 import logging
-from mmc.plugins.inventory import getValues, getMachinesBy, activate
+from mmc.plugins.inventory import getValues, getValuesFuzzy, getValuesWhere, getMachinesBy, activate
 
 activate() # erk...
 
 def possibleQueries():
-    return ['Software/ProductName', 'Hardware/ProcessorType', 'Hardware/OperatingSystem']
+    return ['Software/ProductName', 'Hardware/ProcessorType', 'Hardware/OperatingSystem', 'Drive/TotalSpace']
 
 def queryPossibilities():
     ret = {}
     p1 = re.compile('/')
     for possible in possibleQueries():
         ret[possible] = ['list', funcGet(possible)]
+    ret['Software/Products'] = ['double', funcGet('Software/Products')]
     return ret
     
 def query(criterion, value):
@@ -23,6 +24,8 @@ def query(criterion, value):
 def funcGet(couple):
     if couple == 'Software/ProductName':
         def getSoftwareProductName(value = ''):
+            if value != '':
+                return getValuesFuzzy('Software', 'ProductName', value)
             return getValues('Software', 'ProductName')
         return getSoftwareProductName
     elif couple == 'Hardware/ProcessorType':
@@ -33,3 +36,14 @@ def funcGet(couple):
         def getHardwareOperatingSystem(value = ''):
             return getValues('Hardware', 'OperatingSystem')
         return getHardwareOperatingSystem
+    elif couple == 'Software/Products':
+        def getSoftwareProduct(value = ''):
+            if value != '':
+                return getValuesFuzzy('Software', 'ProductName', value)
+            return getValues('Software', 'ProductName')
+        return getSoftwareProduct
+    elif couple == 'Drive/TotalSpace':
+        def getDriveTotalSpace(value = 0):
+            return getValues('Drive', 'TotalSpace')
+        return getDriveTotalSpace
+    
