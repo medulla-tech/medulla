f567057cb5bc681839ff649f34f1c08b104cd8ad
Olivier Roussy <oroussy@mandriva.com>
2008-05-14 12:19:37 +0000

    . can output a csv with multiple tables in input (used for hardware tab : bios + hardware) .

--- web/modules/inventory/inventory/csv.php
+++ web/modules/inventory/inventory/csv.php
@@ -3,20 +3,47 @@
 require_once("modules/inventory/includes/xmlrpc.php");
 
 $table = $_GET['table'];
-$uuid = $_GET['uuid'];
+$get_uuid = $_GET['uuid'];
 $gid = $_GET['gid'];
-$datum = getLastMachineInventoryPart($table, array('gid'=>$gid, 'uuid'=>$uuid));
 
 ob_end_clean();
 
+$filename = implode('.', explode('|', $table));
 header("Content-type: text/txt");
-header('Content-Disposition: attachment; filename="'.$table.'.csv"');
+header('Content-Disposition: attachment; filename="'.$filename.'.csv"');
 
+$tables = explode('|', $table);
+
+$datum = array();
+if (count($tables) > 1) {
+    foreach ($tables as $table) {
+        $machines = getLastMachineInventoryPart($table, array('gid'=>$gid, 'uuid'=>$get_uuid));
+        foreach ($machines as $machine) {
+            $name = $machine[0];
+            $uuid = $machine[2];
+            $content = $machine[1];
+            if ($datum[$uuid] == null) {
+                $datum[$uuid] = array($name, array(), $uuid);
+            }
+            foreach ($content as $k=>$v) {
+                $datum[$uuid][1][$k] = $v; // = $datum[$uuid][1] + $content;
+            }
+        }
+    }
+    $datum = array_values($datum);
+} else {
+    $datum = getLastMachineInventoryPart($table, array('gid'=>$gid, 'uuid'=>$get_uuid));
+}
+
+$firstline = true;
 foreach ($datum as $machine) {
     $name = $machine[0];
     $uuid = $machine[2];
     $content = $machine[1];
-    print "\"Machine\",\"".implode('","', array_keys($content[0]))."\"\n";
+    if ($firstline) {
+        print "\"Machine\",\"".implode('","', array_keys($content[0]))."\"\n";
+        $firstline = false;
+    }
     foreach ($content as $line) {
         print "\"$name\",\"".implode('","', array_values($line))."\"\n";
     }
