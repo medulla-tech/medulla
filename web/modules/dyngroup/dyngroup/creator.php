46e11e0a27947dabc266e1bb8ef1a9fa0ef16934
Olivier Roussy <oroussy@mandriva.com>
2008-06-11 10:13:20 +0000

    . simplify the process .

--- web/modules/dyngroup/dyngroup/creator.php
+++ web/modules/dyngroup/dyngroup/creator.php
@@ -160,9 +160,11 @@ if (!$request->isEmpty())  {
     print "<table>";
     print "<tr><td><a href='".
         urlStr("base/computers/save", array('id'=>$id, 'request'=>$request->toS(), 'save_type'=>0)).
-        "'>"._T("Save result", "dyngroup")."</a></td><td><a href='".
-        urlStr("base/computers/save", array('id'=>$id, 'request'=>$request->toS(), 'save_type'=>1)).
-        "'>"._T("Save query", "dyngroup")."</a></td></tr>";
+        "'>"._T("Go to save step", "dyngroup")."</a></td></tr>";
+#        urlStr("base/computers/save", array('id'=>$id, 'request'=>$request->toS(), 'save_type'=>0)).
+#        "'>"._T("Save result", "dyngroup")."</a></td><td><a href='".
+#        urlStr("base/computers/save", array('id'=>$id, 'request'=>$request->toS(), 'save_type'=>1)).
+#        "'>"._T("Save query", "dyngroup")."</a></td></tr>";
     print "</table>";
 }
 ?>
