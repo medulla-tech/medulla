fedc0ce2cab8b272542a452f937579e22722352c
Olivier Roussy <oroussy@mandriva.com>
2008-06-15 15:50:04 +0000

    . does what old ruby code did .

--- services/pulse2/package_server/common/__init__.py
+++ services/pulse2/package_server/common/__init__.py
@@ -28,6 +28,7 @@
 import twisted.web.html
 import twisted.web.xmlrpc
 import os
+import re
 import shutils
 import logging
 from pulse2.package_server.parser import PackageParser
@@ -52,7 +53,7 @@ class Common(Singleton):
             if len(self.config.mirrors) > 0:
                 for mirror_params in self.config.mirrors:
                     try:
-                        _getPackages(
+                        self._getPackages(
                             mirror_params['mount_point'],
                             mirror_params['src'],
                             proto,
@@ -66,19 +67,19 @@ class Common(Singleton):
             if len(self.config.package_api_get) > 0:
                 for mirror_params in self.config.package_api_get:
                     try:
-                        _getPackages(mirror_params['mount_point'], mirror_params['src'])
+                        self._getPackages(mirror_params['mount_point'], mirror_params['src'])
                     except Exception, e:
                         self.logger.error(e)
 
             if len(self.config.package_api_put) > 0:
                 for mirror_params in self.config.package_api_put:
                     try:
-                        _getPackages(mirror_params['mount_point'], mirror_params['src'])
+                        self._getPackages(mirror_params['mount_point'], mirror_params['src'])
                     except Exception, e:
                         self.logger.error(e)
 
-            _buildReverse()
-            _buildFileList()
+            self._buildReverse()
+            self._buildFileList()
 
             self.logger.info("Common : finish loading %d packages" % (len(self.packages)))
         except Exception, e:
@@ -102,7 +103,7 @@ class Common(Singleton):
             if self.packages.has_key(pid):
                 if self.packages[pid] == pa:
                     return pid
-                raise "ARYDEFPKG"
+                raise Exception("ARYDEFPKG")
             self.packages[pid] = pa
             if not self.reverse.has_key(pa.label):
                 self.reverse[pa.label] = {}
@@ -129,7 +130,7 @@ class Common(Singleton):
     def writePackageTo(self, pid, mp):
         if self.packages.has_key(pid):
             self.logger.error("package %s is not defined"%(pid))
-            raise "UNDEFPKG"
+            raise Exception("UNDEFPKG")
         xml = self.parser.concat(self.packages[pid])
         params = self.h_desc(mp)
         path = params['src']
@@ -169,18 +170,95 @@ class Common(Singleton):
             self.logger.error(e)
         return ret
 
-
-
+# private
     def _getPackages(self, mp, src, file_access_proto = '', file_access_uri = '', file_access_port = '', file_access_path = ''):
         if not os.path.exists(src):
-            raise "Src does not exists for mount point '#{%s}' (%s)" %(mp, src)
+            raise Exception("Src does not exists for mount point '#{%s}' (%s)" %(mp, src))
     
         self.mp2p[mp] = []
+        Find().find(src, self._treatConfFile, (mp, file_access_proto, file_access_uri, file_access_port, file_access_path))
 
-        Find.find2(src) do |file|
-            if File.basename(file) == 'conf.xml'
-                _treatDir(File.dirname(file), mp, file_access_proto, file_access_uri, file_access_port, file_access_path)
-            end
-        end
+    def _treatConfFile(self, file, mp, file_access_proto, file_access_uri, file_access_port, file_access_path):
+        if os.path.basename(file) == 'conf.xml':
+            self._treatDir(os.path.dirname(file), mp, file_access_proto, file_access_uri, file_access_port, file_access_path)
 
+    def _treatDir(self, file, mp, file_access_proto, file_access_uri, file_access_port, file_access_path):
+        try:
+            if os.path.isdir(file):
+                self.logger.debug("loading package metadata (xml) in %s"%(file))
+                l_package = self.parser.parse("%s/conf.xml"%(file))
+                pid = l_package.id
+
+                self.mp2p[mp].append(pid)
+                if self.packages.has_key(pi):
+                    return false
+
+                toRelative = os.path.dirname(file)
+                size = 0
+                self.packages[pid] = self.parser.parse("%s/conf.xml"%(file))
+                if self.packages[pid].specifiedFiles.size > 0:
+                    # just get sizes and md5
+                    for sfile in self.packages(pi).specifiedFiles:
+                        f = "%s%s%s%s%s" % (toRelative, os.sep, pid, toRelative, sfile['filename'])
+                        path = re.sub(os.path.basename(f) , '', "%s%s%s%s" % (os.sep, pid, os.sep, sfile['filename']))
+                        if not os.exists(f):
+                            self.logger.warn("the file %s is declared in the package configuration file, but is not in the package directory"%(sfile['filename']))
+                            raise Exception("MISSINGFILE")
+                        size += self._treatFile(pid, f, path, file_access_proto, file_access_uri, file_access_port, file_access_path, sfile['id'])
+                else:
+                    # find all files and then get sizes and md5
+                    files = self._getFiles(file)
+                    for f in files:
+                        path = re.sub(toRelative, '', os.path.dirname(f))
+                        size += self._treatFile(pid, f, path, file_access_proto, file_access_uri, file_access_port, file_access_path)
+                self.packages[pid].size = size
+        except Exception, err:
+            if err.message == 'MISSINGFILE':
+                self.logger.error("package %s won't be loaded because one of the declared file is missing"% (pid))
+                self.mp2p[mp][pid] = None
+            elif err.message == 'DBLFILE':
+                self.logger.error("package %s won't be loaded because one of its file is already declared in an other package"%(pid))
+                self.mp2p[mp][pid] = None
+            else:
+                self.logger.error(err.message)
+                self.mp2p[mp][pid] = None
+
+    def _treatFile(self, pid, f, path, file_access_proto, file_access_uri, file_access_port, file_access_path, fid = None):
+        (fsize, fmd5) = [0,0]
+        if not self.file_properties.has_key(f):
+            fsize = os.path.getsize(f)
+            fmd5 = md5file(f)
+            self.file_properties[f] = [fsize, fmd5]
+        else:
+            (fsize, fmd5) = @file_properties[f]
+
+        file = type.File(os.path.basename(f), path, fmd5, fsize, file_access_proto, file_access_uri, file_access_port, file_access_path, fid)
+        self.packages[pid].addFile(file)
+        if self.fid2file.has_key(file.id) and self.fid2file[file.id] != file.checksum:
+            raise Exception("DBLFILE")
+        self.fid2file[file.id] = file.checksum
+        return fsize
+
+    def _getFiles(self, path)
+        files = []
+        for pfile in os.listdir(path):
+            if os.path.isdir(pfile):
+                files.extend(self._getFiles(pfile))
+            else:
+                if os.path.basename(pfile) != 'conf.xml':
+                    files.append(pfile)
+        return files
+
+    def _buildReverse(self):
+        for pid in self.packages:
+            package = self.packages[pid]
+            if not self.reverse.has_key(package.label):
+                self.reverse[package.label] = {}
+            self.reverse[package.label][package.version] = pid
+
+    def _buildFileList(self):
+        for pid in self.packages:
+            package = self.packages[pid]
+            for file in package.files:
+                self.files[file.id] = file.toURI()
 
