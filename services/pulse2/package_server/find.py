7c89b479140dd2a582f258c1d98264d21d972da3
Olivier Roussy <oroussy@mandriva.com>
2008-06-15 15:49:04 +0000

    . small utility function to execute a function on files .

---
+++ services/pulse2/package_server/find.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8; -*-
+#
+# (c) 2007-2008 Mandriva, http://www.mandriva.com/
+#
+# $Id: __init__.py 30 2008-02-08 16:40:54Z nrueff $
+#
+# This file is part of Pulse 2, http://pulse2.mandriva.org
+#
+# Pulse 2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pulse 2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pulse 2; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+
+import os
+from os import *
+
+class Find:
+    def find(self, path, func, attr):
+        for p in os.listdir(path):
+            file = "%s%s%s" % (path, sep, p)
+            if os.path.isdir(file):
+                self.find(file, func, attr)
+            func(file, *attr)
+                
+        
+
