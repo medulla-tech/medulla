fe8b968875146493046be280996a3f5c5ce8dc5a
Cédric Delfosse <cdelfosse@mandriva.com>
2008-06-19 12:54:22 +0000

    * Set svn:keywords property

--- agent/mmc/plugins/base/computers.py
+++ agent/mmc/plugins/base/computers.py
@@ -3,7 +3,7 @@
 # (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
 # (c) 2007-2008 Mandriva, http://www.mandriva.com/
 #
-# $Id: __init__.py 204 2007-09-13 07:07:19Z cedric $
+# $Id$
 #
 # This file is part of Mandriva Management Console (MMC).
 #
