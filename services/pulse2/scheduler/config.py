b4f06dd34990853987be218069873f87a2ddc152
Nicolas Rueff <nrueff@mandriva.com>
2008-07-10 08:08:11 +0000

    * Code rationalisation / eyecandy

--- services/pulse2/scheduler/config.py
+++ services/pulse2/scheduler/config.py
@@ -24,17 +24,19 @@
 
 # Misc
 import ConfigParser
-import re
-import pwd
-import grp
-import string
-import logging
+import re           # fo re.compil
+import pwd          # for getpwnam
+import grp          # for getgrpnam
+import string       # for atoi
+import logging      # logging stuff
 
 # MMC
-import mmc.support.mmctools
 from mmc.support.config import MMCConfigParser
 
-class SchedulerConfig(mmc.support.mmctools.Singleton):
+# our stuff
+import pulse2.scheduler.utils
+
+class SchedulerConfig(pulse2.scheduler.utils.Singleton):
     """
     Singleton Class to hold configuration directives
 
@@ -42,26 +44,27 @@ class SchedulerConfig(mmc.support.mmctools.Singleton):
     name = None
     cp = None
 
-    port = 8000
-    host = "127.0.0.1"
+    # [scheduler] section
+    awake_time = 600
+    dbencoding = 'utf-8'
     enablessl = True
     certfile = "/etc/mmc/pulse2/scheduler/keys/cacert.pem"
     privkey = "/etc/mmc/pulse2/scheduler/keys/privkey.pem"
-    username = 'username'
-    password = 'password'
-
-    awake_time = 600
-    dbencoding = 'utf-8'
+    host = "127.0.0.1"
     mode = 'sync'
-
+    password = 'password'
+    port = 8000
+    resolv_order = ['fqdn', 'netbios', 'hosts', 'ip']
     scheduler_path = '/usr/sbin/pulse2-scheduler'
+    username = 'username'
 
-    daemon_user = 0
+    # [daemon] section
     daemon_group = 0
     pid_path = "/var/run/pulse2"
     umask = 0077
+    daemon_user = 0
 
-    resolv_order = ['fqdn', 'netbios', 'hosts', 'ip']
+    # [launcher_xxx] section
     launchers = {
         'launcher_01': {
             'port': 8001,
@@ -84,45 +87,46 @@ class SchedulerConfig(mmc.support.mmctools.Singleton):
         self.cp = MMCConfigParser()
         self.cp.read(config_file)
 
+        # [scheduler] section parsing
         self.name = self.cp.get("scheduler", "id")
 
-        self.setoption("scheduler", "port", "port")
-        self.setoption("scheduler", "listen", "host")
-        self.setoption("scheduler", "username", "username")
-        if self.cp.has_option("scheduler", "password"):
-            self.password =self.cp.getpassword("scheduler", "password")
+        self.setoption("scheduler", "awake_time", "awake_time")
+        self.awake_time = int(self.awake_time)
+        self.setoption("scheduler", "dbencoding", "dbencoding")
         if self.cp.has_option("scheduler", "enablessl"):
-            self.enablessl =self.cp.getboolean("scheduler", "enablessl")
+            self.enablessl = self.cp.getboolean("scheduler", "enablessl")
         if self.enablessl:
             self.setoption("scheduler", "privkey", "privkey")
             self.setoption("scheduler", "certfile", "certfile")
-
-        self.setoption("scheduler", "awake_time", "awake_time")
-        self.setoption("scheduler", "dbencoding", "dbencoding")
+        self.setoption("scheduler", "listen", "host")
         self.setoption("scheduler", "mode", "mode")
-        self.awake_time = int(self.awake_time)
-
-        self.setoption("scheduler", "scheduler_path", "scheduler_path")
-
+        if self.cp.has_option("scheduler", "password"):
+            self.password =self.cp.getpassword("scheduler", "password")
+        self.setoption("scheduler", "port", "port")
+        self.port = int(self.port)
         self.setoption("scheduler", "resolv_order", "resolv_order")
         if not type(self.resolv_order) == type([]):
             self.resolv_order = [self.resolv_order]
+        self.setoption("scheduler", "scheduler_path", "scheduler_path")
+        self.setoption("scheduler", "username", "username")
 
-        self.setoption("daemon", "pid_path", "pid_path")
+        # [daemon] section parsing
         if self.cp.has_section("daemon"):
-            if self.cp.has_option("daemon", "user"):
-                self.daemon_user = pwd.getpwnam(cp.get("daemon", "user"))[2]
             if self.cp.has_option("daemon", "group"):
                 self.daemon_group = grp.getgrnam(cp.get("daemon", "group"))[2]
+            self.setoption("daemon", "pid_path", "pid_path")
             if self.cp.has_option("daemon", "umask"):
                 self.umask = string.atoi(cp.get("daemon", "umask"), 8)
+            if self.cp.has_option("daemon", "user"):
+                self.daemon_user = pwd.getpwnam(cp.get("daemon", "user"))[2]
 
+        # [launcher_xxx] section parsing
         for section in self.cp.sections():
             if re.compile("^launcher_[0-9]+$").match(section):
                 self.launchers[section] = {
-                        'port':self.cp.get(section, "port"),
+                        'enablessl':self.cp.getboolean(section, "enablessl"),
                         'host':self.cp.get(section, "host"),
                         'username':self.cp.get(section, "username"),
                         'password':self.cp.getpassword(section, "password"),
-                        'enablessl':self.cp.getboolean(section, "enablessl")
+                        'port':self.cp.get(section, "port")
                     }
