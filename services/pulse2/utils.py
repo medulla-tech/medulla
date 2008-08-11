81347d6675633cdcbfee72c865ac44047c0d56de
Nicolas Rueff <nrueff@mandriva.com>
2008-08-11 14:14:37 +0000

    * Added Pulse2ConfigParser(), to remove uses of MMCConfigParser when needed

--- services/pulse2/utils.py
+++ services/pulse2/utils.py
@@ -21,9 +21,42 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
+from ConfigParser import * # to build Pulse2ConfigParser on top of ConfigParser()
+
 class Singleton(object):
+    """
+        Duplicate from the Singleton() class from the MMC Project,
+        to remove unwanted dependancies
+    """
     def __new__(type):
         if not '_the_instance' in type.__dict__:
             type._the_instance = object.__new__(type)
         return type._the_instance
+
+class Pulse2ConfigParser(ConfigParser):
+    """
+        Duplicate from the MMCConfigParser() class from the MMC Project,
+        to remove unwanted dependancies
+    """
+
+    def __init__(self):
+        ConfigParser.__init__(self)
+
+    def getpassword(self, section, option):
+        """
+        Like get, but interpret the value as a obfuscated password if a
+        password scheme is specified.
+
+        For example: passwd = {base64}bWFuL2RyaXZhMjAwOA==
+        """
+        value = self.get(section, option)
+        m = re.search('^{(\w+)}(.+)$', value)
+        if m:
+            scheme = m.group(1)
+            obfuscated = m.group(2)
+            ret = obfuscated.decode(scheme)
+        else:
+            ret = value
+        return ret
+
 
