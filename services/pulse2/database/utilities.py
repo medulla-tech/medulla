a7120b71ce684487f19409ce37124b304d716da6
Olivier Roussy <oroussy@mandriva.com>
2008-08-13 10:37:34 +0000

    . required for SA exception .

--- services/pulse2/database/utilities.py
+++ services/pulse2/database/utilities.py
@@ -21,6 +21,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
+from sqlalchemy import *
+
 def unique(s):
     """Return a list of the elements in s, but without duplicates.
 
