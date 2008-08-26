3e8d65192ab048c1503c237fa3a94d526c08e4b8
Olivier Roussy <oroussy@mandriva.com>
2008-08-26 10:26:34 +0000

    . used in dyngroup save process to display the new visible groups in the left menu .

---
+++ web/modules/dyngroup/dyngroup/save_detail.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+ * (c) 2007 Mandriva, http://www.mandriva.com
+ *
+ * $Id: save.php 120 2008-07-22 15:58:02Z oroussy $
+ *
+ * This file is part of Mandriva Management Console (MMC).
+ *
+ * MMC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MMC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MMC; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+require("modules/base/computers/localSidebar.php");
+require("graph/navbar.inc.php");
+require_once("modules/dyngroup/includes/includes.php");
+require("modules/base/graph/computers/index.css");
+
+$p = new PageGenerator(_T("Request saver", "dyngroup"));
+$p->setSideMenu($sidemenu);
+$p->display();
+
+$id = idGet();
+$group = null; 
+if ($id) { $group = new Group($id, true); } else { exit(-1); }
+$name = $group->getName();
+$request = $group->getRequest();
+
+if ($group->isRequest()) { // request save
+    $r = new Request();
+    $r->parse($request);
+    $r->displayReqListInfos();
+    print sprintf(_T("This request has been saved as %s (id=%s)", "dyngroup"), $name, $id);
+} else { // result save
+    print sprintf(_T("This result has been saved as %s (id=%s)", "dyngroup"), $name, $id);
+    displayStatic($group, 0, 10, '', $id);
+}
+
+
+function displayStatic($group, $start, $end, $filter, $gid) {
+    $_GET['gid'] = $gid;
+    $_GET['start'] = $start;
+    $_GET['end'] = $end;
+
+    $group->prettyDisplay();
+}
+
+?>
