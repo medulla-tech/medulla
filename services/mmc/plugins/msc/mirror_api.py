a1ecb1807715044d96fbfeb1c913309dd890e85d
Nicolas Rueff <nrueff@mandriva.com>
2008-09-24 15:44:43 +0000

    * Added a handful of useful functions

--- services/mmc/plugins/msc/mirror_api.py
+++ services/mmc/plugins/msc/mirror_api.py
@@ -11,13 +11,13 @@ from mmc.support.mmctools import Singleton
 
 from mmc.client import XmlrpcSslProxy, makeSSLContext
 
-# need to get a PackageApiManager, it will manage a PackageApi for each mirror 
+# need to get a PackageApiManager, it will manage a PackageApi for each mirror
 # defined in the conf file.
 class MirrorApi(Singleton):
     def __init__(self):
         self.logger = logging.getLogger()
         self.config = mmc.plugins.msc.MscConfig("msc")
-        
+
         if self.config.ma_enablessl:
             self.server_addr = 'https://'
         else:
@@ -26,7 +26,7 @@ class MirrorApi(Singleton):
         if self.config.ma_username != '':
             self.server_addr += self.config.ma_username
             if self.config.ma_password != '':
-                self.server_addr += ":"+self.config.ma_password 
+                self.server_addr += ":"+self.config.ma_password
             self.server_addr += "@"
 
         self.server_addr += self.config.ma_server+':'+str(self.config.ma_port) + self.config.ma_mountpoint
@@ -76,7 +76,7 @@ class MirrorApi(Singleton):
         d = self.maserver.callRemote("getFallbackMirrors", machines)
         d.addErrback(self.onError, "MirrorApi:getFallbackMirrors", machines)
         return d
-        
+
     def getApiPackage(self, machine):
         self.logger.debug(machine)
         if self.initialized_failed:
@@ -99,8 +99,6 @@ class MirrorApi(Singleton):
             machine = {'uuid':machine}
         return machine
 
-# FIMXE: Looks like the following class Mirror is unused
-
 class Mirror:
     def __init__(self, server):
         self.logger = logging.getLogger()
@@ -113,8 +111,23 @@ class Mirror:
         except:
             self.logger.warn("Mirror cant connect to %s" % (server))
             self.initialized_failed = True
-        
+
+    def isInitialized(self):
+        """ True if inititialized, or False if something goes wrong """
+        return not self.initialized_failed
+
+    def getServerDetails(self):
+        """ gather stuff about our mirror """
+        if self.initialized_failed:
+            return False
+        try:
+            return self.xmlrpc.getServerDetails()
+        except: # when the api is not available, the package is also unavailable
+            self.logger.warn("Mirror:getServerDetails fails")
+            return False
+
     def isAvailable(self, pid):
+        """ Is my package (identified by pid) available ? """
         if self.initialized_failed:
             return False
         try:
@@ -124,6 +137,7 @@ class Mirror:
             return False
 
     def getFilePath(self, fid):
+        """ convert from a fid (File ID) to a file path """
         if self.initialized_failed:
             return False
         try:
@@ -131,4 +145,4 @@ class Mirror:
         except:
             self.logger.warn("Mirror:getFilePath %s fails"%(fid))
             return False
-   
+
