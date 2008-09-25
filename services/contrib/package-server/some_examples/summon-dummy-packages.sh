13f10cc0eeb7abedd623e8cd1066195f2da754eb
Nicolas Rueff <nrueff@mandriva.com>
2008-09-25 07:50:49 +0000

    * Enhanced dummy-package generator

--- services/contrib/package-server/some_examples/summon-dummy-packages.sh
+++ services/contrib/package-server/some_examples/summon-dummy-packages.sh
@@ -3,11 +3,12 @@
 for i in 1 2 4 7; do # units
         for j in 1 10 100 1000; do # magnitude orders (kB)
                 l=$(($i * $j))
-                mkdir $l.kB
-                pushd $l.kB
+                f=`printf "dummy-package-%.4d-kB" $l`
+                mkdir $f
+                pushd $f
                 dd if=/dev/urandom of=data.bin bs=1k count=$l
                 uuencode -m data.bin data.bin > data.b64
-                cat ../summon-dummy-packages.xml | sed "s/##ID##/$l.kB/" | sed "s/##CMD##/cat data.b64/" > conf.xml
+                cat ../summon-dummy-packages.xml | sed "s/##ID##/$f/" | sed "s/##CMD##/cat data.b64/" > conf.xml
                 popd
         done
 done
