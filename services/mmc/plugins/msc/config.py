e00ce582406097e8db8f753316e28c50e3d720eb
Cédric Delfosse <cdelfosse@mandriva.com>
2008-09-26 12:26:03 +0000

    * Get db_pool_recycle option value as an int

--- services/mmc/plugins/msc/config.py
+++ services/mmc/plugins/msc/config.py
@@ -150,7 +150,7 @@ class MscConfig(PluginConfig):
         if self.has_option("msc", "db_debug"):
             self.db_debug = self.get("msc", "db_debug")
         if self.has_option("msc", "db_pool_recycle"):
-            self.dbpoolrecycle = self.get("msc", "db_pool_recycle")
+            self.dbpoolrecycle = self.getint("msc", "db_pool_recycle")
         # SSL connection support
         if self.has_option("msc", "db_ssl_enable"):
             self.db_ssl_enable = self.getboolean("msc", "db_ssl_enable")
