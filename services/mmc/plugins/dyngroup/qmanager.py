08857bb2bc5f97f606d563169804f248769afab3
Cédric Delfosse <cdelfosse@mandriva.com>
2008-10-17 07:11:55 +0000

    * Dyngroup QueryManage uses now PluginManager to know which plugins are   enabled, so that we don't call again the activate method of each plugin

--- services/mmc/plugins/dyngroup/qmanager.py
+++ services/mmc/plugins/dyngroup/qmanager.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8; -*-
+#
+# (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+# (c) 2007-2008 Mandriva, http://www.mandriva.com
+#
+# $Id$
+#
+# This file is part of Mandriva Management Console (MMC).
+#
+# MMC is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# MMC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MMC; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+from mmc.agent import PluginManager
 from mmc.support.mmctools import shLaunch
 import mmc.support.mmctools
 import logging
@@ -42,10 +66,10 @@ class QueryManager(Singleton):
         """
         Check in existing plugins which one support the query manager
         """
-        
+        pm = PluginManager()
         ret = {}
 
-        for plugin in getAvailablePlugins("plugins/"):
+        for plugin in pm.getEnabledPluginNames():
             if os.path.exists(os.path.join('plugins/', plugin, 'querymanager', '__init__.py')):
                 self.logger.debug("QueryManager is trying to load plugin "+plugin)
                 f, p, d = imp.find_module('querymanager', [os.path.join('plugins/', plugin)])
@@ -54,6 +78,7 @@ class QueryManager(Singleton):
                     func = getattr(mod, 'activate')
                     if func():
                         ret[plugin] = mod
+                        self.logger.info('QueryManager plugin ' + plugin + ' loaded')
                     else:
                         self.logger.info('QueryManager plugin '+ plugin+ ' is disabled by configuration.')
                     
