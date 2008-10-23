9ae0540b324b2f2cad2278cdfbf5d9817e416a05
Cédric Delfosse <cdelfosse@mandriva.com>
2008-10-23 13:17:35 +0000

    * Mark string for translation

--- web/modules/msc/msc/download_file.php
+++ web/modules/msc/msc/download_file.php
@@ -54,7 +54,7 @@ $ajax->display();
 $f = new Form(array('id' => 'dl'));
 $f->addSummary(_T('Click below to start downloading file from this computer. Warning: the download may last a long time', 'msc'));
 $computer = getMachine(array('uuid'=>$_GET['objectUUID']), $ping = False);
-$f->addButton('bconfirm', _T(sprintf('Start download from computer %s', $computer->hostname), 'msc'));
+$f->addButton('bconfirm', sprintf(_T('Start download from computer %s', 'msc'), $computer->hostname));
 $f->display();
 
 print '<br />';
