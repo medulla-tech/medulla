81e6618add5c98d45b8dd0bc45e60ca7ee4a333d
Olivier Roussy <oroussy@mandriva.com>
2008-10-24 09:12:25 +0000

    . correct bug #435 : we dont need to ask msc to get information we already have... .

--- web/modules/glpi/glpi/tabs.php
+++ web/modules/glpi/glpi/tabs.php
@@ -2,25 +2,25 @@
 
 require("modules/base/computers/localSidebar.php");
 require("graph/navbar.inc.php");
-require('modules/msc/includes/machines.inc.php'); // TODO machines should be given by base, not by glpi !
+
+if (!isset($_GET['hostname'])) { $_GET['hostname'] = $_GET['cn']; }
+if (!isset($_GET['uuid'])) { $_GET['uuid'] = $_GET['objectUUID']; }
+if (!isset($_GET['part'])) { $_GET['part'] = 'Hardware'; }
 
 $uuid = '';
-if (isset($_GET['uuid'])) {
-    $uuid = $_GET['uuid'];
-} elseif (isset($_GET['objectUUID'])) {
-    $uuid = $_GET['objectUUID'];
-}
-$machine = getMachine(array('uuid'=>$uuid), $ping = False);
+$hostname = '';
+if (isset($_GET['uuid'])) { $uuid = $_GET['uuid']; }
+if (isset($_GET['hostname'])) { $hostname = $_GET['hostname']; }
 
 $p = new TabbedPageGenerator();
 $p->setSideMenu($sidemenu);
-$p->addTop(sprintf(_T("%s's inventory (GLPI)", "glpi"), $machine->hostname), "modules/glpi/glpi/header.php");
-$p->addTab("tab0", _T("Hardware", "glpi"), "", "modules/glpi/glpi/view_hardware.php", array('hostname'=>$machine->hostname, 'uuid'=>$uuid));
+$p->addTop(sprintf(_T("%s's inventory (GLPI)", "glpi"), $hostname), "modules/glpi/glpi/header.php");
+$p->addTab("tab0", _T("Hardware", "glpi"), "", "modules/glpi/glpi/view_hardware.php", array('hostname'=>$hostname, 'uuid'=>$uuid));
 
 $i = 1;
 // TODO get the list with trads from agent (conf file...)
 foreach (array('Network'=>_T('Network', "glpi"), 'Controller'=>_T('Controller', "glpi")) as $tab=>$str) {
-    $p->addTab("tab$i", $str, "", "modules/glpi/glpi/view_part.php", array('hostname'=>$machine->hostname, 'uuid'=>$uuid, 'part' => $tab));
+    $p->addTab("tab$i", $str, "", "modules/glpi/glpi/view_part.php", array('hostname'=>$hostname, 'uuid'=>$uuid, 'part' => $tab));
     $i++;
 }
 
