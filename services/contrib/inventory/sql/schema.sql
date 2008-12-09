fd7ace36d05dd82acf52f539d74c14ecae1f73c7
Nicolas Rueff <nrueff@mandriva.com>
2008-12-09 18:28:02 +0000

    * Added GPL notice

--- services/contrib/inventory/sql/schema.sql
+++ services/contrib/inventory/sql/schema.sql
@@ -1,5 +1,24 @@
--- MySQL dump 9.11
 --
+-- (c) 2008 Mandriva, http://www.mandriva.com/
+--
+-- $Id$
+--
+-- This file is part of Pulse 2, http://pulse2.mandriva.org
+--
+-- Pulse 2 is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- Pulse 2 is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Pulse 2; if not, write to the Free Software
+-- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+-- MA 02110-1301, USA.
 
 --
 -- Table structure for table `Bios`
@@ -211,7 +230,7 @@ CREATE TABLE Pci (
   Vendor varchar(32) default NULL,
   Device varchar(128) default NULL,
   Class varchar(32) default NULL,
-  Type varchar(32) default NULL,  
+  Type varchar(32) default NULL,
   PRIMARY KEY  (id)
 ) TYPE=MyISAM;
 
