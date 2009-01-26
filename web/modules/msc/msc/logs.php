83624db065c8a98d5d2971d1100f7fb291605a7c
Olivier Roussy <oroussy@mandriva.com>
2009-01-26 15:41:47 +0000

    . add the textual plus led status line on top of the detailed command on host .

--- web/modules/msc/msc/logs.php
+++ web/modules/msc/msc/logs.php
@@ -48,6 +48,8 @@ if (strlen($_GET['uuid'])) {
         }
         print "<hr/><br/>";
         $coh_id = $_GET['coh_id'];
+        $coh = new CommandOnHost($_GET['coh_id']);
+        $coh->quickDisplay(); 
         $ch = new CommandHistory($coh_id);
         $ch->display();
     } elseif (strlen($_GET['cmd_id'])) {
@@ -60,6 +62,8 @@ if (strlen($_GET['uuid'])) {
         if ($_GET['cmd_id'] == -2) { new NotifyWidgetFailure(_T("The group you are working on is empty.", "msc")); }
         $coh_ids = get_command_on_host_in_commands($_GET['cmd_id']);
         $coh_id = $coh_ids[0]; # we know there is only one because we are in uuid (one machine)
+        $coh = new CommandOnHost($coh_id);
+        $coh->quickDisplay(); 
         $ch = new CommandHistory($coh_id);
         $ch->display();
     } else {
