46801c8874af22e561aded3f4693d41b903e267f
Olivier Roussy <oroussy@mandriva.com>
2009-02-06 14:45:37 +0000

    . move the common part of msc/database in pulse2.database.msc so that it can be accessed from everywhere .

---
+++ services/pulse2/database/msc/orm/commands_history.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8; -*-
+#
+# (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+# (c) 2007 Mandriva, http://www.mandriva.com/
+#
+# $Id: commands_history.py 484 2008-10-27 14:15:56Z nrueff $
+#
+# This file is part of Mandriva Management Console (MMC).
+#
+# MMC is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# MMC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MMC; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+# big modules
+import logging
+import time
+
+# uses SA to handle sessions
+import sqlalchemy.orm
+
+""" Class to map msc.commands_history to SA
+"""
+class CommandsHistory(object):
+    """ Mapping between msc.commands_history and SA
+    """
+    def toH(self):
+        return {
+            'id': self.id,
+            'fk_commands_on_host': self.fk_commands_on_host,
+            'date': self.date,
+            'stderr': self.stderr,
+            'stdout': self.stdout,
+            'error_code': self.error_code,
+            'state': self.state
+        }
+
+    def flush(self):
+        """ Handle SQL flushing """
+        session = sqlalchemy.orm.create_session()
+        session.save_or_update(self)
+        session.flush()
+        session.close()
+
