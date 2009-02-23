802dff14140599a49f941445bfa64f2e95687186
Olivier Roussy <oroussy@mandriva.com>
2009-02-23 16:18:06 +0000

    . it is now possible to put the machine in a specific entity (specified by its name or its id) (#577) .

--- services/contrib/inventory/pulse2-inventory-populate-root-entity.py
+++ services/contrib/inventory/pulse2-inventory-populate-root-entity.py
@@ -34,17 +34,30 @@ import sqlalchemy
 import logging
 
 def usage(argv):
-    print >> sys.stderr, 'Usage: %s [db_conn_string]' % argv[0]
+    print >> sys.stderr, 'Usage: %s db_conn_string [--id entity_id|--name entity_name]' % argv[0]
     print >> sys.stderr, 'Where db_conn_string is a SQLAlchemy connection string, e.g. mysql://user:password@host/database'
     return 1
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 2 and len(sys.argv) != 4:
         sys.exit(usage(sys.argv))
 
     if not sqlalchemy.__version__.startswith('0.4'):
         print "Wrong version of SqlAlchemy found, please install a 0.4 version !"
         sys.exit(1)
+
+    get_entity = False
+    if len(sys.argv) == 4:
+        if sys.argv[2] == '--id':
+            id_entity = sys.argv[3]
+            get_entity = True
+        elif sys.argv[2] == '--name':
+            id_entity = -1
+            name_entity = sys.argv[3]
+            get_entity = True
+        else:
+            print "dont know this option : %s" % sys.argv[2]
+            sys.exit(1)
 
     mysql_db = create_engine(sys.argv[1])
     metadata = MetaData(mysql_db)
@@ -57,9 +70,21 @@ if __name__ == "__main__":
     hashardware = Table("hasHardware", metadata, autoload = True)
 
     computers = select([inventory.c.id, machine.c.id], and_(machine.c.id == hashardware.c.machine, hashardware.c.inventory == inventory.c.id, inventory.c.Last == 1)).execute().fetchall()
+    
+    entity_id = 1
+    if get_entity:
+        ent = []
+        if id_entity != -1:
+            ent = select([entity.c.id], and_(entity.c.id == id_entity)).execute().fetchall()
+        else:
+            ent = select([entity.c.id], and_(entity.c.Label == name_entity)).execute().fetchall()
+        if ent == None or len(ent) == 0:
+            print "Cant get the required entity"
+            sys.exit(1)
+        entity_id = ent[0][0]
 
     into_hasentity = []
     for computer in computers:
-        into_hasentity.append({'machine' : computer[1], 'entity' : 1, 'inventory': computer[0]})
+        into_hasentity.append({'machine' : computer[1], 'entity' : entity_id, 'inventory': computer[0]})
     connection.execute(hasentity.insert(), into_hasentity)
     
