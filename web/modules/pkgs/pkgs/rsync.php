cc4d073279e91f48e1b21eafc4114fc859509d89
Olivier Roussy <oroussy@mandriva.com>
2009-03-04 16:15:05 +0000

    . if the package is in the list for association reason, tell it .

--- web/modules/pkgs/pkgs/rsync.php
+++ web/modules/pkgs/pkgs/rsync.php
@@ -27,21 +27,26 @@ require_once("modules/msc/includes/package_api.php");
 
 $pid = base64_decode($_GET['pid']);
 $p_api = base64_decode($_GET["p_api"]);
-$mirrors_status = getRsyncStatus($p_api, $pid);
 
-print "<table style='width:90%'>";
-foreach ($mirrors_status as $mirror) {
-    print "<tr><td>$mirror[0]</td>";
-    $status = $mirror[1];
-    if ($status == 'OK') {
-        print "<td style='background-color:green;'>&nbsp;</td>";
-    } elseif ($status == 'NOK') {
-        print "<td style='background-color:red;'>&nbsp;</td>";
-    } else {
-        print "<td style='background-color:orange;'>&nbsp;</td>";
+if ($p['why'] || $p['why'] == 'association') {
+    print sprintf(_T("The package %s (%s) is not associated", "pkgs"), $_GET["plabel"], $_GET['pversion']);
+} else {
+    $mirrors_status = getRsyncStatus($p_api, $pid);
+    
+    print "<table style='width:90%'>";
+    foreach ($mirrors_status as $mirror) {
+        print "<tr><td>$mirror[0]</td>";
+        $status = $mirror[1];
+        if ($status == 'OK') {
+            print "<td style='background-color:green;'>&nbsp;</td>";
+        } elseif ($status == 'NOK') {
+            print "<td style='background-color:red;'>&nbsp;</td>";
+        } else {
+            print "<td style='background-color:orange;'>&nbsp;</td>";
+        }
+        print "</tr>";
     }
-    print "</tr>";
+    print "</table>";
 }
-print "</table>";
 
 ?>
