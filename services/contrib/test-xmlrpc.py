5b61c98fd8dac35de710cd6ac8af7cbb492a95d1
Nicolas Rueff <nrueff@mandriva.com>
2009-04-08 08:37:07 +0000

    * Update XMLRPC test stuff

--- services/contrib/test-xmlrpc.py
+++ services/contrib/test-xmlrpc.py
@@ -42,11 +42,12 @@ Arguments must follow this convention:
 
   => use --func myfunc --args "a,b|c,d" to request func(['a', 'b'], ['c', 'd'])
 """
- 
+
 import twisted.python.usage
 import twisted.internet.reactor
 import twisted.web.xmlrpc
 import sys
+import time
 
 class Options(twisted.python.usage.Options):
 
@@ -56,16 +57,66 @@ class Options(twisted.python.usage.Options):
         ["server", None, None, "The XML RPC server to contact, URI format"],
         ]
 
-
 def _cb(result): # server do answer us
-    print(result)
-    twisted.internet.reactor.stop()
-    return 0
+    print "RESULT : %s" % result
+    twisted.internet.reactor.callLater(0, _end)
 
-def _eb(reason): # can't contact scheduler, log and continue
-    print(reason)
+def _eb(reason): # can't contact scheduler
+    print "ERROR : %s" % reason
+    twisted.internet.reactor.callLater(0, _end)
+
+def _start():
+    (method, parsedargs) = parseCliArgs(config)
+    return twisted.web.xmlrpc.Proxy(config["server"]).\
+        callRemote(method, *parsedargs).\
+        addCallback(_cb).\
+        addErrback(_eb)
+
+def _end():
     twisted.internet.reactor.stop()
-    return 1
+
+# parse cli args
+def parseCliArgs(config):
+    args=[]
+    method = config["func"]
+    if config["args"]:
+        args = config["args"].split(';')
+
+    parsedargs = []
+    for arg in args: # parse args
+        tokenlist = arg.split('|')                     # split arrays args
+        for token in tokenlist:                        # iterate over array content
+            if token.count('=') == 1:                  # found a dict token
+                (key, val) = token.split('=')          # process it
+                try:
+                    items[key] = val
+                except:
+                    items = {key: val}
+            elif token.count(',') > 0:         # found a list token
+                try:
+                    items += [token.split(',')]
+                except:
+                    items = [token.split(',')]
+            elif token.count('~') == 1:        # found a number
+                try:
+                    items += int(token.split('~')[1])
+                except:
+                    items = [int(token.split('~')[1])]
+            else:                                      # found something else (simple value ?)
+                try:
+                    items += token
+                except:
+                    items = [token]
+
+        if type(items) == type({}):
+            parsedargs.append(items)
+        elif type(items) == type([]):
+            parsedargs += items
+        else:
+            parsedargs += items
+        del(items)
+
+    return (method, parsedargs)
 
 config = Options()
 try:
@@ -75,45 +126,5 @@ except twisted.python.usage.UsageError, errortext:
     print '%s: Try --help for usage details.' % (sys.argv[0])
     sys.exit(1)
 
-# parse cli args
-args=[]
-method = config["func"]
-if config["args"]:
-    args = config["args"].split(';')
-
-parsedargs = []
-for arg in args: # parse args
-    tokenlist = arg.split('|')                     # split arrays args
-    for token in tokenlist:                        # iterate over array content
-        if token.count('=') == 1:                  # found a dict token
-            (key, val) = token.split('=')          # process it
-            try:
-                items[key] = val
-            except:
-                items = {key: val}
-	elif token.count(',') > 0:		   # found a list token
-            try:
-                items += [token.split(',')]
-            except:
-                items = [token.split(',')]
-	elif token.count('~') == 1:		   # found a number
-            try:
-                items += int(token.split('~')[1])
-            except:
-                items = [int(token.split('~')[1])]
-        else:                                      # found something else (simple value ?)
-            try:
-                items += token
-            except:
-                items = [token]
-
-    if type(items) == type({}):
-        parsedargs.append(items)
-    elif type(items) == type([]):
-        parsedargs += items
-    else:
-        parsedargs += items
-    del(items)
-
-twisted.web.xmlrpc.Proxy(config["server"]).callRemote(method, *parsedargs).addCallback(_cb).addErrback(_eb)
+twisted.internet.reactor.callWhenRunning(_start)
 twisted.internet.reactor.run()
