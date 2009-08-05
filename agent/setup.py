9efa3e18321738fdaf9307eaad7c51c738ea1ae7
Cédric Delfosse <cdelfosse@mandriva.com>
2009-08-05 14:04:21 +0000

    * Add copyright header * Set svn Id keyword property * Fix builded MMC Python packages list to suit the new SVN tree

--- agent/setup.py
+++ agent/setup.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8; -*-
+#
+# (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+# (c) 2007-2009 Mandriva, http://www.mandriva.com
+#
+# $Id$
+#
+# This file is part of Mandriva Management Console (MMC).
+#
+# MMC is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# MMC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MMC.  If not, see <http://www.gnu.org/licenses/>.
+
 from distutils.core import setup
 
 setup(
@@ -8,5 +30,5 @@ setup(
     author_email = "cdelfosse@mandriva.com",
     maintainer = "Cedric Delfosse",
     maintainer_email = "cdelfosse@mandriva.com",
-    packages = ["mmc", "mmc.support", "mmc.plugins", "mmc.plugins.base", "mmc.plugins.samba", "mmc.plugins.proxy", "mmc.plugins.mail", "mmc.plugins.network"],
+    packages = ["mmc", "mmc.support", "mmc.plugins", "mmc.plugins.base"],
 )
