217e02859eb034bcbc653fc7343a7edf7d145e74
Nicolas Rueff <nrueff@mandriva.com>
2009-08-19 14:03:15 +0000

    * Merged changeset r.411 from branch 1.2.4:   * Really check that the scheduler did not reject our command even if XMLRPC request succeeded

--- services/pulse2/launcher/remote_exec.py
+++ services/pulse2/launcher/remote_exec.py
@@ -811,7 +811,10 @@ def __cb_async_process_end(shprocess):
     def _cb(result, id):
         # as we successfuly sent our result to our scheduler, command can be safely removed from our list
         shprocess.isnotifyingparent = False
-        pulse2.launcher.process_control.ProcessList().removeProcess(id)
+        if result == True:
+            pulse2.launcher.process_control.ProcessList().removeProcess(id)
+        else:
+            logging.getLogger().warn('launcher %s: failed to send results of command #%s to our scheduler at %s: %s' % (LauncherConfig().name, id, scheduler, "command was rejected; lock issue?"))
     def _eb(reason, id):
         # no result can be sent, log and keep our process in our list
         shprocess.isnotifyingparent = False
