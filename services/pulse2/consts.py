b5d7baf732a7a42b4437d98eae72d125e19cffad
Nicolas Rueff <nrueff@mandriva.com>
2009-08-26 08:42:03 +0000

    * Merged changesets 216,217,218,219,221,222 fromp svn:pulse-common/branches/PULSE_1_2_4 :   * "Constantified" a lot of Pulse 2 consts   * Some cleanup in error codes

--- services/pulse2/consts.py
+++ services/pulse2/consts.py
@@ -30,9 +30,29 @@ PULSE2_WRAPPER_ARG_SEPARATOR_STR = '·'
 PULSE2_WRAPPER_ARG_SEPARATOR_UNICODE = PULSE2_WRAPPER_ARG_SEPARATOR_STR.decode(PULSE2_PREFERRED_ENCODING)
 PULSE2_WRAPPER_ARG_SEPARATOR = PULSE2_WRAPPER_ARG_SEPARATOR_UNICODE
 
-# wrapper error codes, see http://pulse2.mandriva.org/ticket/664
+# important error code :D
+PULSE2_SUCCESS_ERROR    = 0
+
+# pulse 2 errors ranges
+PULSE2_UNKNOWN_ERROR_MIN    = 1000
+PULSE2_UNKNOWN_ERROR_MAX    = 1999
+PULSE2_TARGET_ERROR_MIN     = 2000
+PULSE2_TARGET_ERROR_MAX     = 2999
+PULSE2_BUNDLE_ERROR_MIN     = 3000
+PULSE2_BUNDLE_ERROR_MAX     = 3999
+PULSE2_PROXY_ERROR_MIN      = 4000
+PULSE2_PROXY_ERROR_MAX      = 4999
+PULSE2_PSERVER_ERROR_MIN    = 4000
+PULSE2_PSERVER_ERROR_MAX    = 4999
+
+
+"""
+    ####################################################################
+    wrapper error codes, see http://pulse2.mandriva.org/ticket/664
+    ####################################################################
+"""
 # standard codes
-PULSE2_WRAPPER_ERROR_SUCCESS = 0
+PULSE2_WRAPPER_ERROR_SUCCESS = PULSE2_SUCCESS_ERROR
 PULSE2_WRAPPER_ERROR_FAILURE = 1
 PULSE2_WRAPPER_ERROR_PRECHECK = 1
 PULSE2_WRAPPER_ERROR_CONNFAILED = 255
@@ -70,3 +90,132 @@ PULSE2_WRAPPER_ERROR_PRECHECK_TABLE = {
     'IP'        : PULSE2_WRAPPER_ERROR_PRECHECK_BASE + 2,
     'MAC'       : PULSE2_WRAPPER_ERROR_PRECHECK_BASE + 3
 }
+
+"""
+    ####################################################################
+    Standard codes from workflow
+    ####################################################################
+"""
+
+PULSE2_UNKNOWN_ERROR        = PULSE2_UNKNOWN_ERROR_MIN
+
+"""
+    ####################################################################
+    Standard codes for target
+    ####################################################################
+"""
+
+PULSE2_TARGET_ERROR                 = PULSE2_TARGET_ERROR_MIN
+PULSE2_TARGET_NOTENOUGHINFO_ERROR   = PULSE2_TARGET_ERROR_MIN + 1
+
+"""
+    ####################################################################
+    Standard codes for bundle
+    ####################################################################
+"""
+
+PULSE2_BUNDLE_ERROR                     = PULSE2_BUNDLE_ERROR_MIN
+PULSE2_BUNDLE_MISSING_MANDATORY_ERROR   = PULSE2_BUNDLE_ERROR_MIN + 1
+
+"""
+    ####################################################################
+    Standard codes for proxy
+    ####################################################################
+"""
+
+PULSE2_PROXY_ERROR                      = PULSE2_PROXY_ERROR_MIN
+PULSE2_PROXY_WAITINGFORDEAD_ERROR       = PULSE2_PROXY_ERROR_MIN + 1
+
+"""
+    ####################################################################
+    Standard codes for proxy
+    ####################################################################
+"""
+
+PULSE2_PSERVER_ERROR                        = PULSE2_PSERVER_ERROR_MIN
+PULSE2_PSERVER_PACKAGEISUNAVAILABLE_ERROR   = PULSE2_PSERVER_ERROR_MIN + 1
+
+
+"""
+    some known states
+"""
+
+""" Commands in this state are definitively terminated """
+
+PULSE2_FAILURE_STATE = [
+    'failed',
+    'over_timed'
+]
+
+PULSE2_SUCCESS_STATE = [
+    'done'
+]
+
+PULSE2_TERMINATED_STATES = [
+] + PULSE2_FAILURE_STATE + PULSE2_SUCCESS_STATE
+
+""" Commands in this state won't be preempted """
+PULSE2_UNPREEMPTABLE_STATES = [
+    'stop',
+    'stopped',
+    'pause',
+    'paused'
+] + PULSE2_TERMINATED_STATES
+
+""" Commands in these states are executed somewhere """
+PULSE2_PROGRESSING_STATES = [
+    'upload_in_progress',
+    'execution_in_progress',
+    'delete_in_progress',
+    'inventory_in_progress'
+    'reboot_in_progress',
+    'halt_in_progress',
+]
+
+""" Commands in these states are running somewhere (WOL is, says, 'special') """
+PULSE2_RUNNING_STATES = [
+    'wol_in_progress'
+] + PULSE2_PROGRESSING_STATES
+
+""" Commands in these states have all passed the 'inventory' stage """
+PULSE2_POST_INVENTORY_STATES = [
+    'reboot_in_progress',
+    'reboot_done',
+    'reboot_failed',
+    'halt_in_progress',
+    'halt_done',
+    'halt_failed'
+] + PULSE2_TERMINATED_STATES
+
+""" Commands in these states have all passed the 'halt' stage """
+PULSE2_POST_HALT_STATES = [
+] + PULSE2_TERMINATED_STATES
+
+""" The stages a command will cross by, KEEP IN THAT ORDER ! """
+PULSE2_STAGES = [
+    'awoken',
+    'uploaded',
+    'executed',
+    'deleted',
+    'inventoried',
+    'rebooted',
+    'halted'
+]
+
+""" The stages prefixes KEEP IN THAT ORDER ! KEEP IN SYNC WITH PREVIOUS ! """
+PULSE2_STATE_PREFIXES = [
+    'wol',
+    'upload',
+    'execution',
+    'delete',
+    'inventory',
+    'reboot',
+    'halt'
+]
+
+PULSE2_STAGE_TODO               = 'TODO'
+PULSE2_STAGE_WORK_IN_PROGRESS   = 'WORK_IN_PROGRESS'
+PULSE2_STAGE_DONE               = 'DONE'
+PULSE2_STAGE_IGNORED            = 'IGNORED'
+PULSE2_STAGE_FAILED             = 'FAILED'
+
