dc7732c817495b1cd1a11707958ad667de65d92d
Nicolas Rueff <nrueff@mandriva.com>
2009-09-18 16:05:13 +0000

    * Updated copyright

--- doc/install/pki/create-pki.sh
+++ doc/install/pki/create-pki.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
+#
+# (c) 2008-2009 Mandriva, http://www.mandriva.com
+#
+# $Id$
+#
+# This file is part of Pulse 2, http://pulse2.mandriva.org
+#
+# Pulse 2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pulse 2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pulse 2; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
 
 PKI_PATH=PULSE2
 PKI_KEYS_PATH=$PKI_PATH/private
 PKI_CERTS_PATH=$PKI_PATH/newcerts
-PKI_REQS_PATH=$PKI_PATH/req 
+PKI_REQS_PATH=$PKI_PATH/req
 PASSPHRASE=pulse2
 PULSE2_SERVICES="mmc_agent launcher_01 scheduler_01 inventory_server package_server"
 
