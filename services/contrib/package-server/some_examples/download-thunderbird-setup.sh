01fad534e070dd9d9c0ab5d21e8d71253fddb5fd
Nicolas Rueff <nrueff@mandriva.com>
2009-09-18 15:59:02 +0000

    * Updated copyright * Checked SVN keywords

--- services/contrib/package-server/some_examples/download-thunderbird-setup.sh
+++ services/contrib/package-server/some_examples/download-thunderbird-setup.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
+#
+# (c) 2008-2009 Mandriva, http://www.mandriva.com
+#
+# $Id$
+#
+# This file is part of Pulse 2, http://pulse2.mandriva.org
+#
+# Pulse 2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pulse 2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pulse 2; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
 
 BASE=http://download.mozilla.org
 
 if [ "$#" -ne "3" ]; then
-	echo "usage: $0 <plateform> <version> <lang>"
-	exit 1
+    echo "usage: $0 <plateform> <version> <lang>"
+    exit 1
 fi
 
 PLATEFORM=$1
