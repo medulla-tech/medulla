8a3d05de1f30d907197a961e6b8785da1773f1f9
Olivier Roussy <oroussy@mandriva.com>
2009-10-26 17:36:04 +0000

    . addapt script to build pot to the new architecturre .

--- scripts/build_pot.sh
+++ scripts/build_pot.sh
@@ -16,16 +16,20 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-POT="modules/base/locale/base.pot"
+set -x
+for POT_D in `find . -name "locale" -type d`; do 
+    POT_D=`dirname "$POT_D"`
+    POT_N=`basename "$POT_D"`
+    POT="$POT_D/locale/$POT_N.pot"
 
-rm ${POT}
-touch ${POT}
-find . -iname "*.php" -exec xgettext -C -j -o ${POT} --language=PHP --keyword=_ {} \;
-
-for name in `find ${1} -type f -name *.po`; do
-    echo -n "updating ${name}..."
-    msgmerge --update --add-location --sort-output ${name} ${POT}
-    echo "done"
+    rm -f ${POT}
+    touch ${POT}
+    find "$POT_D" -iname "*.php" -exec xgettext -C -j -o ${POT} --language=PHP --keyword=_T {} \;
+    for name in `find ${POT_D} -type f -name *.po`; do
+        echo -n "updating ${name}..."
+        msgmerge --update --add-location --sort-output ${name} ${POT}
+        echo "done"
+    done
 done
-
 exit 0
+
