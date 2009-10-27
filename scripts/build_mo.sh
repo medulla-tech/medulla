35563c71265e8fa4f6a9f8333624f6c5105094cf
Olivier Roussy <oroussy@mandriva.com>
2009-10-27 09:30:05 +0000

    . update the license .

--- scripts/build_mo.sh
+++ scripts/build_mo.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 #
-# Copyright (C) 2006, Adam Cécile et Jérôme Wax pour Linbox FAS
+# (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+# (c) 2007-2009 Mandriva, http://www.mandriva.com
 #
-# This program is free software; you can redistribute it and/or modify
+# $Id$
+#
+# This file is part of Mandriva Management Console (MMC).
+#
+# MMC is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# MMC is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# along with MMC.  If not, see <http://www.gnu.org/licenses/>.
 
 for name in `find ${1} -type f -name *.po`
   do
