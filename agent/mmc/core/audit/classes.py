5880af49d3570a527a8f7fce01d6055f4afb27fb
Cédric Delfosse <cdelfosse@mandriva.com>
2009-11-27 09:20:03 +0000

    * Remove unneeded import

--- agent/mmc/core/audit/classes.py
+++ agent/mmc/core/audit/classes.py
@@ -20,7 +20,6 @@
 # You should have received a copy of the GNU General Public License
 # along with MMC.  If not, see <http://www.gnu.org/licenses/>.
 
-import sqlalchemy
 from sqlalchemy import func
 
 class Action(object):
