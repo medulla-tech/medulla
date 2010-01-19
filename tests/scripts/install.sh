b0f554e3558cc29abfc1b19a82c145665c931d11
Cédric Delfosse <cdelfosse@mandriva.com>
2010-01-19 15:56:31 +0000

    * Setup SSH connection without password on localhost after SSH key generation

--- tests/scripts/install.sh
+++ tests/scripts/install.sh
@@ -195,6 +195,8 @@ sed -i "s/server = localhost/server = $IPADDRESS/" /etc/mmc/plugins/pkgs.ini
 if [ ! -f /root/.ssh/id_dsa ];
 then
     ssh-keygen -t dsa -f /root/.ssh/id_dsa -N ""
+    # Allow SSH connection without password on localhost
+    cat /root/.ssh/id_dsa.pub > /root/.ssh/authorized_keys
 fi
 
 # Launch all service of Pulse 2
