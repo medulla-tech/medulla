941e7a4726dc99aa25c2dd5ee17cd4b43af4ac4d
Nicolas Rueff <nrueff@mandriva.com>
2010-01-25 12:42:23 +0000

    * Fixed install script to handle empty passwords

--- services/contrib/dyngroup/sql/install.sh
+++ services/contrib/dyngroup/sql/install.sh
@@ -3,20 +3,28 @@
 SCHEMA_NAME=schema
 SCHEMA_MAXVERSION=3
 
-[ -z $MYSQL_HOST ] && echo 'Enter MYSQL host (default : "localhost", or $MYSQL_DATABASE if defined)' && read
-[ ! -z $REPLY ] && MYSQL_HOST=$REPLY
-[ -z $MYSQL_HOST ] && MYSQL_HOST='localhost'
+if [ ! -n "${MYSQL_HOST+x}" ]; then
+    echo 'Enter MYSQL host (default : "localhost", or $MYSQL_DATABASE if defined)' && read
+    [ ! -z $REPLY ] && MYSQL_HOST=$REPLY
+    [ -z $MYSQL_HOST ] && MYSQL_HOST='localhost'
+fi
 
-[ -z $MYSQL_BASE ] && echo 'Enter MYSQL database (default : "dyngroup", or $MYSQL_BASE if defined)' && read
-[ ! -z $REPLY ] && MYSQL_BASE=$REPLY
-[ -z $MYSQL_BASE ] && MYSQL_BASE='dyngroup'
+if [ ! -n "${MYSQL_BASE+x}" ]; then
+    echo 'Enter MYSQL database (default : "dyngroup", or $MYSQL_BASE if defined)' && read
+    [ ! -z $REPLY ] && MYSQL_BASE=$REPLY
+    [ -z $MYSQL_BASE ] && MYSQL_BASE='dyngroup'
+fi
 
-[ -z $MYSQL_USER ] && echo 'Enter MYSQL user (default : "root", or $MYSQL_USER if defined)' && read
-[ ! -z $REPLY ] && MYSQL_USER=$REPLY
-[ -z $MYSQL_USER ] && MYSQL_USER='root'
+if [ ! -n "${MYSQL_USER+x}" ]; then
+    echo 'Enter MYSQL user (default : "root", or $MYSQL_USER if defined)' && read
+    [ ! -z $REPLY ] && MYSQL_USER=$REPLY
+    [ -z $MYSQL_USER ] && MYSQL_USER='root'
+fi
 
-[ -z $MYSQL_PWD ] && echo 'Enter MYSQL password (default : <empty>, or $MYSQL_PWD if defined)' && read
-[ ! -z $REPLY ] && MYSQL_PWD=$REPLY
+if [ ! -n "${MYSQL_PWD+x}" ]; then
+    echo 'Enter MYSQL password (default : <empty>, or $MYSQL_PWD if defined)' && read
+    [ ! -z $REPLY ] && MYSQL_PWD=$REPLY
+fi
 
 MYSQL_CNF=`mktemp`
 trap "rm -f MYSQL_CNF" EXIT
