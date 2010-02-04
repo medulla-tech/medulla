373ae3ce1d40946c01baacd01a84d21a1c7e433f
Cédric Delfosse <cdelfosse@mandriva.com>
2010-02-04 17:52:54 +0000

    * Add imaging API XML-RPC client to connect to the MMC agent

---
+++ services/pulse2/package_server/imaging/api/client.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8; -*-
+#
+# (c) 2007-2010 Mandriva, http://www.mandriva.com/
+#
+# $Id$
+#
+# This file is part of Pulse 2, http://pulse2.mandriva.org
+#
+# Pulse 2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pulse 2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pulse 2.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Imaging API XML-RPC client to connect to the MMC agent.
+"""
+
+from pulse2.apis.clients import Pulse2Api
+
+class ImagingXMLRPCClient(Pulse2Api):
+
+    """
+    Imaging API XML-RPC client to connect to the MMC agent.
+    """
+
+    name = "ImagingXMLRPCClient"
