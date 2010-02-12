dc63b81522d6e6b107730e2f9cf61d1cbc4d5a4a
Nicolas Rueff <nrueff@mandriva.com>
2010-02-12 11:19:43 +0000

    * Added some docs

--- services/hooks/imaging-server/generic_hook.py
+++ services/hooks/imaging-server/generic_hook.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8; -*-
 #
-# (c) 2010 Nicolas Rueff / Mandriva, http://www.mandriva.com
+# (c) 2010 Mandriva, http://www.mandriva.com
 #
 # $Id$
 #
@@ -25,33 +25,43 @@
     This is a generic hook skeleton to be used with pulse 2 imaging server
 """
 import sys
-
 import twisted.internet.reactor
-
-import pulse2.apis.clients.imaging
-import pulse2.imaging.config
 import logging
 import logging.config
+import pulse2.apis.clients.imaging
+import pulse2.imaging.config
 
-ERROR_OK = 0
-ERROR_SERVER = 1
-ERROR_CLIENT = 2
-ERROR_UNKNOWN = 3
+CONFIG_FILE = '/etc/mmc/pulse2/imaging-server/imaging-server.ini' #: config file location
 
-exitcode = ERROR_UNKNOWN
+# default error code
+ERROR_OK = 0        #: no error
+ERROR_SERVER = 1    #: error server-side
+ERROR_CLIENT = 2    #: error client-side (here)
+ERROR_UNKNOWN = 3   #: unknow (and default) error
+exitcode = ERROR_UNKNOWN #: global error code, used when exiting
 
 ########################################################################
 #         NOTHING SHOULD BE ALTERED ABOVE THIS LINE                    #
 ########################################################################
 
 def myCall():
+    """
+        Design your own call here
 
+        a deferred should be passed to callFunction()
+        @woot
+    """
     callFunction(
         imagingAPI.imagingServerStatus(
         )
     )
 
 def myTreatment(result):
+    """
+        Design your own treatment here
+
+        don't forget to set exitcode and finally call endBack()
+    """
     global exitcode
     if result:
         exitcode= ERROR_OK
@@ -64,9 +74,20 @@ def myTreatment(result):
 ########################################################################
 
 def endBack():
+    """
+        take the reactor down
+    """
     twisted.internet.reactor.callLater(0, twisted.internet.reactor.stop)
 
 def callBack(result):
+    """
+        XMLRPC result treatment
+
+        check if it was a success by analysing result
+        if it is a success, call myCall(result)
+        if not, exitcode is set to ERROR_SERVER then call endBack()
+    """
+
     global exitcode
     # if result is a list and the first arg a string and its value,
     # 'PULSE2_ERR', then something went wrong
@@ -80,21 +101,30 @@ def callBack(result):
         myTreatment(result)
 
 def errorBack(reason):
+    """
+        XMLRPC error treatment
+
+        just set exitcode to ERROR_CLIENT then call endBack()
+    """
     global exitcode
     exitcode = ERROR_CLIENT
     endBack()
 
 def callFunction(deffered):
-    d = imagingAPI.imagingServerStatus()
+    """
+        XMLRPC request handling
+
+        attach callBack() and errorBack() to the deferred
+    """
     deffered.addCallbacks( # deferred handling
         callBack,
         errorBack
     )
 
 # Parse the command line
-config = pulse2.imaging.config.ImagingConfig()
-config.setup('/etc/mmc/pulse2/imaging-server/imaging-server.ini')
-logging.config.fileConfig('/etc/mmc/pulse2/imaging-server/imaging-server.ini')
+config = pulse2.imaging.config.ImagingConfig(CONFIG_FILE) #: ConfigParser object
+config.setup()
+logging.config.fileConfig(CONFIG_FILE)
 
 # Instanciate the API
 imagingAPI = pulse2.apis.clients.imaging.ImagingApi({
@@ -107,7 +137,7 @@ imagingAPI = pulse2.apis.clients.imaging.ImagingApi({
     "verifypeer" : config.pserver_verifypeer,
     "localcert" : config.pserver_localcert,
     "cacert" : config.pserver_cacert,
-})
+}) #: Object which will be used to speak with our pserver
 
 # fire the reactor
 twisted.internet.reactor.callWhenRunning(myCall)
