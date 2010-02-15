4d5596b766670e55b212b65d1921271044bf6439
Jean-Philippe Braun <jpbraun@mandriva.com>
2010-02-15 15:55:21 +0000

    Audit translation codes for base module

---
+++ web/modules/base/includes/auditCodes.php
@@ -0,0 +1,29 @@
+<?
+
+$module_audit_codes = array(
+    'BASE_ADD_USER' => _T("Add user", "base"),
+    'BASE_ENABLE_USER'  => _T("Enable user", "base"),
+    'BASE_DISABLE_USER' => _T("Disable user", "base"),
+    'BASE_MOD_USER_ATTR' => _T("Change user attributes", "base"),
+    'BASE_MOD_GROUP_ATTR' => _T("Change group attributes", "base"),
+    'BASE_ADD_GROUP' => _T("Add group", "base"),
+    'BASE_ADD_USER_TO_GROUP' => _T("Add user to group", "base"),
+    'BASE_DEL_USER_FROM_GROUP' => _T("Del user from group", "base"),
+    'BASE_DEL_USER_FROM_ALL_GROUPS' => _T("Del user from all group", "base"),
+    'BASE_MOD_USER_PGROUP' => _T("Change user primary group", "base"),
+    'BASE_DEL_USER' => _T("Del user", "base"),
+    'BASE_DEL_USER_ATTR' => _T("Del user attributes", "base"),
+    'BASE_MOD_USER_PASSWORD' => _T("Change user Password", "base"),
+    'BASE_DEL_GROUP' => _T("Del group", "base"),
+    'BASE_BACKUP_USER' => _T("Backup user", "base"),
+    'BASE_AUTH_USER' => _T("User authentication", "base"),
+    'BASE_MOVE_USER_HOME' => _T("Move user home", "base"),
+    'BASE_ADD_OU' => _T("Add organizational unit", "base"),    
+    'USER' => _T("User", "base"),
+    'GROUP' => _T("Group", "base"),
+    'ATTRIBUTE' => _T("Attribute", "base"),
+    'ORGANIZATIONAL_UNIT' => _T("Organisational unit", "base"),
+    'MMC-BASE' => _T("base", "base"),
+);
+
+?>
