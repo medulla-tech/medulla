937e9c3180dde5d46de56b0492ccddafe80763aa
Cédric Delfosse <cdelfosse@mandriva.com>
2010-02-17 14:11:37 +0000

    * Remove only the plugins part of MMC * Also remove pulse2-* binaries in sbin directory

--- tests/scripts/uninstall.sh
+++ tests/scripts/uninstall.sh
@@ -51,8 +51,9 @@ if [ -f /usr/bin/mysqladmin ];
 fi
 
 rm -fr $PREFIX/lib/python2.*/site-packages/pulse2
-rm -fr $PREFIX/lib/python2.*/site-packages/mmc
+rm -fr $PREFIX/lib/python2.*/site-packages/mmc/plugins/
 rm -f /etc/init.d/pulse2-*
+rm -f $PREFIX/sbin/pulse2-*
 rm -fr /var/lib/pulse2/packages/*-*-*-*-*
 rm -fr /tmp/package_tmp
 
