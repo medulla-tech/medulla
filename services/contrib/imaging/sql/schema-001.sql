42559c6e59b16322a5266a334f36a84d0b6f5c71
Nicolas Rueff <nrueff@mandriva.com>
2010-02-18 15:57:20 +0000

    * Removed useless constraints * Added a default menu

--- services/contrib/imaging/sql/schema-001.sql
+++ services/contrib/imaging/sql/schema-001.sql
@@ -128,15 +128,15 @@ CREATE TABLE ImagingServer (
 CREATE TABLE Menu (
   id INT NOT NULL AUTO_INCREMENT,
   default_name Text NOT NULL,
-  fk_name INT NOT NULL,
-  timeout INT NOT NULL,
+  fk_name INT,
+  timeout INT,
   background_uri Text DEFAULT '',
-  message Text NOT NULL DEFAULT '', -- Warning! Your PC is being backed up or restored. Do not reboot !',
-  fk_default_item INT NOT NULL,
-  fk_default_item_WOL INT NOT NULL,
-  fk_protocol INT DEFAULT 0,
+  message Text NOT NULL DEFAULT '',
+  fk_default_item INT,
+  fk_default_item_WOL INT,
+  fk_protocol INT DEFAULT 1,
   PRIMARY KEY (id)
-);
+) AUTO_INCREMENT = 1000;
 
 -- PostInstallScriptOnImagingServer
 CREATE TABLE PostInstallScriptOnImagingServer (
@@ -181,9 +181,8 @@ CREATE TABLE MenuItem (
   hidden Bool DEFAULT 1,
   hidden_WOL Bool DEFAULT 1,
   fk_menu INT NOT NULL,
-  fk_name INT NOT NULL,
   PRIMARY KEY (id)
-);
+) AUTO_INCREMENT=1000;
 
 -- ImageInMenu
 CREATE TABLE ImageInMenu (
@@ -319,7 +318,6 @@ CREATE INDEX fk_image_on_imaging_server_imaging_server_idx  ON ImageOnImagingSer
 CREATE INDEX fk_partition_image_idx ON Partition(fk_image);
 
 CREATE INDEX fk_menu_item_menu_idx ON MenuItem(fk_menu);
-CREATE INDEX fk_menu_item_name_idx ON MenuItem(fk_name);
 
 CREATE INDEX fk_image_in_menu_image_idx     ON ImageInMenu(fk_image);
 CREATE INDEX fk_image_in_menu_menuitem_idx  ON ImageInMenu(fk_menuitem);
@@ -355,19 +353,36 @@ INSERT INTO Language (label) VALUES ('English');
 INSERT INTO Language (label) VALUES ('Français');
 INSERT INTO Language (label) VALUES ('Español');
 
-INSERT INTO Internationalization (id, fk_language, label) VALUES (1, 2, 'Continuer le démarrage normalement');
-INSERT INTO Internationalization (id, fk_language, label) VALUES (2, 2, 'Démarrer comme d\'habitude');
-INSERT INTO Internationalization (id, fk_language, label) VALUES (3, 2, 'Ajouter comme client Pulse 2');
-INSERT INTO Internationalization (id, fk_language, label) VALUES (4, 2, 'Enregistrer ce poste auprès du server Pulse 2');
-INSERT INTO Internationalization (id, fk_language, label) VALUES (5, 2, 'Sauvegarder comme image Master');
-INSERT INTO Internationalization (id, fk_language, label) VALUES (6, 2, 'Créer une sauvegarde pour utilisation comme master plus tard');
-INSERT INTO Internationalization (id, fk_language, label) VALUES (7, 2, 'Démarrage sans disque');
-INSERT INTO Internationalization (id, fk_language, label) VALUES (8, 2, 'Charger un environnement sans disque et obtenir une invite de commande');
-INSERT INTO Internationalization (id, fk_language, label) VALUES (9, 2, 'Test mémoire');
-INSERT INTO Internationalization (id, fk_language, label) VALUES (10, 2, 'Réaliser une vérification complète de la mémoire');
+INSERT INTO Internationalization (id, fk_language, label) VALUES (1, 2, 'Menu par défaut');
+INSERT INTO Internationalization (id, fk_language, label) VALUES (2, 2, 'Continuer le démarrage normalement');
+INSERT INTO Internationalization (id, fk_language, label) VALUES (3, 2, 'Démarrer comme d\'habitude');
+INSERT INTO Internationalization (id, fk_language, label) VALUES (4, 2, 'Ajouter comme client Pulse 2');
+INSERT INTO Internationalization (id, fk_language, label) VALUES (5, 2, 'Enregistrer ce poste auprès du server Pulse 2');
+INSERT INTO Internationalization (id, fk_language, label) VALUES (6, 2, 'Sauvegarder comme image Master');
+INSERT INTO Internationalization (id, fk_language, label) VALUES (7, 2, 'Créer une sauvegarde pour utilisation comme master plus tard');
+INSERT INTO Internationalization (id, fk_language, label) VALUES (8, 2, 'Démarrage sans disque');
+INSERT INTO Internationalization (id, fk_language, label) VALUES (9, 2, 'Charger un environnement sans disque et obtenir une invite de commande');
+INSERT INTO Internationalization (id, fk_language, label) VALUES (10, 2, 'Test mémoire');
+INSERT INTO Internationalization (id, fk_language, label) VALUES (11, 2, 'Réaliser une vérification complète de la mémoire');
 
 INSERT INTO BootService (id, default_name, default_desc, fk_name, fk_desc, `value`) VALUES (1, 'Continue Normal Startup', 'Start as usual', 1, 2, 'root (hd0)\r\nchainloader +1');
 INSERT INTO BootService (id, default_name, default_desc, fk_name, fk_desc, `value`) VALUES (2, 'Register as Pulse 2 Client', 'Record this computer in Pulse 2 Server', 3, 4, 'identify L=##PULSE2_LANG## P=none\nreboot');
 INSERT INTO BootService (id, default_name, default_desc, fk_name, fk_desc, `value`) VALUES (3, 'Create a Master image', 'Create a backup to be used as Master later', 5, 6, 'kernel ##PULSE2_NETDEVICE##/##PULSE2_F_DISKLESS##/##PULSE2_K_DISKLESS## revosavedir=/##PULSE2_F_MASTERS##/##PULSE2_F_IMAGE## revoroot=##PULSE2_F_BASE## quiet\ninitrd ##PULSE2_NETDEVICE##/##PULSE2_F_DISKLESS##/##PULSE2_I_DISKLESS##');
 INSERT INTO BootService (id, default_name, default_desc, fk_name, fk_desc, `value`) VALUES (4, 'Diskless Boot', 'Load diskless environment then get a prompt', 7, 8, 'kernel ##PULSE2_NETDEVICE##/##PULSE2_F_DISKLESS##/##PULSE2_K_DISKLESS## revodebug revoroot=##PULSE2_F_BASE## quiet\ninitrd ##PULSE2_NETDEVICE##/##PULSE2_F_DISKLESS##/##PULSE2_I_DISKLESS##');
 INSERT INTO BootService (id, default_name, default_desc, fk_name, fk_desc, `value`) VALUES (5, 'Memory test', 'Run a full memory check', 9, 10, 'kernel --kernel-type=openbsd ##PULSE2_NETDEVICE##/##PULSE2_F_TOOLS##/##PULSE2_MEMTEST##');
+
+INSERT INTO Menu (id, default_name, fk_name, timeout, background_uri, message, fk_default_item, fk_default_item_WOL, fk_protocol) VALUES (1, 'Default Boot Menu', 1, NULL, '/##PULSE2_F_DISKLESS##/##PULSE2_F_BOOSPLASH##', '-- Warning! Your PC is being backed up or restored. Do not reboot !', NULL, NULL, 1);
+
+INSERT INTO MenuItem (id, `order`, hidden, hidden_WOL, fk_menu) VALUES (1, 1, 0, 0, 1);
+INSERT INTO MenuItem (id, `order`, hidden, hidden_WOL, fk_menu) VALUES (2, 2, 0, 0, 1);
+INSERT INTO MenuItem (id, `order`, hidden, hidden_WOL, fk_menu) VALUES (3, 3, 0, 0, 1);
+INSERT INTO MenuItem (id, `order`, hidden, hidden_WOL, fk_menu) VALUES (4, 4, 0, 0, 1);
+INSERT INTO MenuItem (id, `order`, hidden, hidden_WOL, fk_menu) VALUES (5, 5, 0, 0, 1);
+
+INSERT INTO BootServiceInMenu (fk_bootservice, fk_menuitem) VALUES (1, 1);
+INSERT INTO BootServiceInMenu (fk_bootservice, fk_menuitem) VALUES (2, 2);
+INSERT INTO BootServiceInMenu (fk_bootservice, fk_menuitem) VALUES (3, 2);
+INSERT INTO BootServiceInMenu (fk_bootservice, fk_menuitem) VALUES (4, 4);
+INSERT INTO BootServiceInMenu (fk_bootservice, fk_menuitem) VALUES (5, 5);
+
+UPDATE Menu SET fk_default_item = 1, fk_default_item_WOL = 1 where id = 1;
