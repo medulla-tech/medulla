8402bb9d7a9539a55b4bfd322cdbaaf0c45522d0
Jean-Philippe Braun <jpbraun@mandriva.com>
2010-02-23 13:42:01 +0000

    * Add audit codes for ppolicy module

---
+++ web/modules/ppolicy/includes/auditCodes.php
@@ -0,0 +1,14 @@
+<?
+
+$module_audit_codes = array(
+    'PPOLICY_MOD_USER_ATTR' => _T("Change ppolicy attribute", "ppolicy"),
+    'PPOLICY_DEL_USER_ATTR' => _T("Delete ppolicy attribute", "ppolicy"),
+    'PPOLICY_ADD_USER_PPOLICY_ATTR' => _T("Add ppolicy attributes", "ppolicy"),
+    'PPOLICY_DEL_USER_PPOLICY_ATTR' => _T("Delete ppolicy attributes", "ppolicy"),
+    'PPOLICY_MOD_ATTR' => _T("Change ppolicy attribute", "ppolicy"),
+    'PPOLICY_DEL_ATTR' => _T("Delete ppolicy attribute", "ppolicy"),
+    'PPOLICY' => _T("ppolicy", "ppolicy"),
+    'MMC-PPOLICY' => _T("ppolicy", "ppolicy"),
+);
+
+?>
