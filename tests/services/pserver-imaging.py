1405b071c99889b6920d4d4110b25abf90394e0f
Cédric Delfosse <cdelfosse@mandriva.com>
2010-02-25 10:56:16 +0000

    * Update package server imaging tests

--- tests/services/pserver-imaging.py
+++ tests/services/pserver-imaging.py
@@ -29,7 +29,40 @@ from testutils import ipconfig
 
 ipserver = ipconfig()
 protocol = 'https' # protocol's server
-server = xmlrpclib.ServerProxy('%s://%s:9990/imaging' % (protocol,ipserver))
+server = xmlrpclib.ServerProxy('%s://%s:9990/imaging_api' % (protocol,ipserver))
+menu = { 'timeout' : 20,
+         'background_uri' : u'/##PULSE2_F_DISKLESS##/##PULSE2_F_BOOTSPLASH##',
+         'name' : u'Default Boot Menu',
+         'message' : u'-- Warning! Your PC is being backed up or restored. Do not reboot !',
+         'protocol' : 'nfs',
+         'default_item' : 1,
+         'default_item_wol' : 1,
+         'bootservices' : { '1': { 'name' : u'Continue Normal Startup',
+                                 'desc' : u'Start as usual',
+                                 'value' : u'root (hd0)\nchainloader +1',
+                                 'hidden' : 0,
+                                 'hidden_WOL' : 0,
+                                 'value' : u'root (hd0)\nchainloader +1' },
+                           '2': { 'name' : u'Register a Pulse 2 Client',
+                                'desc' : u'Record this computer in Pulse 2 Server',
+                                'value' : u'identify L=##PULSE2_LANG## P=none\nreboot' }
+                            },
+         'images' : { '5' : { 'uuid': u'302c19d2-212b-4b9a-b04e-4fced0b83466',
+                            'name' : u'Image computer.example.net',
+                            'desc' : u'(Thu Dec  3 15:20:02 2009)',
+                            'post_install_script' : { 'id' : 2,
+                                                      'name' : u'...',
+                                                      'desc' : u'...',
+                                                      'value' : '...'
+                                                    }
+                            }
+                    },
+         'target' : { 'name' : u'...',
+                      'kernel_parameters' : u'...',
+                      'image_parameters' : u'....',
+                    }
+        }
+
 
 class Imaging(unittest.TestCase):
 
@@ -41,12 +74,43 @@ class Imaging(unittest.TestCase):
         self.assertTrue('faultCode' in result and
                         'TypeError' in result['faultCode'])
         result = server.computerRegister('foobar', '00:11:22:33:44:55')
-        self.assertEqual(0, result)
+        self.assertEqual(1, result)
 
+    def test_logClientAction(self):
+        result = server.logClientAction('mac', 'level', 'phase', 'message')
+        self.assertTrue('faultCode' in result and
+                        'TypeError' in result['faultCode'])
+        result = server.logClientAction('00:11:22:33:44:55', 'level', 'phase', 'message')
+        self.assertTrue(result)
+
+    def test_computerUpdate(self):
+        result = server.computerUpdate('BADMAC')
+        self.assertTrue('faultCode' in result and
+                        'TypeError' in result['faultCode'])        
+        
     def test_status(self):
         result = server.imagingServerStatus()
-        self.assertEqual(int, type(result))
-        self.assertTrue(result == -1 or result >= 0 or result <= 100)
+        self.assertEqual(dict, type(result))
+        self.assertTrue('space_available' in result)
+        self.assertTrue('mem_info' in result)
+        self.assertTrue('uptime' in result)
+        self.assertTrue('stats' in result)
+
+    def test_injectInventory(self):
+        pass
+
+    def test_getComputerByMAC(self):
+        result = server.getComputerByMac('BADMAC')
+        self.assertTrue('faultCode' in result and
+                        'TypeError' in result['faultCode'])
+        result = server.getComputerByMac('00:11:22:33:44:55')
+        self.assertTrue('uuid' in result and result['uuid'] == 'FAKE_UUID')
+
+    def test_computersMenuSet(self):
+        result = server.computersMenuSet([('UUID1', {})])
+        self.assertEqual(['UUID1'], result)
+        result = server.computersMenuSet([('UUID1', menu)])
+        self.assertEqual(['UUID1'], result)
 
 if __name__ == '__main__':
     unittest.main()
