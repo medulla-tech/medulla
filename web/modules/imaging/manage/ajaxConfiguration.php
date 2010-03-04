e9a19f03032b50b89e804f1d828951022233c212
Olivier Roussy <oroussy@mandriva.com>
2010-03-04 13:34:54 +0000

    . small changes in the location imaging server edition to hide useless fields .

--- web/modules/imaging/manage/ajaxConfiguration.php
+++ web/modules/imaging/manage/ajaxConfiguration.php
@@ -22,7 +22,7 @@
  * along with MMC; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
- 
+
 /* Get MMC includes */
 require("../../../includes/config.inc.php");
 require("../../../includes/i18n.inc.php");
@@ -33,7 +33,7 @@ require("../includes/includes.php");
 require("../includes/xmlrpc.inc.php");
 
 $location = getCurrentLocation();
- 
+
 $t = new TitleElement(_T("Imaging server configuration", "imaging"));
 $t->display();
 
@@ -41,9 +41,9 @@ if (xmlrpc_doesLocationHasImagingServer($location)) {
     $config = xmlrpc_getImagingServerConfig($location);
     $imaging_server = $config[0];
     $default_menu = $config[1];
-    
+
     $f = new ValidatingForm(array("action"=>urlStrRedirect("imaging/manage/save_configuration"),));
-    
+
     $f->add(new HiddenTpl("is_uuid"),                       array("value" => $imaging_server['imaging_uuid'], "hide" => True));
     /* We dont have this information right now in the database schema
 
@@ -54,32 +54,32 @@ if (xmlrpc_doesLocationHasImagingServer($location)) {
     $ifaces->setElements($interfaces);
     $ifaces->setElementsVal($interfaces);
     $f->add(
-        new TrFormElement(_T("Network interface on which traffic shaping is done", "imaging"), 
+        new TrFormElement(_T("Network interface on which traffic shaping is done", "imaging"),
         $ifaces)
     );
     $f->add(
-        new TrFormElement(_T("Network interface theorical throughput (Mbit)", "imaging"), 
+        new TrFormElement(_T("Network interface theorical throughput (Mbit)", "imaging"),
         new InputTpl("net_output")), array("value" => "1000")
     );
     $f->add(
-        new TrFormElement(_T("Max. throughput for TFTP restoration (Mbit)", "imaging"), 
+        new TrFormElement(_T("Max. throughput for TFTP restoration (Mbit)", "imaging"),
         new InputTpl("net_tftp")), array("value" => "1000")
     );
     $f->add(
-        new TrFormElement(_T("Max. throughput for NFS restoration (Mbit)", "imaging"), 
+        new TrFormElement(_T("Max. throughput for NFS restoration (Mbit)", "imaging"),
         new InputTpl("net_nfs")), array("value" => "1000")
     );
     $f->pop();*/
 
     $f->add(new TitleElement(_T("Default menu parameters", "imaging")));
     $f->push(new Table());
-    
+
     $f->add(
-        new TrFormElement(_T('Default menu label', 'imaging'),        
+        new TrFormElement(_T('Default menu label', 'imaging'),
         new InputTpl("default_m_label")), array("value" => $default_menu['default_name'])
     );
     $f->pop();
- 
+
     $f->add(new TitleElement(_T("Restoration options", "imaging")));
     $f->push(new Table());
     $possible_protocols = web_def_possible_protocols();
@@ -103,9 +103,9 @@ if (xmlrpc_doesLocationHasImagingServer($location)) {
             $protocols_values[$p['imaging_uuid']] = $p['imaging_uuid'];
         }
     }
-    
+
     $rest_type = new RadioTpl("rest_type");
-    
+
     $rest_type->setChoices($protocols_choices);
     $rest_type->setValues($protocols_values);
     $rest_type->setSelected($rest_selected);
@@ -113,35 +113,35 @@ if (xmlrpc_doesLocationHasImagingServer($location)) {
         new TrFormElement(_T("Restoration type", "imaging"), $rest_type)
     );
     $f->add(
-        new TrFormElement(_T("Restoration: MTFTP maximum waiting (in sec)", "imaging"), 
+        new TrFormElement(_T("Restoration: MTFTP maximum waiting (in sec)", "imaging"),
         new InputTpl("rest_wait")), array("value" => $default_menu['timeout'])
     );
     $f->pop();
-    
+
     $f->add(new TitleElement(_T("Boot options", "imaging")));
     $f->push(new Table());
     $f->add(
-        new TrFormElement(_T("Full path to the XPM displayed at boot", "imaging"), 
+        new TrFormElement(_T("Full path to the XPM displayed at boot", "imaging"),
         new InputTpl("boot_xpm")), array("value" => $default_menu['background_uri'])
     );
     $f->add(
-        new TrFormElement(_T("Message displayed during backup/restoration", "imaging"), 
+        new TrFormElement(_T("Message displayed during backup/restoration", "imaging"),
         new TextareaTpl("boot_msg")), array("value" => $default_menu['message']) //"Warning ! Your PC is being backed up or restored. Do not reboot !")
     );
     $f->add(
-        new TrFormElement(_T("Keyboard mapping (empty/fr)", "imaging"), 
+        new TrFormElement(_T("Keyboard mapping (empty/fr)", "imaging"),
         new InputTpl("boot_keyboard")), array("value" => "")
     );
     $f->pop();
-    
-    $f->add(new TitleElement(_T("Administration options", "imaging")));
+
+    /*$f->add(new TitleElement(_T("Administration options", "imaging")));
     $f->push(new Table());
     $f->add(
-        new TrFormElement(_T("Password for adding a new client", "imaging"), 
+        new TrFormElement(_T("Password for adding a new client", "imaging"),
         new InputTpl("misc_passwd")), array("value" => "")
     );
-    $f->pop();
-    
+    $f->pop();*/
+
     $_GET["action"] = "save_configuration";
     $f->addButton("bvalid", _T("Validate"));
     $f->pop();
@@ -153,5 +153,5 @@ if (xmlrpc_doesLocationHasImagingServer($location)) {
     print "<br/><br/><br/>";
     $ajax->displayDivToUpdate();
 }
- 
+
  ?>
