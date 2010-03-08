da3da4067866148396aa7dc10f4a6a10f5f3b823
Cédric Delfosse <cdelfosse@mandriva.com>
2010-03-08 10:03:46 +0000

    * Add missing pulse2.imaging and pulse2.imaging_server module in setup.py

--- services/setup.py
+++ services/setup.py
@@ -59,6 +59,8 @@ setup(
         "pulse2.database.inventory",
         "pulse2.database.msc",
         "pulse2.database.msc.orm",
+        "pulse2.imaging",
+        "pulse2.imaging_server",
         "pulse2.inventoryserver",
         "pulse2.launcher",
         "pulse2.managers",
