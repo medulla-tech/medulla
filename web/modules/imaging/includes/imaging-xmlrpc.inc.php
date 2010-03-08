18faae357b15294463d7ff4384c4cb21d21281e3
Olivier Roussy <oroussy@mandriva.com>
2010-03-08 18:19:58 +0000

    . remove all unused xmlrpc calls .

--- web/modules/imaging/includes/imaging-xmlrpc.inc.php
+++ web/modules/imaging/includes/imaging-xmlrpc.inc.php
@@ -21,41 +21,12 @@
  * along with MMC; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
- /* DEPRECATED */
-function getPublicImagesList() {
-    return xmlCall("imaging.getPublicImagesList", null);
-}
-
-function getPublicImageInfos($imagename) {
-    return xmlCall("imaging.getPublicImageInfos", array($imagename));
-}
-
-function delPublicImage($imagename) {
-    return xmlCall("imaging.deletePublicImage", array($imagename));
-}
-
-function isAPublicImage($imagename) {
-    return xmlCall("imaging.isAnImage", array($imagename));
-}
-
-function duplicatePublicImage($imagename, $newimagename) {
-    return xmlCall("imaging.duplicatePublicImage", array($imagename, $newimagename));
-}
-
-function setPublicImageData($imagename, $name, $title, $desc) {
-    return xmlCall("imaging.setPublicImageData", array($imagename, $name, $title, $desc));
-}
-
-function createIsoFromImage($imagename, $name, $size) {
-    return xmlCall("imaging.createIsoFromImage", array($imagename, $name, $size));
-}
-
 function humanReadable($num, $unit='B', $base=1024) {
     foreach (array('', 'K', 'M', 'G', 'T') as $i) {
         if ($num < $base) {
             return sprintf("%3.1f %s%s", $num, $i, $unit);
-        }    
+        }
         $num /= $base;
-    }    
+    }
 }
 ?>
