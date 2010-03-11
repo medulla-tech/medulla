1c5b9640be05e312aaeacc8f3f282bb310022e50
Cédric Delfosse <cdelfosse@mandriva.com>
2010-03-11 10:53:53 +0000

    * Declare strings coming from the MMC agent so that they can be translated

---
+++ web/modules/base/includes/computers-i18n.php
@@ -0,0 +1,29 @@
+<?
+/**
+ * (c) 2010 Mandriva, http://www.mandriva.com/
+ *
+ * $Id$
+ *
+ * This file is part of Mandriva Management Console (MMC).
+ *
+ * MMC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MMC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MMC.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Declare the following strings so that they can be translated */
+
+_("Computer's IP address");
+_("Computer's MAC address");
+_("Computer's network address");
+
+?>
