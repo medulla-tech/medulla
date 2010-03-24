536763e387b1e20ce1a8be861c8e4d0868bf48d7
Cédric Delfosse <cdelfosse@mandriva.com>
2010-03-24 15:56:59 +0000

    * Fix tuple built when calling imaging.getDefaultMenuForRegistering

--- services/pulse2/package_server/imaging/api/__init__.py
+++ services/pulse2/package_server/imaging/api/__init__.py
@@ -115,7 +115,7 @@ class ImagingApi(MyXmlrpc):
         self.logger.debug('Starting package server internals initialization')
         client = self._getXMLRPCClient()
         func = 'imaging.getDefaultMenuForRegistering'
-        args = (self.config.imaging_api['uuid'])
+        args = (self.config.imaging_api['uuid'], )
         d = client.callRemote(func, *args)
         d.addCallbacks(_cbDefaultMenu, _errDefaultMenu)
 
