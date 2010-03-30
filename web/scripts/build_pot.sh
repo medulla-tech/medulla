8e72d3d0c0ee529e15f2d91fc0247702e69af492
Cédric Delfosse <cdelfosse@mandriva.com>
2010-03-30 07:51:21 +0000

    * Update to also handle the ppolicy module

--- web/scripts/build_pot.sh
+++ web/scripts/build_pot.sh
@@ -1,31 +1,43 @@
 #!/bin/sh
 #
-# Copyright (C) 2006, Jérôme Wax and Adam Cécile  for Linbox FAS
+# (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+# (c) 2007-2010 Mandriva, http://www.mandriva.com
 #
-# This program is free software; you can redistribute it and/or modify
+# $Id$
+#
+# This file is part of Mandriva Management Console (MMC).
+#
+# MMC is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# MMC is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# along with MMC.  If not, see <http://www.gnu.org/licenses/>.
 
-POT="modules/base/locale/base.pot"
-
-rm ${POT}
-touch ${POT}
-find . -iname "*.php" -exec xgettext -C -j -o ${POT} --language=PHP --keyword=_ {} \;
-
-for name in `find ${1} -type f -name *.po`; do
-    echo -n "updating ${name}..."
-    msgmerge --update --add-location --sort-output ${name} ${POT}
-    echo "done"
+for module in base ppolicy; do
+    POT="modules/$module/locale/$module.pot"
+    rm -f $POT
+    touch $POT
+    # Change gettext keyword according to the module
+    if [ $module == base ]; then
+	keyword="_"
+	fpath=.
+    else
+	keyword=_T
+	fpath=modules/$module
+    fi
+    find $fpath -iname "*.php" -exec xgettext -C -j -o ${POT} --language=PHP --keyword=$keyword {} \;
+    for name in `find modules/$module/locale -type f -name *.po`; do
+        echo -n "updating ${name}..."
+        msgmerge --update --add-location --sort-output ${name} ${POT}
+        echo "done"
+    done
 done
 
 exit 0
