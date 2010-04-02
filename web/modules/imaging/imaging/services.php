07d222031c9c58c0741a63a46e9520c29b118f0f
Olivier Roussy <oroussy@mandriva.com>
2010-04-02 12:31:04 +0000

    . remove useless commented code .

--- web/modules/imaging/imaging/services.php
+++ web/modules/imaging/imaging/services.php
@@ -73,49 +73,6 @@ if (($type == '' && xmlrpc_isComputerRegistered($target_uuid)) || ($type == 'gro
         $ajax->display();
         echo '<br/><br/><br/>';
         $ajax->displayDivToUpdate();
-        /*
-        $params = getParams();
-
-        $addActions = array();
-
-        $addAction = new ActionPopupItem(_T("Add service to boot menu", "imaging"), "addservice", "addbootmenu", "image", "base", "computers", null, 300, "add");
-        $delAction = new ActionPopupItem(_T("Remove service from boot menu"), "bootmenu_remove", "delbootmenu", "item", "base", "computers", $type."tabbootmenu", 300, "delete");
-
-        $emptyAction = new EmptyActionItem();
-
-        // show services list
-        $a_label = array();
-        $a_desc = array();
-        $a_in_boot_menu = array();
-        $params['from'] = 'tabservices';
-        $i = -1;
-        foreach ($menu as $entry) {
-            $i = $i+1;
-            $list_params[$i] = $params;
-            $list_params[$i]["itemlabel"] = $entry['default_name'];
-            $list_params[$i]["itemid"] = $entry['imaging_uuid'];
-            $list_params[$i]['targetid'] = $targetid;
-            // don't show action if service is in bootmenu
-            if(!isset($entry['menu_item'])) {
-                $addActions[] = $addAction;
-            } else {
-                $addActions[] = $delAction;
-                $list_params[$i]["mi_itemid"] = $entry['menu_item']['imaging_uuid'];
-            }
-
-            $a_label[]= sprintf("%s%s", ($script['is_local']?'':'X) '), $entry['default_name']);
-            $a_desc[]= $entry['default_desc'];
-            $a_in_boot_menu[]= (isset($entry['menu_item'])? True:False);
-
-        }
-        $l = new ListInfos($a_label, _T("Label", "imaging"));
-        $l->addExtraInfo($a_desc, _T("Description", "imaging"));
-        $l->addExtraInfo($a_in_boot_menu, _T("In bootmenu", "imaging"));
-        $l->setParamInfo($list_params);
-        $l->addActionItemArray($addActions);
-        $l->disableFirstColumnActionLink();
-        $l->display();
-         */
     }
     switch($mod) {
         case 'add':
