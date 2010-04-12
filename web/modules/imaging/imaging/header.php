f4e3d16f5f0fc86db8e3c2baa5a854e8e54c2343
Nicolas Rueff <nrueff@mandriva.com>
2010-04-12 14:51:08 +0000

    * Removed PHP notice

--- web/modules/imaging/imaging/header.php
+++ web/modules/imaging/imaging/header.php
@@ -35,7 +35,7 @@ if ($stateid == $SYNCHROSTATE_TODO) {
     $f->addButton("bsync", _T("Synchronize", "imaging"));
     $f->display();
     print "</td></tr></table>";
-} elseif (($_GET['tab'] == 'tabbootmenu' || $_GET['tab'] == 'grouptabbootmenu') && isExpertMode()) {
+} elseif (isset($_GET['tab']) && ($_GET['tab'] == 'tabbootmenu' || $_GET['tab'] == 'grouptabbootmenu') && isExpertMode()) {
     print "<table><tr><td>";
     print _T('Click on "Force synchronize" if you want to force the synchronization', 'imaging');
     print "</td><td>";
