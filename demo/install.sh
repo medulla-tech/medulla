e4bd24746a792ae785fb0ade8721d56bf0ccaddd
Olivier Roussy <oroussy@mandriva.com>
2010-04-14 15:13:16 +0000

    * correct some file patching to get a working installation

--- demo/install.sh
+++ demo/install.sh
@@ -1,12 +1,34 @@
-#!/bin/sh
+#!/bin/bash -e
 
-# urpmi screen
 #
+# (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+# (c) 2007-2010 Mandriva, http://www.mandriva.com
+#
+# $Id$
+#
+# This file is part of Mandriva Management Console (MMC).
+#
+# MMC is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# MMC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MMC.  If not, see <http://www.gnu.org/licenses/>.
+
+export LANG=C
+export LC_ALL=C
 
 VERSION='trunk'
 if [ "x$1" != "x" ]; then VERSION=$1; fi
 
 ############ install code
+echo "installing pulse2 and mmc-core services"
 urpmi lsb-release
 urpmi --force svn
 wget http://mds.mandriva.org/svn/mmc-projects/pulse2/server/trunk/tests/scripts/bootstrap.sh
@@ -15,6 +37,7 @@ sh bootstrap.sh
 ############ install files and configuration
 FILEDIR='cs4demo'
 
+echo "installing the documentation"
 svn export http://pulse2.mandriva.org/svn/pulse2-build-tools/branches/build_mandriva/build_vmware_image/cs4demo/ $FILEDIR
 
 # get the documentation files that correspond to the wanted version
@@ -33,6 +56,7 @@ else
     NAMEEN=`ls pulse2-doc/Pulse2-Manual-*-EN.pdf | tail -n 1 | xargs basename`
 fi
 
+echo "installing the web files"
 cp $FILEDIR/var/www/html/index.html /var/www/html/index.html.bkp -f
 perl -e "
 while (<>) {
@@ -49,6 +73,7 @@ while (<>) {
 rm -f /var/www/html/index.html.bkp
 
 # do mysql stuff
+echo "modifying the default mysqld configuration"
 perl -pi -e 's/skip-networking/#skip-networking/' /etc/my.cnf
 service mysqld restart
 sleep 10 # let some time for mysql to start
@@ -57,6 +82,7 @@ sleep 10 # let some time for mysql to start
 mkdir -p /var/lib/pulse2/packages /var/lib/pulse2/downloads /tmp/package_tmp/put
 
 # prepare files
+echo "patch configuration files"
 cat /etc/mmc/pulse2/launchers/launchers.ini | \
     sed -e 's/# username =.*/username =/' | \
     sed -e 's/# password =.*/password =/' | \
@@ -76,6 +102,7 @@ cat /etc/mmc/plugins/base.ini | \
 
 perl -pi -e 's/# username = .*/username =/; s/# password =.*/password =/; s/username =.*/username =/; s/password =.*/password =/' /etc/mmc/pulse2/scheduler/scheduler.ini
 
+echo "installing template files"
 cp -rf cs4demo/etc/mmc/template /etc/mmc/
 cp -rf cs4demo/etc/mmc/live_template.sh /etc/mmc/
 head -n 7 cs4demo/etc/sysconfig/network-scripts/ifup.d/mmc-ip > /etc/sysconfig/network-scripts/ifup.d/mmc-ip
@@ -95,6 +122,7 @@ cp -rf cs4demo/home/guest /home
 chown guest:guest -R /home/guest/
 
 # startup
+echo "configuration of icewm"
 mkdir -p /home/guest/.icewm
 echo """
 #!/bin/sh
@@ -117,6 +145,7 @@ EXEC=/usr/bin/startx.autologi
 
 
 # patch some configuration files
+echo "patching pulse2 configuration files"
 perl -pi -e 's/disable = 0/disable = 1/' /etc/mmc/plugins/glpi.ini
 perl -pi -e 's/# \[computers\]/[computers]/' /etc/mmc/plugins/base.ini
 perl -pi -e 's/# method = inventory/method = inventory/' /etc/mmc/plugins/base.ini
@@ -128,6 +157,8 @@ perl -pi -e 's&# halfstatic = Registry/Value/display name::Path##DisplayName&hal
 perl -pi -e 's/PROMPT=.*/PROMPT=NO/' /etc/sysconfig/init
 perl -pi -e 's/# default_module =.*/default_module = inventory/' /etc/mmc/plugins/dyngroup.ini
 perl -pi -e 's/activate = 0/activate = 1/' /etc/mmc/plugins/dyngroup.ini
+echo "# ##IPADDR##" >> /etc/mmc/template/etc/mmc/plugins/base.ini
+echo "# ##IPADDR##" >> /etc/mmc/template/etc/mmc/plugins/inventory.ini
 
 mkdir /etc/mmc/template/etc/mmc/pulse2/imaging-server/ -p
 cp /etc/mmc/pulse2/imaging-server/imaging-server.ini /etc/mmc/template/etc/mmc/pulse2/imaging-server/ -f
@@ -136,9 +167,13 @@ perl -pi -e 's/host =.*/host = ##IPADDR##/' /etc/mmc/template/etc/mmc/pulse2/ima
 # patch some files
 perl -pi -e 's/canAddComputer/False && canAddComputer/' /usr/share/mmc/modules/base/computers/localSidebar.php
 
+echo "patch some init.d files"
+# imaging server has to start before package server!
+perl -pi -e 's/# chkconfig: 345 99 60/# chkconfig: 345 98 60/' /etc/init.d/pulse2-imaging-server
+
+
 # generate ssh key
-#rm -f /root/.ssh/id_dsa*
-#ssh-keygen -t dsa -f /root/.ssh/id_dsa -N ""
+echo "creating win32 agent with the new ssh keys"
 cp -f /root/.ssh/id_dsa.pub /var/www/html/
 
 # generate agent with the new key
@@ -146,6 +181,7 @@ urpmi --force p7zip
 sh build_win32-agents.sh
 
 # restart all services
+echo "restart services"
 cd /etc/mmc/ && ./live_template.sh
 
 /etc/init.d/httpd restart
@@ -160,21 +196,34 @@ chkconfig --add pulse2-package-server
 /etc/init.d/pulse2-imaging-server restart
 chkconfig --add pulse2-imaging-server
 
-/etc/init.d/mmc-agent restart
+/etc/init.d/mmc-agent stop
+rm -f /var/run/mmc-agent.pid
+# I don't know but mmc-agent don't want to clean it's pid file here.
+/etc/init.d/mmc-agent start
 chkconfig --add mmc-agent
 
 ## START AGENT and register the package server
+echo "registering the imaging server in the mmc-agent"
 pulse2-package-server-register-imaging -m https://mmc:s3cr3t@localhost:7080/mmc -n "Localhost Imaging Server"
+
+echo "associate imaging server to entity"
+python -c "
+import xmlrpclib
+agent = xmlrpclib.ServerProxy('%s://mmc:s3cr3t@%s:7080' % ('https', '127.0.0.1'))
+agent.imaging.linkImagingServerToLocation('UUID1', 'UUID1', 'root')
+"
 
 ## DOC :
 # arreter vmware
 # modifier le dhcpd de l'interface nat de vmware et ajouter la ligne "next-server <l'ip de la vm pulse>;" dans le tronc principal
+
 #
+echo "installing nfs exports"
 mv /etc/exports /etc/exports.bkp
 svn export http://mds.mandriva.org/svn/mmc-projects/pulse2/server/trunk/services/contrib/imaging-server/exports /etc/exports
-#/etc/init.d/nfs-common restart
-#/etc/init.d/nfs-server restart
 
+#
+echo "installing atftp server"
 mv /etc/sysconfig/atftpd /etc/sysconfig/atftpd.bkp
 echo "
 ## Path:    Network/FTP/Atftpd
@@ -208,7 +257,7 @@ touch /var/log/mmc/atftpd.log
 chmod 777 /var/log/mmc/atftpd.log
 /etc/init.d/atftpd restart
 
-
+echo "modifying the dhcpd configuration"
 mv /etc/dhcpd.conf /etc/dhcpd.conf.bkp
 echo 'allow booting;
 allow bootp;
@@ -220,6 +269,7 @@ subnet 172.16.13.0 netmask 255.255.255.0 {
 ' > /etc/dhcpd.conf
 
 # clean
+echo "cleaning"
 rm -fr "$FILEDIR" "pulse2-doc" "bootstrap.sh"
 
 # and halt
