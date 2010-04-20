bd87f0e6df43fb333a701cb49673c22aafed3ad8
Cédric Delfosse <cdelfosse@mandriva.com>
2010-04-20 16:40:50 +0000

    * Add license header, set svn:keywords to Id * Mark strings for translation !

--- web/modules/base/includes/auditCodes.php
+++ web/modules/base/includes/auditCodes.php
@@ -1,30 +1,53 @@
 <?
 
+/**
+ * (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+ * (c) 2007-2010 Mandriva, http://www.mandriva.com
+ *
+ * $Id$
+ *
+ * This file is part of Mandriva Management Console (MMC).
+ *
+ * MMC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MMC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MMC.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 $module_audit_codes = array(
-    'BASE_ADD_USER' => _T("Add user", "base"),
-    'BASE_ENABLE_USER'  => _T("Enable user", "base"),
-    'BASE_DISABLE_USER' => _T("Disable user", "base"),
-    'BASE_MOD_USER_ATTR' => _T("Change user attribute", "base"),
-    'BASE_MOD_GROUP_ATTR' => _T("Change group attribute", "base"),
-    'BASE_ADD_GROUP' => _T("Add group", "base"),
-    'BASE_ADD_USER_TO_GROUP' => _T("Add user to group", "base"),
-    'BASE_DEL_USER_FROM_GROUP' => _T("Del user from group", "base"),
-    'BASE_DEL_USER_FROM_ALL_GROUPS' => _T("Del user from all group", "base"),
-    'BASE_MOD_USER_PGROUP' => _T("Change user primary group", "base"),
-    'BASE_DEL_USER' => _T("Del user", "base"),
-    'BASE_DEL_USER_ATTR' => _T("Del user attribute", "base"),
-    'BASE_MOD_USER_PASSWORD' => _T("Change user Password", "base"),
-    'BASE_DEL_GROUP' => _T("Del group", "base"),
-    'BASE_BACKUP_USER' => _T("Backup user", "base"),
-    'BASE_AUTH_USER' => _T("User authentication", "base"),
-    'BASE_MOVE_USER_HOME' => _T("Move user home", "base"),
-    'BASE_ADD_OU' => _T("Add organizational unit", "base"),    
-    'USER' => _T("User", "base"),
-    'SYSTEMUSER' => _T("System user", "base"),
-    'GROUP' => _T("Group", "base"),
-    'ATTRIBUTE' => _T("Attribute", "base"),
-    'ORGANIZATIONAL_UNIT' => _T("Organisational unit", "base"),
-    'MMC-BASE' => _T("base", "base"),
+    'BASE_ADD_USER' => _("Add user"),
+    'BASE_ENABLE_USER'  => _("Enable user"),
+    'BASE_DISABLE_USER' => _("Disable user"),
+    'BASE_MOD_USER_ATTR' => _("Change user attribute"),
+    'BASE_MOD_GROUP_ATTR' => _("Change group attribute"),
+    'BASE_ADD_GROUP' => _("Add group"),
+    'BASE_ADD_USER_O_GROUP' => _("Add user to group"),
+    'BASE_DEL_USER_FROM_GROUP' => _("Del user from group"),
+    'BASE_DEL_USER_FROM_ALL_GROUPS' => _("Del user from all group"),
+    'BASE_MOD_USER_PGROUP' => _("Change user primary group"),
+    'BASE_DEL_USER' => _("Del user"),
+    'BASE_DEL_USER_ATTR' => _("Del user attribute"),
+    'BASE_MOD_USER_PASSWORD' => _("Change user Password"),
+    'BASE_DEL_GROUP' => _("Del group"),
+    'BASE_BACKUP_USER' => _("Backup user"),
+    'BASE_AUTH_USER' => _("User authentication"),
+    'BASE_MOVE_USER_HOME' => _("Move user home"),
+    'BASE_ADD_OU' => _("Add organizational unit"),
+    'USER' => _("User"),
+    'SYSTEMUSER' => _("System user"),
+    'GROUP' => _("Group"),
+    'ATTRIBUTE' => _("Attribute"),
+    'ORGANIZATIONAL_UNIT' => _("Organizational unit"),
+    'MMC-BASE' => _("base"),
 );
 
 ?>
