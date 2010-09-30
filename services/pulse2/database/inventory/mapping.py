d419f99bc9266341c5fdc2900ce31d60cf9f8199
Cédric Delfosse <cdelfosse@mandriva.com>
2010-09-30 11:48:13 +0000

    * Successfully parse XML OCS inventory string declared as UTF-8, but   effectively encoded using latin-1, and vice versa. (closes #937)

--- services/pulse2/database/inventory/mapping.py
+++ services/pulse2/database/inventory/mapping.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8; -*-
 #
 # (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
-# (c) 2007-2009 Mandriva, http://www.mandriva.com/
+# (c) 2007-2010 Mandriva, http://www.mandriva.com/
 #
 # $Id$
 #
@@ -22,10 +22,40 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-from pulse2.utils import Singleton
+"""
+Contains main method to parse OCS Inventory XML string, and to map all XML
+data to a Python dict.
+"""
+
+import logging
+
 from xml.dom.minidom import parse, parseString
+from xml.parsers import expat
+from xml.dom.expatbuilder import ExpatBuilderNS
+
+from pulse2.utils import Singleton
+
+
+class EncodingSafeParser(ExpatBuilderNS):
+
+    """
+    Class that returns an Expat parser which parsing encoding option can be
+    enforced.
+    """
+
+    def setEncoding(self, encoding):
+        """
+        Allow to force parser encoding
+        """
+        self.encoding = encoding
+
+    def createParser(self):
+        """Create a new parser object."""
+        return expat.ParserCreate(self.encoding)
+
 
 class OcsMapping(Singleton):
+
     def initialize(self, xmlmapping):
         self.doc = parse(xmlmapping)
         self.tables = {}
@@ -47,9 +77,54 @@ class OcsMapping(Singleton):
                         self.nomenclatures[xmlclass] = {}
                     self.nomenclatures[xmlclass][xmlto] = True
 
+    def parseSafe(self, xmlstring):
+        """
+        Try to parse the file with other encodings
+        """
+        xml = None
+        for encoding in ['ISO-8859-1', 'UTF-8']:
+            self.logger.info('Trying to parse with enforced %s encoding'
+                             % encoding)
+            try:
+                builder = EncodingSafeParser()
+                builder.setEncoding(encoding)
+                xml = builder.parseString(xmlstring)
+                self.logger.info('String successfully parsed')
+                # Exit for loop if success
+                break
+            except expat.ExpatError, e:
+                self.logger.error('Parsing failed')
+                if expat.errors.XML_ERROR_INVALID_TOKEN in str(e):
+                    pass
+                else:
+                    # Unhandled error, just re-raise it
+                    raise e
+        if xml:
+            return xml
+        else:
+            raise Exception("Can't parse inventory XML string")
+
     def parse(self, xmltext):
+        """
+        Parse the given XML string which is the content of a OCS inventory.
+        """
+        self.logger = logging.getLogger()
+        xml = None
+        try:
+            xml = parseString(xmltext)
+        except expat.ExpatError, e:
+            self.logger.error("Can't parse inventory XML string")
+            if expat.errors.XML_ERROR_INVALID_TOKEN in str(e):
+                self.logger.error('The XML string may use a wrong encoding')
+            else:
+                # Unhandled error, just re-raise it
+                raise e
+
+        if not xml:
+            # Let's try another encoding
+            xml = self.parseSafe(xmltext)
+
         inventory = {}
-        xml = parseString(xmltext)
         for tablename in self.tables:
             try:
                 dbtablename = self.tables[tablename][0]
