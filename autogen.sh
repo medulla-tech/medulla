109f85960cff0e4b3a115a428b1b09e4fbd4cc39
Jean Parpaillon <jparpaillon@mandriva.com>
2011-06-16 09:12:55 +0000

    * mmc build: use copy mode for automake

--- autogen.sh
+++ autogen.sh
@@ -66,7 +66,7 @@ if test -f $(dirname $0)/configure.ac; then
         $ACLOCAL -I m4
         libtoolize -c
         #autoheader
-        $AUTOMAKE --add-missing
+        $AUTOMAKE --add-missing --copy
         autoconf
         )
 fi
