c74dcbd316adfd02a9d94de144dd03d361644aab
Jean Parpaillon <jparpaillon@mandriva.com>
2011-06-23 12:51:23 +0000

    * mmc-core: replace autogen.sh with autoreconf   Fix build on MES<

--- autogen.sh
+++ autogen.sh
@@ -23,52 +23,8 @@ set -e
 
 $(dirname $0)/clean.sh
 
-#
-# check tools version number -- we require >= 1.10
-#
-find_tools() {
-    tool=$1
-
-    if [ -s "$(which $tool-1.10)" ]; then
-        TOOL=$tool-1.10
-    elif [ -s "$(which $tool)" ]; then
-        major=`$tool --version | grep $tool | awk {'print \$4'} | awk -F '.' {'print \$1'}`
-        minor=`$tool --version | grep $tool | awk {'print \$4'} | awk -F '.' {'print \$2'}`
-        if test "$major" -gt 1; then
-            TOOL=$tool
-        elif test "$major" -eq 1 -a "$minor" -ge 10; then
-            TOOL=$tool
-        else
-            echo "Required: $tool version >= 1.10" >&2
-            exit 1
-        fi
-    else
-        echo "Required: $tool version >= 1.10" >&2
-        exit 1
-    fi
-
-    echo "$TOOL"
-}
-
-# Find required tools:
-ACLOCAL=$(find_tools aclocal)
-AUTOMAKE=$(find_tools automake)
-if [ -z "$(which libtool)" ]; then
-    echo "Required: libtool"
-    exit 1
-fi
-
-# Run the actual process:
-if test -f $(dirname $0)/configure.ac; then
-    (
-        cd $(dirname $0)
-        echo "Regenerating autoconf files"
-        $ACLOCAL -I m4
-        libtoolize -c
-        #autoheader
-        $AUTOMAKE --add-missing --copy
-        autoconf
-        )
-fi
+# Replace old stuff with the new intelligent autoreconf
+# http://www.gnu.org/s/libtool/manual/automake/Error-required-file-ltmain_002esh-not-found.html
+autoreconf --install
 
 exit 0
