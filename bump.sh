553c09b901adaa4114e3ae2c48a096c4dcf5ca58
Jean-Philippe Braun <jpbraun@mandriva.com>
2012-06-22 14:26:32 +0200

    core: simplify bump script

--- bump.sh
+++ bump.sh
@@ -7,11 +7,11 @@ if [ ! -z $1 ]; then
 
     sed -i "s/^AC_INIT.*$/AC_INIT\(project, [$1], [http:\/\/projects.mandriva.org\/projects\/mmc]\)/" configure.ac
     sed -i "s/^VERSION = .*$/VERSION = \"$1\"/" agent/mmc/agent.py
-    sed -i "s/^VERSION = .*$/VERSION = \"$1\"/" agent/mmc/plugins/admin/__init__.py
-    sed -i "s/^VERSION = .*$/VERSION = \"$1\"/" agent/mmc/plugins/base/__init__.py
-    sed -i "s/^VERSION = .*$/VERSION = \"$1\"/" agent/mmc/plugins/ppolicy/__init__.py
-    sed -i "s/^\$mod->setVersion.*/\$mod->setVersion(\"$1\");/" web/modules/base/infoPackage.inc.php
-    sed -i "s/^\$mod->setVersion.*/\$mod->setVersion(\"$1\");/" web/modules/ppolicy/infoPackage.inc.php
+    for plugin in admin base ppolicy
+    do
+        sed -i "s/^VERSION = .*$/VERSION = \"$1\"/" agent/mmc/plugins/${plugin}/__init__.py
+        sed -i "s/^\$mod->setVersion.*/\$mod->setVersion(\"$1\");/" web/modules/${plugin}/infoPackage.inc.php
+    done
 
     git diff
     if [ ! -z $2 ]; then
