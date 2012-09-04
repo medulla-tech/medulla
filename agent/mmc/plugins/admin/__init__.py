a69b36fb1f6fde0243533d8f33618fb11d68e1e2
Jean-Philippe Braun <jpbraun@mandriva.com>
2012-09-04 11:00:41 +0200

    core: bump version to 3.0.5

--- agent/mmc/plugins/admin/__init__.py
+++ agent/mmc/plugins/admin/__init__.py
@@ -1,6 +1,6 @@
 from configuration import *
 
-VERSION = "3.0.4.94"
+VERSION = "3.0.5"
 APIVERSION = "4:1:3"
 REVISION = int("$Rev: 0$".split(':')[1].strip(' $'))
 
--- agent/mmc/plugins/base/__init__.py
+++ agent/mmc/plugins/base/__init__.py
@@ -72,7 +72,7 @@ INI = mmcconfdir + "/plugins/base.ini"
 
 modList= None
 
-VERSION = "3.0.4.94"
+VERSION = "3.0.5"
 APIVERSION = "9:0:5"
 REVISION = scmRevision("$Rev$")
 
