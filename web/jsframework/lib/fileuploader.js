e5f8df1cb307f42753b56433b130f007a351c66a
Jean-Christophe VASSORT <jcvassort@mandriva.com>
2012-09-17 14:45:24 +0200

    Fixes #1892: can't upload files if files previously uploaded
    To reproduce bug:
    
    * Add files to queue
    * Click on "Upload Queued Files"
    * Add another files to Queue...
    * Click on "Upload Queued Files"
    
    Another files are not uploaded
    
    Error on browser console:
        Error: element not found spinner
    
    this commit fix it

--- web/jsframework/lib/fileuploader.js
+++ web/jsframework/lib/fileuploader.js
@@ -340,9 +340,11 @@ qq.FileUploaderBasic.prototype = {
     uploadStoredFiles: function(){
         for (var i = 0; i < this._storedFileIds.length; i++) {
             var item = this._getItemByFileId(this._storedFileIds[i]);
-            this._find(item, 'spinner').style.display = "";
-            this._filesInProgress++;
-            this._handler.upload(this._storedFileIds[i], this._options.params);
+            if (!$(item).hasClassName("qq-upload-success")) {
+                this._find(item, 'spinner').style.display = "";
+                this._filesInProgress++;
+                this._handler.upload(this._storedFileIds[i], this._options.params);
+            }
         }
     },
     clearStoredFiles: function(){
