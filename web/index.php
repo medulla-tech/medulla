1f841f79868b8d6efae667684a6213d0aceb8cca
Jean-Philippe Braun <jpbraun@mandriva.com>
2012-11-05 10:51:09 +0100

    core: fix login page DOCTYPE to use standard mode in IE
    Refs #1912

--- web/index.php
+++ web/index.php
@@ -79,8 +79,8 @@ if (isset($_GET["agentsessionexpired"])) {
 
 ?>
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
 	<title>Mandriva Linux / Mandriva Management Console</title>
