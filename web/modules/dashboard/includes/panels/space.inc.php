a1f51566645988e370ddacd59b5fa495d873a47d
Jean-Philippe Braun <jpbraun@mandriva.com>
2012-11-26 16:46:45 +0100

    dashboard: add animation to space pies

--- web/modules/dashboard/includes/panels/space.inc.php
+++ web/modules/dashboard/includes/panels/space.inc.php
@@ -69,7 +69,24 @@ class SpacePanel extends Panel {
             r.piechart(x, y + 5, radius, data,
                        {legend: legend,
                         legendpos: "east",
-                        colors: colors});
+                        colors: colors})
+             .hover(function () {
+                this.sector.stop();
+                this.sector.animate({ transform: 's1.1 1.1 ' + this.cx + ' ' + this.cy }, 800, "elastic");
+
+                if (this.label) {
+                    this.label[0].stop();
+                    this.label[0].attr({ r: 7.5 });
+                    this.label[1].attr({ "font-weight": 800 });
+                }
+             }, function () {
+                this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 800, "elastic");
+
+                if (this.label) {
+                    this.label[0].animate({ r: 5 }, 500, "bounce");
+                    this.label[1].attr({ "font-weight": 400 });
+                }
+             });
             y += (radius * 2) + margin + 5;
         }
         r.setSize(200, partitions.length * (radius * 2 + margin) + 10);
