215696f36ce55eea6a46e4c8a5a1fd3e18c96740
Jean-Philippe Braun <jpbraun@mandriva.com>
2012-12-05 18:41:24 +0100

    base: display licence info only if there is some limits

--- web/modules/base/includes/panels/support.inc.php
+++ web/modules/base/includes/panels/support.inc.php
@@ -34,10 +34,12 @@ class SupportPanel extends Panel {
     function display_content() {
         echo '<p><strong>' . $this->data['product_name'][0] . '</strong></p>';
         echo '<p>' . _("Support :") . ' <a href="mailto:' . $this->data['support_mail']. '">' . $this->data['support_mail'] . '</a></p>';
-        echo '<div class="subpanel"><h4>' . _("License") . '</h4>';
-        $this->display_licence("users", _("Users : "));
-        $this->display_licence("computers", _("Computers : "));
-        echo '</div>';
+        if ($this->data['users'] > 0 || $this->data['computers'] > 0) {
+            echo '<div class="subpanel"><h4>' . _("License") . '</h4>';
+            $this->display_licence("users", _("Users : "));
+            $this->display_licence("computers", _("Computers : "));
+            echo '</div>';
+        }
         if ($this->data['upgrade_url'])
             echo '<div style="text-align: right"><a class="btn btn-info btn-small" href="' . $this->data['upgrade_url'] . '">' . _('Upgrade') . '</a></div>';
     }
