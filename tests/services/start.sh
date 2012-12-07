9d87dadd141c7dc50f560f909c5e97e49aadcb5e
Adam Cécile (Le_Vert) <gandalf@le-vert.net>
2012-12-08 00:22:13 +0100

    UnitTests: improve launch script
    Fix Python modules path
    Do not set -e but handle errors after each script
    Enable scheduler balance module test script

--- tests/services/start.sh
+++ tests/services/start.sh
@@ -1,7 +1,10 @@
-#!/bin/bash -e
+#!/bin/bash
+
+ERROR=0
+ERRORLOG=""
 
 RPCPSERVER=`mktemp`
-cat /usr/lib/python2.*/site-packages/pulse2/package_server/mirror/__init__.py /usr/lib/python2.*/site-packages/pulse2/package_server/mirror_api/__init__.py /usr/lib/python2.*/site-packages/pulse2/package_server/package_api_put/__init__.py /usr/lib/python2.*/site-packages/pulse2/package_server/package_api_get/__init__.py /usr/lib/python2.*/site-packages/pulse2/package_server/scheduler_api/__init__.py /usr/lib/python2.*/site-packages/pulse2/package_server/user_package_api/__init__.py > $RPCPSERVER
+cat /usr/lib/python2.*/dist-packages/pulse2/package_server/mirror/__init__.py /usr/lib/python2.*/dist-packages/pulse2/package_server/mirror_api/__init__.py /usr/lib/python2.*/dist-packages/pulse2/package_server/package_api_put/__init__.py /usr/lib/python2.*/dist-packages/pulse2/package_server/package_api_get/__init__.py /usr/lib/python2.*/dist-packages/pulse2/package_server/scheduler_api/__init__.py /usr/lib/python2.*/dist-packages/pulse2/package_server/user_package_api/__init__.py > $RPCPSERVER
 
 echo ""
 echo "Statistics of Pulse 2 unittest"
@@ -39,21 +42,21 @@ echo ""
 echo "Statistics of inventory unittest mmcagent-inventory-empty.py"
 echo ""
 echo ""
-python statistics.py /usr/lib/python2.*/site-packages/mmc/plugins/inventory/__init__.py mmcagent-inventory-empty.py
+python statistics.py /usr/lib/python2.*/dist-packages/mmc/plugins/inventory/__init__.py mmcagent-inventory-empty.py
 
 echo ""
 echo ""
 echo "Statistics of dyngroup unittest mmcagent-dyngroup-empty.py"
 echo ""
 echo ""
-python statistics.py /usr/lib/python2.*/site-packages/mmc/plugins/dyngroup/__init__.py mmcagent-dyngroup-empty.py
+python statistics.py /usr/lib/python2.*/dist-packages/mmc/plugins/dyngroup/__init__.py mmcagent-dyngroup-empty.py
 
 echo ""
 echo ""
 echo "Statistics of package unittest mmcagent-pkgs-empty.py"
 echo ""
 echo ""
-python statistics.py /usr/lib/python2.*/site-packages/mmc/plugins/pkgs/__init__.py mmcagent-pkgs-empty.py
+python statistics.py /usr/lib/python2.*/dist-packages/mmc/plugins/pkgs/__init__.py mmcagent-pkgs-empty.py
 
 echo ""
 echo ""
@@ -68,21 +71,59 @@ echo ""
 echo ""
 echo "Inventory plugin"
 python mmcagent-inventory-empty.py
+if [ ! $? -eq 0 ]; then
+  ERROR=`expr ${ERROR} + 1`;
+  ERRORLOG="${ERRORLOG}\nFAILURE: Clean: mmcagent-inventory-empty.py"
+fi 
 echo "Dyngroup plugin"
 python mmcagent-dyngroup-empty.py
+if [ ! $? -eq 0 ]; then
+  ERROR=`expr ${ERROR} + 1`;
+  ERRORLOG="${ERRORLOG}\nFAILURE: Clean: mmcagent-dyngroup-empty.py"
+fi 
 echo "Dyngroup Pkgs"
 python mmcagent-pkgs-empty.py
+if [ ! $? -eq 0 ]; then
+  ERROR=`expr ${ERROR} + 1`;
+  ERRORLOG="${ERRORLOG}\nFAILURE: Clean: mmcagent-pkgs-empty.py"
+fi 
 
 echo "Testing Package Server with no package (pserverempty.py)"
 python pserverempty.py
+if [ ! $? -eq 0 ]; then
+  ERROR=`expr ${ERROR} + 1`;
+  ERRORLOG="${ERRORLOG}\nFAILURE: Package Server: pserverempty.py"
+fi 
 echo "Testing Package Server with one package (pserver.py)"
 python pserver.py
+if [ ! $? -eq 0 ]; then
+  ERROR=`expr ${ERROR} + 1`;
+  ERRORLOG="${ERRORLOG}\nFAILURE: Package Server: pserver.py"
+fi 
 echo "Testing Launcher"
 python launcher.py
+if [ ! $? -eq 0 ]; then
+  ERROR=`expr ${ERROR} + 1`;
+  ERRORLOG="${ERRORLOG}\nFAILURE: Launcher: launcher.py"
+fi 
 echo "Testing Scheduler"
 python scheduler.py
+if [ ! $? -eq 0 ]; then
+  ERROR=`expr ${ERROR} + 1`;
+  ERRORLOG="${ERRORLOG}\nFAILURE: Scheduler: scheduler.py"
+fi 
 echo "Testing Package Server imaging API"
 python pserver-imaging.py
+if [ ! $? -eq 0 ]; then
+  ERROR=`expr ${ERROR} + 1`;
+  ERRORLOG="${ERRORLOG}\nFAILURE: Package Server / Imaging: pserver-imaging.py"
+fi 
+echo "Testing Scheduler Balance module"
+python balance.py
+if [ ! $? -eq 0 ]; then
+  ERROR=`expr ${ERROR} + 1`;
+  ERRORLOG="${ERRORLOG}\nFAILURE: Scheduler Balance Module: balance.py"
+fi 
 
 echo ""
 echo ""
@@ -90,10 +131,26 @@ echo "Testing inventory injection"
 echo ""
 echo ""
 python mmcagent-inventory-inject.py
+if [ ! $? -eq 0 ]; then
+  ERROR=`expr ${ERROR} + 1`;
+  ERRORLOG="${ERRORLOG}\nFAILURE: Inventory Injection: mmcagent-inventory-inject.py"
+fi 
 
 
 echo ""
 echo ""
 echo "`date` - Ending Python Pulse2 tests."
 
-exit 0
+echo
+echo
+echo
+echo
+echo
+echo "#################################################################"
+echo "##################### UNIT TESTS - SUMMARY  #####################"
+echo "#################################################################"
+echo
+echo "Number of failed test: ${ERROR}"
+echo -e ${ERRORLOG}
+
+exit ${ERROR}
