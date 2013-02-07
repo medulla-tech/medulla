5151f3df1d5ca8a09e0c3458b57c79955dca45c1
Jean-Christophe VASSORT <jcvassort@mandriva.com>
2013-02-07 14:59:21 +0100

    dyngroup: Fix dyngroup on GLPI Entity doesn't work
    Connected as user: doesn't filter by entity: return all computers of entities
    Connected as root: traceback
    
    This commit fix it
    
    Replace map + lambda methods in querymanager __init__.py file

--- services/mmc/plugins/glpi/database_08.py
+++ services/mmc/plugins/glpi/database_08.py
@@ -452,12 +452,11 @@ class Glpi08(DyngroupDatabaseHelper):
         Map a table name on a table mapping
         """
         base = []
-        if ctx.userid != 'root':
-            base.append(self.location)
+        base.append(self.location)
         if query[2] == 'OS':
             return base + [self.os]
-        elif query[2] == 'ENTITY':
-            return base + [self.location]
+        elif query[2] == 'Entity':
+            return base
         elif query[2] == 'SOFTWARE':
             return base + [self.inst_software, self.licenses, self.software]
         elif query[2] == 'Nom':
@@ -532,7 +531,7 @@ class Glpi08(DyngroupDatabaseHelper):
     def __getPartsFromQuery(self, query):
         if query[2] == 'OS':
             return [[self.os.c.name, query[3]]]
-        elif query[2] == 'ENTITY':
+        elif query[2] == 'Entity':
             return [[self.location.c.name, query[3]]]
         elif query[2] == 'SOFTWARE':
             return [[self.software.c.name, query[3]]]
@@ -550,13 +549,13 @@ class Glpi08(DyngroupDatabaseHelper):
             return [[self.locations.c.completename, query[3]]]
         elif query[2] == 'ServicePack':
             return [[self.os_sp.c.name, query[3]]]
-        elif query[2] == 'Groupe': # TODO double join on ENTITY
+        elif query[2] == 'Groupe': # TODO double join on Entity
             return [[self.group.c.name, query[3]]]
         elif query[2] == 'Reseau':
             return [[self.net.c.name, query[3]]]
-        elif query[2] == 'Logiciel': # TODO double join on ENTITY
+        elif query[2] == 'Logiciel': # TODO double join on Entity
             return [[self.software.c.name, query[3]]]
-        elif query[2] == 'Version': # TODO double join on ENTITY
+        elif query[2] == 'Version': # TODO double join on Entity
             return [[self.software.c.name, query[3][0]], [self.softwareversions.c.name, query[3][1]]]
         return []
 
@@ -564,7 +563,7 @@ class Glpi08(DyngroupDatabaseHelper):
     def __getTable(self, table):
         if table == 'OS':
             return self.os.c.name
-        elif table == 'ENTITY':
+        elif table == 'Entity':
             return self.location.c.name
         elif table == 'SOFTWARE':
             return self.software.c.name
@@ -1485,7 +1484,7 @@ class Glpi08(DyngroupDatabaseHelper):
         ret = query.group_by(self.group.c.name).all()
         session.close()
         return ret
-    def getMachineByGroup(self, ctx, filt):# ENTITY!
+    def getMachineByGroup(self, ctx, filt):# Entity!
         """ @return: all machines that have this contact number """
         session = create_session()
         query = session.query(Machine).select_from(self.machine.join(self.group))
--- services/mmc/plugins/glpi/querymanager/__init__.py
+++ services/mmc/plugins/glpi/querymanager/__init__.py
@@ -67,91 +67,87 @@ def query(ctx, criterion, value):
     logging.getLogger().info(value)
     machines = []
     if criterion == 'OS':
-        machines = map(lambda x: x.name, Glpi().getMachineByOs(ctx, value))
-    elif criterion == 'ENTITY':
-        machines = map(lambda x: x.name, Glpi().getMachineByEntity(ctx, value))
+        machines = [x.name for x in Glpi().getMachineByOs(ctx, value)]
+    elif criterion == 'ENTITY' or criterion == 'Entity':
+        machines = [x.name for x in Glpi().getMachineByEntity(ctx, value)]
     elif criterion == 'SOFTWARE' or criterion == 'Logiciel':
-        machines = map(lambda x: x.name, Glpi().getMachineBySoftware(ctx, value))
+        machines = [x.name for x in Glpi().getMachineBySoftware(ctx, value)]
     elif criterion == 'Version':
-        machines = map(lambda x: x.name, Glpi().getMachineBySoftwareAndVersion(ctx, value))
+        machines = [x.name for x in Glpi().getMachineBySoftwareAndVersion(ctx, value)]
     elif criterion == 'Nom':
-        machines = map(lambda x: x.name, Glpi().getMachineByHostname(ctx, value))
+        machines = [x.name for x in Glpi().getMachineByHostname(ctx, value)]
     elif criterion == 'Contact':
-        machines = map(lambda x: x.name, Glpi().getMachineByContact(ctx, value))
+        machines = [x.name for x in Glpi().getMachineByContact(ctx, value)]
     elif criterion == 'Numero du contact':
-        machines = map(lambda x: x.name, Glpi().getMachineByContactNum(ctx, value))
+        machines = [x.name for x in Glpi().getMachineByContactNum(ctx, value)]
     elif criterion == 'Comments':
-        machines = map(lambda x: x.name, Glpi().getMachineByComment(ctx, value))
+        machines = [x.name for x in Glpi().getMachineByComment(ctx, value)]
     elif criterion == 'Modele':
-        machines = map(lambda x: x.name, Glpi().getMachineByModel(ctx, value))
+        machines = [x.name for x in Glpi().getMachineByModel(ctx, value)]
     elif criterion == 'Lieu':
-        machines = map(lambda x: x.name, Glpi().getMachineByLocation(ctx, value))
+        machines = [x.name for x in Glpi().getMachineByLocation(ctx, value)]
     elif criterion == 'ServicePack':
-        machines = map(lambda x: x.name, Glpi().getMachineByOsSp(ctx, value))
+        machines = [x.name for x in Glpi().getMachineByOsSp(ctx, value)]
     elif criterion == 'Groupe':
-        machines = map(lambda x: x.name, Glpi().getMachineByGroup(ctx, value))
+        machines = [x.name for x in Glpi().getMachineByGroup(ctx, value)]
     elif criterion == 'Reseau':
-        machines = map(lambda x: x.name, Glpi().getMachineByNetwork(ctx, value))
+        machines = [x.name for x in Glpi().getMachineByNetwork(ctx, value)]
     #elif criterion == '':
     #    machines = map(lambda x: x.name, Glpi().getMachineBy(ctx, value))
     return [machines, True]
 
 def getAllOs(ctx, value = ''):
-    return unique(map(lambda x:x.name, Glpi().getAllOs(ctx, value)))
+    return unique([x.name for x in Glpi().getAllOs(ctx, value)])
 
 def getAllEntities(ctx, value = ''):
-    return unique(map(lambda x:x.name, Glpi().getAllEntities(ctx, value)))
+    return unique([x.name for x in Glpi().getAllEntities(ctx, value)])
 
 def getAllSoftwares(ctx, value = ''):
-    ret = unique(map(lambda x:x.name, Glpi().getAllSoftwares(ctx, value)))
+    ret = unique([x.name for x in Glpi().getAllSoftwares(ctx, value)])
     ret.sort()
     return ret
 
 def getAllSoftwaresAndVersions(ctx, softname = "", version = None):
     ret = []
     if version == None:
-        if Glpi().glpi_chosen_version().find('0.8') == 0: # glpi in 0.8
-            ret = unique(map(lambda x:x.name, Glpi().getAllSoftwares(ctx, softname)))
-        else:
-            ret = unique(map(lambda x:x.name, Glpi().getAllSoftwares(ctx, softname)))
+        ret = unique([x.name for x in Glpi().getAllSoftwares(ctx, softname)])
     else:
         if Glpi().glpi_chosen_version().find('0.8') == 0: # glpi in 0.8
-            ret = unique(map(lambda x:x.name, Glpi().getAllVersion4Software(ctx, softname, version)))
+            ret = unique([x.name for x in Glpi().getAllVersion4Software(ctx, softname, version)])
         else:
             if Glpi().glpi_version_new():
-                ret = unique(map(lambda x:x.name, Glpi().getAllVersion4Software(ctx, softname, version)))
+                ret = unique([x.name for x in Glpi().getAllVersion4Software(ctx, softname, version)])
             else:
-                ret = unique(map(lambda x:x.version, Glpi().getAllVersion4Software(ctx, softname, version)))
+                ret = unique([x.version for x in Glpi().getAllVersion4Software(ctx, softname, version)])
     ret.sort()
     return ret
 
 def getAllHostnames(ctx, value = ''):
-    return unique(map(lambda x:x.name, Glpi().getAllHostnames(ctx, value)))
+    return unique([x.name for x in Glpi().getAllHostnames(ctx, value)])
 
 def getAllContacts(ctx, value = ''):
-    return unique(map(lambda x:x.contact, Glpi().getAllContacts(ctx, value)))
+    return unique([x.contact for x in Glpi().getAllContacts(ctx, value)])
 
 def getAllContactNums(ctx, value = ''):
-    return unique(map(lambda x:x.contact_num, Glpi().getAllContactNums(ctx, value)))
+    return unique([x.contact_num for x in Glpi().getAllContactNums(ctx, value)])
 
 def getAllComments(ctx, value = ''):
     if Glpi().glpi_chosen_version().find('0.8') == 0:
-        return unique(map(lambda x:x.comment, Glpi().getAllComments(ctx, value)))
+        return unique([x.comment for x in Glpi().getAllComments(ctx, value)])
     else:
-        return unique(map(lambda x:x.comments, Glpi().getAllComments(ctx, value)))
+        return unique([x.comments for x in Glpi().getAllComments(ctx, value)])
 
 def getAllModels(ctx, value = ''):
-    return unique(map(lambda x:x.name, Glpi().getAllModels(ctx, value)))
+    return unique([x.name for x in Glpi().getAllModels(ctx, value)])
 
 def getAllLocations(ctx, value = ''):
-    return unique(map(lambda x:x.completename, Glpi().getAllLocations(ctx, value)))
+    return unique([x.completename for x in Glpi().getAllLocations(ctx, value)])
 
 def getAllOsSps(ctx, value = ''):
-    return unique(map(lambda x:x.name, Glpi().getAllOsSps(ctx, value)))
+    return unique([x.name for x in Glpi().getAllOsSps(ctx, value)])
 
 def getAllGroups(ctx, value = ''):
-    return unique(map(lambda x:x.name, Glpi().getAllGroups(ctx, value)))
+    return unique([x.name for x in Glpi().getAllGroups(ctx, value)])
 
 def getAllNetworks(ctx, value = ''):
-    return unique(map(lambda x:x.name, Glpi().getAllNetworks(ctx, value)))
-
+    return unique([x.name for x in Glpi().getAllNetworks(ctx, value)])
