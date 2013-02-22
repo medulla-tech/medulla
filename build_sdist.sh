e1756ed448567deb180c47e76e7573f15d08f28f
Jean-Philippe Braun <jpbraun@mandriva.com>
2013-02-22 16:26:35 +0100

    core: add script to build the source distribution

---
+++ build_sdist.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -f mmc-core-*.tar.gz mmc-core-*.tar.gz.md5
+git clean -fdx && ./autogen.sh && ./configure --sysconfdir=/etc --localstatedir=/var --disable-python-check --disable-conf && make dist
+if [ $? -eq 0 ]; then
+    for tarball in mmc-core-*.tar.gz; do
+        md5sum $tarball > $tarball.md5
+    done
+fi
