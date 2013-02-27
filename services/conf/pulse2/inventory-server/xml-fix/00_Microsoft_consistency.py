21bc20f54a1798b6ddc1681cd41fe0f5cc4abe33
Adam Cécile <acecile@mandriva.com>
2013-02-27 19:09:20 +0100

    inventory-server: update Microsoft inventory fixing script

--- services/conf/pulse2/inventory-server/xml-fix/00_Microsoft_consistency.py
+++ services/conf/pulse2/inventory-server/xml-fix/00_Microsoft_consistency.py
@@ -26,52 +26,54 @@ def xml_fix(xml):
   tree = ET.ElementTree(xml)
   root = tree.getroot()
   for subelem1 in root:
-    if subelem1.tag == 'SOFTWARES':
+    if subelem1.tag == 'CONTENT':
       for subelem2 in subelem1:
-        
-        if subelem2.tag == 'PUBLISHER':
+        if subelem2.tag == 'SOFTWARES':
+          for subelem3 in subelem2:
+            
+            if subelem3.tag == 'PUBLISHER':
 
-          # Microsoft vendor name should allways be the same
-          if subelem2.text in ['Microsoft', 'MICROSOFT']:
-            subelem2.text = 'Microsoft Corporation'
+              # Microsoft vendor name should allways be the same
+              if subelem3.text in ['Microsoft', 'MICROSOFT']:
+                subelem3.text = 'Microsoft Corporation'
 
-        if subelem2.tag == 'NAME':
-         
-          # Convert Microsoft KB updates
-          #  from: {CE2CDD62-0124-36CA-84D3-9F4DCF5C5BD9}.KB960043
-          #  to: Update (KB960043)
-          # Also handle KB1234v5 or KB1234-v5 naming scheme
-          if re.match('^\{[\dA-Fa-f]{8}-[\dA-Fa-f]{4}-[\dA-Fa-f]{4}-[\dA-Fa-f]{4}-[\dA-Fa-f]{12}\}\.KB[0-9]+(-?v[0-9]+)?$',subelem2.text):
-            subelem2.text = re.sub('\{[\dA-Fa-f]{8}-[\dA-Fa-f]{4}-[\dA-Fa-f]{4}-[\dA-Fa-f]{4}-[\dA-Fa-f]{12}\}\.','Update (',subelem2.text)
-            subelem2.text = re.sub('$',')',subelem2.text)
+            if subelem3.tag == 'NAME':
+             
+              # Convert Microsoft KB updates
+              #  from: {CE2CDD62-0124-36CA-84D3-9F4DCF5C5BD9}.KB960043
+              #  to: Update (KB960043)
+              # Also handle KB1234v5 or KB1234-v5 naming scheme
+              if re.match('^\{[\dA-Fa-f]{8}-[\dA-Fa-f]{4}-[\dA-Fa-f]{4}-[\dA-Fa-f]{4}-[\dA-Fa-f]{12}\}\.KB[0-9]+(-?v[0-9]+)?$',subelem3.text):
+                subelem3.text = re.sub('\{[\dA-Fa-f]{8}-[\dA-Fa-f]{4}-[\dA-Fa-f]{4}-[\dA-Fa-f]{4}-[\dA-Fa-f]{12}\}\.','Update (',subelem3.text)
+                subelem3.text = re.sub('$',')',subelem3.text)
 
-          # Convert Microsoft KB updates
-          #  from: KB960043
-          #  to: Update (KB960043)
-          # Also handle KB1234v5 or KB1234-v5 naming scheme
-          if re.match('^KB[0-9]+(-?v[0-9]+)?$',subelem2.text):
-            subelem2.text = re.sub('^','Update (',subelem2.text)
-            subelem2.text = re.sub('$',')',subelem2.text)
+              # Convert Microsoft KB updates
+              #  from: KB960043
+              #  to: Update (KB960043)
+              # Also handle KB1234v5 or KB1234-v5 naming scheme
+              if re.match('^KB[0-9]+(-?v[0-9]+)?$',subelem3.text):
+                subelem3.text = re.sub('^','Update (',subelem3.text)
+                subelem3.text = re.sub('$',')',subelem3.text)
 
-          # Contains KB1234v5 or KB123467 in its name without any publisher ?
-          # Publisher set to Microsoft Corporation
-          if re.search('KB[0-9]+(v[0-9]+)?',subelem2.text) and not subelem1.findall('PUBLISHER'):
-            children = ET.SubElement(subelem1,'PUBLISHER')
-            children.text = 'Microsoft Corporation'
+              # Contains KB1234v5 or KB123467 in its name without any publisher ?
+              # Publisher set to Microsoft Corporation
+              if re.search('KB[0-9]+(v[0-9]+)?',subelem3.text) and not subelem2.findall('PUBLISHER'):
+                children = ET.SubElement(subelem2,'PUBLISHER')
+                children.text = 'Microsoft Corporation'
 
-          # Windows Media and .NET framework stuff needs Microsoft vendor too
-          if re.search('(Windows Media|Microsoft .NET Framework)',subelem2.text) and not subelem1.findall('PUBLISHER'):
-            children = ET.SubElement(subelem1,'PUBLISHER')
-            children.text = 'Microsoft Corporation'
+              # Windows Media and .NET framework stuff needs Microsoft vendor too
+              if re.search('(Windows Media|Microsoft .NET Framework)',subelem3.text) and not subelem2.findall('PUBLISHER'):
+                children = ET.SubElement(subelem2,'PUBLISHER')
+                children.text = 'Microsoft Corporation'
 
-          # French to english
-          if re.search(u'Mise à jour de sécurité pour',subelem2.text):
-            subelem2.text = re.sub(u'Mise à jour de sécurité pour','Security Update for',subelem2.text)
-          if re.search(u'Mise à jour pour',subelem2.text):
-            subelem2.text = re.sub(u'Mise à jour pour','Update for',subelem2.text)
-          if re.search('Correctif pour',subelem2.text):
-            subelem2.text = re.sub('Correctif pour','Update for',subelem2.text)
-          if re.search('Lecteur Windows Media',subelem2.text):
-            subelem2.text = re.sub('Lecteur Windows Media','Windows Media Player',subelem2.text)
+              # French to english
+              if re.search(u'Mise à jour de sécurité pour',subelem3.text):
+                subelem3.text = re.sub(u'Mise à jour de sécurité pour','Security Update for',subelem3.text)
+              if re.search(u'Mise à jour pour',subelem3.text):
+                subelem3.text = re.sub(u'Mise à jour pour','Update for',subelem3.text)
+              if re.search('Correctif pour',subelem3.text):
+                subelem3.text = re.sub('Correctif pour','Update for',subelem3.text)
+              if re.search('Lecteur Windows Media',subelem3.text):
+                subelem3.text = re.sub('Lecteur Windows Media','Windows Media Player',subelem3.text)
 
   return ET.tostring(root)
