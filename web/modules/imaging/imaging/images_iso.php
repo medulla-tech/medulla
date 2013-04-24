5f8720c953f629d6125ebaac7585b0aca9e22430
Jean-Christophe VASSORT <jcvassort@mandriva.com>
2013-04-24 12:32:34 +0200

    imaging: Fix / Add supports for image iso generation
    In Generate ISO tool for Pulse computers images:
    
     * Fix DVD size
     * Add DVD-DL, Blu-ray 25 and 50 Go

--- web/modules/imaging/imaging/images_iso.php
+++ web/modules/imaging/imaging/images_iso.php
@@ -89,7 +89,10 @@ if ($_POST) {
 <select name="media" />
 <option value="681574400">CD (650 Mo)</option>
 <option value="734003200">CD (700 Mo)</option>
-<option value="5046586572">DVD (4.7 Go)</option>
+<option value="4617089843">DVD (4.3 Go)</option>
+<option value="8375186227">DVD-DL (7.8 Go)</option>
+<option value="24696061952">Blu-ray (25 Go)</option>
+<option value="49392123904">Blu-ray (50 Go)</option>
 </select>
 </td></tr></table>
 <br/><br/>
