6f4fb9e08abe87904937a9bebd1f720ea1ee0ad7
Jean-Christophe VASSORT <jcvassort@mandriva.com>
2013-06-11 17:10:14 +0200

    msc: Fix traceback on quick actions when no netmask

--- services/mmc/plugins/msc/database.py
+++ services/mmc/plugins/msc/database.py
@@ -142,6 +142,9 @@ class MscDatabase(msc.MscDatabase):
                 bcastAddresses.append(h_mac2bcast[i])
                 netmasks.append(h_mac2netmask[i])
 
+        if len(netmasks) == 0 or None in netmasks:
+            netmasks = ['']
+
         # Multiple IP addresses or IP addresses may be separated by "||"
         targetMac = '||'.join(macAddresses)
         targetIp = '||'.join(ipAddresses)
