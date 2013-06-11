92e05fed1b832beff3d5326122345678bd0a2639
Jean-Christophe VASSORT <jcvassort@mandriva.com>
2013-06-11 10:20:24 +0200

    pkgs: Refs #2112 file upload consume memory
    In save() function, stream_copy_to_stream() is called twice, so memory
    consumption is double.
    
    This commit fix it.

--- web/modules/pkgs/lib/fileuploader/fileuploader.php
+++ web/modules/pkgs/lib/fileuploader/fileuploader.php
@@ -44,18 +44,16 @@ class qqUploadedFileXhr {
      */
     function save($path) {
         $input = fopen("php://input", "r");
-        $temp = tmpfile();
-        $realSize = stream_copy_to_stream($input, $temp);
+        $target = fopen($path, "w");
+        $realSize = stream_copy_to_stream($input, $target);
         fclose($input);
-        
-        if ($realSize != $this->getSize()){            
+        fclose($target);
+
+        if ($realSize != $this->getSize()){
+            unlink($path);
             return false;
         }
-        
-        $target = fopen($path, "w");        
-        fseek($temp, 0, SEEK_SET);
-        stream_copy_to_stream($temp, $target);
-        fclose($target);
+
         return true;
     }
     function getName() {
