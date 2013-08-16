14a47ee31b4075ee33256dad8277a5dd4b31fc15
Jean-Christophe VASSORT <jcvassort@mandriva.com>
2013-08-16 17:12:39 +0200

    core: move Pulse database classes into core
    pulse: remove unused files
    
    backuppc: Fix bad import

---
+++ agent/mmc/database/sqlalchemy_tests.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8; -*-
+#
+# (c) 2008-2009 Mandriva, http://www.mandriva.com/
+#
+# $Id$
+#
+# This file is part of Pulse 2, http://pulse2.mandriva.org
+#
+# Pulse 2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pulse 2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pulse 2; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+
+from sqlalchemy import __version__
+
+MIN_VERSION = '0.6.3' # Debian Squeeze version
+MAX_VERSION = '0.7.9' # Debian Jessie version
+CUR_VERSION = __version__
+
+def checkSqlalchemy():
+    if MIN_VERSION <= CUR_VERSION <= MAX_VERSION:
+        return True
+    else:
+        return False
