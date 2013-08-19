bcaf1ec5fb41baf944f65abf8d4f664e2b7811c0
Jean-Philippe Braun <eon@patapon.info>
2013-08-19 10:20:37 +0200

    base: fix default page CSS

--- web/modules/base/main/default.php
+++ web/modules/base/main/default.php
@@ -71,7 +71,7 @@ function display_mod($mod) {
                     <div class="portlet-content">
     <?php foreach (getSorted($mod->getSubmodules()) as $submod) {
     ?>
-        
+
                     <?php display_submod($submod,$mod); $i++; ?>
     <?php
     }
@@ -100,93 +100,4 @@ $p->display();
     <div class="clearer"></div>
 </div>
 
-
-<style>
-.column { width: 230px; float: left; padding-bottom: 100px; }
-.portlet { margin: 0 1em 1em 0; border:1px solid #333; -moz-border-radius: 5px; border-radius: 5px; }
-.portlet-header { margin: 0.3em; padding-bottom: 4px; padding-left: 0.2em; font-size:14px; background:#324C96; color:#fff; padding:5px; -moz-border-radius: 5px; border-radius: 5px; cursor:move; }
-.portlet-header .ui-icon { float: right; }
-.portlet-content { padding: 0.4em; }
-.ui-sortable-placeholder { border: 2px dotted #324C96; visibility: visible !important; height: 50px !important; }
-.ui-sortable-placeholder * { visibility: hidden; }
-</style>
-
-
-<script type="text/javascript">
-// function that writes the list order to a cookie
-function saveOrder() {
-    jQuery(".column script").remove();
-    jQuery(".column").each(function(index, value){
-        var colid = value.id;
-        var cookieName = "home-cookie-" + colid;
-        // Get the order for this column.
-        var order = jQuery('#' + colid).sortable("toArray");
-        // For each portlet in the column
-        for ( var i = 0, n = order.length; i < n; i++ ) {
-            // Determine if it is 'opened' or 'closed'
-            var v = jQuery('#' + order[i] ).find('.portlet-content').is(':visible');
-            // Modify the array we're saving to indicate what's open and
-            //  what's not.
-            order[i] = order[i] + ":" + v;
-        }
-        jQuery.cookie(cookieName, order, { path: "/", expiry: new Date(2012, 1, 1)});
-    });
-}
-
-// function that restores the list order from a cookie
-function restoreOrder() {
-    jQuery(".column").each(function(index, value) {
-        var colid = value.id;
-        var cookieName = "home-cookie-" + colid
-        var cookie = jQuery.cookie(cookieName);
-        if ( cookie == null ) { return; }
-        var IDs = cookie.split(",");
-        for (var i = 0, n = IDs.length; i < n; i++ ) {
-            var toks = IDs[i].split(":");
-            if ( toks.length != 2 ) {
-                continue;
-            }
-            var portletID = toks[0];
-            var visible = toks[1]
-            var portlet = jQuery(".column")
-                .find('#' + portletID)
-                .appendTo(jQuery('#' + colid));
-            if (visible === 'false') {
-                portlet.find(".ui-icon").toggleClass("ui-icon-minus");
-                portlet.find(".ui-icon").toggleClass("ui-icon-plus");
-                portlet.find(".portlet-content").hide();
-            }
-        }
-    });
-} 
-
-jQuery(document).ready( function () {
-    jQuery(".column").sortable({
-        connectWith: ['.column'],
-        stop: function(event,ui) { ui.item.css('opacity',1);saveOrder(); },
-        sort: function(event,ui) { ui.item.css('opacity',0.7); }
-    }); 
-
-    jQuery(".portlet")
-        .addClass("ui-widget ui-widget-content")
-        .addClass("ui-helper-clearfix ui-corner-all")
-        .find(".portlet-header")
-        .addClass("ui-widget-header ui-corner-all")
-        .prepend('<span class="ui-icon ui-icon-minus"></span>')
-        .end()
-        .find(".portlet-content");
-
-    restoreOrder();
-
-    jQuery(".portlet-header .ui-icon").click(function() {
-        jQuery(this).toggleClass("ui-icon-minus");
-        jQuery(this).toggleClass("ui-icon-plus");
-        jQuery(this).parents(".portlet:first").find(".portlet-content").toggle();
-        saveOrder(); // This is important
-    });
-    jQuery(".portlet-header .ui-icon").hover(
-        function() {jQuery(this).addClass("ui-icon-hover"); },
-        function() {jQuery(this).removeClass('ui-icon-hover'); }
-    );
-});  
-</script>
+<script type="text/javascript" src="jsframework/portlet.js"></script>
--- web/modules/dashboard/main/default.php
+++ web/modules/dashboard/main/default.php
@@ -30,6 +30,7 @@ require("modules/dashboard/includes/dashboard-xmlrpc.inc.php");
 <script type="text/javascript" src="jsframework/lib/raphael/g.line-min.js"></script>
 <script type="text/javascript" src="jsframework/lib/raphael/g.bar-min.js"></script>
 <script type="text/javascript" src="jsframework/lib/raphael/utilities.js"></script>
+<script type="text/javascript" src="jsframework/portlet.js"></script>
 <?php
 
 $d = new Div(array("id" => "dashboard"));
@@ -80,105 +81,3 @@ for ($i = $z; $i<=8; $i++)
     print '<div class="column" id="col'.$i.'"></div>';
 
 ?>
-
-<style>
-.column { width: 230px; float: left; padding-bottom: 100px; }
-.portlet { margin: 0 1em 1em 0; }
-.portlet-header { font-size:14px; color: #666; padding: 0 0 10px; cursor:move; }
-.portlet-header .ui-icon { float: right; }
-.portlet-content { padding: 0.4em; }
-.ui-sortable-placeholder { border: 1px dotted #324C96; visibility: visible !important; height: 50px !important; }
-.ui-sortable-placeholder * { visibility: hidden; }
-</style>
-
-<script type="text/javascript">
-// function that writes the list order to a cookie
-function saveOrder() {
-    jQuery(".column script").remove();
-    jQuery(".column").each(function(index, value){
-        var colid = value.id;
-        var cookieName = "cookie-" + colid;
-        // Get the order for this column.
-        var order = jQuery('#' + colid).sortable("toArray");
-        // For each portlet in the column
-        for ( var i = 0, n = order.length; i < n; i++ ) {
-            // Determine if it is 'opened' or 'closed'
-            var v = jQuery('#' + order[i] ).find('.portlet-content').is(':visible');
-            // Modify the array we're saving to indicate what's open and
-            //  what's not.
-            order[i] = order[i] + ":" + v;
-        }
-        jQuery.cookie(cookieName, order, { path: "/", expiry: new Date(2012, 1, 1)});
-        jQuery.cookie(cookieName+'-width',jQuery(this).width());
-    });
-}
-
-// function that restores the list order from a cookie
-function restoreOrder() {
-    jQuery(".column").each(function(index, value) {
-        var colid = value.id;
-        var cookieName = "cookie-" + colid
-        var cookie = jQuery.cookie(cookieName);
-        if ( cookie == null ) { return; }
-        var IDs = cookie.split(",");
-        for (var i = 0, n = IDs.length; i < n; i++ ) {
-            var toks = IDs[i].split(":");
-            if ( toks.length != 2 ) {
-                continue;
-            }
-            var portletID = toks[0];
-            var visible = toks[1]
-            var portlet = jQuery(".column")
-                .find('#' + portletID)
-                .appendTo(jQuery('#' + colid));
-            if (visible === 'false') {
-                portlet.find(".ui-icon").toggleClass("ui-icon-minus");
-                portlet.find(".ui-icon").toggleClass("ui-icon-plus");
-                portlet.find(".portlet-content").hide();
-            }
-        }
-        jQuery(this).width(jQuery.cookie(cookieName+'-width'));
-    });
-}
-
-jQuery(document).ready( function () {
-
-    // Make columns sortable
-    jQuery(".column").sortable({
-        connectWith: ['.column'],
-        stop: function(event,ui) { ui.item.css('opacity',1);saveOrder();},
-        sort: function(event,ui) { ui.item.css('opacity',0.7); }
-    });
-
-    jQuery(".portlet")
-        .addClass("ui-widget")
-        .addClass("ui-helper-clearfix ui-corner-all")
-        .find(".portlet-header")
-        .addClass("ui-widget-header ui-corner-all")
-        .prepend('<span class="ui-icon ui-icon-minus"></span>')
-        .end()
-        .find(".portlet-content");
-
-    // Restore order from cookie
-    restoreOrder();
-
-    jQuery(".portlet-header .ui-icon").click(function() {
-        jQuery(this).toggleClass("ui-icon-minus");
-        jQuery(this).toggleClass("ui-icon-plus");
-        jQuery(this).parents(".portlet:first").find(".portlet-content").toggle();
-        saveOrder(); // This is important
-    });
-    jQuery(".portlet-header .ui-icon").hover(
-        function() {jQuery(this).addClass("ui-icon-hover"); },
-        function() {jQuery(this).removeClass('ui-icon-hover'); }
-    );
-
-    setTimeout(function(){
-        jQuery('.portlet-content').resizable({handles:'e'}).resize(function(){
-            jQuery(this).parents('.column:first').width(jQuery(this).width()+25);
-            saveOrder();
-        })
-    },1000);
-
-});
-</script>
