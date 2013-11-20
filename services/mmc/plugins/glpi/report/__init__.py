b6bed90c3665afdbef480d5f2284e69ecd0a063d
Jean-Philippe Braun <jpbraun@mandriva.com>
2013-11-20 18:05:03 +0100

    report: reimplement getComputerCountBySoftwares
    Can count multiple softwares with vendor and version filters

--- services/mmc/plugins/glpi/report/__init__.py
+++ services/mmc/plugins/glpi/report/__init__.py
@@ -22,12 +22,15 @@
 """
 Report methods for glpi plugin
 """
-
+import logging
 import operator
 
 from mmc.support.mmctools import SecurityContext
 from mmc.plugins.glpi.database import Glpi
 from mmc.plugins.glpi.database_utils import toUUID, fromUUID
+
+
+logger = logging.getLogger()
 
 
 class exportedReport(object):
@@ -44,6 +47,10 @@ class exportedReport(object):
         @param entitites: a list of entities uuids
         @return: list of entities ids
         """
+        # Be sure to get all entities
+        self.ctx.locations = None
+        if hasattr(self.ctx, 'locationsid'):
+            del self.ctx.locationsid
         # get all entities uuids for the current user
         if entities == []:
             entities = [entity.toH()['uuid'] for entity in self.db.getAllEntities(self.ctx)]
@@ -64,7 +71,7 @@ class exportedReport(object):
                 count = False
                 for result in results:
                     if os_result['entity_id'] == result['entity_id']:
-                         oper(result['value'], os_result['value'])
+                         result['value'] = oper(result['value'], os_result['value'])
                          count = True
                 if not count and oper == operator.add:
                     results.append(os_result)
@@ -75,7 +82,7 @@ class exportedReport(object):
         Get computer count for os_names
 
         @param os_names: list of OS names to count
-        @param exclude: list of OS names to exclude
+        @param exclude_names: list of OS names to exclude
 
         @return: count of machines
         """
@@ -107,10 +114,84 @@ class exportedReport(object):
             result.append({'entity_id': toUUID(entity), 'value': state_count})
         return result
 
-    def getComputerCountBySoftware(self, entities, name, vendor=None, version=None):
+    def _constructSoftwareTuple(self, soft):
+        """
+        @param soft: string or dict
+        @return: software tuple:
+            (name, version, vendor)
+        """
+        name = version = vendor = None
+        if soft and isinstance(soft, basestring):
+            name = soft
+        elif type(soft) == dict:
+            name = soft.get('name', None)
+            if name:
+                name = name.replace('*', '%')
+            version = soft.get('version', None)
+            if version:
+                version = version.replace('*', '%')
+            vendor = soft.get('vendor', None)
+            if vendor:
+                vendor = vendor.replace('*', '%')
+        if name is None:
+            logger.error("Missing software name")
+            return None
+        return (name, version, vendor)
+
+    def _constructSoftwaresList(self, softs):
+        """
+        @param softs: dict, string, list of mixed string and dict
+        @return: list of software tuples
+            [(name, version, vendor),
+             (name, version, vendor),
+             (name, version, vendor),
+             ...]
+        """
+        if type(softs) == list:
+            return [self._constructSoftwareTuple(soft) for soft in softs]
+        if softs:
+            return [self._constructSoftwareTuple(softs)]
+        return []
+
+    def _getComputerCountBySoftware(self, entities, soft):
         result = []
         for entity in self._getEntitiesIds(entities):
             self.ctx.locationsid = [entity]
-            software_count = self.db.getMachineBySoftware(self.ctx, name, vendor=vendor, version=version, count=1)
-            result.append({'entity_id': toUUID(entity), 'value': software_count})
+            soft_count = self.db.getMachineBySoftware(self.ctx, soft[0], version=soft[1], vendor=soft[2], count=1)
+            result.append({'entity_id': toUUID(entity), 'value': soft_count})
         return result
+
+    def _getComputerCountBySoftwares(self, entities, softs, results, oper=operator.add):
+        for soft in softs:
+            soft_results = self._getComputerCountBySoftware(entities, soft)
+            for soft_result in soft_results:
+                count = False
+                for result in results:
+                    if soft_result['entity_id'] == result['entity_id']:
+                         result['value'] = oper(result['value'], soft_result['value'])
+                         count = True
+                if not count and oper == operator.add:
+                    results.append(soft_result)
+        return results
+
+    def getComputerCountBySoftwares(self, entities, soft_names, exclude_names={}):
+        """
+        Get computer count for software names
+
+        @param soft_names: list of softwares to count
+        @param exclude_names: list of softwares to exclude
+
+        soft_names can be a simple string (ie: 'Mozilla Firefox*')
+        soft_names can be a dict (ie: {'name': 'Mozilla Firefox', 'version': '24.0', vendor:'Mozilla'})
+        soft_names can be a list of mixed dict and strings
+
+        Same for exclude_names
+
+        @return: count of machines by entity
+        """
+        softs = self._constructSoftwaresList(soft_names)
+        excludes = self._constructSoftwaresList(exclude_names)
+        results = self._getComputerCountBySoftwares(entities, softs, [])
+        results = self._getComputerCountBySoftwares(entities, excludes,
+                                                    results, operator.sub)
+        return results
