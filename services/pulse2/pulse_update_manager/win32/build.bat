0ec8854f17d5d46da5fe974338d855b74219b49a
Ishak BELAHMAR <ibelahmar@mandriva.com>
2013-12-20 09:49:52 +0100

    update: correct newline in Update manager build script

--- services/pulse2/pulse_update_manager/win32/build.bat
+++ services/pulse2/pulse_update_manager/win32/build.bat
@@ -1,1 +1,3 @@
-rem Replace C:\Python\python.exe with your python executable pathC:\Python27\python.exe setup.py build
+rem Replace C:\Python\python.exe with your python executable path
+
+C:\Python27\python.exe setup.py build
