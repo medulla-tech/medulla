50ff7ad95b7051f9c21abd2c7731abcf757f09c9
Ishak BELAHMAR <ibelahmar@mandriva.com>
2013-12-24 16:41:42 +0100

    scheduler: Update is_installed field when target entry exists

--- services/pulse2/scheduler/utils.py
+++ services/pulse2/scheduler/utils.py
@@ -21,9 +21,9 @@
 
 import logging
 try :
-    import cPickle as pickle 
+    import cPickle as pickle
 except ImportError :
-    import pickle # pyflakes.ignore 
+    import pickle # pyflakes.ignore
 from StringIO import StringIO
 
 import MySQLdb
@@ -128,7 +128,7 @@ class UnixProtocol (object, LineReceiver):
         d.addCallback(self._send_response)
         d.addErrback(self._eb_call_failed, name)
 
-       
+
     def _send_response(self, response):
         logging.getLogger().debug("response: %s" % str(response))
         try:
@@ -143,7 +143,7 @@ class UnixProtocol (object, LineReceiver):
 
     def _lookup_procedure(self, name):
         this_class_dict = self.__class__.__mro__[0].__dict__
-        method_matches = [f for (k, f) in this_class_dict.items() 
+        method_matches = [f for (k, f) in this_class_dict.items()
                                       if k == name and callable(f)]
         if len(method_matches)==1 :
             return method_matches[0]
@@ -164,11 +164,11 @@ def chooseClientNetwork(target):
 
 def launcher_proxymethod(*options):
     """
-    Decorator to share a decorated method with the scheduler-proxy 
+    Decorator to share a decorated method with the scheduler-proxy
     which is processing the incoming requests from launcher.
     """
     try:
-    
+
         if len(options) == 1 and callable(options[0]) :
             # if only one option, this is a decorator without a parameter
             # so first parameter is decorated function
@@ -176,7 +176,7 @@ def launcher_proxymethod(*options):
             name = fn.__name__
             aliased = False
         else :
-            # first argument supposed as alias 
+            # first argument supposed as alias
             name = options[0]
             aliased = True
 
@@ -188,7 +188,7 @@ def launcher_proxymethod(*options):
                     if method_name not in self._proxy_methods :
                         self._proxy_methods[method_name] = f#(self, f)#(f, args, kwargs)
 
-                else : 
+                else :
                     return f(self, *args, **kwargs)
 
             inner.is_proxy_fnc = True
@@ -206,14 +206,14 @@ class ProxyProcessProtocol(ProcessProtocol):
 
      def processExited(self, reason):
          logging.getLogger().warn("XML Proxy: Process exited: %s" % (reason.value.exitCode))
- 
+
      def errReceived(self, data):
          logging.getLogger().warn("XMLRPC Proxy: STDERR: %s" % repr(data))
-    
+
      def outConnectionLost(self, reason):
          logging.getLogger().info("XMLRPC Proxy: Connection lost: %s" % (reason.value.exitCode))
- 
- 
+
+
 class SpawnProxy :
     def __init__(self, path):
          self.protocol = ProxyProcessProtocol()
@@ -221,14 +221,14 @@ class SpawnProxy :
              self.path = [path]
          else:
              self.path = path
-  
+
     def run(self):
          return reactor.spawnProcess(self.protocol,
-                                     self.path[0], 
-                                     self.path, 
+                                     self.path[0],
+                                     self.path,
                                      env=None,
-                                     childFDs={0 :"w", 
-                                               1 :"r", 
+                                     childFDs={0 :"w",
+                                               1 :"r",
                                                2:'r'}
                                     )
 
@@ -241,7 +241,7 @@ class WUInjectDB :
 
     def __init__(self):
         self.config = SchedulerDatabaseConfig()
-      
+
         self.logger = logging.getLogger()
         self.connect()
 
@@ -269,11 +269,11 @@ class WUInjectDB :
             return self.conn.cursor()
         except Exception, exc:
             self.logger.error("Error while creating cursor: %s" % str(exc))
-       
+
 
     def uuid_exists(self, uuid):
         query = "SELECT 1 FROM updates WHERE uuid = '%s';" % (uuid)
-    
+
         c = self.cursor
         c.execute(query)
         if len(c.fetchall()) == 1:
@@ -283,7 +283,7 @@ class WUInjectDB :
 
     def get_update_id(self, uuid):
         query = "SELECT id FROM updates WHERE uuid = '%s';" % (uuid)
-    
+
         c = self.cursor
         c.execute(query)
 
@@ -295,7 +295,7 @@ class WUInjectDB :
 
     def target_exists(self, target_uuid, update_id):
         query = "SELECT 1 FROM targets WHERE uuid = '%s' AND update_id = %d;" % (target_uuid, update_id)
-    
+
         c = self.cursor
         c.execute(query)
         if len(c.fetchall()) == 1:
@@ -307,7 +307,7 @@ class WUInjectDB :
         """
         Inserts the target with a update reference.
         """
-        
+
         stat = "INSERT INTO targets (update_id, uuid, is_installed) "
         stat += "VALUES (%d, '%s', %d);" % (update_id, uuid, is_installed)
 
@@ -320,7 +320,25 @@ class WUInjectDB :
             self.conn.rollback()
             self.logger.error("Error while insert target into 'update' db: %s" % str(exc))
 
-    def insert_WU(self, 
+
+    def update_target(self, update_id, uuid, is_installed):
+        """
+        Update the target with a update reference.
+        """
+
+        stat = "UPDATE targets SET is_installed = %d " % is_installed
+        stat += "WHERE update_id = %d AND uuid = %d;" % (update_id, uuid)
+
+        self.logger.debug("\033[33m%s\033[0m" % stat)
+        try:
+            c = self.cursor
+            c.execute(stat)
+            self.conn.commit()
+        except Exception, exc:
+            self.conn.rollback()
+            self.logger.error("Error while insert target into 'update' db: %s" % str(exc))
+
+    def insert_WU(self,
                   uuid,
                   title,
                   kb_number,
@@ -331,12 +349,12 @@ class WUInjectDB :
         """ Windows Updates insert """
 
         os_class_id = 3
-        
+
         stat = "INSERT INTO updates "
-        stat += "(uuid, title, kb_number, type_id, os_class_id, need_reboot, " 
+        stat += "(uuid, title, kb_number, type_id, os_class_id, need_reboot, "
         stat += "request_user_input, info_url) "
-        stat += "VALUES ('%s', '%s', '%s', %d, %d, %d, %d, '%s');" 
-        stat = stat % (uuid, 
+        stat += "VALUES ('%s', '%s', '%s', %d, %d, %d, %d, '%s');"
+        stat = stat % (uuid,
                        title,
                        kb_number,
                        type_id,
@@ -346,7 +364,7 @@ class WUInjectDB :
                        info_url)
 
         self.logger.debug("\033[33m%s\033[0m" % stat)
- 
+
 
         try:
             c = self.cursor
@@ -355,7 +373,7 @@ class WUInjectDB :
         except Exception, exc:
             self.conn.rollback()
             self.logger.error("Error while insert an update record into 'update' db: %s" % str(exc))
- 
+
 
     def inject(self,
                target_uuid,
@@ -384,4 +402,7 @@ class WUInjectDB :
 
         if not self.target_exists(target_uuid, update_id):
             self.insert_target(update_id, target_uuid, is_installed)
+        else:
+            # Set the NEW is_installed field
+            self.update_target(update_id, target_uuid, is_installed)
 
