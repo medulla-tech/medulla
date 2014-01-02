53ef15a3446d4ebb62c961b92d370faaa4571ef3
Jean-Philippe Braun <jpbraun@mandriva.com>
2014-01-02 19:25:44 +0100

    computers: add sidebar icons

--- web/modules/base/computers/localSidebar.php
+++ web/modules/base/computers/localSidebar.php
@@ -1,10 +1,8 @@
 <?php
 
-$sidemenu= new SideMenu();
-
+$sidemenu = new SideMenu();
 $sidemenu->setClass("computers");
-
-$sidemenu->addSideMenuItem(new SideMenuItem(_("All computers"), "base", "computers", "index", "img/machines/icn_global_active.gif", "img/machines/icn_global.gif"));
+$sidemenu->addSideMenuItem(new SideMenuItem(_("All computers"), "base", "computers", "index", "img/machines/icn_allMachines_active.gif", "img/machines/icn_allMachines_ro.gif"));
 
 if (canAddComputer()) {
     $sidemenu->addSideMenuItem(new SideMenuItem(_("Add computer"), "base", "computers", "add", "img/machines/icn_addMachines_active.gif", "img/machines/icn_addMachines_ro.gif"));
