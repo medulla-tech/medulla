406a45980d24e15f5b536c9cadbd655bb59798d2
Jean-Philippe Braun <jpbraun@mandriva.com>
2014-01-08 11:06:56 +0100

    update: smaller submodule name for the nav bar

--- web/modules/update/infoPackage.inc.php
+++ web/modules/update/infoPackage.inc.php
@@ -31,18 +31,18 @@ $mod->setDescription(_T("Update manager", "update"));
 $mod->setAPIVersion("0:0:0");
 $mod->setPriority(990);
 
-$submod = new SubModule("update", _T("Update manager", "update"));
+$submod = new SubModule("update", _T("Updates", "update"));
 $submod->setDefaultPage("update/update/index");
 $submod->setImg('modules/update/graph/navbar/update');
 $submod->setPriority(990);
 
-$page = new Page("index", _T("Update manager", "update"));
+$page = new Page("index", _T("All updates", "update"));
 $submod->addPage($page);
 
-$page = new Page("viewUpdates", _T("View updates", "update"));
+$page = new Page("viewUpdates", _T("Updates", "update"));
 $submod->addPage($page);
 
-$page = new Page("ajaxUpdates", _T("Update managment", "update"));
+$page = new Page("ajaxUpdates", _T("Updates list", "update"));
 $page->setOptions(array("visible" => False, "noHeader" => True));
 $submod->addPage($page);
 
