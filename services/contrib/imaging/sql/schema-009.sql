c2173c73bf86dd9acd7f181f69b7c1367edbacc0
Jean-Christophe VASSORT <jcvassort@mandriva.com>
2014-01-10 10:41:52 +0100

    imaging: Remove ICH5 / change sid and name postinst

---
+++ services/contrib/imaging/sql/schema-009.sql
@@ -0,0 +1,32 @@
+--
+-- (c) 2012 Mandriva, http://www.mandriva.com/
+--
+-- $Id$
+--
+-- This file is part of Pulse 2, http://pulse2.mandriva.org
+--
+-- Pulse 2 is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- Pulse 2 is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Pulse 2; if not, write to the Free Software
+-- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+-- MA 02110-1301, USA.
+
+SET SESSION character_set_server=UTF8;
+SET NAMES 'utf8';
+
+-- Delete ich5 / change sid and name PostInstallScript
+DELETE FROM PostInstallScript WHERE id IN (3, 9);
+
+-- Delete ich5 / change sid and name Internationalization
+DELETE FROM Internationalization WHERE id IN (26, 15, 31, 21);
+
+UPDATE version set Number = 9;
