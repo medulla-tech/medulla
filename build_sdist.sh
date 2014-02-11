b39f0b75126fadd7a477b5a007fb2d2afd5a654b
Jean-Philippe Braun <jpbraun@mandriva.com>
2014-02-11 18:22:39 +0100

    core, mds: use make distcheck for tarballs

--- build_sdist.sh
+++ build_sdist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -f mmc-core-*.tar.gz mmc-core-*.tar.gz.md5
-git clean -fdx && ./autogen.sh && ./configure --sysconfdir=/etc --localstatedir=/var --disable-python-check --disable-conf && make dist
+git clean -fdx && ./autogen.sh && ./configure --sysconfdir=/etc --localstatedir=/var --disable-python-check --disable-conf && make distcheck
 if [ $? -eq 0 ]; then
     for tarball in mmc-core-*.tar.gz; do
         md5sum $tarball > $tarball.md5
