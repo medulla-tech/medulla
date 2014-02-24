c897a6691cb564ff596d89d09bac50566240fce1
Jean-Christophe VASSORT <jcvassort@mandriva.com>
2014-02-24 16:27:54 +0100

    launcher: Refs #2278 get first ip address algo
    When creating tcp_sproxy if tcp_sproxy_host is not provided, get list of
    all interfaces and IP, and get the first one.

--- services/pulse2/launcher/tcp_sproxy.py
+++ services/pulse2/launcher/tcp_sproxy.py
@@ -26,6 +26,7 @@ import logging
 import socket
 import fcntl
 import struct
+import array
 
 import twisted.internet.reactor
 import twisted.internet.protocol
@@ -137,14 +138,46 @@ def establishProxy(client, requestor_ip, requested_port):
             struct.pack('256s', ifname[:15])
         )[20:24])
 
+    def get_all_interfaces():
+        """
+        Return list of all available interfaces with IP
+        @see: https://gist.github.com/pklaus/289646
+        """
+        def format_ip(addr):
+            return '.'.join([str(ord(x)) for x in addr])
+        max_possible = 128  # arbitrary. raise if needed.
+        bytes = max_possible * 32
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        names = array.array('B', '\0' * bytes)
+        outbytes = struct.unpack('iL', fcntl.ioctl(
+            s.fileno(),
+            0x8912,  # SIOCGIFCONF
+            struct.pack('iL', bytes, names.buffer_info()[0])
+        ))[0]
+        namestr = names.tostring()
+        lst = []
+        for i in range(0, outbytes, 40):
+            name = namestr[i:i+16].split('\0', 1)[0]
+            ip   = format_ip(namestr[i+20:i+24])
+            if not ip.startswith('127.'):
+                lst.append({
+                    'name': name,
+                    'ip': ip,
+                })
+        return lst
+
     def parse_result():
         if LauncherConfig().tcp_sproxy_host:
             tcp_sproxy_host = LauncherConfig().tcp_sproxy_host
         else:
             # Take the first network interface
             logging.getLogger().info('tcp_sproxy_host param was not specified in launcher config')
-            logging.getLogger().info('taking first interface IP Address')
-            tcp_sproxy_host = get_ip_address('eth0')
+            available_interfaces = get_all_interfaces()
+            if available_interfaces:
+                tcp_sproxy_host = available_interfaces[0]['ip']
+                logging.getLogger().info('Using %(name)s %(ip)s as IP address for tcp_sproxy_host' % available_interfaces[0])
+            else:
+                raise Exception('No IP found for tcp_sproxy_host')
         return LauncherConfig().name, tcp_sproxy_host, proxy_port, auth_key
     # Waiting to establish the proxy
     ret = task.deferLater(twisted.internet.reactor, 2, parse_result)
