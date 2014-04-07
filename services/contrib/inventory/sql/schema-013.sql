f3c896abcfe2a25a31137d9d3f6691b2ac70e9eb
Jean-Christophe VASSORT <jcvassort@mandriva.com>
2014-04-07 14:19:51 +0200

    inventory: Add Virtual Field in Network table
    This new field stores if a network card type is virtual of not.
    
    So, when inventory server try to associate a MAC to an existing machine,
    only real network cards are checked, not virtual ones.

---
+++ services/contrib/inventory/sql/schema-013.sql
@@ -0,0 +1,26 @@
+--
+-- (c) 2010 Mandriva, http://www.mandriva.com/
+--
+-- $Id$
+--
+-- This file is part of Pulse 2, http://pulse2.mandriva.org
+--
+-- Pulse 2 is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- Pulse 2 is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with MMC.  If not, see <http://www.gnu.org/licenses/>.
+
+START TRANSACTION;
+
+ALTER TABLE `Network` ADD `Virtual` TINYINT NOT NULL DEFAULT '0';
+UPDATE Version SET Number = 13;
+
+COMMIT;
