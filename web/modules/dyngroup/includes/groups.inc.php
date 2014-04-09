a9e0fec2b1030d4fc83b74067fe6b80fa12fd438
Jean-Christophe VASSORT <jcvassort@mandriva.com>
2014-04-09 10:36:27 +0200

    dyngroup: Invert arrows in shared group form
    Refs #2318

--- web/modules/dyngroup/includes/groups.inc.php
+++ web/modules/dyngroup/includes/groups.inc.php
@@ -52,8 +52,8 @@ function drawGroupShare($nonmemb, $members, $listOfMembers, $diff, $gid, $name) 
  </td>
  <td style="border: none;">
   <div>
-   <input type="image" name="bdeluser" style="padding: 5px;" src="img/common/icn_arrowleft.gif" value="<--" /><br/>
-   <input type="image" name="badduser" style="padding: 5px;" src="img/common/icn_arrowright.gif" value = "-->"/>
+   <input type="image" name="badduser" style="padding: 5px;" src="img/common/icn_arrowright.gif" value = "-->"/><br/>
+   <input type="image" name="bdeluser" style="padding: 5px;" src="img/common/icn_arrowleft.gif" value="<--" />
   </div>
  </td>
  <td style="border: none;">
