4916807d1f33216a430ebf595d2e95876a102753
Ishak BELAHMAR <ibelahmar@mandriva.com>
2014-07-25 11:02:51 +0200

    p2agents: Delete rpm temp

--- services/clients/linux/rpm/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-workdir=/tmp/pulse_agents_$(date +%N)/
-mkdir -p $workdir
-cd $workdir
-wget http://tyrion/downloads/linux/deb/deb_agent.tar.gz
-tar zxf deb_agent.tar.gz
-sh install.sh
--- services/clients/linux/rpm/template/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./ssh.sh
-./fusion_inventory.sh
-vnc/vnc.sh
-./shorewall.sh
-./update_manager.sh
