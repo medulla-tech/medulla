97b95aee1dbb7c65f63fe569f6d1348dc3d26435
Ishak BELAHMAR <ibelahmar@mandriva.com>
2014-11-19 16:38:35 +0100

    glpi: Entity and entity rule managment feature

--- web/modules/base/computers/localSidebar.php
+++ web/modules/base/computers/localSidebar.php
@@ -8,6 +8,10 @@ if (canAddComputer()) {
     $sidemenu->addSideMenuItem(new SideMenuItem(_("Add computer"), "base", "computers", "add", "img/machines/icn_addMachines_active.gif", "img/machines/icn_addMachines_ro.gif"));
 }
 
+if (in_array("glpi", $_SESSION["modulesList"])) {
+    require("modules/glpi/glpi/localSidebar.php");
+}
+
 if (in_array("dyngroup", $_SESSION["modulesList"])) {
     require("modules/dyngroup/dyngroup/localSidebar.php");
 }
