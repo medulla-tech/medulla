ff5a9e8accfec71089c3b7630eb62cf0a2f9312b
Jean-Christophe VASSORT <jcvassort@mandriva.com>
2014-12-01 10:02:39 +0100

    update: Add Windows 8 support
    Refs #2420

---
+++ services/contrib/update/sql/schema-004.sql
@@ -0,0 +1,32 @@
+--
+-- (c) 2010 Mandriva, http://www.mandriva.com/
+--
+-- $Id$
+--
+-- This file is part of Pulse 2, http://pulse2.mandriva.org
+--
+-- Pulse 2 is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- Pulse 2 is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Pulse 2; if not, write to the Free Software
+-- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+-- MA 02110-1301, USA.
+
+-- ----------------------------------------------------------------------
+-- Database version
+-- ----------------------------------------------------------------------
+
+INSERT INTO `os_classes` (`id`, `name`, `enabled`, `pattern`) VALUES
+(4, 'Windows 8', 1, '*windows*8*');
+
+UPDATE  `version` SET  `Number` =  '4';
+
+COMMIT;
