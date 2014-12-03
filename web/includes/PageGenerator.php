3b27cbc3a32b0b08ffce01cf8a33777b152897be
Ladislav CABELKA <lcabelka@mandriva.com>
2014-12-03 12:55:43 +0100

    Refs #2454: Avoid to add an untitled package

--- web/includes/PageGenerator.php
+++ web/includes/PageGenerator.php
@@ -2592,6 +2592,13 @@ class ValidatingForm extends Form {
     }
 
 }
+class ValidatingFormRequiresId extends ValidatingForm {
+
+    function ValidatingFormRequiresId($options = array()) {
+        $this->ValidatingForm($options);
+	$this->options["onsubmit"] = "selectAll('" . $this->options["id"] . "'); return validateForm('" . $this->options["id"] . "');";
+    }
+}
 
 /**
  *
