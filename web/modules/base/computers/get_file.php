0c54817fcb903b21b3c6e80d603d5edbba39d179
inkhey <mail@inkey-art.net>
2014-12-05 09:40:08 +0100

    get_file() file for computer inventory

---
+++ web/modules/base/computers/get_file.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+ * (c) 2007-2008 Mandriva, http://www.mandriva.com
+ *
+ * $Id$
+ *
+ * This file is part of Mandriva Management Console (MMC).
+ *
+ * MMC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MMC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MMC; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+/*
+ * Function to permit big files transfer
+ * @see: http://teddy.fr/2007/11/28/how-serve-big-files-through-php/
+ */
+
+function readfile_chunked($filename, $retbytes = true) {
+    $chunksize = 1 * (1024 * 1024); // how many bytes per chunk
+    $buffer = '';
+    $cnt = 0;
+    $handle = fopen($filename, 'rb');
+    if ($handle === false) {
+        return false;
+    }
+    while (!feof($handle)) {
+        $buffer = fread($handle, $chunksize);
+        echo $buffer;
+        ob_flush();
+        flush();
+        if ($retbytes) {
+            $cnt += strlen($buffer);
+        }
+    }
+    $status = fclose($handle);
+    if ($retbytes && $status) {
+        return $cnt; // return num. bytes delivered like readfile() does.
+    }
+    return $status;
+}
+
+function startswith($haystack, $needle) {
+    return substr($haystack, 0, strlen($needle)) === $needle;
+}
+
+if (isset($_GET['path']) && startswith($_GET['path'], '/var/tmp/report-')) {
+    // Prevent download to stop after PHP timeout
+    set_time_limit(0);
+
+    $filepath = $_GET['path'];
+    $file_name = basename($filepath);
+
+    // Prevent from corrupting files due to indesirable prints
+    ob_end_clean();
+
+    // Getting file mimetype
+    $finfo = finfo_open(FILEINFO_MIME_TYPE); // return mime type ala mimetype extension
+    $mime_type = finfo_file($finfo, $filepath);
+    finfo_close($finfo);
+
+    // Sending HTTP headers
+    header("Content-Type: $mime_type");
+    header("Content-Transfer-Encoding: Binary");
+    header("Content-disposition: attachment; filename=\"" . $file_name . "\"");
+
+    // Sending binary data
+    readfile_chunked($filepath);
+    die('');    // Stopping flow
+}
+?>
