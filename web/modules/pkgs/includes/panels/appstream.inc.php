bccfa3f72fecf04998045a51be692b2d8dea8e0b
Ishak BELAHMAR <ibelahmar@mandriva.com>
2014-12-05 16:16:23 +0100

    pkgs: Update appstream widget with featured packages

--- web/modules/pkgs/includes/panels/appstream.inc.php
+++ web/modules/pkgs/includes/panels/appstream.inc.php
@@ -35,25 +35,65 @@ class AppstreamPanel extends Panel {
 
     function display_content() {
 		
-		$notifications = getAppstreamNotifications();
-		$update_count = count($notifications);
-		$view_updates_text = _T('View updates', 'pkgs');
+        $featured_link_label = json_encode(_T('_X_ AppStream(s) available for subscription', 'pkgs'));
+        $no_featured_text = json_encode(_T('No featured appstream found.', 'pkgs'));
+        $excluded_packages = array_keys(getActivatedAppstreamPackages());
+        $available = getAvailableAppstreamPackages();
+        $excluded_packages = array_merge($excluded_packages, array_map(function($item){
+            return $item['options']['package_name'];
+        }, $available['product'])); 
+        $excluded_packages = json_encode(array_unique($excluded_packages));
 
-		print '<center>';
-		
-		if (count($notifications) == 0)
-			printf('<p><strong>%s</strong></p>', _T('No update available.', 'pkgs'));
-		else{
-			printf('<p><strong>%d %s</strong></p>', $update_count, _T('AppStream updates.', 'update'));
-			
-			print <<<EOS
-			<a title="View updates" class="btnSecondary"
-				href="javascript:;"
-				onclick="PopupWindow(event,'main.php?module=pkgs&amp;submod=pkgs&amp;action=viewAppstreamUpdates', 300); return false;"
-				>$view_updates_text</a><br/><br/>
-			</center>
+        print '<div id="appstream_panel_content"></div>';
+        print <<<EOS
+<script type="text/javascript">
+var $=jQuery;
+$(function(){
+    var appstream_featured_url = 'http://serviceplace.mandriva.com/api/v1/services/appstream/1.0/?mandriva_featured=True&category=appstream';
+    var panel = $('#appstream_panel_content');
+    var excluded_packages = $excluded_packages;
+    var featured_packages = [];
+   $.get(appstream_featured_url, function(result){
+        panel.html('');
+        if (!result.length)
+            return;
+        for (var i=0; i<result.length; i++){
+            item = result[i];
+            var featured = true;
+            for (var j=0; j<item.variable_options[0].data.choices.length; j++)
+            {
+                if (excluded_packages.indexOf(item.variable_options[0].data.choices[j][0]) != -1){
+                    featured = false;
+                    break;
+                }
+            }
+            
+            if (featured){
+                featured_packages.push(item);
+            }
+        }
+        if (featured_packages.length == 0)
+        {
+            panel.append($no_featured_text);
+            return;
+        }
+        var featured_link = $('<a>').attr('href', 'http://serviceplace.mandriva.com/services/?p=140');
+        featured_link.html($featured_link_label.replace('_X_', featured_packages.length));
+        panel.append($('<center>').append(featured_link)); 
+        panel.append('<br/>');
+        panel.append('<br/>');
+
+        for (var i=0; i<featured_packages.length; i++){
+            item = featured_packages[i];
+            console.log(item);  
+            var appstream_link = $('<a>').attr('href', item.url);
+            appstream_link.html(item.name);
+            panel.append(appstream_link);
+            
+        }
+    });
+});
+</script>
 EOS;
-			
-		}   
     }
 }
