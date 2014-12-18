7aee758aef12dda000ed6e4d8f2246f4b731a77b
Ladislav CABELKA <lcabelka@mandriva.com>
2014-12-18 18:00:29 +0100

    base: PEP8 fixes

--- agent/mmc/plugins/base/output.py
+++ agent/mmc/plugins/base/output.py
@@ -30,7 +30,7 @@ localedir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "locale")
 class XLSGenerator(object):
     _gettext = None
 
-    def __init__(self, path='/tmp/report.xls',lang="en_US"):
+    def __init__(self, path='/tmp/report.xls', lang="en_US"):
         self.wbk = xlwt.Workbook(encoding='utf-8')
         self.path = path
         self._setup_lang(lang)
@@ -48,51 +48,51 @@ class XLSGenerator(object):
         return self._gettext(text).decode('utf8')
 
 
-    def get_summary_sheet(self,datas):
+    def get_summary_sheet(self, datas):
         sheet = self.wbk.add_sheet(self._T('Summary'))
-        self.create_summary_table(sheet,0,0,"",datas)
+        self.create_summary_table(sheet, 0, 0, "", datas)
 
-    def get_hardware_sheet(self,processors_data,controllers_data,
-        graphic_card_data,sound_card_data):
+    def get_hardware_sheet(self, processors_data, controllers_data,
+                           graphic_card_data, sound_card_data):
         sheet = self.wbk.add_sheet(self._T('Hardware'))
         line = 0
         if processors_data:
-            line = self.create_simple_table(sheet,line,0,self._T("Processors"),processors_data)
+            line = self.create_simple_table(sheet, line, 0, self._T("Processors"), processors_data)
         if controllers_data:
-            line = self.create_simple_table(sheet,line,0,self._T("Controllers"),controllers_data)
+            line = self.create_simple_table(sheet, line, 0, self._T("Controllers"), controllers_data)
         if graphic_card_data:
-            line = self.create_simple_table(sheet,line,0,self._T("Graphic Cards"),graphic_card_data)
+            line = self.create_simple_table(sheet, line, 0, self._T("Graphic Cards"), graphic_card_data)
         if sound_card_data:
-            line = self.create_simple_table(sheet,line,0,self._T("Sound Cards"),sound_card_data)
+            line = self.create_simple_table(sheet, line, 0, self._T("Sound Cards"), sound_card_data)
 
-    def get_storage_sheet(self,datas):
+    def get_storage_sheet(self, datas):
         sheet = self.wbk.add_sheet(self._T('Storage'))
         if datas:
-            self.create_simple_table(sheet,0,0,self._T("Storage"),datas)
+            self.create_simple_table(sheet, 0, 0, self._T("Storage"), datas)
 
-    def get_software_sheet(self,datas):
+    def get_software_sheet(self, datas):
         sheet = self.wbk.add_sheet(self._T('Software'))
         if datas:
-            self.create_simple_table(sheet,0,0,self._T("Software"),datas)
+            self.create_simple_table(sheet, 0, 0, self._T("Software"), datas)
 
-    def get_network_sheet(self,datas):
+    def get_network_sheet(self, datas):
         sheet = self.wbk.add_sheet(self._T('Network'))
         if datas:
-            self.create_simple_table(sheet,0,0,self._T("Network"),datas)
+            self.create_simple_table(sheet, 0, 0, self._T("Network"), datas)
 
-    def create_summary_table(self,sheet,line,column,table_name, datas):
+    def create_summary_table(self, sheet, line, column, table_name, datas):
         if table_name:
-            sheet.write(line,column,table_name)
+            sheet.write(line, column, table_name)
             line +=1
         for section in datas:
             for row in section:
                 for cell in row:
                     try:
-                        if isinstance (cell,basestring):
+                        if isinstance (cell, basestring):
                             sheet.write(line,column,self._T(cell))
                         else:
-                            if isinstance (cell[2],basestring):
-                                sheet.write(line,column,cell[2])
+                            if isinstance (cell[2], basestring):
+                                sheet.write(line, column,cell[2])
                     except Exception:
                         pass
                     column += 1
@@ -106,16 +106,16 @@ class XLSGenerator(object):
         chmod(self.path, 0644)
         return self.path
 
-    def create_simple_table(self,sheet,line,column,table_name, datas):
+    def create_simple_table(self, sheet, line, column, table_name, datas):
         if table_name:
-            sheet.write(line,column,table_name)
+            sheet.write(line, column, table_name)
             line += 1
         #write headers
         headers=[]
         for section in datas:
             for elements in section :
                 try:
-                    if isinstance (elements[0],basestring):
+                    if isinstance (elements[0], basestring):
                         if elements[0] not in headers:
                             headers.append(elements[0])
                             sheet.write(line,column,self._T(elements[0]))
@@ -127,8 +127,8 @@ class XLSGenerator(object):
         for section in datas:
             for elements in section :
                 try:
-                    if isinstance (elements[0],basestring) and isinstance (elements[1],basestring):
-                        sheet.write(line,headers.index(elements[0]),elements[1])
+                    if isinstance (elements[0], basestring) and isinstance (elements[1], basestring):
+                        sheet.write(line, headers.index(elements[0]), elements[1])
                 except Exception:
                     pass
                 column += 1
