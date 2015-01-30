0e83cc16f41ad3962dcd9e7220bc82c1083aabc4
Jean-Christophe VASSORT <jcvassort@mandriva.com>
2015-01-30 18:53:50 +0100

    pulse2: remove all startswith and endswith declaration
    These methods were included into core by commit
    09ef63b67673e319b76efa7057636cd7d60e9766

--- web/modules/base/computers/get_file.php
+++ web/modules/base/computers/get_file.php
@@ -51,10 +51,6 @@ function readfile_chunked($filename, $retbytes = true) {
     return $status;
 }
 
-function startswith($haystack, $needle) {
-    return substr($haystack, 0, strlen($needle)) === $needle;
-}
-
 if (isset($_GET['path']) && startswith($_GET['path'], '/var/tmp/report-')) {
     // Prevent download to stop after PHP timeout
     set_time_limit(0);
