fa1d839d2e51f945ab127fab6ad1735a6991f3fa
Ladislav CABELKA <lcabelka@mandriva.com>
2015-02-05 00:11:23 +0100

    vpn: added builder of softether silent installer

---
+++ services/clients/vpn/softether/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Copyright (C) 2015 Mandriva S.A
+# http://www.mandriva.com
+#
+# This file is part of Mandriva Pulse2 project.
+#
+# This software is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+
+VPN_ARCHIVE="SoftEtherVPNClient.zip"
+VPN_URL="http://pulse2.mandriva.org/pub/pulse2/misc/${VPN_ARCHIVE}"
+
+if [ ! -e ${VPN_ARCHIVE} ]; then
+    echo "Downloading ${VPN_ARCHIVE}..."
+    curl --progress-bar -o ${VPN_ARCHIVE} ${VPN_URL}
+    if [ ! -e ${VPN_ARCHIVE} ]; then
+        echo
+        echo "${VPN_ARCHIVE} download failed. Please restart."
+        echo
+        exit 1
+    fi	
+fi
+
+unzip -o ${VPN_ARCHIVE}
+makensis -V1 softether-installer.nsi
