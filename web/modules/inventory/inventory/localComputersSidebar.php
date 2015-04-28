443ef910682306334a74fcc30bb7075af05f0d70
Jean-Christophe VASSORT <jcvassort@mandriva.com>
2015-04-28 09:11:47 +0200

    base: move inventory side menu entries
    Move specific inventory side menu from computer's page to inventory
    module

---
+++ web/modules/inventory/inventory/localComputersSidebar.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * (c) 2015 Mandriva, http://www.mandriva.com
+ *
+ * $Id$
+ *
+ * This file is part of Pulse 2, http://pulse2.mandriva.org
+ *
+ * Pulse 2 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MMC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MMC; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+ * MA 02110-1301, USA
+ */
+
+// These entries are on computers page, loaded from base/computers/localSidebar.php
+
+$sidemenu->addSideMenuItem(new SideMenuItem(_T("Entities", "inventory"), "base", "computers",  "entityList", "", ""));
+$sidemenu->addSideMenuItem(new SideMenuItem(_T("Add an entity", "inventory"), "base", "computers", "addEntity", "", ""));
+$sidemenu->addSideMenuItem(new SideMenuItem(_T("Entity rules", "inventory"), "base", "computers", "entityRules", "", ""));
+$sidemenu->addSideMenuItem(new SideMenuItem(_T("Add entity rule", "inventory"), "base", "computers", "addEntityRule", "", ""));
+?>
