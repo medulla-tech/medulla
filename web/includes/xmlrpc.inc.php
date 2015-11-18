c8397d7fcde0b25bb32c3a2f1b692c3f24d7ed7b
Nicolas Lécureuil <nicolas.lecureuil@siveo.net>
2015-11-18 23:31:28 +0100

    Rename

--- web/includes/xmlrpc.inc.php
+++ web/includes/xmlrpc.inc.php
@@ -5,7 +5,7 @@
  *
  * $Id$
  *
- * This file is part of Mandriva Management Console (MMC).
+ * This file is part of Management Console (MMC).
  *
  * MMC is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
