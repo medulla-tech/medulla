c11d9bf08e905cc92cbf0767b56392fe3cacf026
Nicolas Lécureuil <neoclust@mageia.org>
2016-01-04 23:37:43 +0100

    pulse: bump version to 3.0.2

--- bump.sh
+++ bump.sh
@@ -4,7 +4,7 @@ version=$1
 
 if [ ! -z $1 ]; then
 
-    sed -i "s/^AC_INIT.*$/AC_INIT\(project, [$1], [http:\/\/www.mandriva.com]\)/" configure.ac
+    sed -i "s/^AC_INIT.*$/AC_INIT\(project, [$1], [http:\/\/www.siveo.org]\)/" configure.ac
 
     git diff
     git commit -a -m "pulse: bump version to $1"
