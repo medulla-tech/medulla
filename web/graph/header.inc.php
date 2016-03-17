dd9fded164d5f7f2c16cd8715a1c1bdfa3276f38
SPO <stephane.pointu@siveo.net>
2016-03-17 17:11:29 +0100

    Update title of web page

--- web/graph/header.inc.php
+++ web/graph/header.inc.php
@@ -25,7 +25,7 @@ $root = $conf["global"]["root"];
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Mandriva Management Console</title>
+        <title>SIVEO Console</title>
         <link href="graph/master.css" rel="stylesheet" media="screen" type="text/css" />
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="imagetoolbar" content="false" />
