454cf4e0edcf363523b9c05b2cfb9041b56fee99
Nicolas Lécureuil <neoclust@mageia.org>
2016-05-30 10:31:21 +0200

    Cosmetics

--- services/contrib/test-xmlrpc.php
+++ services/contrib/test-xmlrpc.php
@@ -24,7 +24,8 @@
 
 <?php
 
-function decode_entities($text) {
+function decode_entities($text)
+{
     $text = html_entity_decode($text,ENT_QUOTES,"ISO-8859-1"); /* NOTE: UTF-8 does not work! */
     $text= preg_replace('/&#(\d+);/me',"chr(\\1)",$text); /* decimal notation */
     $text= preg_replace('/&#x([a-f0-9]+);/mei',"chr(0x\\1)",$text);  /* hex notation */
