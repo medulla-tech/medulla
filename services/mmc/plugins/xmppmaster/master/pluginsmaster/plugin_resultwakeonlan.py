b156917b51ef76867c94f3f3e818a3314f9e2322
Jean-françois kneib <jean-francois.kneib@siveo.net>
2016-10-14 08:29:25 +0200

    pep8 plugins

--- services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_resultwakeonlan.py
+++ services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_resultwakeonlan.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8; -*-
 import base64, json, os
+
 #plugin_resultwakeonLan
 def action( objetxmpp, action, sessionid, data, message, ret, dataobj):
+    print "plugin_resultwakeonLan"
     try:
         print data
     except Exception as e:
--- services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_wakeonlan.py
+++ services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_wakeonlan.py
@@ -3,23 +3,19 @@ import base64, json, os, sys
 from pulse2.database.xmppmaster import XmppMasterDatabase
 import traceback
 from utils import name_random
-#plugin_masterwakeonLan
+
+# plugin run wake on lan for mac adress
+
 def action( objetxmpp, action, sessionid, data, message, ret, dataobj):
-    print "kkkkkkkkkkkkkkk"
-    #try:
-    print data
-    #except Exception as e:
-        #print "ERREUR DANS PLUGINS %s : %s"%(action, str(e))
+    print "plugin Master wakeonlan"
     try:
-            listserverrelais = XmppMasterDatabase().listserverrelay()
-            senddataplugin = {'action' : 'wakeonLan',
-                            'sessionid': name_random(5, "wakeonLan"),
-                            'data' : {'macaddress': data['macadress'] }}
-            for serverrelais in listserverrelais:
-                objetxmpp.send_message(  mto = serverrelais[0],
-                                    mbody = json.dumps(senddataplugin, encoding='latin1'),
-                                    mtype = 'chat')
+        listserverrelais = XmppMasterDatabase().listserverrelay()
+        senddataplugin = {'action' : 'wakeonLan',
+                        'sessionid': name_random(5, "wakeonLan"),
+                        'data' : {'macaddress': data['macadress'] }}
+        for serverrelais in listserverrelais:
+            objetxmpp.send_message(  mto = serverrelais[0],
+                                mbody = json.dumps(senddataplugin, encoding='latin1'),
+                                mtype = 'chat')
     except:
         traceback.print_exc(file=sys.stdout)
-
-
