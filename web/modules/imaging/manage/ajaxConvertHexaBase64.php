cc4356eee85510e170c91ad90bdd518d3e65c313
Noël-Antoine Kneib <noel.kneib@siveo.net>
2016-11-16 11:33:31 +0100

    Add function which crypts sysprep passwords.

---
+++ web/modules/imaging/manage/ajaxConvertHexaBase64.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * (c) 2015-2016 Siveo, http://www.siveo.net
+ *
+ * $Id$
+ *
+ * This file is part of Management Console (MMC).
+ *
+ * MMC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MMC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MMC; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+function strtohex($string)
+{
+  $string = str_split($string);
+  foreach($string as &$char)
+    $char = dechex(ord($char));
+  return implode('',$string);
+}
+
+function cryptSysprepPassword($string)
+{
+	$string .= 'Password';
+	$strArray = str_split($string);
+	$hex = array();
+	foreach($strArray as $char)
+	{
+		$hex[] = strtohex($char);
+		$hex[] = '00';
+	}
+	$hex = implode($hex);
+
+	return base64_encode(pack('H*',$hex));
+}
+$password = cryptSysprepPassword($_GET['data']);
+// echo substr($password,0,-2);
+echo $password;
+?>
+
