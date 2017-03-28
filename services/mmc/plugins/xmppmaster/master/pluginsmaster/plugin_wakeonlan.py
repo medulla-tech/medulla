28de5e43dc9dc3ff87cb64184fbd2d187f0fc19d
jfkneib <jean-francois.kneib@siveo.net>
2017-03-28 08:30:07 +0200

    chang plugin wake on lan : accept uuid and macadress

--- services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_wakeonlan.py
+++ services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_wakeonlan.py
@@ -1,21 +1,40 @@
 # -*- coding: utf-8; -*-
 import base64, json, os, sys
 from pulse2.database.xmppmaster import XmppMasterDatabase
+from mmc.plugins.glpi.database import Glpi
 import traceback
 from utils import name_random
 
 # plugin run wake on lan on mac adress
 
 def action( xmppobject, action, sessionid, data, message, ret, dataobj):
-    print "plugin Master wakeonlan"
+    print "call master plugin Master wakeonlan"
+
     try:
         listserverrelay = XmppMasterDatabase().listserverrelay()
-        senddataplugin = {'action' : 'wakeonLan',
-                        'sessionid': name_random(5, "wakeonLan"),
-                        'data' : {'macaddress': data['macadress'] }}
-        for serverrelay in listserverrelay:
-            xmppobject.send_message(  mto = serverrelay[0],
-                                mbody = json.dumps(senddataplugin, encoding='latin1'),
-                                mtype = 'chat')
+        if 'macadress' in data:
+            senddataplugin = {'action' : 'wakeonLan',
+                            'sessionid': name_random(5, "wakeonLan"),
+                            'data' : {'macaddress': data['macadress'] }}
+            for serverrelay in listserverrelay:
+                xmppobject.send_message(  mto = serverrelay[0],
+                                    mbody = json.dumps(senddataplugin, encoding='latin1'),
+                                    mtype = 'chat')
+        elif 'UUID' in data:
+            listadressmacs = Glpi().getMachineMac(data['UUID'])
+            for macadress in listadressmacs:
+                if macadress == '00:00:00:00:00:00':
+                    continue
+                senddataplugin = {'action' : 'wakeonLan',
+                                  'sessionid': name_random(5, "wakeonLan"),
+                                  'data' : {'macaddress': macadress }}
+                for serverrelay in listserverrelay:
+                    xmppobject.send_message( mto = serverrelay[0],
+                                             mbody = json.dumps(senddataplugin, encoding='latin1'),
+                                             mtype = 'chat')
+        else:
+            raise
+
     except:
+        print "error plugin plugin_wakeonlan %s"%data
         traceback.print_exc(file=sys.stdout)
