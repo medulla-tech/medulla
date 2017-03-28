816785dd41b797f943ffb68f9f4c7ba13a2c2fa1
jfkneib <jean-francois.kneib@siveo.net>
2017-03-28 08:38:48 +0200

    add call action direct deploy

---
+++ web/modules/xmppmaster/xmppmaster/actionrestart.php
@@ -0,0 +1,37 @@
+<?php
+session_name("PULSESESSION");
+session_start();
+?>
+<?php
+/*
+ * (c) 2015 Siveo, http://http://www.siveo.net
+ *
+ * $Id$
+ *
+ * This file is part of Mandriva Management Console (MMC).
+ *
+ * MMC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MMC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MMC.  If not, see <http://www.gnu.org/licenses/>.
+ */
+require_once("../../../includes/config.inc.php");
+require_once("../../../includes/i18n.inc.php");
+require_once("../../../includes/acl.inc.php");
+require_once("../../../includes/session.inc.php");
+require_once("../../../includes/PageGenerator.php");
+
+require_once('../includes/xmlrpc.php');
+
+require_once("../../pulse2/includes/locations_xmlrpc.inc.php");
+
+echo  xmlrpc_callrestart($_GET['objectUUID']);
+?>
