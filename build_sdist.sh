057bfc59a41229bfcb47eb9f7b1c36bb5c247244
Nicolas Lécureuil <neoclust@mageia.org>
2017-04-24 11:31:55 +0200

    Add workaround to add setup.py

--- build_sdist.sh
+++ build_sdist.sh
@@ -2,6 +2,9 @@
 
 rm -f pulse2-*.tar.gz pulse2-*.tar.gz.md5
 git clean -fdx && ./autogen.sh && ./configure --sysconfdir=/etc --localstatedir=/var --disable-python-check --disable-conf && make distcheck
+tar xzvf pulse2-3.9.90.tar.gz
+cp setup.py pulse2-3.9.90
+tar czvf pulse2-3.9.90.tar.gz pulse2-3.9.90
 if [ $? -eq 0 ]; then
     for tarball in pulse2-*.tar.gz; do
         md5sum $tarball > $tarball.md5
