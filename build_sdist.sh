807b7002c5d92b8dce08d7008582d0db02020ac4
Nicolas Lécureuil <neoclust@mageia.org>
2017-04-26 15:11:11 +0200

    Fix tarball name

--- build_sdist.sh
+++ build_sdist.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+VERSION='3.9.90'
+
 rm -f pulse2-*.tar.gz pulse2-*.tar.gz.md5
 git clean -fdx && ./autogen.sh && ./configure --sysconfdir=/etc --localstatedir=/var --disable-python-check --disable-conf && make distcheck
-tar xzvf pulse2-3.9.90.tar.gz
-cp setup.py pulse2-3.9.90
-tar czvf pulse2-3.9.90.tar.gz pulse2-3.9.90
+tar xzvf pulse2-$VERSION.tar.gz
+cp setup.py pulse2-$VERSION
+tar czvf pulse2-$VERSION.tar.gz pulse2-$VERSION
+mv pulse2-$VERSION.tar.gz pulse2_$VERSION.orig.tar.gz
+rm -fr pulse2-$VERSION/
 if [ $? -eq 0 ]; then
-    for tarball in pulse2-*.tar.gz; do
+    for tarball in pulse2*.tar.gz; do
         md5sum $tarball > $tarball.md5
     done
 fi
