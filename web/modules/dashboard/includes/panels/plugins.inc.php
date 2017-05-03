c4a10b251a76b18f7f1e4fc33d8a7c6eeedc66fa
Noël-Antoine Kneib <noel.kneib@siveo.net>
2017-05-03 11:48:02 +0200

    Add plugins panel to the dashboard

---
+++ web/modules/dashboard/includes/panels/plugins.inc.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * (c) 2016-2017 Siveo, http://www.siveo.net
+ *
+ * This file is part of Mandriva Management Console (MMC).
+ *
+ * MMC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MMC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MMC; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+include_once("modules/dashboard/includes/panel.class.php");
+
+$options = array(
+    "class" => "PluginsPanel",
+    "id" => "plugins",
+    "title" => _T("Plugins loaded", "dashboard"),
+    "enable" => true,
+);
+
+class PluginsPanel extends Panel {
+
+    function display_content() {
+        foreach($_SESSION['supportModList'] as $plugin){
+            echo '<span>'.$plugin.'</span><br/>';
+        }
+    }
+}
+
+?>
