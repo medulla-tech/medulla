c4ed889cb189be1c3a2fd451cfe54e29eb747431
Nicolas Lécureuil <neoclust@mageia.org>
2017-05-16 10:47:04 +0200

    Increase version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
 
     keywords='pulse2',
     name='pulse2', 
-    version='3.9.90',
+    version='3.9.91',
     description = 'pulse2',
     url='https://www.siveo.net/',
     packages=['pulse2'],
