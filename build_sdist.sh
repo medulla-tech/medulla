2c60e66862a0a848b708ba7722b297b77f3027c1
Nicolas Lécureuil <neoclust@mageia.org>
2017-05-23 11:09:36 +0200

    Fix version

--- build_sdist.sh
+++ build_sdist.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+VERSION='3.9.90'
+
 rm -f mmc-core-*.tar.gz mmc-core-*.tar.gz.md5
 git clean -fdx && ./autogen.sh && ./configure --sysconfdir=/etc --localstatedir=/var --disable-python-check --disable-conf && make distcheck
-tar xzvf mmc-core-3.9.90.tar.gz
-cp setup.py mmc-core-3.9.90
-tar czvf mmc-core-3.9.90.tar.gz mmc-core-3.9.90
+tar xzvf mmc-core-$VERSION.tar.gz
+cp setup.py mmc-core-$VERSION
+cp -frv debian pulse2-$VERSION
+tar czvf mmc-core-$VERSION.tar.gz mmc-core-$VERSION
 if [ $? -eq 0 ]; then
     for tarball in mmc-core-*.tar.gz; do
         md5sum $tarball > $tarball.md5
