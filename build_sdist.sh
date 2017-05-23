1d0581173a8ee63a8d80a4b84afb80d3a48a3cc2
Nicolas Lécureuil <neoclust@mageia.org>
2017-05-23 11:06:39 +0200

    Add debian folder

--- build_sdist.sh
+++ build_sdist.sh
@@ -6,6 +6,7 @@ rm -f pulse2-*.tar.gz pulse2-*.tar.gz.md5
 git clean -fdx && ./autogen.sh && ./configure --sysconfdir=/etc --localstatedir=/var --disable-python-check --disable-conf && make distcheck
 tar xzvf pulse2-$VERSION.tar.gz
 cp setup.py pulse2-$VERSION
+cp -frv debian pulse2-$VERSION
 tar czvf pulse2-$VERSION.tar.gz pulse2-$VERSION
 mv pulse2-$VERSION.tar.gz pulse2_$VERSION.orig.tar.gz
 rm -fr pulse2-$VERSION/
