bdd6e2a8a51c69896949fa5b34e7d3207e9c20e8
jfkneib <jean-francois.kneib@siveo.net>
2017-07-06 17:48:01 +0200

    chang plugin resultenddeploy

--- services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_resultenddeploy.py
+++ services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_resultenddeploy.py
@@ -5,5 +5,5 @@ import logging
 plugin = { "VERSION" : "1.0", "NAME" : "resultenddeploy", "TYPE" : "master" }
 
 def action( xmppobject, action, sessionid, data, message, ret, dataobj):
-    logging.getLogger().debug("Pulgin %s %version %s"%(plugin['NAME'], plugin['VERSION']))
+    logging.getLogger().debug("Pulgin %s %version"%(plugin))
     
