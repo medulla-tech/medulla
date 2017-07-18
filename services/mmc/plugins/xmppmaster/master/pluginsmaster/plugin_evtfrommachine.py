30470065324ae39f2e57294df3b3775eb9944885
jfkneib <jean-francois.kneib@siveo.net>
2017-07-18 18:11:00 +0200

    chang plugin check event on machine

--- services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_evtfrommachine.py
+++ services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_evtfrommachine.py
@@ -4,10 +4,12 @@ import logging
 
 plugin = { "VERSION" : "1.0", "NAME" : "evtfrommachine", "TYPE" : "master" }
 
-def action( xmppobject, action, sessionid, data, message, ret, dataobj):
-    logging.getLogger().debug("Pulgin %s %version"%(plugin))
-    try:
-        logging.getLogger().debug("EVENT %s on Machine %s"%( data['event'], data['machine']))
-    except Exception as e:
-        logging.getLogger().error("Plugin %s %version\nerror %s"%(plugin,str(e)))
 
+def action( xmppobject, action, sessionid, data, message, ret, dataobj):
+    logging.getLogger().debug(plugin)
+    if data['event'] == "SHUTDOWN_EVENT":
+        msg_changed_status = {
+            "from" : data['machine'],
+            "type" : 'unavailable'
+            }
+        xmppobject.changed_status(msg_changed_status)
