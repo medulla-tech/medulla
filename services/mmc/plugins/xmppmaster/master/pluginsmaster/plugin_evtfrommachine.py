9bf09a0d9648aa7b99507729573389a55e8e4cc1
jfkneib <jean-francois.kneib@siveo.net>
2017-07-18 12:11:34 +0200

    add plugin for log event machine windows

---
+++ services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_evtfrommachine.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8; -*-
+
+import logging
+
+plugin = { "VERSION" : "1.0", "NAME" : "evtfrommachine", "TYPE" : "master" }
+
+def action( xmppobject, action, sessionid, data, message, ret, dataobj):
+    logging.getLogger().debug("Pulgin %s %version"%(plugin))
+    try:
+        logging.getLogger().debug("EVENT %s on Machine %s"%( data['event'], data['machine']))
+    except Exception as e:
+        logging.getLogger().error("Pulgin %s %version\nerreur %s"%(plugin,str(e)))
+
