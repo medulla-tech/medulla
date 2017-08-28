694114ad55a3f1661e913e49f74d2709ed891281
spointu <spointu@gmail.com>
2017-08-28 14:54:37 +0200

    Fix missing transactions in xmppmaster database update scripts

--- services/contrib/xmppmaster/sql/schema-004.sql
+++ services/contrib/xmppmaster/sql/schema-004.sql
@@ -20,6 +20,7 @@
 -- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 -- MA 02110-1301, USA.
 
+START TRANSACTION;
 
 --
 -- Table structure for table `organization` and table `packageslist`
