049a46fee6d856f3ad76b4c34e9763758cddfa09
Nicolas Lécureuil <neoclust@mageia.org>
2017-09-17 12:05:17 +0200

    Fix typo

--- services/mmc/plugins/guacamole/__init__.py
+++ services/mmc/plugins/guacamole/__init__.py
@@ -22,7 +22,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 """
-Plugin to manage the interface with xmppmastr
+Plugin to manage the interface with xmppmaster
 """
 import logging
 import os,sys
