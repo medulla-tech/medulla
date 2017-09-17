76b21adfb5748a3c9b0d37cae2afb4d3d31315cb
Nicolas Lécureuil <neoclust@mageia.org>
2017-09-17 12:03:18 +0200

    Add licence header

--- services/mmc/plugins/guacamole/config.py
+++ services/mmc/plugins/guacamole/config.py
@@ -1,4 +1,24 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8; -*-
+#
+# (c) 2017 Siveo, http://www.siveo.net/
+#
+# $Id$
+#
+# This file is part of Management Console (MMC).
+#
+# MMC is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# MMC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MMC; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 from mmc.support.config import PluginConfig
 
