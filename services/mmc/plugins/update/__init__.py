62f066df9f995f7af8ed07bce88e12962bb5e884
Nicolas Lécureuil <neoclust@mageia.org>
2017-09-21 09:48:11 +0200

    Add pulse-xmpp-agent as file to follow for updates

--- services/mmc/plugins/update/__init__.py
+++ services/mmc/plugins/update/__init__.py
@@ -201,7 +201,7 @@ class RpcProxy(RpcProxyI):
         last_update_check_ts = None
         available_updates = []
 
-        pulse_packages_filter = "|grep -e '^python-mmc' -e '^python-pulse2' -e '^mmc-web' -e '^pulse' -e '^mmc-agent$'"
+        pulse_packages_filter = "|grep -e '^python-mmc' -e '^python-pulse2' -e '^mmc-web' -e '^pulse' -e '^mmc-agent$' -e '^pulse-xmpp-agent$'"
         install_cmd = "LANG=C dpkg -l|awk '{print $2}' %s|xargs apt-get -y install" % pulse_packages_filter
         install_cmd = "%s -l|awk '{print $1}' %s|xargs %s -i" % (self.updMgrPath, pulse_packages_filter, self.updMgrPath)
 
