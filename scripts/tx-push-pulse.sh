7ce4c44beb4769c1d44d55b2232cd7cbd6e7f600
Nicolas Lécureuil <neoclust@mageia.org>
2017-10-11 10:22:33 +0200

    Update translations - Update pot files - Add script to upload pot files on transifex server

---
+++ scripts/tx-push-pulse.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script maps PO and POT files to transifex ressources
+# on https://transifex.mandriva.com
+
+SCRIPT_PROJECT=`pwd`
+
+which tx > /dev/null
+if [ $? -ne 0 ]; then
+	echo "Install the transifex client v0.4 (pip install transifex-client==0.4)"
+	exit 1
+fi
+
+test -d .tx || tx init --host=https://fr.transifex.com/
+
+args=$@
+
+modules="dyngroup glpi imaging inventory msc pkgs pulse2 backuppc update support guacamole"
+
+for mod in $modules
+do
+	cd $SCRIPT_PROJECT/../web/modules/$mod/locale/
+	tx push -r pulse-1.${mod} -s -t -f --no-interactive
+done
