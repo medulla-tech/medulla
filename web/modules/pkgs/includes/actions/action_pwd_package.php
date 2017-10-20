f79cdb3eaefde5c26cd686758cfc5aab1e1cae82
Noël-Antoine Kneib <noel.kneib@siveo.net>
2017-10-20 17:45:18 +0200

    Add a select box for packages in action_pdw_package

--- web/modules/pkgs/includes/actions/action_pwd_package.php
+++ web/modules/pkgs/includes/actions/action_pwd_package.php
@@ -1,6 +1,12 @@
+<?php
+require_once("../xmlrpc.php");
+require_once("../../../../includes/session.inc.php");
+require_once("../../../../includes/xmlrpc.inc.php");
+?>
 <div class="header">
     <h1>Go to package folder</h1>
 </div>
+
 
 <div class="content">
     <div>
@@ -8,14 +14,30 @@
         <input type="hidden" name="actionlabel" value="<?php echo (isset($_POST['actionlabel']))? $_POST['actionlabel'] : uniqid(); ?>"/>
         <input type="hidden" name="action" value="action_pwd_package" />
 
+
         <?php
+        $packageList = xmpp_packages_list();
+        $options = "";
+
+        foreach($packageList as $id=>$package)
+        {
+            if(isset($_POST['packageuuid']) && $_POST['packageuuid'] == $package['uuid'])
+            {
+                $options .= "<option value='".$package['uuid']."' selected>".$package['name']."</option>";
+            }
+            else
+                $options .= "<option value='".$package['uuid']."'>".$package['name']."</option>";
+        }
+
         if(isset($_POST['packageuuid']))
         {
-            echo '<p><input type="checkbox" checked onclick="if(jQuery(this).is(\':checked\')){jQuery(this).next().prop(\'disabled\',false);}else{jQuery(this).next().prop(\'disabled\',true);}" />Package uuid <input type="text" value="'.$_POST['packageuuid'].'" name="packageuuid"  /></p>';
+            echo '<p><input type="checkbox" checked onclick="if(jQuery(this).is(\':checked\')){jQuery(this).next().prop(\'disabled\',false);}else{jQuery(this).next().prop(\'disabled\',true);}" />Package uuid <select name="packageuuid">'.$options.'</select></p>';
+
         }
         else{
-            echo '<p><input type="checkbox" onclick="if(jQuery(this).is(\':checked\')){jQuery(this).next().prop(\'disabled\',false);}else{jQuery(this).next().prop(\'disabled\',true);}" />Package uuid <input type="text" value="" disabled name="packageuuid"  /></p>';
+            echo '<p><input type="checkbox" onclick="if(jQuery(this).is(\':checked\')){jQuery(this).next().prop(\'disabled\',false);}else{jQuery(this).next().prop(\'disabled\',true);}" />Package uuid <select disabled name="packageuuid">'.$options.'</select></p>';
         }
+
         ?>
 
     </div>
