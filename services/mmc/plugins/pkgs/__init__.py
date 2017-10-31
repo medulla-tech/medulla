e571be125e680853e108a0618887425fb7460771
Nicolas Lécureuil <neoclust@mageia.org>
2017-10-31 09:48:16 +0100

    Use only one commit per line

--- services/mmc/plugins/pkgs/__init__.py
+++ services/mmc/plugins/pkgs/__init__.py
@@ -22,7 +22,8 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 import logging
-import os, shutil
+import os
+import shutil
 import requests, json, tempfile
 import urllib2
 from contextlib import closing
