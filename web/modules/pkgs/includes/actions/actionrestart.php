de6cc652e2106c8817e1cd2eb651ff40a85a8333
jfkneib <jean-francois.kneib@siveo.net>
2017-11-30 12:51:54 +0100

    chang restart propriety hide

--- web/modules/pkgs/includes/actions/actionrestart.php
+++ web/modules/pkgs/includes/actions/actionrestart.php
@@ -1,6 +1,9 @@
 <?php
 extract($_POST);
 $lab =  (isset($actionlabel))? $actionlabel : uniqid();
+$tableToggle =  "tableToggle".uniqid();
+$toggleable  =  "toggleable".uniqid();
+$idclass     =  "#".$tableToggle.' tr.'.$toggleable;
 ?>
 <div class="header">
     <h1>Restart</h1>
@@ -10,10 +13,10 @@ $lab =  (isset($actionlabel))? $actionlabel : uniqid();
     <div>
         <input type="hidden" name="action" value="actionrestart" />
         <input type="hidden" name="step" />
+        <table id="<?php echo $tableToggle;?>">
         <?php
-        echo'
-            <table>
-                <tr>
+        echo '<tr class="'.$toggleable.'">';
+            echo'
                     <th width="16%">Step label : </th>
                     <th width="25%">
                     <input type="text" name="actionlabel" value="'.$lab.'"/>';
@@ -29,4 +32,11 @@ $lab =  (isset($actionlabel))? $actionlabel : uniqid();
         <!-- All extra options are added here-->
     </div>
  <input class="btn btn-primary" type="button" onclick="jQuery(this).parent().parent('li').detach()" value="Delete" />
+ <input  class="btn btn-primary" id="property" onclick='jQuery("<?php echo $idclass;?>").toggle();' type="button" value="propriety" />
 </div>
+<script type="text/javascript">
+    jQuery(document).ready(function(){
+        jQuery("#<?php echo $tableToggle.' tr.'.$toggleable;?>" ).hide();
+    });
+</script>
+
--- web/modules/pkgs/includes/actions/actionrestartbot.php
+++ web/modules/pkgs/includes/actions/actionrestartbot.php
@@ -9,6 +9,9 @@
 //         """
 extract($_POST);
 $lab =  (isset($actionlabel))? $actionlabel : uniqid();
+$tableToggle =  "tableToggle".uniqid();
+$toggleable  =  "toggleable".uniqid();
+$idclass     =  "#".$tableToggle.' tr.'.$toggleable;
 ?>
 <div class="header">
     <h1>Restart Xmpp Agent</h1>
@@ -18,10 +21,10 @@ $lab =  (isset($actionlabel))? $actionlabel : uniqid();
     <div>
         <input type="hidden" name="action" value="actionrestartbot" />
         <input type="hidden" name="step" />
+        <table id="<?php echo $tableToggle;?>">
         <?php
-        echo'
-            <table>
-                <tr>
+            echo '<tr class="'.$toggleable.'">';
+            echo'
                     <th width="16%">Step label : </th>
                     <th width="25%">
                     <input type="text" name="actionlabel" value="'.$lab.'"/>';
@@ -37,4 +40,10 @@ $lab =  (isset($actionlabel))? $actionlabel : uniqid();
         <!-- All extra options are added here-->
     </div>
  <input class="btn btn-primary" type="button" onclick="jQuery(this).parent().parent('li').detach()" value="Delete" />
+ <input  class="btn btn-primary" id="property" onclick='jQuery("<?php echo $idclass;?>").toggle();' type="button" value="propriety" />
 </div>
+<script type="text/javascript">
+    jQuery(document).ready(function(){
+        jQuery("#<?php echo $tableToggle.' tr.'.$toggleable;?>" ).hide();
+    });
+</script>
