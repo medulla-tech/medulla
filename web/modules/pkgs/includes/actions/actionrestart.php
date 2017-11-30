94a80b78d0c96fd1ebc925d6ecb1f5b15dc5b2a3
jfkneib <jean-francois.kneib@siveo.net>
2017-11-30 17:25:39 +0100

    shares the "restart step" with an option (AM Agent Machine) or (MA Machine)

--- web/modules/pkgs/includes/actions/actionrestart.php
+++ web/modules/pkgs/includes/actions/actionrestart.php
@@ -1,9 +1,36 @@
 <?php
+
+// {
+//     "action": "actionrestart", 
+//     "step": 0, 
+//     "actionlabel": "bec6f486", 
+//     "targetrestart": "AM"
+// } 
 extract($_POST);
 $lab =  (isset($actionlabel))? $actionlabel : uniqid();
 $tableToggle =  "tableToggle".uniqid();
 $toggleable  =  "toggleable".uniqid();
 $idclass     =  "#".$tableToggle.' tr.'.$toggleable;
+
+
+$TargetstartList = array(
+                        array(  "Labelselect"=>"Restart Machine",
+                                "val" => "MA"),
+                        array(  "Labelselect"=>"Restart Agent Machine",
+                                "val" => "AM")
+);
+$targetrestart = (isset($targetrestart))?$targetrestart : "machine";
+$options = "";
+    foreach($TargetstartList as $obj)
+        {
+            if(isset($targetrestart) && $targetrestart == $obj['val'])
+            {
+                $options .= "<option value='".$obj['val']."' selected>".$obj['Labelselect']."</option>";
+            }
+            else{
+                $options .= "<option value='".$obj['val']."'>".$obj['Labelselect']."</option>";
+            }
+        }
 ?>
 <div class="header">
     <h1>Restart</h1>
@@ -15,6 +42,7 @@ $idclass     =  "#".$tableToggle.' tr.'.$toggleable;
         <input type="hidden" name="step" />
         <table id="<?php echo $tableToggle;?>">
         <?php
+        //---------------------label-----------------------------------
         echo '<tr class="'.$toggleable.'">';
             echo'
                     <th width="16%">Step label : </th>
@@ -24,11 +52,24 @@ $idclass     =  "#".$tableToggle.' tr.'.$toggleable;
                     </th>
                     <th></th>
                     <th></th>
-                </tr>
-                <tr>
+                </tr>';
+
+            echo '<tr class="'.$toggleable.'">';
+ 
+                echo '<td width="16%">Target Restart</td>
+                    <td width="25%">
+                        <select title="" name="targetrestart">'.$options.'</select>
+                    </td>';
+        echo '
+             </tr>
             </table>
                 ';
+        //-------------------------------------------------------------
+        
         ?>
+        
+        
+        
         <!-- All extra options are added here-->
     </div>
  <input class="btn btn-primary" type="button" onclick="jQuery(this).parent().parent('li').detach()" value="Delete" />
