7c4505216d302bfd951173ace884fd60cb2d3f43
Nicolas Lécureuil <neoclust@mageia.org>
2018-01-09 10:00:59 +0100

    Clean doc

--- doxydoc/html/search/variables_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['logger',['logger',['../classpulse2_1_1database_1_1dyngroup_1_1DyngroupDatabase.html#a0b14e488ae28d98d262453f3e9cd6e4d',1,'pulse2::database::dyngroup::DyngroupDatabase']]]
-];
--- doxydoc/html/search/variables_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['mem_5finfo',['mem_info',['../classpulse2_1_1imaging_1_1bootinventory_1_1BootInventory.html#a3df9c0e4bef549a10902c0943fa1a921',1,'pulse2::imaging::bootinventory::BootInventory']]]
-];
