ecf2a8f6d307b15d898b63a20b7b92dd31c72b56
spointu <spointu@gmail.com>
2018-02-01 13:56:35 +1300

    Update path of root used for file transfers

--- services/mmc/plugins/xmppmaster/config.py
+++ services/mmc/plugins/xmppmaster/config.py
@@ -79,8 +79,8 @@ class xmppMasterConfig(PluginConfig, XmppMasterDatabaseConfig):
         self.passwordconnexionmuc=self.get('chatroom', 'password')
 
         #######configuration bowserfile#######
-        self.defaultdir     = os.path.join("/","var","lib","pulse2", "transfert")
-        self.rootfilesystem = os.path.join("/","var","lib","pulse2","transfert")
+        self.defaultdir     = os.path.join("/","var","lib","pulse2", "file-transfer")
+        self.rootfilesystem = os.path.join("/","var","lib","pulse2","file-transfer")
         if self.has_option("bowserfile", "defaultdir"):
             self.defaultdir = self.get('Bowserfile', 'defaultdir')
         if self.has_option("bowserfile", "rootfilesystem"):
