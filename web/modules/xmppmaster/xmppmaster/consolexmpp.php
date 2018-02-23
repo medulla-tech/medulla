48511ea84832aee04796f5ee07c2da2440a5c27c
jfkneib <jean-francois.kneib@siveo.net>
2018-02-23 12:25:50 +0100

    add action manu in view console

--- web/modules/xmppmaster/xmppmaster/consolexmpp.php
+++ web/modules/xmppmaster/xmppmaster/consolexmpp.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * (c) 2016 Siveo, http://www.siveo.net
+ * (c) 2016-2018 Siveo, http://www.siveo.net
  *
  * $Id$
  *
@@ -19,104 +19,167 @@
  * You should have received a copy of the GNU General Public License
  * along with MMC; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * file : xmppmaster/consolexmpp.php
  */
 ?>
 <style type='text/css'>
-textarea        {
-width:50% ;
-height:150px;
-margin:auto; /* exemple pour centrer */
-display:block;/* pour effectivement centrer ! */
+textarea {
+    width:50% ;
+    height:150px;
+    margin:auto;   /* exemple pour centrer */
+    display:block; /* pour effectivement centrer ! */
 }
 </style>
 
-
 <?
-require("modules/base/computers/localSidebar.php");
-require("graph/navbar.inc.php");
-require_once("modules/xmppmaster/includes/xmlrpc.php");
+    require("modules/base/computers/localSidebar.php");
+    require("graph/navbar.inc.php");
+    require_once("modules/xmppmaster/includes/xmlrpc.php");
 
-$p = new PageGenerator(_T("Console", 'xmppmaster'));
-$p->setSideMenu($sidemenu);
-$p->display();
+    $uuid  = isset($_GET['objectUUID']) ? $_GET['objectUUID'] : ( isset($_POST['objectUUID']) ? $_POST['objectUUID'] : "");
+    $machine  = isset($_POST['Machine']) ? $_POST['Machine'] : "";
+    $command = isset($_POST['command']) ? $_POST['command'] : "";
+    $tab = explode("/",$machine);
+    $uiduniq = uniqid ("shellcommande");
+    //
+    if ($machine == "") 
+        $p = new PageGenerator(_T("Console Agent Relay Server", 'xmppmaster'));
+    else
+        $p = new PageGenerator(_T("Console", 'xmppmaster')." $machine");
+    
+    $p->setSideMenu($sidemenu);
+    $p->display();
 
- if (   isset($_POST['bvalid']) &&
+
+    $imss = xmlrpc_getListPresenceRelay();
+    $elt = array();
+    $elt_values = array();
+    $resultrelay = array();
+    foreach (range(0, count($imss)-1) as $i) {
+        $elt_values[$i] = $imss[$i]['jid'];
+        $elt[$i]  = $imss[$i]['type']." : ".$imss[$i]['hostname'];
+        $resultrelay[] = array( 'name' =>  $elt[$i],
+                                'jid' => $elt_values[$i]);
+    }
+
+    if (
         isset($_POST['command']) &&
         isset($_POST['Machine']) &&
         trim($_POST['Machine'])!= "" &&
         trim($_POST['command'])!= ""
         ){
-        $_POST['result']='';
-        $result = xmlrpc_runXmppCommand(trim($_POST['command']),trim($_POST['Machine']));
- }else
- {
-    $result="";
- }
-
-        $f = new ValidatingForm();
-        $f->push(new Table());
-        //$imss = xmlrpc_getListPresenceMachine();
-        $imss = xmlrpc_getListPresenceRelay();
-        
-        $elt = array();
-        $elt_values = array();
-        foreach (range(0, count($imss)-1) as $i) {
-           $elt_values[$i] =$imss[$i]['jid'];
-          $elt[$i]  =$imss[$i]['type']." : ".$imss[$i]['hostname'];
-        }
-        $imss = new SelectItem("Machine");
-        $imss->setElements($elt);
-        $imss->setElementsVal($elt_values);
-
-        $f->add(
-            new TrFormElement(_T("Select a machine", "xmppmaster"), $imss)
-        );
-
-        $e=new InputTpl('command');
-        $f->add(
-            new TrFormElement(_T("Shell command", "xmppmaster"), $e)
-        );
-        if ( isset($_POST['result'])){
-            $f->add(
-                        new TrFormElement("<br>",  new SpanElement(_T("Command result", "xmppmaster")." : ". trim($_POST['command'])))
-                    );
+            $_POST['result']='';
+            $tabdata = array(
+                                "command" => $command,
+                                "machine" => $machine,
+                                "uidunique" => $uiduniq
+            );
+            xmlrpc_runXmppCommand("plugin_asynchromeremoteshell", $machine, array($tabdata));
+            $recherche = true;
         }
 
-        $f->pop();
-        if ( isset($_POST['result'])){
-            $ear = new TextareaTpl('result');
-            $ear->setCols(190);
-            if (array_key_exists('result', $result['data'])) {
-                $f->add(
-                    new TrFormElement
-                    (
-                        "",
-                        $ear
-                    ),
-                    array("value" => htmlspecialchars($result['data']['result']))
-                );
-            }
-            else{
-                $f->add(
-                        new TrFormElement
-                        (
-                            "",
-                            $ear
-                        ),
-                        array("value" => htmlspecialchars($result['data']['msg']))
-                    );
-            }
-        }
-
-        $f->push(new Table());
-        if ($result['ret'] != 0){
-            $result['ret']=' <font color="red">'.$result['ret'].'</font>';
-        }
-
-        $f->add(
-                    new TrFormElement("",  new SpanElement(_T("Code Return : ", "xmppmaster").$result['ret']))
-                );
-                $f->addValidateButton("bvalid");
-        $f->pop();
-        $f->display();
 ?>
+
+<form method="post" id="Form">
+    <table cellspacing="0">
+    <input  type="hidden" id="machine" value="<? echo $machine; ?>" name="Machine"/>
+
+
+        <tr>
+            <td class="label" width="40%" style = "text-align: right;">
+                Select a machine
+            </td>
+            <td>
+                <select name="Machine" id="Machine">
+                    <?php
+                        foreach($resultrelay  as $dd){
+                            printf ('<option value="%s" >%s</option>', $dd['jid'], $dd['name'] );
+                        }
+                    ?>
+                </select>
+            </td>
+        </tr>
+
+        <tr>
+            <td class="label" width="40%" style = "text-align: right;">Natif Shell command</td>
+            <td>
+                <span id="container_input_command">
+                    <input value="<? echo $_POST['command']; ?>"
+                        title="<? echo _T("return key to start your order", 'xmppmaster'); ?>"
+                        name="command" 
+                        id="command" 
+                        type="text" 
+                        size="23"  
+                        placeholder=""  
+                        data-regexp="/.+/" 
+                        autocomplete="off" />
+                </span>
+            </td>
+        </tr>
+
+        <tr>
+            <td class="label" width="40%" style = "text-align: right;"><br></td>
+            <td><img id="imagewait" src="graph/ajax_loading.gif" alt="" /><span>Command result : </span><span><? echo $_POST['command']; ?></span></td>
+        </tr>
+
+        <tr>
+            <td class="label" width="40%" style = "text-align: right;">Error Code :</td>
+            <td><span id="codereturn"></span></td>
+        </tr>
+        
+        
+        
+    </table>
+    <textarea rows="15"
+              id="resultat" 
+              spellcheck="false" 
+              style = "height : 400px;
+                       background : black;
+                       color : white;
+                       FONT-SIZE : 15px;
+                       font-family : 'Courier New', Courier, monospace;
+                       border:10px solid ;
+                       padding : 15px;
+                       border-width:1px;
+                       border-radius: 25px;
+                       border-color:#FFFF00;
+                       box-shadow: 6px 6px 0px #6E6E6E;"
+    ></textarea>
+    
+    
+    <!--<button class="btn btn-small">submit</button>-->
+</form>
+
+
+<script type="text/javascript">
+    jQuery( document ).ready(function() {
+
+    jQuery( "#imagewait" ).hide();
+    calldata = function() {
+        jQuery.get( "modules/xmppmaster/xmppmaster/xmppremotecmdshell.php",{
+                        "command" : jQuery( "#command" ).val(),
+                        "machine" : jQuery( "#machine" ).val(),
+                        "uidunique" : "<? echo $uiduniq; ?>"
+                    },
+                    function( data ) {
+                        if(data['stop'] == true){
+                            jQuery( "#resultat" ).text( data['result'] );
+                            jQuery( "#codereturn" ).text( data['codereturn'] );
+                            jQuery( "#imagewait" ).hide();
+                        }else
+                        {
+                            setTimeout(calldata, 5000)
+                        }
+                    });
+}
+<?php
+    if ($recherche == true){
+        echo "var uniqid = \"".$uiduniq."\";";
+        echo "var recherche = true;";
+        echo "var x = setTimeout(calldata, 2000);";
+        echo 'jQuery( "#imagewait" ).show();';
+    }
+?>
+    });
+</script>
+
