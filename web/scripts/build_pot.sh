27adbcf7a51965dc73b8c152fcb9bd5284389f49
Nicolas Lécureuil <neoclust@mageia.org>
2018-02-27 17:15:57 +0100

    (i18n) Fix bashism

--- web/scripts/build_pot.sh
+++ web/scripts/build_pot.sh
@@ -34,7 +34,7 @@ for module in base ppolicy services dashboard report xmppmaster; do
     fi
     find $fpath -iname "*.php" -exec xgettext -C -j -o ${POT} --language=PHP --keyword=${keyword} {} \;
     # Build only the POT files
-    for name in `find modules/${module}/locale -type f -name *.po`; do
+    for name in $(find modules/${module}/locale -type f -name *.po); do
         echo -n "updating ${name}..."
         msgmerge --update --add-location --sort-output ${name} ${POT}
         echo "done"
