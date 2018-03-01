4e15d8cf9363aecf4c66723b99014b10ad855b9b
Nicolas Lécureuil <neoclust@mageia.org>
2018-03-01 23:13:56 +0100

    pulse: bump version to 4.3

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
 
     keywords='pulse2',
     name='pulse2', 
-    version='4.2',
+    version='4.3',
     debian_distro='stretch',
     description = 'pulse2',
     url='https://www.siveo.net/',
