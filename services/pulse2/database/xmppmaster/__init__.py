35321d5effe8807d67e9b8e5ac5904196ad60811
Noël Kneib <noel.kneib@siveo.fr>
2018-05-15 10:30:23 +0200

    (kiosk) Add a function to get the ou_machine and ou_user for the logged computers.

--- services/pulse2/database/xmppmaster/__init__.py
+++ services/pulse2/database/xmppmaster/__init__.py
@@ -2553,3 +2553,25 @@ class XmppMasterDatabase(DatabaseHelper):
         session.flush()
         return [x for x in result]
 
+    @DatabaseHelper._sessionm
+    def get_machine_ad_infos(self, session, uuid_inventory):
+    """
+    Select the founded OUs of the logged machine.
+    Param:
+        uuid_inventory: str. This param is the uuid of the inventory of the machine received by xmpp.
+
+    Returns:
+        List of tuple. The tuple contains all the ou_machine and ou_user founded.
+    """
+        sql = """
+        SELECT
+            ad_ou_machine, ad_ou_user
+        FROM
+            machines
+        WHERE
+            uuid_inventorymachine = '%s';"""%(uuid_inventory)
+
+        result = session.execute(sql)
+        session.commit()
+        session.flush()
+        return [x for x in result]
