c53bf7accbf4fcb9c8e9ed14206cd8bf2753479c
jfkneib <jean-francois.kneib@siveo.net>
2018-06-07 15:34:03 +0200

    Add callremoteXmppMonitoring

--- services/mmc/plugins/xmppmaster/master/agentmaster.py
+++ services/mmc/plugins/xmppmaster/master/agentmaster.py
@@ -131,6 +131,11 @@ def callremotecommandshell(jidmachine, command="", timeout=10):
                                                  "data": command,
                                                  "timeout" : timeout}, timeout)
 
+def callremoteXmppMonitoring( jidmachine, suject,  timeout=10):
+    return ObjectXmpp().iqsendpulse(jidmachine, {"action" : "remotexmppmonitoring",
+                                                 "data": suject,
+                                                 "timeout" : timeout}, timeout)
+
 def calllocalfile(currentdir=""):
     return ObjectXmpp().xmppbrowsingpath.listfileindir(currentdir)
 
