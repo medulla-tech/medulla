bb1ae42b87c043d7fc56aee1a1aa8bc4ac7cfb5b
jfkneib <jean-francois.kneib@siveo.net>
2018-06-12 16:50:26 +0200

    plugin gestion remote QA

---
+++ services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_asynchroremoteQA.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; -*-
+#
+# (c) 2016-2017 siveo, http://www.siveo.net
+#
+# This file is part of Pulse 2, http://www.siveo.net
+#
+# Pulse 2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pulse 2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pulse 2; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# file pluginsmaster/plugin_asynchromeremoteshell.py
+
+import logging
+import traceback
+import sys
+import json
+
+plugin = { "VERSION" : "1.0", "NAME" : "asynchroremoteQA", "TYPE" : "master" }
+
+def action( xmppobject, action, sessionid, data, message, ret, dataobj):
+    logging.getLogger().debug("=====================================================")
+    logging.getLogger().debug(plugin)
+    logging.getLogger().debug("=====================================================")
+    print json.dumps(data, indent=4)
+    datasend={
+        "action" : action,
+        "sessionid" : sessionid,
+        "data" : data,
+        'base64' : False }
+
+    # call plugin asynchromeremoteshell to machine or relay
+    xmppobject.send_message( mto = data['data']['jid'],
+                             mbody = json.dumps(datasend),
+                             mtype = 'chat')
