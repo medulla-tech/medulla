d81490c9e49d686b039a018c39ed09c8eb8c3705
jfkneib <jean-francois.kneib@siveo.net>
2018-06-13 17:49:38 +0200

    Custom Quick Action for group

--- web/modules/xmppmaster/xmppmaster/ActionQuickGroup.php
+++ web/modules/xmppmaster/xmppmaster/ActionQuickGroup.php
@@ -30,15 +30,94 @@ require("modules/base/computers/localSidebar.php");
 require("graph/navbar.inc.php");
 require_once("modules/xmppmaster/includes/xmlrpc.php");
 
-
-    $ajax = new AjaxFilter(urlStrRedirect("xmppmaster/xmppmaster/ajaxGroupactionquick"));
-
-    $p = new PageGenerator(_T("Action Quick Group", 'xmppmaster'));
-    $p->setSideMenu($sidemenu);
-    $p->display();
+global $conf;
+$maxperpage = $conf["global"]["maxperpage"];
+$filter  = isset($_GET['filter'])?$_GET['filter']:"";
+$start = isset($_GET['start'])?$_GET['start']:0;
+$end   = (isset($_GET['end'])?$_GET['end']:$maxperpage-1);
 
 $ajax->setRefresh(20000);
 $ajax->display();
 $ajax->displayDivToUpdate();
+
+$p = new PageGenerator(_T("Action Quick Group", 'xmppmaster'));
+$p->setSideMenu($sidemenu);
+$p->display();
+
+$machinegroup = xmlrpc_getCommand_action_time(10000, $start, $end, $filter);
+$dd = $machinegroup['result'];
+$nbitem = $machinegroup['nbtotal'];
+// echo "<pre>";
+// print_r($dd);
+// echo "</pre>";
+$groupormachine = array();
+foreach ($dd[4] as $val ){
+    if ($val != "") $groupormachine[] = "Grp";else $groupormachine[] = "Mach";
+}
+
+$startdate =  array();
+foreach ($dd[5] as $val ){
+    $startdate[] = date('Y-m-d H:i:s', $val->timestamp);
+}
+$machinetarget =  array();
+foreach ($dd[7] as $val ){
+    $target = getRestrictedComputersList(0, -1, array('uuid' => $val), False);
+    $machine = $target[$val][1];
+    $namemachine = $machine['cn'][0];
+    $usermachine = $machine['user'][0];
+    $machinetarget[] = $namemachine;
+}
+
+
+$resultmachine = new ActionItem(_T("Os", "xmppmaster"),"QAcustommachgrp","logfile","computer", "xmppmaster", "xmppmaster");
+
+$listnamegroup=[];
+$params =  array();
+for ($i=0;$i< count( $dd[0] );$i++){
+    $param=array();
+    $param['cmd_id'] = $dd[0][$i];
+    $param['gid']    = $dd[5][$i];
+    $param['uuid']   = $dd[7][$i];
+    if($param['gid'] != ""){
+        $gr = xmlCall("dyngroup.get_group", array($param['gid'], false, false));
+        $listnamegroup[] = "Grp : ". $gr['name'];
+        $param['groupname'] = $_GET["groupname"];
+        $param['machname'] =  "";
+    }
+    else{
+        $param['groupname'] = "";
+        $machinelist = getRestrictedComputersList(0, -1, array('uuid' => $param['uuid']), False);
+        $machine = $machinelist[$_GET['uuid']][1];
+        $namemachine = $machine['cn'][0];
+        $usermachine = $machine['user'][0];
+        $listnamegroup[] = "Mach : ".$namemachine;
+        $param['machname'] =  $namemachine;
+    }
+    $param['login']  = $dd[2][$i];
+    $param['os']     = $dd[3][$i];
+    $param['date']   = $startdate[$i];
+    $param['namecmd'] = $_GET["namecmd"];
+    $logs[] = $resultmachine;
+    $params[] = $param;
+}
+
+$n = new OptimizedListInfos( $dd[1], _T("Custom Command", "xmppmaster"));
+$n->setCssClass("package");
+$n->disableFirstColumnActionLink();
+$n->addExtraInfo( $dd[3], _T("Os", "xmppmaster"));
+$n->addExtraInfo( $startdate, _T("Start date", "xmppmaster"));
+$n->addExtraInfo( $dd[2], _T("User", "xmppmaster"));
+$n->addExtraInfo( $listnamegroup, _T("Type", "xmppmaster"));
+$n->addExtraInfo( $machinetarget, _T("Computer", "xmppmaster"));
+$n->addExtraInfo( $dd[0], _T("Commandid", "xmppmaster"));
+$n->setTableHeaderPadding(1);
+$n->setItemCount($nbitem);
+$n->setNavBar(new AjaxNavBar($nbitem, $filter));
+$n->addActionItemArray($logs);
+$n->setParamInfo($params);
+$n->start = 0;
+$n->end = $nbitem;
+$n->display();
+echo "<br>";
 
 ?>
