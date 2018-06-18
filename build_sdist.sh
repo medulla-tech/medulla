e5a742f7b63e148136b2d2d60ce8b7f70414e5c3
Nicolas Lécureuil <neoclust@mageia.org>
2018-06-18 16:15:04 +0200

    (packaging) Fix version

--- build_sdist.sh
+++ build_sdist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION='4.3'
+VERSION='4.3.1'
 
 rm -f pulse2-*.tar.gz pulse2-*.tar.gz.md5
 git clean -fdx && ./autogen.sh && ./configure --sysconfdir=/etc --localstatedir=/var --disable-python-check --disable-conf && make distcheck
