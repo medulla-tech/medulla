c9e3e63edee7881b1cfec1c9dc83f745e4b5fb0c
Nicolas Lécureuil <neoclust@mageia.org>
2018-06-21 16:24:07 +0200

    core: bump version to 4.5

--- agent/mmc/agent.py
+++ agent/mmc/agent.py
@@ -65,7 +65,7 @@ sys.path.append("plugins")
 
 Fault = xmlrpclib.Fault
 ctx = None
-VERSION = "4.4"
+VERSION = "4.5"
 
 
 class MmcServer(xmlrpc.XMLRPC, object):
--- agent/mmc/plugins/services/__init__.py
+++ agent/mmc/plugins/services/__init__.py
@@ -34,7 +34,7 @@ from mmc.plugins.services.config import ServicesConfig
 
 logger = logging.getLogger()
 
-VERSION = "4.4"
+VERSION = "4.5"
 APIVERSION = "0:1:0"
 REVISION = ""
 
