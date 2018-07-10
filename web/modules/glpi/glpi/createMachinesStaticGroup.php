32732453131ca30b669fe3ea06e7aa96b8b6ce82
Noël Kneib <noel.kneib@siveo.net>
2018-07-10 19:35:24 +0200

    (dashboard) Improve the creation of the groups for machineOnline widget.
    When the group is composed by a big amount of machines, the group takes several seconds to create it.

--- web/modules/glpi/glpi/createMachinesStaticGroup.php
+++ web/modules/glpi/glpi/createMachinesStaticGroup.php
@@ -23,15 +23,27 @@ require_once("modules/dyngroup/includes/dyngroup.php"); // For Group Class
 require_once("modules/glpi/includes/xmlrpc.php"); // For total_machines
 require_once("modules/xmppmaster/includes/xmlrpc.php"); // For machines_online
 require_once("modules/dyngroup/includes/xmlrpc.php");
+require_once("modules/glpi/includes/xmlrpc.php"); // For xmlrpc_getListPresenceMachine
 
+$t0 = microtime(true);
 $machines_online = xmlrpc_getListPresenceMachine();
 $uuids_online = [];
-$all_machines = getComputersList();
-$total_machines = count($all_machines);
+$t1 = microtime(true);
+$d01 = $t1 - $t0;
+echo '<p> get online machines : '.$d01.'</p>';
+
+$t0 = microtime(true);
+$all_machines = xmlrpc_get_all_uuids_and_hostnames();
+$t1 = microtime(true);
+$d01 = $t1 - $t0;
+echo '<p> get all machines : '.$d01.'</p>';
+
+
 $machines_offline = [];
 
 $list = [];
 
+$t0 = microtime(true);
 // Get the uuids of the machines
 foreach($machines_online as $machine)
 {
@@ -42,27 +54,34 @@ foreach($machines_online as $machine)
     else
         unset($machine);
 }
+$t1 = microtime(true);
+$d01 = $t1 - $t0;
+echo '<p> get online machines uuids : '.$d01.'</p>';
 
 // Clean the array
 $machines_online = [];
 
+$t0 = microtime(true);
 // Create the offline an online machines lists
 foreach($all_machines as $machine)
 {
     $tmp = [];
 
     // Create formated machine
-    $tmp[$machine[1]["objectUUID"][0].'##'.$machine[1]["cn"][0]] = ["hostname" => $machine[1]["cn"][0], 'uuid' =>$machine[1]["objectUUID"][0]];
-
-    if(!in_array($machine[1]['objectUUID'][0],$uuids_online))
+    //$tmp[$machine[1]["objectUUID"][0].'##'.$machine[1]["cn"][0]] = ["hostname" => $machine[1]["cn"][0], 'uuid' =>$machine[1]["objectUUID"][0]];
+    $tmp[$machine["uuid"].'##'.$machine["hostname"]] = ["hostname" => $machine["hostname"], 'uuid' =>$machine["uuid"]];
+    if(!in_array($machine["uuid"],$uuids_online))
         $machines_offline += $tmp;
 
     else
         $machines_online += $tmp;
 }
+$t1 = microtime(true);
+$d01 = $t1 - $t0;
+echo '<p> get the machine lists : '.$d01.'</p>';
+
 
 if($_GET['machines'] == 'online'){
-
     $groupname = sprintf (_T("Machines online at %s", "glpi"), date("Y-m-d H:i:s"));
     $groupmembers = $machines_online;
 }
@@ -73,10 +92,13 @@ else {
     $groupmembers = $machines_offline;
 }
 
+$t0 = microtime(true);
 $group = new Group();
 $group->create($groupname, False);
 $group->addMembers($groupmembers);
-
+$t1 = microtime(true);
+$d01 = $t1 - $t0;
+echo '<p> add the list to the group : '.$d01.'</p>';
 $truncate_limit = getMaxElementsForStaticList();
 if ($truncate_limit == count($groupmembers)) new NotifyWidgetWarning(sprintf(_T("Computers list has been truncated at %d computers", "dyngroup"), $truncate_limit));
 
