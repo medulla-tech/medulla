28627c971f85255c1a8d35ba5fb14a58e23d74e4
jfkneib <jean-francois.kneib@siveo.net>
2018-07-18 17:05:37 +0200

    (feature) Agent auto update The agent now updates itself like we already do for the plugins. So no more deployement needed

--- services/mmc/plugins/xmppmaster/master/lib/update_remote_agent.py
+++ services/mmc/plugins/xmppmaster/master/lib/update_remote_agent.py
@@ -26,21 +26,34 @@ import logging
 from utils import file_get_contents
 import json
 
+
+
+
 class Update_Remote_Agent:
     """
         this class make finderspring of agent.
-        Param : diragentbase is location of agent
+        Param : dir_agent_base is location of agent
         Param : autoupdate is switch for enable or disable remote update. 
     """
-    def __init__(self, diragentbase, autoupdate = True):
-        self.diragentbase = diragentbase
+    def __init__(self, dir_agent_base, autoupdate = True):
+        self.dir_agent_base = dir_agent_base
         self.autoupdate = autoupdate
-        #verify exist dir
-        if not os.path.exists(diragentbase):
-            os.makedirs(diragentbase)
-            os.chmod(diragentbase, 0777)
-            logging.getLogger().debug("Creating folder for remote base agent : %s"%diragentbase)
-        self.load_list_md5_agentbase()
+        self.directory = {  "program_agent" : {},
+                            "version" : "",
+                            "version_agent" : "",
+                            "lib_agent" : {},
+                            "script_agent" : {},
+                            "fingerprint" : ""}
+        #verify exist dir and create si not exit. The default mode is 0777 (octal)
+        dir_create = [dir_agent_base,
+                      os.path.join(dir_agent_base, 'lib'),
+                      os.path.join(dir_agent_base, 'script')]
+        for path_dir_remoteagent in dir_create:
+            if not os.path.exists(path_dir_remoteagent):
+                os.makedirs(path_dir_remoteagent)
+                logging.getLogger().debug("Creating folder for remote base agent : %s"%dir_agent_base)
+        if os.path.exists(os.path.join(dir_agent_base,'agentversion')):
+            self.load_list_md5_agentbase()
 
     def get_md5_descriptor_agent(self):
         return self.directory
@@ -59,17 +72,20 @@ class Update_Remote_Agent:
                             "lib_agent" : {},
                             "script_agent" : {},
                             "fingerprint" : ""}
-        self.directory["version"] = file_get_contents( os.path.join(self.diragentbase,'agentversion'))
+        self.directory["version"] = file_get_contents( os.path.join(self.dir_agent_base,'agentversion'))
         self.directory["version_agent"] = hashlib.md5(self.directory["version"]).hexdigest()
         listmd5.append(self.directory["version_agent"])
-        for fichiername in [ x for x in os.listdir(self.diragentbase) if x[-3:]== ".py"]:
-            self.directory["program_agent"][fichiername] = hashlib.md5(file_get_contents( os.path.join(self.diragentbase,fichiername))).hexdigest()
+        list_script_python_for_update = ['agentxmpp.py', 'launcher.py', 'connectionagent.py']
+
+        #for fichiername in [ x for x in os.listdir(self.dir_agent_base) if x[-3:]== ".py"]:
+        for fichiername in list_script_python_for_update:
+            self.directory["program_agent"][fichiername] = hashlib.md5(file_get_contents( os.path.join(self.dir_agent_base,fichiername))).hexdigest()
             listmd5.append(self.directory["program_agent"][fichiername])
-        for fichiername in [ x for x in os.listdir(os.path.join(self.diragentbase, 'lib')) if x[-3:]== ".py"]:
-            self.directory["lib_agent"][fichiername]= hashlib.md5(file_get_contents( os.path.join(self.diragentbase,'lib',fichiername))).hexdigest()
+        for fichiername in [ x for x in os.listdir(os.path.join(self.dir_agent_base, 'lib')) if x[-3:]== ".py"]:
+            self.directory["lib_agent"][fichiername]= hashlib.md5(file_get_contents( os.path.join(self.dir_agent_base,'lib',fichiername))).hexdigest()
             listmd5.append(self.directory["lib_agent"][fichiername])
-        for fichiername in [ x for x in os.listdir(os.path.join(self.diragentbase, 'script')) if x[-3:]== ".py"]:
-            self.directory["script_agent"][fichiername]= hashlib.md5(file_get_contents( os.path.join(self.diragentbase,'script',fichiername))).hexdigest()
+        for fichiername in [ x for x in os.listdir(os.path.join(self.dir_agent_base, 'script')) if x[-3:]== ".py"]:
+            self.directory["script_agent"][fichiername]= hashlib.md5(file_get_contents( os.path.join(self.dir_agent_base,'script',fichiername))).hexdigest()
             listmd5.append(self.directory["script_agent"][fichiername])
         listmd5.sort()
         self.directory["fingerprint"]=hashlib.md5(json.dumps(listmd5)).hexdigest()
---
+++ services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_resultupdateagent.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; -*-
+#
+# (c) 2016-2018 siveo, http://www.siveo.net
+#
+# This file is part of Pulse 2, http://www.siveo.net
+#
+# Pulse 2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pulse 2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pulse 2; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# file pluginsmaster /pluginsmaster/plugin_resultupdateagent.py
+
+import base64
+import json
+import os
+import logging
+import zlib
+from time import sleep
+from utils import name_random, file_put_contents, file_get_contents
+
+
+logger = logging.getLogger()
+
+plugin = { "VERSION" : "1.0", "NAME" : "resultupdateagent", "TYPE" : "master" }
+
+def action( xmppobject, action, sessionid, data, message, ret, dataobj):
+    logger.debug("#################################################")
+    logger.debug(plugin)
+    logger.debug(json.dumps(data, indent = 4))
+    logger.debug("#################################################")
+    if "subaction" in data :
+        if data['subaction'] == "update_me":
+            #load version agent agentversion
+            version = file_get_contents(os.path.join(xmppobject.config.diragentbase,"agentversion"))
+            if 'descriptoragent' in data:
+                if 'program_agent' in data['descriptoragent'] and len(data['descriptoragent']['program_agent']) != 0:
+                    logger.debug("Update program script in remote agent [%s]"%message['from'])
+                    for script_program_file in data['descriptoragent']['program_agent']:
+                        logger.debug("\t- Update program script [%s]"%(script_program_file))
+                        sleep(2)
+                        load_and_send_remote_agent_file(xmppobject,message['from'], script_program_file, "program_agent", version)
+                if 'lib_agent' in data['descriptoragent'] and len(data['descriptoragent']['lib_agent']) != 0:
+                    logger.debug("Update lib script in remote agent [%s]"%message['from'])
+                    for script_lib_file in data['descriptoragent']['lib_agent']:
+                        logger.debug("\t- Update lib script [%s]"%(script_lib_file))
+                        sleep(2)
+                        load_and_send_remote_agent_file(xmppobject, message['from'], script_lib_file, "lib_agent", version)
+                if 'script_agent' in data['descriptoragent'] and len(data['descriptoragent']['script_agent']) != 0:
+                    logger.debug("Update script in remote agent [%s]"%message['from'])
+                    for script_script_file in data['descriptoragent']['script_agent']:
+                        logger.debug("\t- Update script [%s]"%(script_script_file))
+                        sleep(2)
+                        load_and_send_remote_agent_file(xmppobject, message['from'], script_script_file, "script_agent", version)
+
+def load_and_send_remote_agent_file(xmppobject, jid, filename, type, version):
+        msg_script_file_to_remote_agent = { "action" : "updateagent",
+                                            "sessionid" : name_random(3, "update_script_agent") ,
+                                            "data" : {"version" : version} }
+        if type == "program_agent":
+            namescriptfile = os.path.join( xmppobject.config.diragentbase, filename )
+        elif type == "lib_agent":
+            namescriptfile = os.path.join( xmppobject.config.diragentbase,"lib", filename )
+        elif type == "script_agent":
+            namescriptfile = os.path.join( xmppobject.config.diragentbase,"script", filename )
+        else:
+            logger.error("script type incorect for transfert script to update remote agent")
+            return
+        if os.path.isfile(namescriptfile):
+            logger.debug("File script found %s" % namescriptfile)
+        else:
+            logger.error("File script missing %s" % namescriptfile)
+            return
+        try:
+            # lit contenue binaire du fichier a installer
+            filescript = open(namescriptfile, "rb")
+            datacontentbinary = filescript.read()
+            #compress puis encode en base64
+            content = base64.b64encode(zlib.compress(datacontentbinary, 9))
+            filescript.close()
+        except Exception as e:
+            logger.error(str(e))
+            traceback.print_exc(file=sys.stdout)
+            return
+
+        msg_script_file_to_remote_agent['data'] = {}
+        msg_script_file_to_remote_agent['data']['subaction'] = 'install_%s'%type
+        msg_script_file_to_remote_agent['data']['content'] = content
+        msg_script_file_to_remote_agent['data']['namescript'] = filename
+        msg_script_file_to_remote_agent['base64'] = False #only seul content of data is base 64
+        try:
+             xmppobject.send_message(mto=jid,
+                              mbody=json.dumps(msg_script_file_to_remote_agent),
+                              mtype='chat')
+        except:
+            traceback.print_exc(file=sys.stdout)
