007b4ca630323674d6b0ba9fb2805497acb73754
jfkneib <jean-francois.kneib@siveo.net>
2018-07-18 15:46:29 +0200

    get machine by entities.

--- web/modules/dyngroup/dyngroup/add_groups.php
+++ web/modules/dyngroup/dyngroup/add_groups.php
@@ -263,7 +263,10 @@ if (isset($_POST["bdelmachine_x"])) {
     if (!$listOfMembers) { $listOfMembers = array(); }
 
     $truncate_limit = getMaxElementsForStaticList();
-    $listOfMachines = getRestrictedComputersList(0, $truncate_limit, array('get'=>array('cn', 'objectUUID'), 'imaging_server'=>$imaging_server), False);
+    //$listOfMachines = getRestrictedComputersList(0, $truncate_limit, array('get'=>array('cn', 'objectUUID'), 'imaging_server'=>$imaging_server), False);
+    $listOfMachines = getMachineforentityList(0, $truncate_limit, array('get'=>array('cn', 'objectUUID'), 
+                                                    'imaging_server'=>$imaging_server,
+                                                    'fk_entity' => $entitieval));
     $count = getRestrictedComputersListLen(array('imaging_server'=>$imaging_server));
     if ($truncate_limit < $count) {
         new NotifyWidgetWarning(sprintf(_T("Computers list has been truncated at %d computers", "dyngroup"), $truncate_limit));
