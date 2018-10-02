0ae66f52f789e0fdc71028a7d79155f3220c52c7
Botheis <noel.kneib@siveo.net>
2018-10-02 09:56:48 +0200

    Typo remove useless spaces

--- web/modules/xmppmaster/xmppmaster/deployquickgroup.php
+++ web/modules/xmppmaster/xmppmaster/deployquickgroup.php
@@ -51,7 +51,7 @@ input[type="text"] {
     $nbr_presense = 0;
     foreach($list as $key =>$value){
         if( xmlrpc_getPresenceuuid($key) != 0 ){
-            $nbr_presense ++; 
+            $nbr_presense ++;
         }
     }
     $nbr_absent = $count - $nbr_presense;
@@ -59,7 +59,7 @@ input[type="text"] {
 
 
         <div style="width : 600px;">
-        <? 
+        <?
             echo "<h1>Quick Actions group</h1>";
             echo "<h2>groupName ".$grouptype." : ".$_GET['groupname']."</h2>";
             echo "<h3>".$count." machine in group ".$_GET['groupname']."</h3>";
@@ -84,7 +84,7 @@ input[type="text"] {
 //             datasets: [{
 //                 data: [10, 20, 30]
 //             }],
-// 
+//
 //             // These labels appear in the legend and in the tooltips when hovering different arcs
 //             labels: [
 //                 'Red',
@@ -331,7 +331,7 @@ input[type="text"] {
     }
 
     jQuery('#wol,#wol0').unbind().on('click', function(){
-        groupinfo['wol'] = jQuery('#checkboxwol').is(':checked'); 
+        groupinfo['wol'] = jQuery('#checkboxwol').is(':checked');
         jQuery.get( "modules/xmppmaster/xmppmaster/actionwakeonlan.php", groupinfo )
             .done(function( data ) {
                 wol(data)
