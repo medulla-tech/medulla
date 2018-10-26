ccd359cb5a45ec609af5cf7d2e9c820c139d4b44
Nicolas Lécureuil <neoclust@mageia.org>
2018-10-26 17:16:45 +0200

    (i18n) Specify the catalog to use

--- web/modules/dashboard/includes/panels/backup.inc.php
+++ web/modules/dashboard/includes/panels/backup.inc.php
@@ -37,8 +37,8 @@ class BackupPanel extends Panel {
         $machines_backup = get_count_of_backuped_hosts();
         $machines_not_backup = $total_machines - $machines_backup;
         echo _T("Total machines : ") .$total_machines.'<br/>';
-        echo '<span style="color:green">' . _T("Backup configured : ") . $machines_backup.'</span><a href="'.$urlRedirect.'&backup=yes"><img title="'._T("Create a group","dashboard").'" style="height: 10px; padding-left: 3px;" src="img/machines/icn_machinesList.gif" /></a><br/>';
-        echo '<span style="color:red">' . _T("Backup not configured : ") .$machines_not_backup.'</span><a href="'.$urlRedirect.'&backup=no"><img title="'._T("Create a group","dashboard").'" style="height: 10px; padding-left: 3px;" src="img/machines/icn_machinesList.gif" /></a><br/>';
+        echo '<span style="color:green">' . _T("Backup configured : ", "dashboard") . $machines_backup.'</span><a href="'.$urlRedirect.'&backup=yes"><img title="'._T("Create a group","dashboard").'" style="height: 10px; padding-left: 3px;" src="img/machines/icn_machinesList.gif" /></a><br/>';
+        echo '<span style="color:red">' . _T("Backup not configured : ", "dashboard") .$machines_not_backup.'</span><a href="'.$urlRedirect.'&backup=no"><img title="'._T("Create a group","dashboard").'" style="height: 10px; padding-left: 3px;" src="img/machines/icn_machinesList.gif" /></a><br/>';
     }
 }
 ?>
