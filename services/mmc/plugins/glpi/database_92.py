a6fbe380f2e1fc93cd6f99e4c95a7f46e0d192e7
jfkneib <jean-francois.kneib@siveo.net>
2018-11-05 14:23:17 +0100

    fix call function dynamic group

--- services/mmc/plugins/glpi/database_92.py
+++ services/mmc/plugins/glpi/database_92.py
@@ -566,7 +566,7 @@ class Glpi92(DyngroupDatabaseHelper):
         elif "query" in filt and filt['query'][0] == "AND":
             for q in filt['query'][1]:
                 if q[2] == "Online computer" or q[2] == "OU user" or q[2] == "OU machine":
-                    listid = XmppMasterDatabase().__getxmppmasterfilterforglpi(q)
+                    listid = XmppMasterDatabase().getxmppmasterfilterforglpi(q)
                     ret[q[2]] = [q[1], q[2], q[3], listid]
         return ret
 
