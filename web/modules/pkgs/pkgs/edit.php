7f3e4882dcbd2a4aa82b2185cc41f1bb1e0b8133
spointu <spointu@gmail.com>
2018-11-12 17:37:53 +0800

    Revert "pushrysnch choose by default"
    This reverts commit e356afc650a4b527b233f9c605e4cc897637bc07.

--- web/modules/pkgs/pkgs/edit.php
+++ web/modules/pkgs/pkgs/edit.php
@@ -404,8 +404,8 @@ if(isExpertMode())
         $setmethodetransfert = 'pushrsync';
     }
     $methodtransfer = new SelectItem('methodetransfert');
-    $methodtransfer->setElements(['pushrsync', 'pullcurl']);
-    $methodtransfer->setElementsVal(['pushrsync', 'pullcurl']);
+    $methodtransfer->setElements(['pullcurl','pushrsync']);
+    $methodtransfer->setElementsVal(['pullcurl','pushrsync']);
     $methodtransfer->setSelected($setmethodetransfert);
 
     $f->add(new TrFormElement(_T('Transfer method','pkgs'),$methodtransfer, ['trid'=>'trTransfermethod']),['value'=>'']);
