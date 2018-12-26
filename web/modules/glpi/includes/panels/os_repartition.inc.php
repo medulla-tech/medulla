808258909a03dd937fd9962e0a31a1096cc07975
Noël <noel.kneib@siveo.net>
2018-12-26 12:04:48 +0100

    Use d3pie.js lib instead of raphael.js lib
    Moreover the results are more merged and classed by name and version

--- web/modules/glpi/includes/panels/os_repartition.inc.php
+++ web/modules/glpi/includes/panels/os_repartition.inc.php
@@ -25,7 +25,12 @@ require_once("modules/pulse2/includes/locations_xmlrpc.inc.php");
 include_once("modules/dashboard/includes/panel.class.php");
 require_once("modules/base/includes/computers.inc.php");
 require_once("modules/glpi/includes/xmlrpc.php");
+?>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/5.7.0/d3.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/d3pie@0.2.1/d3pie/d3pie.min.js"></script>
+
+<?php
 $options = array(
   "class" => "os_repartitionPanel",
   "id" => "osrepartition",
@@ -53,92 +58,84 @@ class os_repartitionPanel extends Panel {
       {
         unset($os);
       }
-      $osLabels[] = $os['os'].' '.$os['version'].' ('.$os['count'].')';
+      $osLabels[] = $os['os'].' '.$os['version'];
       $osCount[] = (int)$os['count'];
 
       $links[] = $urlRedirect.'&os='.$os['os'].'&version='.$os['version'];
     }
 
-    echo 'total : '.$cumul.'<br />';
     $osLabels = json_encode(array_values($osLabels));
     $osCount = json_encode(array_values($osCount));
     $links = json_encode(array_values($links));
 
     $createGroupText = json_encode(_T("Create a group", "glpi"));
         echo <<< SPACE
-        <div id="os-graphs"></div>
-        <script type="text/javascript">
-            var r = Raphael("os-graphs"),
-                radius = 70,
-                margin = 100,
-                x = 75,
-                y = 75;
 
-            var data = $osCount,
-                createGroupText = $createGroupText,
-                legend = $osLabels,
-                //Add "#000-color-color" into the colors variable if all the os are not displayed
-                //colors = ["000-#000000-#666665","000-#73d216-#42780D","000-#ef2929-#A31A1A","000-#003399-#0251ED","000-#7e1282-#c98fcb","000-#b36919-#e8c6a2","000-#2eb9f3-#4297ba","000-#168eff-#28c96c","000-#a9751a-#cdbda1","000-#72ed62-#72ed62","000-#000000-#666665","000-#000000-#666665","000-#000000-#666665"],
-                href = $links,
-                title = 'OS Repartition';
 
-            /*r.text(5, y - radius - 10, title)
-             .attr({ font: "12px sans-serif" })
-             .attr({ "text-anchor": "start" });*/
-            data = getPercentageData(data);
+        <div id="pieChart"></div>
+        <ul id="os-list"></ul>
+        <script>
 
-            pie = r.piechart(x, y + 5, radius, data,
-                       {//colors: colors,
-                       legend:$osLabels,
-                       href:$links,
-                       legendpos: "south"})
-             .hover(function () {
-                this.sector.stop();
-                this.sector.animate({ transform: 's1.1 1.1 ' + this.cx + ' ' + this.cy }, 800, "elastic");
+jQuery(function(){
 
-                if (this.label) {
-                    this.label[0].stop();
-                    this.label[0].attr({ r: 7.5 });
-                    this.label[1].attr({ "font-weight": 800 });
-                }
-                //jQuery('#os-graphs ul:first').find('li')
-             }, function () {
-                this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 800, "elastic");
+  var labels = $osLabels;
+  var count = $osCount;
+  var links = $links;
 
-                if (this.label) {
-                    this.label[0].animate({ r: 5 }, 500, "bounce");
-                    this.label[1].attr({ "font-weight": 400 });
-                }
-             });
+  var datapoints = [];
 
-            var selectors = jQuery("#os-graphs svg text tspan");
-            var links = $links;
-            jQuery.each(selectors, function(i, element){
-              var old = jQuery(element).text();
+  for(var i =0; i< labels.size(); i++)
+  {
+    datapoints.push({value: count[i], label:labels[i], href:links[i]})
+  }
 
-              /*var icon = r.image("img/machines/icn_machinesList.gif", 175, margin+1.3*50+17*i, 18,10);
-              jQuery(icon[0]).attr("class","pointer")
+  pie = new d3pie("pieChart", {
+    "size": {
+		"canvasHeight": 205,
+		"canvasWidth": 205,
+		"pieOuterRadius": "100%"
+	},
+	data: {
+		content: datapoints
+	},
+  "labels": {
+    "outer": {
+  		"format": "none",
+  		"pieDistance": 10
+		},
+    "inner": {
+	    "format": "none",
+		},
+  },
+  "tooltips": {
+		"enabled": true,
+		"type": "placeholder",
+		"string": "{label}: {value}, {percentage}%"
+	},
+	callbacks: {
+		onClickSegment: function(a) {
+			window.location.href =a.data.href;
+		}
+	}
+});
 
-              jQuery(icon[0]).on("click", function(){
-                window.location.href = links[i];
-              });*/
-              var new_legend = "<a href='"+links[i]+"'>"+old+"</a>";
+for(var i =0; i< labels.size(); i++)
+{
+  jQuery("#os-list").append("<li>"+labels[i]+" <a href='"+links[i]+"'> <img title='Créer un groupe' style='height: 10px; padding-left: 3px;' src='img/machines/icn_machinesList.gif'></a></li>")
+}
 
-              jQuery(element).html(new_legend);
-            });
 
-            y += (radius * 2) + margin + 5;
-
-            r.setSize(210, (radius * 1 + margin) + legend.size()*20);
+        })
         </script>
-        <style>
-          .pointer{
-            cursor:pointer;
-          }
-        </style>
 SPACE;
     }
 
 }
 
 ?>
+<style>
+  #pieChart svg{
+    /*The width of the piechart is ajusted after its generation because the toolstips are truncated*/
+    width:350px;
+  }
+</style>
