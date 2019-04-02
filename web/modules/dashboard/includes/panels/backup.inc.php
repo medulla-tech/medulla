d21a41cd828a3cf1d37aaf1bc4bb7b9808ab50e2
Nicolas Lécureuil <neoclust@mageia.org>
2019-04-02 09:25:58 +0200

    Revert "Modify the red color to orange color for backup widget"
    This reverts commit 3cc3883b1c660150c378971c544492d8c7a4a595.

--- web/modules/dashboard/includes/panels/backup.inc.php
+++ web/modules/dashboard/includes/panels/backup.inc.php
@@ -46,7 +46,6 @@ class BackupPanel extends Panel {
           <script>
             var backupDatas = [
               {"label": "$configured_text", "value":$machines_backup, "href":"$urlRedirect&backup=yes"},
-              {'label': '', 'value': 0, "href": ""},
               {'label': '$not_configured_text', 'value': $machines_not_backup, "href": "$urlRedirect&backup=no"}
             ];
 
