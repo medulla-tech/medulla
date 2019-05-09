8422ab58e79442aaf3b80c77692f631af289f44c
Noël <noel.kneib@siveo.net>
2019-05-09 12:06:58 +0200

    Specify explicitely the join for glpi 9.3 compatibility
    The query that get the computers list made a join which needs to be explicit for glpi9.3

--- services/mmc/plugins/glpi/database_93.py
+++ services/mmc/plugins/glpi/database_93.py
@@ -679,7 +679,7 @@ class Glpi93(DyngroupDatabaseHelper):
                 if 'owner' in self.config.summary or \
                    'owner_firstname' in self.config.summary or \
                    'owner_realname' in self.config.summary:
-                    join_query = join_query.outerjoin(self.user)
+                    join_query = join_query.outerjoin(self.user, Machine.users_id == User.id)
                 try:
                     if regs[0]:
                         join_query = join_query.outerjoin(self.regcontents)
