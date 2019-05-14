ac57ee11c04550323e57c163e7502b9d564cdcab
jfkneib <jean-francois.kneib@siveo.net>
2019-05-14 14:48:14 +0200

    find error XmppUpdateInventoried

--- services/mmc/plugins/xmppmaster/master/agentmaster.py
+++ services/mmc/plugins/xmppmaster/master/agentmaster.py
@@ -1641,7 +1641,8 @@ class MUCBot(sleekxmpp.ClientXMPP):
                     if computer != None:
                         uuid = 'UUID' + str(computer.id)
                         logger.debug("** Update uuid %s for machine %s " % (uuid, machine['jid']))
-                        if machine['uuid_inventorymachine'] != "":
+                        if machine['uuid_inventorymachine'] != "" and \
+                            machine['uuid_inventorymachine'] != None:
                             logger.debug("** Update in Organization_ad uuid %s to %s " % (machine['uuid_inventorymachine'],
                                                                                         uuid))
                             XmppMasterDatabase().replace_Organization_ad_id_inventory(machine['uuid_inventorymachine'],
