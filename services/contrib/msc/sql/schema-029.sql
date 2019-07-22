98096138e195bd93b0e7ac4a090980b8fee73374
jfkneib <jean-francois.kneib@siveo.net>
2019-07-22 09:38:52 +0200

    fix creation index msc base

--- services/contrib/msc/sql/schema-029.sql
+++ services/contrib/msc/sql/schema-029.sql
@@ -19,27 +19,38 @@
 -- MA 02110-1301, USA.
 
 
-
 -- check exist index before creation
+DELIMITER //
+DROP PROCEDURE IF EXISTS createindexidx_star_date;
+create procedure createindexidx_star_date()
+begin
 IF (SELECT 1
     FROM `INFORMATION_SCHEMA`.`STATISTICS`
     WHERE `TABLE_SCHEMA` = 'msc' 
     AND `TABLE_NAME` = 'commands_on_host'
     AND `INDEX_NAME` = 'idx_star_date') IS NULL THEN
-
     ALTER TABLE `msc`.`commands_on_host` ADD INDEX `idx_star_date` (`start_date` ASC);
-
 END IF;
+END;
+//
+DELIMITER ;
 
+DELIMITER //
+DROP PROCEDURE IF EXISTS createindexidx_end_date;
+create procedure createindexidx_end_date()
+begin
 IF (SELECT 1
     FROM `INFORMATION_SCHEMA`.`STATISTICS`
     WHERE `TABLE_SCHEMA` = 'msc' 
     AND `TABLE_NAME` = 'commands_on_host'
     AND `INDEX_NAME` = 'idx_end_date') IS NULL THEN
-
     ALTER TABLE `msc`.`commands_on_host` ADD INDEX `idx_end_date` (`end_date` ASC);
-
 END IF;
+END;
+//
+DELIMITER ;
 
+call createindexidx_star_date();
+call createindexidx_end_date();
 
 UPDATE version SET Number = 29;
