aa7a67d7d28d37214365f11d8808c937c471ac3f
Noël <noel.kneib@siveo.net>
2019-09-02 15:01:31 +0200

    The indentation is wrong
    Because of this the method was inexistant

--- services/pulse2/managers/imaging.py
+++ services/pulse2/managers/imaging.py
@@ -62,14 +62,14 @@ class ComputerImagingManager(Singleton):
                     return False
         return ret
 
-        def getAllImagingServers(self, user_id, associated):
+    def getAllImagingServers(self, user_id, associated):
 
-            if self.main in self.components:
-                klass = self.components[self.main]
-                ret = klass().getAllImagingServers(user_id, associated)
-            else:
-                ret = False
-            return ret
+        if self.main in self.components:
+            klass = self.components[self.main]
+            ret = klass().getAllImagingServers(user_id, associated)
+        else:
+            ret = False
+        return ret
 
     def getImagingServerEntityUUID(self, imaging_uuid):
         klass = self.components[self.main]
