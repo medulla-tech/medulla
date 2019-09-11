45dfe13b2b2fd8976ba4e28b5fac06ef66e4fdb7
Nicolas Lécureuil <neoclust@mageia.org>
2019-09-11 18:59:09 +0200

    Fix tab

--- scripts/tx-pull-pulse.sh
+++ scripts/tx-pull-pulse.sh
@@ -26,9 +26,9 @@ do
         cp  -fv $SCRIPT_PROJECT/../web/modules/$mod/locale/fr/LC_MESSAGES/* $SCRIPT_PROJECT/../web/modules/$mod/locale/fr_FR/LC_MESSAGES
         sed -i 's/Language: fr\\n/Language: fr_FR\\n/' $SCRIPT_PROJECT/../web/modules/$mod/locale/fr_FR/LC_MESSAGES/$mod.po
     else
-	    cd $SCRIPT_PROJECT/../web/modules/$mod/locale/
-	    tx pull -a -f -r pulse-1.${mod} ${lang} ${args}
-        cp  -fv $SCRIPT_PROJECT/../web/modules/$mod/locale/fr/LC_MESSAGES/* $SCRIPT_PROJECT/../web/modules/$mod/locale/fr_FR/LC_MESSAGES
+	cd $SCRIPT_PROJECT/../web/modules/$mod/locale/
+	tx pull -a -f -r pulse-1.${mod} ${lang} ${args}
+	cp  -fv $SCRIPT_PROJECT/../web/modules/$mod/locale/fr/LC_MESSAGES/* $SCRIPT_PROJECT/../web/modules/$mod/locale/fr_FR/LC_MESSAGES
         sed -i 's/Language: fr\\n/Language: fr_FR\\n/' $SCRIPT_PROJECT/../web/modules/$mod/locale/fr_FR/LC_MESSAGES/$mod.po
     fi
 done
