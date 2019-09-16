f66d630e32c8bc94b50b1157b5751b1f6b3f3c2d
Nicolas Lécureuil <neoclust@mageia.org>
2019-09-17 00:50:08 +0200

    Fix ident

--- scripts/tx-pull-pulse.sh
+++ scripts/tx-pull-pulse.sh
@@ -23,5 +23,5 @@ do
 	cd $SCRIPT_PROJECT/../web/modules/$mod/locale/
 	tx pull -a -f -r pulse-1.${mod} ${lang} ${args}
 	cp  -fv $SCRIPT_PROJECT/../web/modules/$mod/locale/fr/LC_MESSAGES/* $SCRIPT_PROJECT/../web/modules/$mod/locale/fr_FR/LC_MESSAGES
-    sed -i 's/Language: fr\\n/Language: fr_FR\\n/' $SCRIPT_PROJECT/../web/modules/$mod/locale/fr_FR/LC_MESSAGES/$mod.po
+        sed -i 's/Language: fr\\n/Language: fr_FR\\n/' $SCRIPT_PROJECT/../web/modules/$mod/locale/fr_FR/LC_MESSAGES/$mod.po
 done
