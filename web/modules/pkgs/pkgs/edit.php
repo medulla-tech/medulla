ca583a38c4164441ebe88cda1f5908a007a36596
Noël <noel.kneib@siveo.net>
2019-09-23 14:14:25 +0200

    Add the pulldirect tranfer method into the page edit package

--- web/modules/pkgs/pkgs/edit.php
+++ web/modules/pkgs/pkgs/edit.php
@@ -302,8 +302,8 @@ if(isExpertMode())
     }
     $methodtransfer = new SelectItem('methodetransfert');
     // Allowed methods are pullcurl, pushrsync, pullrsync, pullscp
-    $methodtransfer->setElements(['pushrsync', 'pullrsync']);
-    $methodtransfer->setElementsVal(['pushrsync', 'pullrsync']);
+    $methodtransfer->setElements(['pushrsync', 'pullrsync', 'pulldirect']);
+    $methodtransfer->setElementsVal(['pushrsync', 'pullrsync', 'pulldirect']);
     $methodtransfer->setSelected($setmethodetransfert);
 
     $f->add(new TrFormElement(_T('Transfer method','pkgs'),$methodtransfer, ['trid'=>'trTransfermethod']),['value'=>'']);
