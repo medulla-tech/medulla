1d96012c52947e465330a59a6f59abce115ce9ac
Nicolas Lécureuil <neoclust@mageia.org>
2019-12-26 13:20:51 +0100

    Fix syntaxe of debug message

--- services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_notifysyncthing.py
+++ services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_notifysyncthing.py
@@ -39,12 +39,12 @@ def action( xmppobject, action, sessionid, data, message, ret, dataobj):
     logger.debug("=====================================================")
     print json.dumps(data, indent = 4)
     if 'suppdir' in data or 'adddir' in data:
-        logger.debug("supprime %s %s %s"%( data['packageid'], 'create', str(message['from'])))
+        logger.debug("removing package %s %s %s"%( data['packageid'], 'create', str(message['from'])))
         PkgsDatabase().pkgs_unregister_synchro_package( data['packageid'],
                                                       None, 
                                                       str(message['from']))
     elif 'MotifyFile' in data:
-        logger.debug("supprime %s %s %s"%( data['packageid'], 'chang', str(message['from'])))
+        logger.debug("removing package %s %s %s"%( data['packageid'], 'chang', str(message['from'])))
         PkgsDatabase().pkgs_unregister_synchro_package( data['packageid'], 
                                                       'chang', 
                                                       str(message['from']))
