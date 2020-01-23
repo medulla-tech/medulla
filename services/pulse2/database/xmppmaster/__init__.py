1533eb08b1cb833a94acaff0ee4b3590930a511a
Nicolas Lécureuil <neoclust@mageia.org>
2020-01-23 12:16:59 +0100

    Remove use of unexisting database

--- services/pulse2/database/xmppmaster/__init__.py
+++ services/pulse2/database/xmppmaster/__init__.py
@@ -4267,7 +4267,7 @@ class XmppMasterDatabase(DatabaseHelper):
             session.commit()
             session.flush()
         except IndexError:
-            logging.getLogger().warning("Configuration agent machine jid [%s]. no jid in base for configuration"%jid)
+            logging.getLogger().warning("Unable to remove the machine from the xmppmaster database, skipping")
             return {}
         except Exception, e:
             logging.getLogger().error(str(e))
