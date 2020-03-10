b4cd26185dc1a35e812a0cfde548c1b08047a572
spointu <spointu@gmail.com>
2020-03-10 10:41:15 +0100

    [deployment] Add status for transfer errors

--- services/contrib/xmppmaster/sql/schema-035.sql
+++ services/contrib/xmppmaster/sql/schema-035.sql
@@ -34,6 +34,11 @@ CREATE TABLE IF NOT EXISTS `xmppmaster`.`def_remote_deploy_status` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 COMMENT = 'this table allows you to define the regexp to apply 1 status according to 1 log of type deploy';
 
+
+LOCK TABLES `def_remote_deploy_status` WRITE;
+INSERT INTO `xmppmaster`.`def_remote_deploy_status` (`regex_logmessage`, `status`) VALUES ('.*Terminate.*ERROR transfer.*$', 'ERROR TRANSFER ERROR');
+UNLOCK TABLES;
+
 -- ----------------------------------------------------------------------
 -- Database version
 -- ----------------------------------------------------------------------
