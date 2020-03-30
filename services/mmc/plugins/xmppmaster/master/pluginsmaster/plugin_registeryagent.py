65a0f17cf3f6167b3daf134a1bc288db5d084099
jfk <jean-francois.kneib@siveo.net>
2020-03-30 08:50:31 +0200

    [Register] Update the machine data if different from descriptor
    (cherry picked from commit 32413a116ef3ae172d3d613d5ca1632f8a18e6f4)

--- services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_registeryagent.py
+++ services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_registeryagent.py
@@ -122,6 +122,13 @@ def action(xmppobject, action, sessionid, data, msg, ret, dataobj):
                         logger.debug("Update it's uuid_inventory_machine")
                         logger.debug("=============")
                         logger.debug("=============")
+                    if data['from'] != machine['jid'] or\
+                        data['baseurlguacamole'] != machine['urlguacamole'] or\
+                        data['deployment'] != machine['groupdeploy']:
+                        machine['jid'] = data['from']
+                        machine['urlguacamole'] = data['baseurlguacamole']
+                        machine['groupdeploy'] = data['deployment']
+                        XmppMasterDatabase().updateMachinejidGuacamoleGroupdeploy(machine['jid'], machine['urlguacamole'], machine['groupdeploy'], machine['id'])
                     if xmppobject.check_uuidinventory and \
                         'uuid_inventorymachine' in machine and \
                             machine['uuid_inventorymachine'] is not None:
