0103f1f1201393f4b5f5e1249d92d9a00b1ed724
botheis <noel.kneib@siveo.net>
2020-06-10 10:52:16 +0200

    Remove useless whitespaces

--- services/mmc/plugins/xmppmaster/master/lib/utils.py
+++ services/mmc/plugins/xmppmaster/master/lib/utils.py
@@ -46,7 +46,7 @@ import uuid
 import time
 from datetime import datetime
 import imp
-import requests 
+import requests
 from Crypto import Random
 from Crypto.Cipher import AES
 import urllib2
@@ -92,7 +92,7 @@ def dump_parameter(para=True, out=True, timeprocess = True):
             start = time.time()
             func_name = decorated_function.__name__
             log = logging.getLogger(func_name)
-            
+
             filepath = os.path.basename(__file__)
             # get function params (args and kwargs)
             if para:
@@ -1027,9 +1027,9 @@ def md5folder(directory):
     return hash.hexdigest()
 
 class geolocalisation_agent:
-    def __init__(self, 
-                 typeuser = "public", 
-                 geolocalisation=True, 
+    def __init__(self,
+                 typeuser = "public",
+                 geolocalisation=True,
                  ip_public=None,
                  strlistgeoserveur=""):
         self.determination = False
@@ -1050,7 +1050,7 @@ class geolocalisation_agent:
             return {}
         return self.localisation
 
-    def getdatafilegeolocalisation(self):    
+    def getdatafilegeolocalisation(self):
         if self.geoinfoexist():
             try:
                 with open(self.filegeolocalisation) as json_data:
@@ -1136,7 +1136,7 @@ class geolocalisation_agent:
             return r.json()
         except:
             return None
-    
+
     @staticmethod
     def call_simple_page_urllib(url):
         try:
