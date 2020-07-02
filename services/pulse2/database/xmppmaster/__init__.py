3d51e02d4be9bb3696101a9e17943b4761a60186
jfk <jean-francois.kneib@siveo.net>
2020-07-02 17:09:14 +0200

    clean old jid

--- services/pulse2/database/xmppmaster/__init__.py
+++ services/pulse2/database/xmppmaster/__init__.py
@@ -1628,12 +1628,23 @@ class XmppMasterDatabase(DatabaseHelper):
                     session.execute(sql)
                     session.commit()
                     session.flush()
+                self.checknewjid(jid)
             except Exception, e:
                 logging.getLogger().error(str(e))
                 msg=str(e)
                 return -1, msg
             return new_machine.id, msg
 
+    @DatabaseHelper._sessionm
+    def checknewjid(self, session, newjidmachine):
+        try:
+            # on appelle la procedure stocke
+            sql = """call afterinsertmachine('%s');"""%newjidmachine
+            session.execute(sql)
+            session.commit()
+            session.flush()
+        except Exception, e:
+            logging.getLogger().error("sql : %s"%traceback.format_exc())
 
     @DatabaseHelper._sessionm
     def is_jiduser_organization_ad(self, session, jiduser):
