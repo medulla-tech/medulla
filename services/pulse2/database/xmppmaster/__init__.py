cf17e81c6c3b0494f2f123b43ab52b9f1d1b98af
jfk <jean-francois.kneib@siveo.net>
2020-07-03 10:14:28 +0200

    [fixe deployement] update status deployement

--- services/pulse2/database/xmppmaster/__init__.py
+++ services/pulse2/database/xmppmaster/__init__.py
@@ -2808,8 +2808,10 @@ class XmppMasterDatabase(DatabaseHelper):
                     `state` = '%s'
                 WHERE
                     (deploy.sessionid = '%s'
-                        AND `state` NOT IN ('DEPLOYMENT SUCCESS' , 'ABORT DEPLOYMENT CANCELLED BY USER')
-                        AND `state` REGEXP '^(\?!ERROR)^(\?!SUCCESS)^(\?!ABORT)');
+                        AND ( `state` NOT IN ('DEPLOYMENT SUCCESS' ,
+                                              'ABORT DEPLOYMENT CANCELLED BY USER')
+                                OR
+                              `state` REGEXP '^(\?!ERROR)^(\?!SUCCESS)^(\?!ABORT)'));
                 """ % (state, sessionid)
             result = session.execute(sql)
             session.commit()
