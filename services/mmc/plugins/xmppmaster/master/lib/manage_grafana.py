d17f1e4402e27c84561b0c3639d0d202b04cb005
SPO <stephane.pointu@siveo.net>
2020-08-19 15:01:27 +0200

    [monitoring] Add functions to manage Grafana via its API (#322)
    * [EX10] #250:  Save states Online/Offline and their durations.
    Then we can print them
    
    * [doc] Start documentation
    
    * [EX10]  # 250 add stat function on uptime_machine/lastday
    
    * Fix pep8 errors
    
    * Fix pep8 errors
    
    * Add documentation
    
    * [status history] Add database schema to makefile
    fixes pulse-project/siveo-todo#250
    
    * [status history] Fix description of function
    fixes pulse-project/siveo-todo#250
    
    * [status history] Update database schema to reflect name change
    fixes pulse-project/siveo-todo#250
    
    * [monitoring] Add functions to manage Grafana via its API
    fixes pulse-project/siveo-todo#250
    
    * [monitoring] Add render_url parameter and shorten variable name length
    fixes pulse-project/siveo-todo#250
    
    * [monitoring] Fix syntax error
    fixes pulse-project/siveo-todo#250
    
    * [monitoring] Fix config
    fixes pulse-project/siveo-todo#250
    
    * [monitoring] Fix what function returns
    fixes pulse-project/siveo-todo#250
    
    * [monitoring] Change variable name
    fixes pulse-project/siveo-todo#250
    
    Co-authored-by: jfk <jean-francois.kneib@siveo.net>
    Co-authored-by: Nicolas Lécureuil <neoclust@mageia.org>

---
+++ services/mmc/plugins/xmppmaster/master/lib/manage_grafana.py
@@ -0,0 +1,330 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; -*-
+#
+# (c) 2020 siveo, http://www.siveo.net
+#
+# This file is part of Pulse 2, http://www.siveo.net
+#
+# Pulse 2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pulse 2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pulse 2; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# file: lib/manage_grafana.py
+
+import os
+import logging
+import ConfigParser
+import json
+import requests
+import socket
+
+logger = logging.getLogger()
+
+
+class manage_grafana:
+    """
+    This class provides the functions needed to create/edit/delete Grafana
+    panels via the API
+    It uses the configuration stored in grafana_api section of the file
+    /etc/mmc/plugins/xmppmaster.ini
+    """
+
+    def __init__(self, hostname, configfilepath=None):
+        self.hostname = hostname
+        self.api_config = {}
+
+        if configfilepath is None:
+            self.configfile = "/etc/mmc/plugins/xmppmaster.ini"
+        else:
+            self.configfile = configfilepath
+        if os.path.exists(self.configfile):
+            self.configuration()
+        else:
+            # No config file found
+            logger.error("/etc/mmc/plugins/xmppmaster.ini not found.")
+
+    def configuration(self):
+        """
+        Reads the configuration from ini and ini.local files
+        """
+        config = ConfigParser.ConfigParser()
+        config.readfp(open(self.configfile))
+        if os.path.isfile(self.configfile + '.local'):
+            config.readfp(open(self.configfile + '.local', 'r'))
+
+        if config.has_option('grafana_api', 'api_key'):
+            self.api_config['api_key'] = config.get('grafana_api', 'api_key')
+        else:
+            logger.error("API Key is not defined. "
+                         "Please define it in xmppmaster.ini.local")
+            return
+        if config.has_option('grafana_api', 'url'):
+            self.api_config['url'] = config.get('grafana_api', 'url')
+        else:
+            self.api_config['url'] = 'http://localhost:3000'
+        if config.has_option('grafana_api', 'render_url'):
+            self.api_config['render_url'] = config.get('grafana_api',
+                                                       'render_url')
+        else:
+            self.api_config['render_url'] = 'http://%s/grafana/render/d-solo' % socket.getfqdn()
+
+        logger.debug("API configuration: %s" % self.api_config)
+        return
+
+    def grafanaGetDashboardForHostname(self):
+        """Returns a JSON containing the dashboard for a hostname"""
+        # Check all parameters and prepare request
+        try:
+            logger.info("Getting dashboard for hostname %s" % self.hostname)
+            headers = {'Authorization': 'Bearer %s'
+                       % self.api_config['api_key']}
+            url = "%s/api/dashboards/uid/%s" % (self.api_config['url'],
+                                                self.hostname)
+        except KeyError:
+            logger.error("API key or url is not defined")
+            return None
+        # Send request to grafana API
+        try:
+            r = requests.get(url, headers=headers)
+            if r.status_code != requests.codes.ok:
+                logger.error("Error getting dashboard for hostname %s"
+                             % self.hostname)
+                logger.error("Received status code %s" % r.status_code)
+                logger.error("Complete response: %s" % r.text)
+                return None
+            # Dashboard found. Return the dashboard's JSON
+            logger.debug("JSON dashboard response: %s"
+                         % json.dumps(r.json(), indent=4))
+            return r.json()
+        except requests.RequestException as e:
+            logger.error("Error getting dashboard for hostname %s: %s"
+                         % (self.hostname, str(e)))
+            return None
+
+    def grafanaGetDashboardIdForHostname(self):
+        try:
+            logger.info("Getting dashboard id for hostname %s" % self.hostname)
+            db_json = self.grafanaGetDashboardForHostname()
+            logger.info("dashboard id for hostname %s: %s"
+                        % (self.hostname, db_json['dashboard']['id']))
+            return db_json['dashboard']['id']
+        except Exception as e:
+            logger.error("Error getting dashboard id for hostname%s: %s"
+                         % (self.hostname, str(e)))
+            return None
+
+    def grafanaWriteDashboardForHostname(self, dashb_json):
+        """Writes the JSON containing the dashboard"""
+        # Check all parameters and prepare request
+        try:
+            logger.info("Creating dashboard for hostname %s" % self.hostname)
+            headers = {'Authorization': 'Bearer %s'
+                       % self.api_config['api_key']}
+            url = "%s/api/dashboards/db" % self.api_config['url']
+        except KeyError:
+            logger.error("API key or url is not defined")
+            return None
+        # Send request to grafana API
+        try:
+            r = requests.post(url, headers=headers, json=dashb_json)
+            if r.status_code != requests.codes.ok:
+                logger.error("Error writing dashboard for hostname %s"
+                             % self.hostname)
+                logger.error("Received status code %s" % r.status_code)
+                logger.error("Complete response: %s" % r.text)
+                return None
+            # Dashboard written successfully. Return dashboard id
+            logger.debug("JSON dashboard response: %s"
+                         % json.dumps(r.json(), indent=4))
+            return r.json()['id']
+        except requests.RequestException as e:
+            logger.error("Error writing dashboard for hostname %s: %s"
+                         % (self.hostname, str(e)))
+            return None
+
+    def grafanaCreateDashboard(self):
+        # Prepare payload
+        payload = {}
+        dashboard = {}
+        dashboard['uid'] = self.hostname
+        dashboard['title'] = self.hostname
+        dashboard['timezone'] = 'utc'
+        dashboard['time'] = {}
+        dashboard['time']['from'] = 'now/d'
+        dashboard['time']['to'] = 'now/d'
+        payload['dashboard'] = dashboard
+        payload['folderId'] = 0
+        payload['overwrite'] = False
+        dashb_id = self.grafanaWriteDashboardForHostname(payload)
+        if dashb_id:
+            logger.info("Dashboard %s created for hostname %s"
+                        % (dashb_id, self.hostname))
+            return dashb_id
+        # Error writing dashboard
+        logger.error("Error creating dashboard for hostname %s"
+                     % self.hostname)
+        return None
+
+    def grafanaGetPanels(self):
+        """Returns a JSON containing the panels for a hostname"""
+        try:
+            logger.info("Getting panels for hostname %s" % self.hostname)
+            db_json = self.grafanaGetDashboardForHostname()
+            panel_json = []
+            for panel in db_json['dashboard']['panels']:
+                panel_json.append({'id': panel['id'], 'title': panel['title']})
+            logger.info("dashboard panels for hostname %s: %s"
+                        % (self.hostname, panel_json))
+            return panel_json
+        except Exception as e:
+            logger.error("Error getting panels for hostname%s: %s"
+                         % (self.hostname, str(e)))
+            return None
+
+    def grafanaGetPanelIdFromTitle(self, panel_title):
+        try:
+            logger.info("Getting panel %s id for hostname %s"
+                        % (panel_title, self.hostname))
+            current_panels = self.grafanaGetPanels()
+            for index, panel in enumerate(current_panels):
+                if panel_title == panel['title']:
+                    logger.info("Panel %s has id %s and index %s"
+                                 % (panel_title, panel['id'], index))
+                    return panel['id']
+            logger.warning("Panel %s not found for hostname %s"
+                           % (panel_title, self.hostname))
+            return None
+        except Exception as e:
+            logger.error("Error while getting panel %s for hostname %s: %s"
+                         % (panel_title, self.hostname, str(e)))
+            return None
+
+    def grafanaEditPanel(self, panel_json):
+        """Add or edit a panel to the dashboard from the JSON contents"""
+        # Check if dashboard exists. If not create it
+        try:
+            db_id = self.grafanaGetDashboardIdForHostname()
+        except Exception:
+            db_id = self.grafanaCreateDashboard()
+        if db_id is None:
+            logger.error("Error getting panel for hostname %s"
+                         % self.hostname)
+            return None
+        # Get current dashboard and add panel if it does not exist
+        # If it exists, update the panel
+        current_panels = self.grafanaGetPanels()
+        db_json = self.grafanaGetDashboardForHostname()
+        for index, panel in enumerate(current_panels):
+            if panel_json['title'] == panel['title']:
+                panel_id = panel['id']
+                panel_index = index
+        if panel_id:
+            logger.info("Updating panel %s for hostname %s"
+                        % (panel_id, self.hostname))
+            # Remove existing panel
+            db_json['dashboard']['panels'].remove(panel_index)
+        else:
+            panel_id = max(panel_json, key=lambda x: x['id'])['id'] + 1
+            logger.info("Adding panel %s for hostname %s"
+                        % (panel_id, self.hostname))
+        # Insert id and add the panel to dashboard json
+        panel_json['id'] = panel_id
+        db_json['dashboard']['panels'].append(panel_json)
+        logger.debug("New dashboard json for hostname %s: %s"
+                     % (self.hostname, db_json))
+        dashb_id = self.grafanaWriteDashboardForHostname(db_json)
+        if dashb_id:
+            logger.info("Dashboard %s updated with panel %s "
+                        "for hostname %s" % (dashb_id,
+                                             panel_json['title'],
+                                             self.hostname))
+            return True
+        # Error writing dashboard
+        logger.error("Error writing panel %s to dashboard "
+                     "for hostname %s" % (panel_json['title'],
+                                          self.hostname))
+        return False
+
+    def grafanaDeleteDashboard(self):
+        # Check all parameters and prepare request
+        try:
+            logger.info("Deleting dashboard for hostname %s" % self.hostname)
+            headers = {'Authorization': 'Bearer %s'
+                       % self.api_config['api_key']}
+            url = "%s/api/dashboards/uid/%s" % (self.api_config['url'],
+                                                self.hostname)
+        except KeyError:
+            logger.error("API key or url is not defined")
+            return None
+        # Send request to grafana API
+        try:
+            r = requests.delete(url, headers=headers)
+            if r.status_code != requests.codes.ok:
+                logger.error("Error deleting dashboard for hostname %s"
+                             % self.hostname)
+                logger.error("Received status code %s" % r.status_code)
+                logger.error("Complete response: %s" % r.text)
+                return None
+            logger.debug("JSON dashboard response: %s"
+                         % json.dumps(r.json(), indent=4))
+            return r.json()
+        except requests.RequestException as e:
+            logger.error("Error deleting dashboard for hostname %s: %s"
+                         % (self.hostname, str(e)))
+            return None
+
+    def grafanaDeletePanel(self, panel_title):
+        # Get dashboard for hostname and delete the panel
+        logger.info("Deleting panel %s for hostname %s" % (panel_title,
+                                                           self.hostname))
+        db_json = self.grafanaGetDashboardForHostname()
+        for index, panel in enumerate(db_json['dashboard']['panels']):
+            if panel['title'] == panel_title:
+                panel_index = index
+        if panel_index:
+            db_json['dashboard']['panels'].remove(panel_index)
+            logger.debug("New dashboard json for hostname %s: %s"
+                         % (self.hostname, db_json))
+            dashb_id = self.grafanaWriteDashboardForHostname(db_json)
+            if dashb_id:
+                logger.info("Panel %s deleted from dashboard %s "
+                            "for hostname %s" % (panel_title,
+                                                 dashb_id,
+                                                 self.hostname))
+                return True
+            # Error writing dashboard
+            logger.error("Error deleting panel %s from dashboard "
+                         "for hostname %s" % (panel_title, self.hostname))
+            return False
+        # No panel found
+        logger.error("Error deleting panel %s from dashboard "
+                     "for hostname %s: Pannel not found"
+                     % (panel_title, self.hostname))
+        return False
+
+    def grafanaGetPanelImage(self, panel_title):
+        """Returns the url of the image generated by Grafana for a panel"""
+        logger.info("Getting image url for panel %s for hostname %s"
+                    % (panel_title, self.hostname))
+        panel_id = self.grafanaGetPanelIdFromTitle(panel_title)
+        if panel_id:
+            img = "%s/%s/%s?panelId=%s&width=800&height=400" % (self.api_config['render_url'],
+                                                                self.hostname,
+                                                                self.hostname,
+                                                                panel_id)
+            logger.info("Image url for panel %s for hostname %s: %s"
+                        % (panel_title, self.hostname, img))
+            return img
+        # No panel found
+        return None
