1b9fc76e69db3172c271c4845607b9111bdb5604
Nicolas Lécureuil <neoclust@mageia.org>
2021-03-29 15:36:49 +0200

    [pkgs] Move pkgs_search_share function into database class
    fixes pulse-project/siveo-todo#262

--- services/mmc/plugins/pkgs/__init__.py
+++ services/mmc/plugins/pkgs/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8; -*-
+pkgs_search_share# -*- coding: utf-8; -*-
 #
 # (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
 # (c) 2007-2008 Mandriva
@@ -163,55 +163,7 @@ def pkgs_search_share(objsearch):
             It returns the the shares and the permissions following the defined rules.
 
     """
-    sharing_result = []
-    order_rules = PkgsDatabase().pkgs_Orderrules()
-
-    wrapper = {"config": {}, "datas": []}
-    wrapper["config"]["centralizedmultiplesharing"] = PkgsConfig("pkgs").centralizedmultiplesharing
-    wrapper["config"]["movepackage"] = PkgsConfig("pkgs").movepackage
-
-    permission = None
-    if "permission" in objsearch:
-        permission = objsearch['permission']
-
-    # global sharing
-    if objsearch['login'] == 'root':
-        # global sharing yes
-        # all local sharing yes
-        logger.debug("As we are using the root user, we can load all the packages from the list.")
-        wrapper["datas"] = PkgsDatabase().pkgs_sharing_admin_profil()
-        return wrapper
-    else:
-        for algo in order_rules:
-            id_algo = algo[0]
-            # User Rule : 1 sharing on login user
-            if id_algo == 1:
-                if 'login' in objsearch:
-                    logger.debug(" algos id is %s [%s]" % (id_algo, algo[2]))
-                    result = PkgsDatabase().pkgs_sharing_rule_search(objsearch['login'],
-                                                                     algoid=id_algo,
-                                                                     permission=permission)
-                    if result:
-                        for t in result:
-                            re = [x['name'] for x in sharing_result]
-                            if t['name'] in re:
-                                continue
-                            sharing_result.append(t);
-                    logger.debug("search sharing local %s" % (result))
-            elif id_algo == 2: # 1 sharing on profile userlogger.
-                if 'profil' in objsearch:
-                    debug("algos id is %s [%s]" % (id_algo, algo[2]))
-                    result = PkgsDatabase().pkgs_sharing_rule_search(objsearch['profil'], algoid=id_algo)
-                    if result:
-                        for t in result:
-                            re = [x['name'] for x in sharing_result]
-                            if t['name'] in re:
-                                continue
-                            sharing_result.append(t);
-                    logger.debug("search sharing local %s" % (result))
-
-    wrapper["datas"] = sharing_result
-    return wrapper
+    return PkgsDatabase().pkgs_search_share(objsearch)
 
 ############### synchro syncthing package #####################
 def pkgs_register_synchro_package(uuidpackage, typesynchro):
--- services/pulse2/database/pkgs/__init__.py
+++ services/pulse2/database/pkgs/__init__.py
@@ -1228,6 +1228,68 @@ class PkgsDatabase(DatabaseHelper):
                 ret.append(resuldict)
         return ret
 
+    def pkgs_search_share(self, objsearch):
+        """
+            This function is used to retrieve the shares and the permissions
+                following the defined rules.
+            Args:
+                objsearch:
+                # TODO: Fix documentation
+            Results:
+                It returns the the shares and the permissions following the defined rules.
+
+        """
+        sharing_result = []
+        order_rules = self.pkgs_Orderrules()
+
+        wrapper = {"config": {}, "datas": []}
+        wrapper["config"]["centralizedmultiplesharing"] = self.config.centralizedmultiplesharing
+        wrapper["config"]["movepackage"] = self.config.movepackage
+
+        permission = None
+        if "permission" in objsearch:
+            permission = objsearch['permission']
+
+        # global sharing
+        if objsearch['login'] == 'root':
+            # global sharing yes
+            # all local sharing yes
+            logger.debug("As we are using the root user, we can load all the packages from the list.")
+            wrapper["datas"] = self.pkgs_sharing_admin_profil()
+            return wrapper
+        else:
+            for algo in order_rules:
+                id_algo = algo[0]
+                # User Rule : 1 sharing on login user
+                if id_algo == 1:
+                    if 'login' in objsearch:
+                        logger.debug(" algos id is %s [%s]" % (id_algo, algo[2]))
+                        result = self.pkgs_sharing_rule_search(objsearch['login'],
+                                                                        algoid=id_algo,
+                                                                        permission=permission)
+                        if result:
+                            for t in result:
+                                re = [x['name'] for x in sharing_result]
+                                if t['name'] in re:
+                                    continue
+                                sharing_result.append(t);
+                        logger.debug("search sharing local %s" % (result))
+                elif id_algo == 2: # 1 sharing on profile userlogger.
+                    if 'profil' in objsearch:
+                        debug("algos id is %s [%s]" % (id_algo, algo[2]))
+                        result = self.pkgs_sharing_rule_search(objsearch['profil'], algoid=id_algo)
+                        if result:
+                            for t in result:
+                                re = [x['name'] for x in sharing_result]
+                                if t['name'] in re:
+                                    continue
+                                sharing_result.append(t);
+                        logger.debug("search sharing local %s" % (result))
+
+        wrapper["datas"] = sharing_result
+        return wrapper
+
+    @DatabaseHelper._sessionm
     def get_shares(self, session):
         """
         This function is used to obtain the list of the shares
