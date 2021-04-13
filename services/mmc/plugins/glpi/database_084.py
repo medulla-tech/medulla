b54a7cc72b1228e4e9dcd53e51b4029d0ab05df2
botheis <noel.kneib@siveo.net>
2021-04-13 15:05:47 +0200

    Do not transform empty uuids for presences

--- services/mmc/plugins/glpi/database_084.py
+++ services/mmc/plugins/glpi/database_084.py
@@ -633,7 +633,7 @@ class Glpi084(DyngroupDatabaseHelper):
         online_machines = XmppMasterDatabase().getlistPresenceMachineid()
 
         if online_machines is not None:
-            online_machines = [int(id.replace("UUID", "")) for id in online_machines if id != "UUID" or id != ""]
+            online_machines = [int(id.replace("UUID", "")) for id in online_machines if id != "UUID" and id != ""]
         query = session.query(Machine.id.label('uuid')).distinct(Machine.id)\
         .join(self.glpi_computertypes, Machine.computertypes_id == self.glpi_computertypes.c.id)\
         .outerjoin(self.user, Machine.users_id == self.user.c.id)\
