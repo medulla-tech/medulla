acff16301dd630882c6798c6cb68261b36cffe08
Nicolas Lécureuil <neoclust@mageia.org>
2021-04-13 16:29:47 +0200

    [GLPI] Do not convert empty ids in the new list of machines

--- services/mmc/plugins/glpi/database_94.py
+++ services/mmc/plugins/glpi/database_94.py
@@ -4021,7 +4021,7 @@ class Glpi94(DyngroupDatabaseHelper):
         online_machines = XmppMasterDatabase().getlistPresenceMachineid()
 
         if online_machines is not None:
-            online_machines = [id.replace("UUID", "") for id in online_machines]
+            online_machines = [int(id.replace("UUID", "")) for id in online_machines if id != "UUID" and id != ""]
         query = session.query(Machine.id.label('uuid')).distinct(Machine.id)\
         .join(self.glpi_computertypes, Machine.computertypes_id == self.glpi_computertypes.c.id)\
         .outerjoin(self.user, Machine.users_id == self.user.c.id)\
