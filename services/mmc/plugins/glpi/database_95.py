c6c0f6495059bc2490528ad483a0625f57ebb1ed
SPO <stephane.pointu@siveo.net>
2021-04-14 18:19:14 +0200

    [glpi] Fix missing commits from various files

--- services/mmc/plugins/glpi/database_95.py
+++ services/mmc/plugins/glpi/database_95.py
@@ -723,7 +723,7 @@ class Glpi95(DyngroupDatabaseHelper):
             query = query.filter(Entities.id.in_(listentity))
 
         # Add all the like clauses to find machines containing the criterion
-        if criterion != "":
+        if criterion != "" and idmachine == "" and uuidsetup == "":
             if field == "":
                 query = query.filter(or_(
                     Machine.name.contains(criterion),
@@ -948,10 +948,11 @@ class Glpi95(DyngroupDatabaseHelper):
 
         # Select machines from the specified entity
         if location != "":
-            query = query.filter(Entities.id == location)
+            listentity=[int(x.strip()) for x in location.split(',')]
+            query = query.filter(Entities.id.in_(listentity))
 
         # Add all the like clauses to find machines containing the criterion
-        if criterion != ""  and idmachine == "" and uuidsetup == "":
+        if criterion != "" and idmachine == "" and uuidsetup == "":
             if field == "":
                 query = query.filter(or_(
                     Machine.name.contains(criterion),
