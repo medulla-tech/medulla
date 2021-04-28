6bdf53e6ae63056345a34175ce382c8f801a24a9
jfkneib <jean-francois.kneib@siveo.net>
2021-04-28 17:34:51 +0200

    (registeryagent plugin) Always define xmppobject.listmodulemmc

--- services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_registeryagent.py
+++ services/mmc/plugins/xmppmaster/master/pluginsmaster/plugin_registeryagent.py
@@ -629,6 +629,10 @@ def action(xmppobject, action, sessionid, data, msg, ret, dataobj):
                                 return
                         else:
                             logger.warning("information about the operating system is missing for %s" % (msg['from'].bare))
+                        try:
+                            xmppobject.listmodulemmc
+                        except AttributeError:
+                            xmppobject.listmodulemmc=[]
                         #if "kiosk" in xmppobject.listmodulemmc and kiosk_presence:
                         if PluginManager().isEnabled("kiosk"):
                             # send a data message to kiosk when an inventory is registered
