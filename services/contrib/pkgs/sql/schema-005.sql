3d3942c939bea798c63655b596e0a5466702cd13
noel kneib <noel.kneib@siveo.net>
2021-04-29 12:16:32 +0200

    Change licence and remove useless whitespaces

--- services/contrib/pkgs/sql/schema-005.sql
+++ services/contrib/pkgs/sql/schema-005.sql
@@ -1,9 +1,9 @@
 --
--- (c) 2010 Mandriva, http://www.mandriva.com/
+-- (c) 2021 Siveo, http://www.siveo.net/
 --
 -- $Id$
 --
--- This file is part of Pulse 2, http://pulse2.mandriva.org
+-- This file is part of Pulse 2, http://www.siveo.net
 --
 -- Pulse 2 is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -30,14 +30,14 @@ START TRANSACTION;
 -- Table `pkgs`.`pkgs_rules_global` suppression FOREIGN KEY
 -- -----------------------------------------------------
 
-ALTER TABLE `pkgs`.`pkgs_rules_global` 
+ALTER TABLE `pkgs`.`pkgs_rules_global`
 DROP FOREIGN KEY `fk_pkgs_rules_global_pkgs_shares1`;
-ALTER TABLE `pkgs`.`pkgs_rules_global` 
+ALTER TABLE `pkgs`.`pkgs_rules_global`
 DROP INDEX `fk_pkgs_rules_global_pkgs_shares1_idx` ;
 
 INSERT INTO `pkgs`.`pkgs_rules_algos` (`id`, `name`, `description`, `level`) VALUES ('3', 'login_cluster_ars_admin', 'Rule Use to add admin rights to the cluster.', '1');
 
-ALTER TABLE `pkgs`.`pkgs_rules_global` 
+ALTER TABLE `pkgs`.`pkgs_rules_global`
 CHANGE COLUMN `pkgs_shares_id` `pkgs_cluster_ars_id` INT(11) NOT NULL ;
 
 UPDATE version SET Number = 5;
