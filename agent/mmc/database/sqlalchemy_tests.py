f630187be880b5dba4382e62f7b8dba46958337c
Nicolas Lécureuil <neoclust@mageia.org>
2022-07-19 15:54:44 +0200

    (sqlalchemy) Add maximum supported version

--- agent/mmc/database/sqlalchemy_tests.py
+++ agent/mmc/database/sqlalchemy_tests.py
@@ -24,7 +24,7 @@ from distutils.version import StrictVersion
 from sqlalchemy import __version__
 
 MIN_VERSION = "0.6.3"  # Debian Squeeze version
-MAX_VERSION = "1.2.18"  # Debian Buster version
+MAX_VERSION = "1.3.22"  # Debian Bullseye version
 CUR_VERSION = __version__
 
 
