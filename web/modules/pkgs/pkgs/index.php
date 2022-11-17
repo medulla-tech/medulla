73631b3f51a68a80ab942a08c16b72a75c14d994
SPO <stephane.pointu@siveo.net>
2022-11-17 08:06:50 +0100

    [updates] The hide/show updates is not needed as this is managed as a normal share

--- web/modules/pkgs/pkgs/index.php
+++ web/modules/pkgs/pkgs/index.php
@@ -32,16 +32,6 @@ $p->setSideMenu($sidemenu);
 $p->display();
 
 require_once("modules/pkgs/includes/xmlrpc.php");
-$toggleupdates = isset($_GET['toggleupdates']) ? $_GET['toggleupdates'] : (isset($_SESSION['toggleupdates']) ? $_SESSION['toggleupdates'] : "hide");
-$_SESSION['toggleupdates'] = $toggleupdates;
-
-echo '<input type="radio" name="updates" id="show-updates" value="show"';
-if ($toggleupdates == "show") echo "checked";
-echo '>';
-echo '<label for="show-updates">Show Updates</label>';
-echo 'Hide updates <input type="radio" name="updates" id="hide-updates" value="hide"';
-if ($toggleupdates == "hide") echo "checked";
-echo '>';
 
 $ajax = new AjaxFilter(urlStrRedirect("pkgs/pkgs/ajaxPackageList"));
 $ajax->display();
@@ -52,43 +42,3 @@ $ajax->displayDivToUpdate();
 <style>
     .noborder { border:0px solid blue; }
 </style>
-
-<script type="text/javascript">
-//jQuery('#location option[value=""]').prop('selected', true);
-
-    function getQuerystringDef(key, default_) {
-        if (default_==null) default_="";
-        key = key.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-        var regex = new RegExp("[\\?&]"+key+"=([^&#]*)");
-        var qs = regex.exec(window.location.href);
-
-        if(qs == null)
-            return default_;
-
-        else
-            return qs[1];
-    }
-    //Radiobox Mode
-    jQuery('input[type=radio][name=updates]').change(function(){
-
-        var valselect  = this.value;
-        var url = window.location.href;
-
-        if( !getQuerystringDef("toggleupdates", false)){
-            var url = window.location.href + "&" + "toggleupdates"  + "=" + valselect;
-
-            window.location = url;
-        }
-        else{
-            var array_url = url.split("?");
-            var adress = array_url[0];
-            var parameters = array_url[1];
-            var parameterlist = parameters.split("&");
-            parameterlist.pop();
-            parameterstring = parameterlist.join('&');
-            var url = adress + "?" + parameterstring + "&" + "toggleupdates"  + "=" + valselect;
-            window.location = url;
-        }
-
-    });
-</script>
