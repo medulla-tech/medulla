75d193dfd44086fe98019ca9e65ce2be3a4811ed
SPO <stephane.pointu@siveo.net>
2022-11-18 15:04:32 +1300

    [updates] Interface for management of grey, white and black lists (#384)
    * [UPDATES] Add actions to the updates lists
    
    * [UPDATE] Add unlist action and approve action
    
    - approve action was not complete
    - unlist was missing
    - change the request for white list
    
    Co-authored-by: botheis <noel.kneib@siveo.net>

--- web/modules/updates/updates/ajaxUpdatesListWin.php
+++ web/modules/updates/updates/ajaxUpdatesListWin.php
@@ -20,144 +20,186 @@
  * along with MMC; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
-
 require_once("modules/updates/includes/xmlrpc.php");
 
-// ###################################################################################### //
-// ########### !!! PENSER A CHANGER I OU J POUR METTRE DE VRAI VARIABLE !!! ############# //
-// ###################################################################################### //
-
-// Global configuration of $maxperpage, $filter, $start, $end
+// Global config for $maxperpage
 global $conf;
 $maxperpage = $conf["global"]["maxperpage"];
 $filter  = isset($_GET['filter'])?$_GET['filter']:"";
 $start = isset($_GET['start'])?$_GET['start']:0;
 $end   = (isset($_GET['end'])?$_GET['start']+$maxperpage:$maxperpage);
 
-// Appel de fonction pour recuperer la Liste Grise dans up_gray_list
+
+// == Get gray, white and black lists ==
 $grey_list = xmlrpc_get_grey_list($start, $end, $filter);
-// Appel de fonction pour recuperer la Liste Grise dans up_gray_list ou valided = 1
-$walter_white = xmlrpc_get_white_list($start, $end, $filter);
-// Appel de fonction pour recuperer la Liste Grise dans up_black_list
+$white_list = xmlrpc_get_white_list($start, $end, $filter);
 $black_list = xmlrpc_get_black_list($start, $end, $filter);
 
-echo "<pre>";
-//print_r($grey_list);
-echo "</pre>";
+// == Declare actions for the lists ==
+// GrayList Actions
+$grayEnableAction = new ActionItem(_T("Enable Update", "updates"),"grayEnable","quick","", "updates", "updates");
+$grayDisableAction = new ActionPopupItem(_T("Disable Update", "updates"), "grayDisable", "display", "updates", "updates");
+$grayApproveAction = new ActionPopupItem(_T("Approve Update", "updates"), "grayApprove", "edit", "updates", "updates");
 
-// The action items for moving updates between lists
-$enableUpd = new ActionItem(_T("Enable Update", "updates"), "enableUpdate", "enableupdate", "", "updates", "updates");
-$disableUpd = new ActionPopupItem(_T("Disable Update", "updates"), "disableUpdate", "disableupdate", "updates", "updates");
-$whitelistUpd = new ActionPopupItem(_T("Approve Update", "updates"), "whitelistUpdate", "deploy", "updates", "updates");
-$greylistUpd = new ActionPopupItem(_T("Unlist Update", "updates"), "greylistUpdate", "unlist", "updates", "updates");
-$blacklistUpd = new ActionPopupItem(_T("Ban Update", "updates"), "blacklistUpdate", "banupdate", "updates", "updates");
-$unBanUpd = new ActionItem(_T("Unban Update", "updates"), "deleteRule", "unlist", "", "updates", "updates");
+// WhiteList Actions
+$whiteUnlistAction = new ActionPopupItem(_T("Unlist Update", "updates"), "whiteUnlist", "display", "updates", "updates");
+$banAction = new ActionPopupItem(_T("Ban Update", "updates"), "banUpdate", "delete", "updates", "updates");
 
-// Initialisation of the arrays that will contain the parameters of each table
+// BlackList Actions
+$blackUnbanAction = new ActionItem(_T("unban Update", "updates"),"blackUnban","quick","", "updates", "updates");
+
+// Actions wrapper
+$whiteActions = [
+    "unlist"=>[],
+    "ban"=>[]
+];
+$grayActions = [
+    "enable"=>[],
+    "disable"=>[],
+    "approve"=>[],
+    "ban"=>[]
+];
+$blackActions = ["unban" =>[]];
+
+// Declare Params wrappers for the 3 tables
 $params_grey = [];
 $params_white = [];
 $params_black = [];
-$params_unBan = [];
 
-// Initialisation of the arrays that will hold the actions
-$actionenableUpds = [];
-$actiondisableUpds = [];
-$actiongreylistUpds = [];
-$actionwhitelistUpds = [];
-$actionblacklistUpds = [];
-$actionunBanUpds = [];
 
-// Counters for each list
+// get updates counts
 $count_grey = $grey_list['nb_element_total'];
-$count_white = $walter_white['nb_element_total'];
+$count_white = $white_list['nb_element_total'];
 $count_black = $black_list['nb_element_total'];
 
-// ########## Greylist loop ########## //
+// Declare some additionnals columns
+$kbs_white = [];
+$kbs_gray = [];
+$kbs_black = [];
+
+$updateids_white = [];
+$updateids_gray = [];
+$updateids_black = [];
+
+$titles_white = [];
+$titles_grey = [];
+$titles_black = [];
+
+// ########## greyList ########## //
 for($i=0; $i < $count_grey; $i++){
-    $actionenableUpds[] = $enableUpd;
-    $actiondisableUpds[] = $disableUpd;
-    $actionwhitelistUpds[] = $whitelistUpd;
-    $actionblacklistUpds[] = $blacklistUpd;
+    $grayActions["enable"][] = $grayEnableAction;
+    $grayActions["disable"][] = $grayDisableAction;
+    $grayActions["approve"][] = $grayApproveAction;
+    $grayActions["ban"][] = $banAction;
 
-    $title_grey[] = $grey_list['title'][$i];
+    $titles_grey[] = $grey_list['title'][$i];
 
-    $params_grey[] = array('updateid' => $grey_list['updateid'][$i]);
+    $params_grey[] = array(
+        'updateid' => $grey_list['updateid'][$i],
+        'title' => $grey_list['title'][$i]
+    );
+    if(strlen($grey_list['updateid'][$i]) < 10){
+        $kbs_gray[] = 'KB'.strtoupper($grey_list['updateid'][$i]);
+        $updateids_gray[] = "";
+    }
+    else{
+        $kbs_gray[] = "";
+        $updateids_gray[] = $grey_list['updateid'][$i];
+    }
 }
 
-// ########## whiteList loop ########## //
+// ########## whiteList ########## //
 for($i=0; $i < $count_white; $i++){
-    $actiongreylistUpds[] = $greylistUpd;
-    $actionblacklistUpds[] = $blacklistUpd;
+    $tmp = [];
+    $whiteActions["unlist"][] = $whiteUnlistAction;
+    $whiteActions["ban"][] = $banAction;
 
-    $title_white[] = $walter_white['title'][$i];
+    $titles_white[] = $white_list['title'][$i];
 
-    $params_white[] = array('updateid' => $walter_white['updateid'][$i]);
+    $tmp = array(
+        'updateid' => $white_list['updateid'][$i],
+        'title' => $white_list['title'][$i]
+    );
+
+    if(strlen($white_list['updateid'][$i]) < 10){
+        $kbs_white[] = 'KB'.strtoupper($white_list['updateid'][$i]);
+        $updateids_white[] = "";
+        $tmp['kb'] = $white_list['updateid'][$i];
+        $tmp['uid'] = "";
+    }
+    else{
+        $kbs_white[] = "";
+        $updateids_white[] = $white_list['updateid'][$i];
+        $tmp['kb'] = "";
+        $tmp['uid'] = $white_list['updateid'][$i];
+    }
+    $params_white[] = $tmp;
 }
 
-// ########## blackList loop ########## //
+// ########## blackList ########## //
 for($i=0; $i < $count_black; $i++){
-    $actionunBanUpds[] = $unBanUpd;
+    $blackActions["unban"][] = $blackUnbanAction;
 
-    $title_black[] = $black_list['title'][$i];
+    $titles_black[] = $black_list['title'][$i];
 
-    $params_black[] = array('updateid' => $black_list['updateid'][$i]);
-    $params_unBan[] = array('updateid' => $black_list['updateid_or_kb'][$i]);
+    $params_black[] = array(
+        'updateid' => $black_list['updateid_or_kb'][$i],
+        'title' => $black_list['title'][$i],
+        'id'=>$black_list['id'][$i]
+    );
+
+    if(strlen($black_list['updateid_or_kb'][$i]) < 10){
+        $kbs_black[] = 'KB'.strtoupper($black_list['updateid_or_kb'][$i]);
+        $updateids_black[] = "";
+    }
+    else{
+        $kbs_black[] = "";
+        $updateids_black[] = $black_list['updateid_or_kb'][$i];
+    }
 }
 
-// ########## Greylist table ########## //
-$g = new OptimizedListInfos($title_grey, _T("Update name", "updates"));
+// ########## GreyList Table ########## //
+$g = new OptimizedListInfos($titles_grey, _T("Update name", "updates"));
 $g->disableFirstColumnActionLink();
-$g->addExtraInfo($id, _T("Updateid", "updates"));
+$g->addExtraInfo($updateids_gray, _T("Update Id", "updates"));
+$g->addExtraInfo($kbs_gray, _T("KB", "updates"));
 $g->setItemCount($count_grey);
 $g->setNavBar(new AjaxNavBar($count_grey, $filter));
 $g->setParamInfo($params_grey);
 echo '<h2> GreyList</h2>';
-$g->addActionItemArray($actionenableUpds);
-$g->addActionItemArray($actiondisableUpds);
-$g->addActionItemArray($actionwhitelistUpds);
-$g->addActionItemArray($actionblacklistUpds);
+$g->addActionItemArray($grayActions['enable']);
+$g->addActionItemArray($grayActions['disable']);
+$g->addActionItemArray($grayActions['approve']);
+$g->addActionItemArray($grayActions['ban']);
+
 $g->display();
 
-// ########## Whitelist table ########## //
-$w = new OptimizedListInfos($title_white, _T("Update name", "updates"));
+
+// ########## WhiteList Table ########## //
+$w = new OptimizedListInfos($titles_white, _T("Update name", "updates"));
 $w->disableFirstColumnActionLink();
+$w->addExtraInfo($updateids_white, _T("Update Id", "updates"));
+$w->addExtraInfo($kbs_white, _T("KB", "updates"));
 $w->setItemCount($count_white);
 $w->setNavBar(new AjaxNavBar($count_white, $filter));
 $w->setParamInfo($params_white);
 echo '</br></br><h2> WhiteList</h2>';
-$w->addActionItemArray($actiongreylistUpds);
-$w->addActionItemArray($actionblacklistUpds);
+$w->addActionItemArray($whiteActions["unlist"]);
+$w->addActionItemArray($whiteActions["ban"]);
 $w->display();
 
-// ########## BlackList table ########## //
-$b = new OptimizedListInfos($title_black, _T("Update name", "updates"));
+// ########## BlackList Table ########## //
+
+$b = new OptimizedListInfos($titles_black, _T("Update name", "updates"));
+$b->addExtraInfo($updateids_black, _T("Update Id", "updates"));
+$b->addExtraInfo($kbs_black, _T("KB", "updates"));
 $b->disableFirstColumnActionLink();
 $b->setItemCount($count_black);
 $b->setNavBar(new AjaxNavBar($count_black, $filter));
-$b->setParamInfo($params_unBan);
+$b->setParamInfo($params_black);
 echo '</br></br><h2> BlackList</h2>';
-$b->addActionItemArray($actionunBanUpds);
+$b->addActionItemArray($blackActions["unban"]);
 $b->display();
 
-
-
-// ##################################################### //
-// ############ EX MODAL SUPPRESSION ################## //
-// $f = new PopupForm(_T("Delete this package"));
-// $hidden = new HiddenTpl("packageUuid");
-// $f->add($hidden, array("value" =>$uuid, "hide" => True));
-// $hidden = new HiddenTpl("p_api");
-// $f->add($hidden, array("value" => $p_api, "hide" => True));
-// $hidden = new HiddenTpl("pid");
-// $f->add($hidden, array("value" => $pid, "hide" => True));
-// $f->add(new HiddenTpl("from"), array("value" => $from, "hide" => True));
-// $f->addValidateButton("bconfirm");
-// $f->addCancelButton("bback");
-
-// ##### Lien genere dans /usr/share/mmc/modules/pkgs/pkgs/ajaxPackageList.php ##### //
-// <a title="Supprimer un package" href="main.php?module=pkgs&amp;submod=pkgs&amp;action=delete&amp;pid=YTYxN2IwNGEtdGVzdGpma196Ynk1dm1jOWhtdXl1Zmprb2oz&amp;packageUuid=a617b04a-testjfk_zby5vmc9hmuyufjkoj3&amp;permission=rw&amp;mod=" onclick="PopupWindow(event,'main.php?module=pkgs&amp;submod=pkgs&amp;action=delete&amp;pid=YTYxN2IwNGEtdGVzdGpma196Ynk1dm1jOWhtdXl1Zmprb2oz&amp;packageUuid=a617b04a-testjfk_zby5vmc9hmuyufjkoj3&amp;permission=rw&amp;mod=', 300); return false;">&nbsp;</a>
-
-// ##### CE QUE JE GENERE COMME LIEN ##### //
-// <a title="Ban Update" href="main.php?module=updates&amp;submod=updates&amp;action=blacklistUpdate&amp;updateid=01f7dc80-5870-4a79-bb59-fe9071e01405&amp;permission=test&amp;mod=">&nbsp;</a>
 
 ?>
