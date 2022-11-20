f5f753aa2e169b1f60eb116534e525c662b04a9c
SPO <stephane.pointu@siveo.net>
2022-11-20 22:04:26 +0100

    [update] fixe get_conformity_update_by_entity
    From commit 9dac97fda48c7855919a4f6bfcefcf1913181b77 minus bad copy-paste

--- web/modules/updates/updates/ajaxEntitiesList.php
+++ web/modules/updates/updates/ajaxEntitiesList.php
@@ -30,6 +30,42 @@ $filter  = isset($_GET['filter'])?$_GET['filter']:"";
 $start = isset($_GET['start'])?$_GET['start']:0;
 $end   = (isset($_GET['end'])?$_GET['start']+$maxperpage:$maxperpage);
 
+function colorconf($conf){
+    switch($conf){
+        case $conf <= 10:
+            $color = "#ff0000";
+            break;
+        case $conf <= 20:
+            $color = "#ff3535";
+            break;
+        case $conf <= 30:
+            $color = "#ff5050";
+            break;
+        case $conf <= 40:
+            $color = "#ff8080";
+            break;
+        case $conf <  50:
+            $color = "#ffA0A0";
+            break;
+        case $conf <=  60:
+            $color = "#c8ffc8";
+            break;
+        case $conf <= 70:
+            $color = "#97ff97";
+            break;
+        case $conf <= 80:
+            $color = "#64ff64";
+            break;
+        case $conf <=  90:
+            $color = "#2eff2e";
+            break;
+        case $conf >90:
+            $color = "#00ff00";
+            break;
+    }
+    return $color;
+}
+
 $entities = getUserLocations();
 $entitycompliances = xmlrpc_get_conformity_update_by_entity();
 
@@ -47,63 +83,81 @@ $entityNames = [];
 $complRates = [];
 $totalMachine = [];
 $nbupadte = [];
+$identity=array();
+
+ foreach ($entitycompliances as $entitycompliance) {
+     $identity[$entitycompliance['entity']]=array(
+         "conformite" => $entitycompliance['conformite'],
+         "totalmach" => $entitycompliance['totalmach'],
+         "nbupdate" => $entitycompliance['nbupdate'],
+         "nbmachines" => $entitycompliance['nbmachines'],
+         "entity" => $entitycompliance['entity']);
+ };
+
 
 $count = count($entities);
 foreach ($entities as $entity) {
+    $id_entity = intval(substr($entity["uuid"],4));
     $actiondetailsByMachs[] = $detailsByMach;
     $actiondetailsByUpds[] = $detailsByUpd;
     $actiondeployAlls[] = $deployAll;
     $actiondeploySpecifics[] = $deploySpecific;
-    
     $entityNames[] = $entity["completename"];
-
     $params[] = array('uuid' => $entity['uuid']);
-}
+    $color = colorconf(100);
+    if (isset($identity[$id_entity])){
+        $conformite = $identity[$id_entity]['conformite'];
+        $color = colorconf(intval($conformite));
+        $totalmach=intval($identity[$id_entity]['totalmach']);
+        $nbupdateentity=intval($identity[$id_entity]['nbupdate']);
 
-foreach ($entitycompliances as $entitycompliance) {
-    $compliancerate = $entitycompliance['conformite'];
-    switch(intval($compliancerate)){
-        case $compliancerate <= 10:
-            $color = "#ff0000";
-            break;
-        case $compliancerate <= 20:
-            $color = "#ff3535";
-            break;
-        case $compliancerate <= 30:
-            $color = "#ff5050";
-            break;
-        case $compliancerate <= 40:
-            $color = "#ff8080";
-            break;
-        case $compliancerate <  50:
-            $color = "#ffA0A0";
-            break;
-        case $compliancerate <=  60:
-            $color = "#c8ffc8";
-            break;
-        case $compliancerate <= 70:
-            $color = "#97ff97";
-            break;
-        case $compliancerate <= 80:
-            $color = "#64ff64";
-            break;
-        case $compliancerate <=  90:
-            $color = "#2eff2e";
-            break;
-        case $compliancerate >90:
-            $color = "#00ff00";
-            break;
+    }else{
+        $conformite = "100";
+        $totalmach=0;
+        $nbupdateentity=0;
     }
-    $complRates[] = "<div class='progress' style='width: ".$compliancerate."%; background : ".$color."; font-weight: bold; color : white; text-align: right;'> ".$compliancerate."% </div>";
-    $totalMachine[] = $entitycompliance['totalmach'];
-    $nbupdate[] = $entitycompliance['nbupdate'];
+    $complRates[] ="<div class='progress' style='width: ".$conformite."%; background : ".$color."; font-weight: bold; color : white; text-align: right;'> ".$conformite."% </div>";
+    $totalMachine[] = $totalmach;
+    $nbupdate[] = $nbupdateentity ;
 }
 
-echo "<pre>";
-// print_r($entities);
-// print_r($entitycompliances);
-// print_r($totalMachine);
-echo "</pre>";
+$n = new OptimizedListInfos($entityNames, _T("Entity name", "updates"));
+$n->disableFirstColumnActionLink();
+
+$n->addExtraInfo($complRates, _T("Compliance rate", "updates"));
+$n->addExtraInfo($nbupdate, _T("Missing Updates", "updates"));
+$n->addExtraInfo($totalMachine, _T("Total Machines", "updates"));
+
+$n->setItemCount($count);
+$n->setNavBar(new AjaxNavBar($count, $filter));
+$n->setParamInfo($params);
+
+$n->addActionItemArray($actiondetailsByMachs);
+$n->addActionItemArray($actiondetailsByUpds);
+$n->addActionItemArray($actiondeployAlls);
+$n->addActionItemArray($actiondeploySpecifics);
+$n->display();
+?>
+$deployAll;
+    $actiondeploySpecifics[] = $deploySpecific;
+    $entityNames[] = $entity["completename"];
+    $params[] = array('uuid' => $entity['uuid']);
+    $color = colorconf(100);
+    if (isset($identity[$id_entity])){
+        $conformite = $identity[$id_entity]['conformite'];
+        $color = colorconf(intval($conformite));
+        $totalmach=intval($identity[$id_entity]['totalmach']);
+        $nbupdateentity=intval($identity[$id_entity]['nbupdate']);
+
+    }else{
+        $conformite = "100";
+        $totalmach=0;
+        $nbupdateentity=0;
+    }
+    $complRates[] ="<div class='progress' style='width: ".$conformite."%; background : ".$color."; font-weight: bold; color : white; text-align: right;'> ".$conformite."% </div>";
+    $totalMachine[] = $totalmach;
+    $nbupdate[] = $nbupdateentity ;
+}
 
 $n = new OptimizedListInfos($entityNames, _T("Entity name", "updates"));
 $n->disableFirstColumnActionLink();
