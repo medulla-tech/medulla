2568c1228b289f848597e967d2f42eac2eb94011
SPO <stephane.pointu@siveo.net>
2022-11-22 15:51:40 +1300

    [updates] Steps for user notifications (#394)
    * [update] formulaire step
    
    * [user notifications] Review user notifications step
    
    Co-authored-by: jfkneib <jean-francois.kneib@siveo.net>

--- web/modules/pkgs/includes/actions/action_notification.php
+++ web/modules/pkgs/includes/actions/action_notification.php
@@ -5,9 +5,11 @@ require_once("../../../../includes/xmlrpc.inc.php");
 require_once("../../../../includes/i18n.inc.php");
 
 extract($_POST);
-
+//     if( base64_decode($message, true) != false){
+//                         $message = base64_decode($message);
+//     }
     $message = (isset($message)) ? base64_decode($message) : "" ;
-
+    $titlemessage= (isset($message)) ? base64_decode($message) : "" ;
         $packageList = xmpp_packages_list();
         $options = "";
 
@@ -23,7 +25,7 @@ extract($_POST);
 $lab =  (isset($actionlabel))? $actionlabel : uniqid();
 ?>
 <div class="header">
-    <h1><?php echo _T('User Notification', 'pkgs'); ?></h1>
+    <h1 data-title="<?php echo _T('Send a notification to the connected user', 'pkgs'); ?>"><?php echo _T('User Notification', 'pkgs'); ?></h1>
 </div>
 <div class="content">
 
@@ -34,18 +36,66 @@ $lab =  (isset($actionlabel))? $actionlabel : uniqid();
     <table id="tableToggle">
         <tr class="toggleable">
             <th><?php echo _T('Step label: ', 'pkgs'); ?></th>
-            <th><input id="laction" type="text" name="actionlabel" value="<?php echo $lab; ?>"/></th>
+            <th><input id="laction" type="text" name="actionlabel" value="<?php echo $lab; ?>"/>
+            </th>
+        </tr>
+
+
+
+
+         <?php
+            $sizeheader = (isset($sizeheader)) ? $sizeheader : 15;
+            $sizemessage = (isset($sizemessage)) ? $sizemessage : 10;
+        ?>
+        <tr>
+            <th>
+                    <?php echo _T('Message box title', 'pkgs'); ?>
+            </th>
+            <th>
+                <span  data-title="<?php echo _T('Insert title of message box', 'pkgs'); ?>">
+                    <textarea class="special_textarea" name="titlemessage" ><?php echo $titlemessage; ?></textarea>
+                </span>
+                <span  data-title="<?php echo _T('Define text size for message box title', 'pkgs'); ?>">
+                    <?php echo _T('Text size', 'pkgs'); ?>
+                    <?php echo'<input style="width:35px;" type="number"  value="'.$sizeheader.'" name="sizeheader" min=10 max=20 />'; ?>
+                </span>
+            </th>
         </tr>
         <tr>
-            <th><?php echo _T('Message', 'pkgs'); ?></th>
             <th>
-                <textarea class="special_textarea" name="message" ><?php echo $message; ?></textarea>
+                    <?php echo _T('Message', 'pkgs'); ?>
             </th>
+            <th>
+                <span  data-title="<?php echo _T('Insert message for user', 'pkgs'); ?>">
+                    <textarea class="special_textarea" name="message" ><?php echo $message; ?></textarea>
+                </span>
+                 <span  data-title="<?php echo _T('Define text size for message', 'pkgs'); ?>">
+                    <?php echo _T('Text size', 'pkgs'); ?>
+                    <?php echo'<input style="width:35px;" type="number"  value="'.$sizemessage.'" name="sizemessage" min=7 max=15 />'; ?>
+                </span>
+            </th>
+        </tr>
+
+        <tr>
+          <?php
+            $textbuttonyes = (isset($textbuttonyes)) ? $textbuttonyes : "OK";
+
+            echo '<th>';
+            echo _T("True button text","pkgs").'</th>';
+            echo '<th>';
+           ?>
+             <span  data-title="<?php echo _T('Define text to be shown on button returning True', 'pkgs'); ?>">
+            <?php echo'<input  type="text"  value="'.$textbuttonyes.'" name="textbuttonyes"  />'; ?>
+            </span>
+            <?php
+            echo '</th>';
+            ?>
         </tr>
 
 
         <tr class="toggleable">
             <?php
+            $textinputcasecoche=_T("Set maximum time waiting for a response","pkgs");
             if(isset($timeout))
             {
                 echo '
@@ -58,9 +108,9 @@ $lab =  (isset($actionlabel))? $actionlabel : uniqid();
                         jQuery(this).closest(\'td\').next().find(\'input\').prop(\'disabled\',true);
                     }" />'._T("Set timeout (in seconds)","pkgs").'
                 </td>
-                <td>
-                    <input " type="number" min="0" value="'.$timeout.'" name="timeout"  />
-                </td>';
+                <td><span data-title="'.$textinputcasecoche.'">'.
+                    '<input " type="number" min="0" value="'.$timeout.'" name="timeout"  />
+                </span></td>';
             }
             else{
                 echo '
@@ -73,8 +123,8 @@ $lab =  (isset($actionlabel))? $actionlabel : uniqid();
                         jQuery(this).closest(\'td\').next().find(\'input\').prop(\'disabled\',true);
                     }" />'._T("Set timeout (in seconds)","pkgs").'
                 </td>
-                <td>
-                    <input type="number" min="0" value="800"  name="timeout"  />
+                <td><span data-title="'.$textinputcasecoche.'">'.
+                    '<input type="number" min="0" value="800"  name="timeout"  />
                 </td>';
             }
             ?>
@@ -88,8 +138,12 @@ $lab =  (isset($actionlabel))? $actionlabel : uniqid();
         <!-- Option timeout -->
     </div>
 
+    <span  data-title="<?php echo _T('Delete this step', 'pkgs'); ?>">
     <input  class="btn btn-primary" type="button" onclick="jQuery(this).parent().parent('li').detach()" value="<?php echo _T("Delete", "pkgs");?>" />
+    </span>
+    <span  data-title="<?php echo _T('Show additional options for this step', 'pkgs'); ?>">
     <input  class="btn btn-primary" id="property" onclick='jQuery(this).parent().find(".toggleable").each(function(){ jQuery(this).toggle()});' type="button" value="<?php echo _T("Options", "pkgs");?>" />
+    </span>
 </div>
 
 <script type="text/javascript">
