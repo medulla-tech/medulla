05d5bc1cd3811a17529f6e6294382b3cced7e3c8
Nicolas Lécureuil <neoclust@mageia.org>
2023-05-03 09:42:38 +0200

    Fix imports to be python3 compatible

--- services/mmc/plugins/xmppmaster/master/__init__.py
+++ services/mmc/plugins/xmppmaster/master/__init__.py
@@ -2,5 +2,5 @@
 # SPDX-FileCopyrightText: 2016-2023 Siveo <support@siveo.net>
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-from lib import utils
-from lib import managesession
+from mmc.plugins.xmppmaster.master.lib  import utils
+from mmc.plugins.xmppmaster.master.lib  import managesession
