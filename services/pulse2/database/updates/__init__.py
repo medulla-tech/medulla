bfef233ca5a83f633bb4497978aa2f4e2e8f42f7
botheis <noel.kneib@siveo.net>
2023-06-21 16:57:01 +0200

    [UPDATES]Add update into gray list if unban
    When the update is unbanned, it is automatically added to the gray_list

--- services/pulse2/database/updates/__init__.py
+++ services/pulse2/database/updates/__init__.py
@@ -360,14 +360,19 @@ class UpdatesDatabase(DatabaseHelper):
     @DatabaseHelper._sessionm
     def delete_rule(self, session, id):
         try:
+            sql_add = """INSERT INTO xmppmaster.up_gray_list (updateid, kb, revisionid, title, updateid_package, payloadfiles)
+        SELECT xmppmaster.up_packages.updateid, xmppmaster.up_packages.kb, xmppmaster.up_packages.revisionid, xmppmaster.up_packages.title, xmppmaster.up_packages.updateid_package, xmppmaster.up_packages.payloadfiles FROM xmppmaster.up_packages
+JOIN xmppmaster.up_black_list ON xmppmaster.up_packages.updateid = xmppmaster.up_black_list.updateid_or_kb or xmppmaster.up_packages.kb = xmppmaster.up_black_list.updateid_or_kb WHERE xmppmaster.up_black_list.id=%s;"""%(id)
+
             sql="""DELETE FROM `xmppmaster`.`up_black_list` WHERE (`id` = '%s');"""%(id)
 
+            result = session.execute(sql_add)
             result = session.execute(sql)
             session.commit()
             session.flush()
             return True
 
-        except Exception, e:
+        except Exception as e:
             logging.getLogger().error(str(e))
         return False
 
