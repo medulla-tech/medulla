1d2f3f600c4fd1307748d8869239b205a7cb37e3
botheis <noel.kneib@siveo.net>
2023-06-26 23:37:50 +0200

    [Update] Update trigger up_black_list_AFTER_INSERT
    This trigger is created in schema-073 and need some modifications

--- services/contrib/xmppmaster/sql/schema-077.sql
+++ services/contrib/xmppmaster/sql/schema-077.sql
@@ -146,6 +146,66 @@ END$$
 DELIMITER ;
 ;
 
+
+DROP TRIGGER IF EXISTS `xmppmaster`.`up_black_list_AFTER_INSERT`;
+
+DELIMITER $$
+USE `xmppmaster`$$
+CREATE DEFINER=`root`@`localhost` TRIGGER `xmppmaster`.`up_black_list_AFTER_INSERT` AFTER INSERT ON `up_black_list` FOR EACH ROW
+BEGIN
+	DELETE FROM `xmppmaster`.`up_gray_list`
+	WHERE
+		`updateid` IN (SELECT
+			updateid_or_kb
+		FROM
+			xmppmaster.up_black_list
+		WHERE
+			userjid_regexp = '.*'
+			AND type_rule = 'id');
+	UPDATE `xmppmaster`.`up_gray_list_flop`
+	SET
+		`updateid` = LEFT(UUID(), 8),
+		`kb` = 'bidon'
+	WHERE
+		`updateid` IN (SELECT
+				updateid_or_kb
+			FROM
+				xmppmaster.up_black_list
+			WHERE
+				userjid_regexp = '.*'
+					AND type_rule = 'id');
+
+	DELETE FROM `xmppmaster`.`up_gray_list`
+	WHERE
+		`kb` IN (SELECT
+			updateid_or_kb
+		FROM
+			xmppmaster.up_black_list
+
+		WHERE
+			userjid_regexp = '.*'
+			AND type_rule = 'kb');
+
+	UPDATE `xmppmaster`.`up_gray_list_flop`
+	SET
+		`kb` = 'bidon'
+	WHERE
+		`updateid` IN (SELECT
+				updateid_or_kb
+			FROM
+				xmppmaster.up_black_list
+			WHERE
+				userjid_regexp = '.*'
+					AND type_rule = 'kb');
+	DELETE FROM `xmppmaster`.`up_gray_list_flop`
+	WHERE
+		(`kb` = 'bidon');
+
+   DELETE FROM `xmppmaster`.`up_white_list` WHERE (`updateid` = new.updateid_or_kb or `kb` = new.updateid_or_kb );
+
+END$$
+DELIMITER ;
+
 -- ----------------------------------------------------------------------
 -- Database version
 -- ----------------------------------------------------------------------
