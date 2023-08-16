e37a78a8213d0c1d369ea2681cc016bef5d00ff4
kneib jean-francois <jean-francois.kneib@siveo.net>
2023-08-16 17:21:23 +0200

    [update] Désinstallation des packages inutilisés depuis plus de 10 jours
    Cet événement automatisé, nommé "Uninstall_unused_packages", vise à optimiser l'utilisation des ressources en supprimant les packages qui n'ont pas été demandés ou utilisés depuis plus de 10 jours.
    
    L'événement surveille les enregistrements dans la base de données et effectue les actions suivantes :
     - Pour les packages non demandés par aucune machine depuis 10 jours, il bascule dans la "graylistflipflop" (liste grise) et les désinstalle.
     - Les packages en liste blanche ("white list") ne sont jamais désinstallés.
     - Si des demandes sont présentes dans la table "up_machine_windows" mais ne sont pas satisfaites, ces demandes sont supprimées.
     - Si une machine refait une demande ultérieurement, le package est automatiquement réinséré dans la "graylist" et réinstallé.
    
    Cet événement contribue à optimiser l'espace et les ressources en supprimant les packages inutilisés et en maintenant la liste des packages pertinente et efficace. Il garantit également que les demandes non satisfaites sont gérées de manière appropriée.
    
    Notez que l'événement fonctionne de manière automatique et continue, contribuant ainsi à la maintenance 1 list beaucoup plus humaine en nombre de package deployable.

--- services/contrib/xmppmaster/sql/schema-081.sql
+++ services/contrib/xmppmaster/sql/schema-081.sql
@@ -450,6 +450,40 @@ where xmppmaster.deploy.id in (
 DELETE FROM msc.commands where package_id like new.updateid_or_kb and end_date > now() ;
 END$$
 DELIMITER ;
+-- ----------------------------------------------------------------------
+-- Database additionne events
+-- ----------------------------------------------------------------------
+-- [Event] Désinstallation des packages inutilisés depuis plus de 10 jours
+
+-- Cet événement automatisé, nommé "Uninstall_unused_packages", vise à optimiser l'utilisation des ressources en supprimant les packages qui n'ont pas été demandés ou utilisés depuis plus de 10 jours.
+
+-- L'événement surveille les enregistrements dans la base de données et effectue les actions suivantes :
+-- - Pour les packages non demandés par aucune machine depuis 10 jours, il bascule dans la "graylistflipflop" (liste grise) et les désinstalle.
+-- - Les packages en liste blanche ("white list") ne sont jamais désinstallés.
+-- - Si des demandes sont présentes dans la table "up_machine_windows" mais ne sont pas satisfaites, ces demandes sont supprimées.
+-- - Si une machine refait une demande ultérieurement, le package est automatiquement réinséré dans la "graylist" et réinstallé.
+
+-- Cet événement contribue à optimiser l'espace et les ressources en supprimant les packages inutilisés et en maintenant la liste des packages pertinente et efficace. Il garantit également que les demandes non satisfaites sont gérées de manière appropriée.
+
+-- Notez que l'événement fonctionne de manière automatique et continue, contribuant ainsi à la maintenance 1 list beaucoup plus humaine en nombre de package deployable.
+
+DROP EVENT IF EXISTS   xmppmaster.Uninstall_old_package;
+DELIMITER $$
+CREATE EVENT xmppmaster.Uninstall_old_package
+ON SCHEDULE EVERY 1 DAY -- Planification chaque jour
+DO
+BEGIN
+    -- Verification que les evenement soit activer.
+    -- command SHOW VARIABLES LIKE 'event_scheduler';
+    -- pour passer event_scheduler a ON
+    -- SET GLOBAL event_scheduler = ON;
+    DELETE FROM xmppmaster.up_machine_windows WHERE xmppmaster.up_machine_windows.update_id IN
+(SELECT t1.updateid FROM xmppmaster.up_gray_list t1 WHERE
+t1.updateid NOT IN (SELECT  updateid  FROM  xmppmaster.up_white_list)
+AND NOW() > t1.validity_date);
+   delete from `xmppmaster`.`up_gray_list` where now() > validity_date;
+END$$
+DELIMITER ;
 
 -- ----------------------------------------------------------------------
 -- Database version
