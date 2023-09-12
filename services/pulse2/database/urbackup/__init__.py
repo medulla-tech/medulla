83c4dc3a1dc6e26d931e460ff30111fcb350fdbe
Hruund <william.vasselin@gmail.com>
2023-09-12 17:17:26 +0200

    (URBACKUP) Add function to enable or disable client on database
    On Database urbackup, we can select state of client and we can edit state on client with his id

--- services/pulse2/database/urbackup/__init__.py
+++ services/pulse2/database/urbackup/__init__.py
@@ -66,6 +66,40 @@ class UrbackupDatabase(DatabaseHelper):
         if not ret:
             raise "Database urbackup connection error"
         return ret
+    
+    @DatabaseHelper._sessionm
+    def getClientStatus(self, session, client_id):
+        try:
+            sql="""SELECT * FROM urbackup.client_state WHERE client_id = '%s';"""%(client_id)
+
+            resultquery = session.execute(sql)
+            session.commit()
+            session.flush()
+            
+            result = [{column: value for column,
+                value in rowproxy.items()}
+                        for rowproxy in resultquery]
+            
+        except Exception as e:
+            logging.getLogger().error(str(e))
+            
+        return result
+    
+    @DatabaseHelper._sessionm
+    def editClientState(self, session, state, client_id):
+        try:
+            sql="""UPDATE client_state SET state = '%s' WHERE client_id = '%s';"""%(state, client_id)
+
+            session.execute(sql)
+            session.commit()
+            session.flush()
+            
+            return True
+            
+        except Exception as e:
+            logging.getLogger().error(str(e))
+            
+            return False
 
     # =====================================================================
     # urbackup FUNCTIONS
