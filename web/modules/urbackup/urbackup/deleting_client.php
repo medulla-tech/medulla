1505ce583fc371d7c216f460c530a2bd9b5400a4
Hruund <william.vasselin@gmail.com>
2023-09-13 11:34:00 +0200

    (URBACKUP) Make sure computer is online before edit state on urbackup database
    We go to check on xmppmaster database machines table before edit state, enable or disable, on urbackup database.
    
    (cherry picked from commit 36594a51f78ecfe88bc1fb4313ab2d792571130a)
    (cherry picked from commit 10640164c1214fbc55d7f020564169d1ab8ea4e0)

--- web/modules/urbackup/urbackup/deleting_client.php
+++ web/modules/urbackup/urbackup/deleting_client.php
@@ -11,31 +11,37 @@ $clientname = htmlspecialchars($_GET["clientname"]);
 $editclient = htmlspecialchars($_GET["editclient"]);
 $authkey = htmlspecialchars($_GET["authkey"]);
 
-$p = new PageGenerator(_T("Delete group", 'urbackup'));
+$p = new PageGenerator(_T("Enable or disable client", 'urbackup'));
 $p->setSideMenu($sidemenu);
 $p->display();
 
-if ($editclient == "disable")
+$clientEnable = xmlrpc_getComputersEnableValue($jidMachine);
+
+if ($clientEnable['0']['enabled'] == '1')
 {
-    $clientRemove = xmlrpc_remove_client($jidMachine, $clientid);
-    $editStateClient = "disable";
-}
+    if ($editclient == "disable")
+    {
+        $clientRemove = xmlrpc_remove_client($jidMachine, $clientid);
+        $editStateClient = "disable";
+    }
 
-if ($editclient == "enable")
+    if ($editclient == "enable")
+    {
+        $client = xmlrpc_get_client_status($clientid);
+        $client_authkey = $client["0"]["authkey"];
+
+        $clientAdd = xmlrpc_enable_client($jidMachine, $clientid, $client_authkey);
+        $editStateClient = "enable";
+    }
+
+    $url = 'main.php?module=urbackup&submod=urbackup&action=list_backups&clientid='.$clientid.'&clientname='.$clientname.'&groupname='.$groupname.'&jidmachine='.$jidMachine.'&editStateClient='.$editStateClient.'&error=false';
+}
+else
 {
-    $client = xmlrpc_get_client_status($clientid);
-    $client_authkey = $client["0"]["authkey"];
-
-    $clientAdd = xmlrpc_enable_client($jidMachine, $clientid, $client_authkey);
-    $editStateClient = "enable";
+    $url = 'main.php?module=urbackup&submod=urbackup&action=list_backups&clientid='.$clientid.'&clientname='.$clientname.'&groupname='.$groupname.'&jidmachine='.$jidMachine.'&error=true';
 }
-
-
 ?>
 <br>
 <?php
-
-$url = 'main.php?module=urbackup&submod=urbackup&action=list_backups&clientid='.$clientid.'&clientname='.$clientname.'&groupname='.$groupname.'&jidmachine='.$jidmachine.'&editStateClient='.$editStateClient;
-
 header("Location: ".$url);
 ?>
