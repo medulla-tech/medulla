023aa43cb2ec54dd4997e57930a83a35172b3132
Adrien T. <a.thaissen@gmail.com>
2023-11-08 14:50:38 +0100

    (FIX) PHP Fatal error to call safeCount() missing require

--- web/modules/pkgs/includes/actions/actionprocessscript.php
+++ web/modules/pkgs/includes/actions/actionprocessscript.php
@@ -26,6 +26,7 @@ require_once("../xmlrpc.php");
 require_once("../../../../includes/session.inc.php");
 require_once("../../../../includes/xmlrpc.inc.php");
 require_once("../../../../includes/i18n.inc.php");
+require_once("../../../../includes/utils.inc.php");
 
 extract($_POST);
 $gotoreturncode = [];
