349d8a7acfc24a12f13e399fbe6763273890f1d7
Nicolas Lécureuil <neoclust@mageia.org>
2024-01-22 23:24:27 +0100

    Add autogenerated documentation
    (cherry picked from commit fa1876177d4dc8a4e56cc54f1d4b9d496aea7e12)
    (cherry picked from commit 712f30da36e6a6b3e071d596c0681d4a6f4df215)

--- services/mmc/plugins/urbackup/urwrapper.py
+++ services/mmc/plugins/urbackup/urwrapper.py
@@ -13,6 +13,42 @@ except BaseException:
 
 
 class UrApiWrapper:
+    """
+    Wrapper for UrBackup API.
+
+    Attributes:
+        url (str): UrBackup server URL.
+        user_login (str): UrBackup server username.
+        password (str): UrBackup server password.
+        ses (str): Session ID.
+        headers (dict): HTTP headers for requests.
+        verify (bool): SSL certificate verification.
+        allow_redirects (bool): Allow redirects in HTTP requests.
+
+    Methods:
+        set_header(self, key, value): Set a custom header.
+        request(self, action, params, method="POST"): Make a request to the UrBackup API.
+        login(self, lang="en"): Log in to the UrBackup server.
+        get_session(self): Get the current session ID.
+        response(resp): Parse and return a standardized response.
+        get_logs(self, clientid=0): Get live logs for a client.
+        add_client(self, clientname): Add a new client to UrBackup.
+        get_stats(self): Get server usage statistics.
+        add_group(self, groupname): Add a new group to UrBackup.
+        remove_group(self, groupid): Remove a group from UrBackup.
+        get_settings_general(self): Get general settings.
+        save_settings(self, clientid, name_data, value_data): Save client settings.
+        get_settings_clientsettings(self, id_client): Get client-specific settings.
+        get_settings_clients(self): Get a list of clients.
+        get_backups(self, client_id): Get a list of backups for a client.
+        delete_backup(self, client_id, backup_id): Delete a specific backup.
+        get_backup_files(self, client_id, backup_id, path): Get files from a backup.
+        client_download_backup_file(self, client_id, backup_id, path, filter_path): Download a specific file from a backup.
+        client_download_backup_file_shahash(self, client_id, backup_id, path, shahash): Download a file by SHA hash from a backup.
+        get_progress(self): Get the progress of ongoing operations.
+        get_status(self): Get the status of the UrBackup server.
+        create_backup(self, type_backup, client_id): Start a new backup operation.
+    """
     url = ""
     user_login = ""
     password = ""
@@ -22,6 +58,9 @@ class UrApiWrapper:
     allow_redirects = True
 
     def __init__(self):
+        """
+        Initialize UrApiWrapper with configuration from UrbackupConfig.
+        """
         _config = config.UrbackupConfig()
         self.url = _config.urbackup_url
         self.user_login = _config.urbackup_username
@@ -35,9 +74,27 @@ class UrApiWrapper:
         self.allow_redirects = True
 
     def set_header(self, key, value):
+        """
+        Set a custom header for the HTTP request.
+
+        Args:
+            key (str): Header key.
+            value (str): Header value.
+        """
         self.headers[key] = value
 
     def request(self, action, params, method="POST"):
+        """
+        Make a request to the UrBackup API.
+
+        Args:
+            action (str): API action to perform.
+            params (dict): Parameters for the API action.
+            method (str): HTTP method (default is "POST").
+
+        Returns:
+            Response: HTTP response object.
+        """
         url = self.url + "?" + urlencode({"a": action})
 
         if method == "GET":
@@ -60,6 +117,15 @@ class UrApiWrapper:
         return response
 
     def login(self, lang="en"):
+        """
+        Log in to the UrBackup server.
+
+        Args:
+            lang (str): Language for the login (default is "en").
+
+        Returns:
+            Response: HTTP response object.
+        """
         params = {
             "username": self.user_login,
             "password": self.password,
@@ -78,12 +144,27 @@ class UrApiWrapper:
         return response
 
     def get_session(self):
+        """
+        Get the current session ID.
+
+        Returns:
+            str: Session ID.
+        """
         self.login()
         session = self.ses
         return session
 
     @staticmethod
     def response(resp):
+        """
+        Parse and return a standardized response.
+
+        Args:
+            resp (Response): HTTP response object.
+
+        Returns:
+            dict: Standardized response containing status code, headers, and content.
+        """
         try:
             resp_json = json.loads(resp.text)
         except BaseException:
@@ -96,6 +177,15 @@ class UrApiWrapper:
         }
 
     def get_logs(self, clientid=0):
+        """
+        Get live logs for a client.
+
+        Args:
+            clientid (int): Client ID (default is 0).
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {"clientid": clientid, "lastid": 0, "ses": self.ses}
         response = self.request("livelog", params)
@@ -103,6 +193,15 @@ class UrApiWrapper:
         return response
 
     def add_client(self, clientname):
+        """
+        Add a new client to UrBackup.
+
+        Args:
+            clientname (str): Name of the new client.
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {"clientname": clientname, "ses": self.ses}
         response = self.request("add_client", params)
@@ -110,6 +209,12 @@ class UrApiWrapper:
         return response
 
     def get_stats(self):
+        """
+        Get server usage statistics.
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {"ses": self.ses}
         response = self.request("usage", params)
@@ -117,6 +222,15 @@ class UrApiWrapper:
         return response
 
     def add_group(self, groupname):
+        """
+        Add a new group to UrBackup.
+
+        Args:
+            groupname (str): Name of the new group.
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {"sa": "groupadd", "name": groupname, "ses": self.ses}
         response = self.request("settings", params)
@@ -124,6 +238,15 @@ class UrApiWrapper:
         return response
 
     def remove_group(self, groupid):
+        """
+        Remove a group from UrBackup.
+
+        Args:
+            groupid (str): ID of the group to be removed.
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {"sa": "groupremove", "id": groupid, "ses": self.ses}
         response = self.request("settings", params)
@@ -131,6 +254,12 @@ class UrApiWrapper:
         return response
 
     def get_settings_general(self):
+        """
+        Get general settings.
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {"sa": "general", "ses": self.ses}
         response = self.request("settings", params)
@@ -138,6 +267,17 @@ class UrApiWrapper:
         return response
 
     def save_settings(self, clientid, name_data, value_data):
+        """
+        Save client settings.
+
+        Args:
+            clientid (str): ID of the client.
+            name_data (str): Name of the setting.
+            value_data: Value of the setting.
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {
             "sa": "clientsettings_save",
@@ -151,6 +291,15 @@ class UrApiWrapper:
         return response
 
     def get_settings_clientsettings(self, id_client):
+        """
+        Get client-specific settings.
+
+        Args:
+            id_client (str): ID of the client.
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {"sa": "clientsettings", "t_clientid": id_client, "ses": self.ses}
         response = self.request("settings", params)
@@ -158,6 +307,12 @@ class UrApiWrapper:
         return response
 
     def get_settings_clients(self):
+        """
+        Get a list of clients.
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {"sa": "listusers", "ses": self.ses}
         response = self.request("settings", params)
@@ -165,6 +320,15 @@ class UrApiWrapper:
         return response
 
     def get_backups(self, client_id):
+        """
+        Get a list of backups for a client.
+
+        Args:
+            client_id (str): ID of the client.
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {"clientid": client_id, "ses": self.ses}
         response = self.request("backups", params)
@@ -172,6 +336,16 @@ class UrApiWrapper:
         return response
 
     def delete_backup(self, client_id, backup_id):
+        """
+        Delete a specific backup.
+
+        Args:
+            client_id (str): ID of the client.
+            backup_id (str): ID of the backup to be deleted.
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {
             "sa": "backups",
@@ -184,6 +358,17 @@ class UrApiWrapper:
         return response
 
     def get_backup_files(self, client_id, backup_id, path):
+        """
+        Get files from a backup.
+
+        Args:
+            client_id (str): ID of the client.
+            backup_id (str): ID of the backup.
+            path (str): Path to the files in the backup.
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {
             "sa": "files",
@@ -197,6 +382,18 @@ class UrApiWrapper:
         return response
 
     def client_download_backup_file(self, client_id, backup_id, path, filter_path):
+        """
+        Download a specific file from a backup.
+
+        Args:
+            client_id (str): ID of the client.
+            backup_id (str): ID of the backup.
+            path (str): Path to the file in the backup.
+            filter_path (str): Filter path.
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {
             "sa": "clientdl",
@@ -211,6 +408,18 @@ class UrApiWrapper:
         return response
 
     def client_download_backup_file_shahash(self, client_id, backup_id, path, shahash):
+        """
+        Download a file by SHA hash from a backup.
+
+        Args:
+            client_id (str): ID of the client.
+            backup_id (str): ID of the backup.
+            path (str): Path to the file in the backup.
+            shahash (str): SHA hash of the file.
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {
             "sa": "clientdl",
@@ -225,6 +434,12 @@ class UrApiWrapper:
         return response
 
     def get_progress(self):
+        """
+        Get the progress of ongoing operations.
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {"ses": self.ses}
         response = self.request("progress", params)
@@ -232,6 +447,12 @@ class UrApiWrapper:
         return response
 
     def get_status(self):
+        """
+        Get the status of the UrBackup server.
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {"ses": self.ses}
         response = self.request("status", params)
@@ -239,6 +460,16 @@ class UrApiWrapper:
         return response
 
     def create_backup(self, type_backup, client_id):
+        """
+        Start a new backup operation.
+
+        Args:
+            type_backup (str): Type of backup operation.
+            client_id (str): ID of the client.
+
+        Returns:
+            Response: HTTP response object.
+        """
         self.login()
         params = {"start_type": type_backup, "start_client": client_id, "ses": self.ses}
         response = self.request("start_backup", params)
