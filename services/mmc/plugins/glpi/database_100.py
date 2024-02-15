09ae9e894f6ef1e556a83c6e69a7bb6a9abeaeb5
SPO <stephane.pointu@siveo.net>
2024-02-15 07:22:20 +0100

    (glpi) Cater for glpi versions 10 up to 10.0.12

--- services/mmc/plugins/glpi/database_100.py
+++ services/mmc/plugins/glpi/database_100.py
@@ -121,7 +121,7 @@ class Glpi100(DyngroupDatabaseHelper):
 
         if LooseVersion(self._glpi_version) >= LooseVersion("10.0") and LooseVersion(
             self._glpi_version
-        ) <= LooseVersion("10.0.10"):
+        ) <= LooseVersion("10.0.12"):
             logging.getLogger().debug("GLPI version %s found !" % self._glpi_version)
             return True
         else:
