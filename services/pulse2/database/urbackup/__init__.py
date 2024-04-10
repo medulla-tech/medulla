b9d1f7956e4de4ade76da7a3d02d5f4fd54702c0
Hruund <william.vasselin@gmail.com>
2024-04-10 11:40:37 +0200

    (urbackup) fix print syntaxe on urbackup database file
    On database urbackup module print syntax was wrong and need to have parenthese

--- services/pulse2/database/urbackup/__init__.py
+++ services/pulse2/database/urbackup/__init__.py
@@ -35,7 +35,7 @@ class UrbackupDatabase(DatabaseHelper):
             self.makeConnectionPath(),
             pool_recycle=self.config.dbpoolrecycle,
             pool_size=self.config.dbpoolsize)
-        print self.makeConnectionPath()
+        print(self.makeConnectionPath())
         if not self.db_check():
             return False
         self.metadata = MetaData(self.db)
