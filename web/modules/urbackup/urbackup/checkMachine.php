de3506bbeee0483aa8e0c694340f947d22f57dfd
Hruund <william.vasselin@gmail.com>
2024-04-17 16:15:34 +0200

    (urbackup) fix first initialization computer on urbackup
    On this first initialization, we will check if user already exist on urbackup and mysql DB and if he is in urbackup profile.
    
    If computer already exist but have not profile, we only search his internet auth key with API and insert it on mysql DB and assign it a choosen profile.
    If computer not exist on any DB, we will use API to insert it on urbackup DB and assign it a choosen profile.
    
    Urbackup DB is urbackup server with local clients (directory of backup, internet auth key and differents settings)
    Mysql DB is mysql Database named Urbackup with table client_state who contain state of urbackup service on computer, internet auth key and client ID.
    
    (cherry picked from commit de0a0440673b8068bd3050c46e7a3b271cbb034a)

--- web/modules/urbackup/urbackup/checkMachine.php
+++ web/modules/urbackup/urbackup/checkMachine.php
@@ -31,8 +31,10 @@ $p = new PageGenerator(_T("Assign profile to computer ".$clientname, 'urbackup')
 $p->setSideMenu($sidemenu);
 $p->display();
 
-$clients = xmlrpc_get_backups_all_client();
-$clients = $clients["clients"];
+$clientAndGroupsAll = xmlrpc_get_clients();
+
+$clients = $clientAndGroupsAll["navitems"]["clients"];
+$groups = $clientAndGroupsAll["navitems"]["groups"];
 
 $ini_array = parse_ini_file("/etc/mmc/plugins/urbackup.ini");
 $ini_array_local = parse_ini_file("/etc/mmc/plugins/urbackup.ini.local");
@@ -40,191 +42,84 @@ $username_urbackup = isset($ini_array_local["usernameapi"]) ? $ini_array_local["
 $password_urbackup = isset($ini_array_local["passwordapi"]) ? $ini_array_local["passwordapi"] : $ini_array["passwordapi"];
 $url_urbackup = isset($ini_array_local["url"]) ? $ini_array_local["url"] : $ini_array["url"];
 
-//-----------------------------------START LOGIN FUNCTION
-$url = $url_urbackup."?a=login";
+$id = "";
+$auth = "";
+$groupid = "";
+$groupname = "";
 
-$curlid = curl_init($url);
-
-curl_setopt($curlid, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($curlid, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($curlid, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($curlid, CURLOPT_POST, true);
-curl_setopt($curlid, CURLOPT_RETURNTRANSFER, true);
-$datas = [
-'username'=>$username_urbackup,
-'password'=>$password_urbackup,
-'plainpw'=>1
-];
-
-$urlencoded = "";
-foreach($datas as $key=>$val){
-$urlencoded .= $key.'='.$val.'&';
-}
-rtrim($urlencoded, '&');
-
-curl_setopt($curlid, CURLOPT_POSTFIELDS, $urlencoded);
-$response = curl_exec($curlid);
-
-if (curl_errno($curlid)) 
+if (count($clients) != 0)
 {
-    echo 'Requête échouée : '.curl_error($curlid).'<br>';
-    $result = [];
-}
-else
-{
-$result = (array)json_decode($response);
-}
-
-curl_close($curlid);
-
-if(isset($result['session'], $result['success']) && $result['success'] == 1){
-    $session = $result['session'];
-}
-//-----------------------------------END LOGIN
-
-//-----------------------------------START SAVE SETTINGS FUNCTION
-$url = $url_urbackup."?a=settings";
-$curlid = curl_init($url);
-
-curl_setopt($curlid, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($curlid, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($curlid, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($curlid, CURLOPT_POST, true);
-curl_setopt($curlid, CURLOPT_RETURNTRANSFER, true);
-
-$datas = [
-    'sa'=>'general',
-    'ses'=>$session,
-];
-
-$urlencoded = "";
-foreach($datas as $key=>$val){
-$urlencoded .= $key.'='.$val.'&';
-}
-rtrim($urlencoded, '&');
-
-curl_setopt($curlid, CURLOPT_POSTFIELDS, $urlencoded);
-$response = curl_exec($curlid);
-
-$result = (array)json_decode($response);
-
-curl_close($curlid);
-
-$settings = $result;
-$array = json_decode(json_encode($settings), true);
-
-$groups = $array['navitems']['groups'];
-$clients_settings = $array['navitems']['clients'];
-
-foreach ($clients_settings as $client)
-{
-    if ($client["name"] == $clientname)
+    foreach ($clients as $client)
     {
-        $id = $client["id"];
-        if ($client['groupname'] != "")
+        $clientExist = "false";
+        $clientHaveProfile = "false";
+
+        if ($client["name"] == $clientname)
         {
-            $exist = "true";
-        }
-        else
-        {
-            $exist = "false";
+            $id = $client["id"];
+            $client_info = xmlrpc_get_auth_client($id);
+            $auth = $client_info["value"];
+            $groupid = $client["group"];
+            $groupname = $client["groupname"];
+
+            $clientExist = "true";
+            
+            if ($groupname != "")
+                $clientHaveProfile = "true";
         }
     }
-    else
-        $exist = "false";
 }
 
 ?>
 <br>
 <?php
-$groupname = "";
-$url = 'main.php?module=urbackup&submod=urbackup&action=list_backups&clientid='.$id.'&clientname='.$clientname.'&groupname='.$groupname.'&jidmachine='.$jidMachine;
 
-if ($exist == "true")
+if ($clientExist = "false") 
 {
-    foreach ($clients_settings as $client)
+    $createClient = xmlrpc_add_client($clientname);
+
+    if ($createClient["added_new_client"] == 1)
     {
-        if ($client["name"] == $clientname)
-        {
-            $groupname = $client['groupname'];
-            $groupid = $client['groupname'];
-        }
+        $id = $createClient["new_clientid"];
+        $groupid = "";
+        $groupname = "";
+        $auth = $createClient["new_authkey"];
     }
-    $url = 'main.php?module=urbackup&submod=urbackup&action=list_backups&clientid='.$id.'&clientname='.$clientname.'&groupname='.$groupname.'&jidmachine='.$jidMachine;
-    header("Location: ".$url);
+}
+
+if ($groupname == "")
+{
+    $insertClientDatabase = xmlrpc_insertNewClient($id, $auth);
+
+    ?>
+    <form name="form" action="main.php?module=urbackup&amp;submod=urbackup&amp;action=add_member_togroup_aftercheck&amp;clientid=<?php echo $id; ?>&amp;clientname=<?php echo $clientname; ?>&amp;auth=<?php echo $auth; ?>&amp;groupname=<?php echo $group['name']; ?>&amp;jidmachine=<?php echo $jidMachine; ?>" method="post">
+        <div>
+            <h3><?php echo _T("Computer name", "urbackup"); ?></h3>
+            <br>
+            <p style="font-weight: bold;"><?php echo "    ".$clientname; ?></p>
+            <br>
+        </div>
+        <div>
+            <h3><?php echo _T("Choose profile to computer", "urbackup"); ?></h3>
+            <select name="group" id="group">
+                <?php
+                foreach($groups as $group)
+                {
+                    if ($group['name'] != "")
+                        echo '<option value="'.$group['id'].'">'.$group['name'].'</option>';
+                }
+                ?>
+            </select>
+            <input type="submit" value="Add <?php echo $clientname; ?> on profile">
+        </div>
+    </form>
+    <?php
 }
 else
 {
-    $create_client = xmlrpc_add_client($clientname);
-    $insertClientDatabase = xmlrpc_insertNewClient($create_client["new_clientid"], $create_client["new_authkey"]);
-
-    if ($create_client["already_exists"] == "true") 
-    {
-        foreach ($clients_settings as $client)
-        {
-            if ($client["name"] == $clientname)
-            {
-                if ($client['groupname'] == "")
-                {
-                    $check_client = xmlrpc_enable_client($jidMachine, $create_client["new_clientid"], $create_client["new_authkey"]);
-                    ?>
-                        <form name="form" action="main.php?module=urbackup&amp;submod=urbackup&amp;action=add_member_togroup_aftercheck&amp;clientid=<?php echo $client["id"]; ?>&amp;clientname=<?php echo $clientname; ?>&amp;groupname=<?php echo $group['name']; ?>&amp;jidmachine=<?php echo $jidMachine; ?>" method="post">
-                            <div>
-                                <h3><?php echo _T("Computer name", "urbackup"); ?></h3>
-                                <br>
-                                <p style="font-weight: bold;"><?php echo "    ".$clientname; ?></p>
-                                <br>
-                            </div>
-                            <div>
-                                <h3><?php echo _T("Choose profile to computer", "urbackup"); ?></h3>
-                                <select name="group" id="group">
-                                    <?php
-                                    foreach($groups as $group)
-                                    {
-                                        echo '<option value="'.$group['id'].'">'.$group['name'].'</option>';
-                                    }
-                                    ?>
-                                </select>
-                                <input type="submit" value="Add <?php echo $clientname; ?> on profile">
-                            </div>
-                        </form>
-                    <?php
-                }
-                else
-                {
-                    print_r(_T("User already exists" ,"urbackup"));
-                    $groupname = $client['groupname'];
-                    $url = 'main.php?module=urbackup&submod=urbackup&action=list_backups&clientid='.$id.'&clientname='.$clientname.'&groupname='.$groupname.'&jidmachine='.$jidMachine;
-                    header("Location: ".$url);
-                }
-            }
-        }
-    }
-    else
-    {
-        $check_client = xmlrpc_enable_client($jidMachine, $create_client["new_clientid"], $create_client["new_authkey"]);
-        ?>
-            <form name="form" action="main.php?module=urbackup&amp;submod=urbackup&amp;action=add_member_togroup_aftercheck&amp;clientid=<?php echo $create_client["new_clientid"]; ?>&amp;clientname=<?php echo $clientname; ?>&amp;groupname=<?php echo $group['name']; ?>&amp;jidmachine=<?php echo $jidMachine; ?>" method="post">
-                <div>
-                    <h3><?php echo _T("Computer name", "urbackup"); ?></h3>
-                    <br>
-                    <p style="font-weight: bold;"><?php echo "    ".$clientname; ?></p>
-                    <br>
-                </div>
-                <div>
-                    <h3><?php echo _T("Choose profile to computer", "urbackup"); ?></h3>
-                    <select name="group" id="group">
-                        <?php
-                        foreach($groups as $group)
-                        {
-                            echo '<option value="'.$group['id'].'">'.$group['name'].'</option>';
-                        }
-                        ?>
-                    </select>
-                    <input type="submit" value="Add <?php echo $create_client["new_clientname"]; ?> on profile">
-                </div>
-            </form>
-        <?php
-    }
+    //User exist and have a profile
+    $url = 'main.php?module=urbackup&submod=urbackup&action=list_backups&clientid='.$id.'&clientname='.$clientname.'&groupname='.$groupname.'&jidmachine='.$jidMachine;
+    header("Location: ".$url);
 }
+
 ?>
