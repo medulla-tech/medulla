eb0fa0dcfdd7007634e4fb767616136be7a45c47
SPO <stephane.pointu@siveo.net>
2024-05-28 16:18:27 +0200

    (QA) Update quick action that displays the last 100 lines of logs

--- services/contrib/xmppmaster/sql/schema-087.sql
+++ services/contrib/xmppmaster/sql/schema-087.sql
@@ -8,7 +8,7 @@ USE `xmppmaster`;
 
 UPDATE `qa_custom_command` SET `customcmd`='start cmd /c "sc stop medullaagent && timeout /t 5 && sc start medullaagent"', `namecmd`='Restart Medulla Agent service' WHERE `namecmd`='Restart Pulse Agent service' AND `os`='windows';
 
-UPDATE `qa_custom_command` SET `customcmd`="powershell.exe Get-Content  'C:\Program Files\Pulse\var\log\xmpp-agent-machine.log' -Tail 100" WHERE `namecmd`="Show last 100 lines of agent logs" AND `os`="windows";
+UPDATE `qa_custom_command` SET `customcmd`="powershell.exe Get-Content  'C:\Program Files\Medulla\var\log\xmpp-agent-machine.log' -Tail 100" WHERE `namecmd`="Show last 100 lines of agent logs" AND `os`="windows";
 
 UPDATE version SET Number = 87;
 
