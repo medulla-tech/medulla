d597d9e71768dd0b5a8786b5ff8a63f2dbbf6399
SPO <stephane.pointu@siveo.net>
2024-06-27 18:47:24 +0200

    (agent) Add two quick actions for managing the membership of pulseuser account to Administrators group

---
+++ services/contrib/xmppmaster/sql/schema-088.sql
@@ -0,0 +1,17 @@
+-- -*- coding: utf-8; -*-
+-- SPDX-FileCopyrightText: 2022-2024 Siveo <support@siveo.net>
+-- SPDX-License-Identifier: GPL-3.0-or-later
+
+START TRANSACTION;
+
+USE `xmppmaster`;
+
+-- ----------------------------------------------------------------------
+-- Quick actions to add or remove pulseuser to Administrators group
+-- ----------------------------------------------------------------------
+INSERT INTO `qa_custom_command` VALUES ('allusers','windows','Add SSH user to admins group','PowerShell Add-LocalGroupMember -SID S-1-5-32-544 -Member pulseuser -Verbose','Add pulseuser account to Administrators group');
+INSERT INTO `qa_custom_command` VALUES ('allusers','windows','Remove SSH user from admins group','PowerShell Remove-LocalGroupMember -SID S-1-5-32-544 -Member pulseuser -Verbose','Remove pulseuser account from Administrators group');
+
+UPDATE version SET Number = 88;
+
+COMMIT;
