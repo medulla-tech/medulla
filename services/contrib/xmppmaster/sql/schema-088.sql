531c13c07573bd45f0ff8af3ab45c88d8c491d14
Nicolas Lécureuil <neoclust@mageia.org>
2024-07-18 10:56:12 +0200

    (xmppmaster SQL scheme) Add and update VNC quick action
    Fix the path for tnvserver in quick action
    Add third screen choice
    
    backport of commit d35596427d7e950ffb176f9124fcbe8f221b3a89

---
+++ services/contrib/xmppmaster/sql/schema-088.sql
@@ -0,0 +1,16 @@
+-- -*- coding: utf-8; -*-
+-- SPDX-FileCopyrightText: 2022-2024 Siveo <support@siveo.net>
+-- SPDX-License-Identifier: GPL-3.0-or-later
+
+START TRANSACTION;
+
+USE `xmppmaster`;
+
+UPDATE `qa_custom_command` SET `customcmd`='\"c:\\Progra~1\\TightVNC\\tvnserver.exe\" -controlservice -shareprimary' WHERE `namecmd`='VNC view primary screen';
+UPDATE `qa_custom_command` SET `customcmd`='\"c:\\Progra~1\\TightVNC\\tvnserver.exe\" -controlservice -sharedisplay 1' WHERE `namecmd`='VNC view secondary screen';
+UPDATE `qa_custom_command` SET `customcmd`='\"c:\\Progra~1\\TightVNC\\tvnserver.exe\" -controlservice -sharefull' WHERE `namecmd`='VNC view all screen';
+INSERT INTO `qa_custom_command` VALUES ('allusers','windows','VNC view third screen','\"c:\\Progra~1\\TightVNC\\tvnserver.exe\" -controlservice -sharedisplay 2','Display third screen');
+
+UPDATE version SET Number = 88;
+
+COMMIT;
