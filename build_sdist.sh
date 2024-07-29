8d7c8305a6b3a37ccbc98baa4cb853660d21b7b2
Nicolas Lécureuil <neoclust@mageia.org>
2024-07-29 18:20:26 +0200

    Fix version build_sdist.sh

--- build_sdist.sh
+++ build_sdist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION='5.0.0'
+VERSION='5.1.1'
 
 rm -f medulla-*.tar.gz medulla-*.tar.gz.md5
 git clean -fdx && ./autogen.sh && ./configure --sysconfdir=/etc --localstatedir=/var --disable-python-check --disable-conf && make distcheck
