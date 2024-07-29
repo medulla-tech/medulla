1f07cd0706b8547efc569780b4aafef0a3e2e50f
Nicolas Lécureuil <neoclust@mageia.org>
2024-07-29 18:22:55 +0200

    Change version to 5.1.2

--- web/modules/admin/infoPackage.inc.php
+++ web/modules/admin/infoPackage.inc.php
@@ -21,7 +21,7 @@
 require_once("modules/medulla_server/version.php");
 
 $mod = new Module("admin");
-$mod->setVersion("5.1.1");
+$mod->setVersion("5.1.2");
 //$mod->setRevision('');
 $mod->setDescription(_T("Admin", "admin"));
 $mod->setAPIVersion("1:0:0");
