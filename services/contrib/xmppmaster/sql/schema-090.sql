1c960346dd5b0006fd33d2977be02bd55b03dc91
SPO <stephane.pointu@siveo.net>
2024-08-09 14:33:31 +0200

    (updates) Rewrite up_search_kb_update procedure Use the max(revisionid) from the group by rather than from the self join

--- services/contrib/xmppmaster/sql/schema-090.sql
+++ services/contrib/xmppmaster/sql/schema-090.sql
@@ -237,6 +237,111 @@ CREATE VIEW IF NOT EXISTS up_machine_activated AS(
 ALTER TABLE relayserver ADD COLUMN `ssh_public_key` TEXT NULL;
 
 
+-- up_search_kb_update procedure
+USE `xmppmaster`;
+DROP procedure IF EXISTS `up_search_kb_update`;
+
+USE `xmppmaster`;
+DROP procedure IF EXISTS `xmppmaster`.`up_search_kb_update`;
+
+DELIMITER $$
+USE `xmppmaster`$$
+CREATE PROCEDURE `up_search_kb_update`(in tablesearch varchar(1024), in KB_LIST varchar(2048) )
+BEGIN
+    DECLARE result VARCHAR(50);
+    -- Vérifier si KB_LIST est vide
+    end_procedure:
+    BEGIN
+        IF KB_LIST IS NULL OR KB_LIST = '' THEN
+             -- Sortir de la procédure
+             LEAVE end_procedure;
+        END IF;
+          CASE
+          WHEN tablesearch IN ( 'up_packages_Win10_X64_21H1',
+                                  'up_packages_Win10_X64_21H2',
+                                  'up_packages_Win10_X64_22H2',
+                                  'up_packages_Win11_X64',
+                                  'up_packages_Win10_X64_1903') THEN
+             SET @query = concat("
+                 SELECT
+                     *
+                 FROM
+                     ",tablesearch," vv
+                 WHERE
+                     revisionid in(  SELECT
+                                       max(revisionid)
+                                       FROM
+                                           ",tablesearch,"
+                                       WHERE
+                                           supersededby = ''
+                                       GROUP BY SUBSTRING(SUBSTRING_INDEX(title, '(', 1), 9)
+                                       ) and
+                     vv.KB NOT IN (",KB_LIST,");");
+        WHEN  tablesearch IN ( 'up_packages_office_2003_64bit',
+                              'up_packages_office_2007_64bit',
+                              'up_packages_office_2010_64bit',
+                              'up_packages_office_2013_64bit',
+                              'up_packages_office_2016_64bit') THEN
+             SET @query = concat("
+                 SELECT
+                     *
+                 FROM
+                     ",tablesearch," vv
+                 WHERE
+                     revisionid in(  SELECT
+                                           max(revisionid)
+                                       FROM
+                                           ",tablesearch,"
+                                       WHERE
+                                           supersededby = '' and updateclassification like 'Security Updates'
+                                    GROUP BY SUBSTRING(SUBSTRING_INDEX(title, '(', 1), 31)
+                                       ) and
+                     vv.KB NOT IN (",KB_LIST,");");
+        WHEN  tablesearch IN ( 'up_packages_Vstudio_2005',
+                              'up_packages_Vstudio_2008',
+                              'up_packages_Vstudio_2010',
+                              'up_packages_Vstudio_2012',
+                              'up_packages_Vstudio_2013',
+                              'up_packages_Vstudio_2015',
+                              'up_packages_Vstudio_2017',
+                              'up_packages_Vstudio_2019',
+                              'up_packages_Vstudio_2022') THEN
+             SET @query = concat("
+                 SELECT
+                     *
+                 FROM
+                     ",tablesearch," vv
+                 WHERE
+                     revisionid in(  SELECT
+                                           max(revisionid)
+                                       FROM
+                                           ",tablesearch,"
+                                       WHERE
+                                           supersededby = '' and updateclassification like 'Security Updates'
+                                    GROUP BY SUBSTRING(SUBSTRING_INDEX(title, '(', 1), 38)
+                                       ) and
+                     vv.KB NOT IN (",KB_LIST,");");
+        WHEN  tablesearch IN ( 'up_packages_Win_Malicious_X64') THEN
+             SET @query = concat("
+                 SELECT
+                     *
+                 FROM
+                     xmppmaster.up_packages_Win_Malicious_X64
+                 WHERE
+                     KB NOT IN (",KB_LIST,")
+                 ORDER BY revisionid DESC
+                 LIMIT 1;");
+        ELSE
+             SET @query = concat("SELECT * FROM xmppmaster.update_data");
+        END CASE;
+     PREPARE stmt FROM @query;
+     EXECUTE stmt ;
+    END;
+END$$
+
+DELIMITER ;
+
+
 UPDATE version SET Number = 90;
 
 COMMIT;
