pulse-project/pulse2

commit 7bce94d0b4c1aff1a946f6e0a3a489eac23bdda8

(updates) Title problem
Fix: use the right translation catalog on some updates titles

web/modules/updates/updates/deploySpecificUpdate.php
@@ -30,7 +30,7 @@ if(!empty($_GET['entity'])) {
     $entity = htmlentities($_GET['entity']);
     $completename = htmlentities($_GET['completename']);
     $params = ["entity" => $entity, "completename" => $completename];
-    $p = new PageGenerator(_T(sprintf("Updates on Entity %s", $completename)));
+    $p = new PageGenerator(_T(sprintf("Updates on Entity %s", $completename), "updates"));
 } elseif(!empty($_GET['gid'])) {
     $gid = htmlentities($_GET['gid']);
     $groupname = htmlentities($_GET['groupname']);
@@ -42,7 +42,10 @@ if(!empty($_GET['entity'])) {
     $inventoryid = (!empty($_GET['inventoryid'])) ? htmlentities($_GET['inventoryid']) : "";
     $cn = (!empty($_GET['cn'])) ? htmlentities($_GET['cn']) : "";
     $params = ["machineid" => $id, "cn" => $cn, "inventoryid" => $inventoryid];
-    $p = new PageGenerator(_T(sprintf("Updates on machine %s", $cn)));
+    $p = new PageGenerator(_T(sprintf("Updates on machine %s", $cn), "updates"));
+}
+else{
+    $p = new PageGenerator(_T("Updates"), "updates");
 }
 
 $p->setSideMenu($sidemenu);

medulla-tech/medulla

commit caee669f0e78bf838acd19d224e22b09803bbead

(deploySpecificUpdate) Fix error introduced in commit 7bce94d0b4c1aff1a946f6e0a3a489eac23bdda8
We do not need to redeclare PageGenerator
Fix wrong position of the )

web/modules/updates/updates/deploySpecificUpdate.php
@@ -43,12 +43,10 @@ if(!empty($_GET['entity'])) {
     $cn = (!empty($_GET['cn'])) ? htmlentities($_GET['cn']) : "";
     $params = ["machineid" => $id, "cn" => $cn, "inventoryid" => $inventoryid];
     $p = new PageGenerator(_T(sprintf("Updates on machine %s", $cn), "updates"));
-}
-else{
-    $p = new PageGenerator(_T("Updates"), "updates");
+} else{
+    $p = new PageGenerator(_T("Updates", "updates"));
 }
 
-$p = new PageGenerator();
 $p->setSideMenu($sidemenu);
 $p->display();
 