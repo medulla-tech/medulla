7c03bde742db2ae5a081254c8562687fa040eaf2
Adrien T. <a.thaissen@gmail.com>
2025-02-04 14:53:51 +0100

    (Group) Fix Checking file upload in CSV import
    Added a check to ensure the uploaded file exists before using it,
    which avoids the "Path cannot be empty" error. If this fails, the group is not created.
    
    (cherry picked from commit 1cabaf9ee4fa1484c2416f8acc1dee8597783698)

--- web/modules/dyngroup/dyngroup/import_from_file.php
+++ web/modules/dyngroup/dyngroup/import_from_file.php
@@ -32,70 +32,75 @@ if (!isset($visibility) || $visibility == '') {
     $visibility = 'hidden';
 }
 $elementare = quickGet('elementare');
+$processingDone = false;
 # check group name
-if (isset($groupname) && $groupname != '' && !xmlrpc_group_name_exists($groupname)) {
-    $group = new Group();
-    $group->create($groupname, ($visibility == 'visible'));
-    $content = file($_FILES['importfile']['tmp_name']);
-    $content = array_map('chop', $content);
+if (isset($groupname) && $groupname != '') {
+    if (xmlrpc_group_name_exists($groupname)) {
+        new NotifyWidgetFailure(sprintf(_T("A group already exists with name '%s'", "dyngroup"), $groupname));
+    } else {
+        if (!isset($_FILES['importfile']) || empty($_FILES['importfile']['tmp_name']) || !file_exists($_FILES['importfile']['tmp_name'])) {
+            new NotifyWidgetFailure(_T("Error: Unloaded file or invalid temporary path."));
+        } else {
+            $group = new Group();
+            $group->create($groupname, ($visibility == 'visible'));
+            $content = file($_FILES['importfile']['tmp_name']);
+            $content = array_map('chop', $content);
 
-    $oldSystemCriterion = [
-        "Group",
-        "Online computer",
-        "Installed version",
-        "Contact number",
-        "Contact",
-        "Installed software (specific version)",
-        "Vendors",
-        "Owner of the machine",
-        "Register key",
-        "Register key value",
-        "Peripheral serial", // Disabled because of non recognized mapping in mmc
-        "Printer serial", // Disabled because of non recognized mapping in mmc
-        "Printer name", // Disabled because of non recognized mapping in mmc
-    ];
-    if(in_array($elementare, $oldSystemCriterion)){
-        $group->importMembers($elementare, $content);
-    }
-    else{
-        $group->importCsvColumn($elementare, $content);
-    }
+            $oldSystemCriterion = [
+                "Group",
+                "Online computer",
+                "Installed version",
+                "Contact number",
+                "Contact",
+                "Installed software (specific version)",
+                "Vendors",
+                "Owner of the machine",
+                "Register key",
+                "Register key value",
+                "Peripheral serial", // Disabled because of non recognized mapping in mmc
+                "Printer serial", // Disabled because of non recognized mapping in mmc
+                "Printer name", // Disabled because of non recognized mapping in mmc
+            ];
+            if (in_array($elementare, $oldSystemCriterion)) {
+                $group->importMembers($elementare, $content);
+            } else {
+                $group->importCsvColumn($elementare, $content);
+            }
 
-    new NotifyWidgetSuccess(_T("Group successfully created", "dyngroup"));
-    header("Location: " . urlStrRedirect("base/computers/display", array('gid'=>$group->id)));
-    exit;
-} elseif (xmlrpc_group_name_exists($groupname)) {
-    new NotifyWidgetFailure(sprintf(_T("A group already exists with name '%s'", "dyngroup"), $groupname));
+            new NotifyWidgetSuccess(_T("Group successfully created", "dyngroup"));
+            header("Location: " . urlStrRedirect("base/computers/display", array('gid' => $group->id)));
+            $processingDone = true;
+        }
+    }
 }
 
-$f = new ValidatingForm(array('enctype'=>"multipart/form-data"));
-$f->push(new Table());
+if (!$processingDone) {
+    $f = new ValidatingForm(array('enctype'=>"multipart/form-data"));
+    $f->push(new Table());
 
-$f->add( new TrFormElement(_T("Group name", "dyngroup"), new InputTpl("groupname")), array("required" => True, "value"=>$groupname));
+    $f->add(new TrFormElement(_T("Group name", "dyngroup"), new InputTpl("groupname")), array("required" => true, "value" => $groupname));
 
-$r = new RadioTpl('visible');
-$r->setChoices(array(_T('Yes', 'dyngroup'), _T('No', 'dyngroup')));
-$r->setValues(array('visible', 'hidden'));
-$r->setSelected($visibility);
-$f->add( new TrFormElement(_T("Add shortcut", "dyngroup"), $r), array("required" => True));
+    $r = new RadioTpl('visible');
+    $r->setChoices(array(_T('Yes', 'dyngroup'), _T('No', 'dyngroup')));
+    $r->setValues(array('visible', 'hidden'));
+    $r->setSelected($visibility);
+    $f->add(new TrFormElement(_T("Add shortcut", "dyngroup"), $r), array("required" => true));
 
-$f->add( new TrFormElement(_T("Select the file you want to import", "dyngroup"), new FileTpl('importfile')), array("required" => True));
+    $f->add(new TrFormElement(_T("Select the file you want to import", "dyngroup"), new FileTpl('importfile')), array("required" => true));
 
-$a = getPossiblesCriterionsInMainModule();
-$r = new RadioTpl('elementare');
-$r->setChoices($a);
-$r->setValues($a);
-if (!isset($elementare) || $elementare == '') {
-    $elementare = $a[0];
+    $a = getPossiblesCriterionsInMainModule();
+    $r2 = new RadioTpl('elementare');
+    $r2->setChoices($a);
+    $r2->setValues($a);
+    if (!isset($elementare) || $elementare == '') {
+        $elementare = $a[0];
+    }
+    $r2->setSelected($elementare);
+    $f->add(new TrFormElement(_T("Select what is inside the file", "dyngroup"), $r2), array("required" => true));
+
+    $f->pop();
+    $f->addValidateButton("bimport");
+
+    $f->display();
 }
-$r->setSelected($elementare);
-$f->add( new TrFormElement(_T("Select what is inside the file", "dyngroup"), $r), array("required" => True));
-
-$f->pop();
-
-$f->addValidateButton("bimport");
-
-$f->display();
-
-
 ?>
