fe519ede92fde0b9a63253f247cbc6b8e530a3ca
Nicolas Lécureuil <neoclust@mageia.org>
2025-02-10 23:51:56 +0100

    Remove exclamation point

--- web/modules/xmppmaster/xmppmaster/rescheduleconvergence.php
+++ web/modules/xmppmaster/xmppmaster/rescheduleconvergence.php
@@ -5,10 +5,10 @@ if (isset($_GET['convergence']) && $_GET['convergence'] == 1) {
     $cmd_id = $_GET['cmd_id'];
     xmlrpc_convergence_reschedule($cmd_id);
     header("location: " . urlStrRedirect($_GET['module'] . '/' . $_GET['submod'] . '/' . $_GET['previous']));
-    new NotifyWidgetSuccess(_T("Convergence has been successfully rescheduled !"));
+    new NotifyWidgetSuccess(_T("Convergence has been successfully rescheduled.", "xmppmaster"));
     exit;
 } else {
     header("location: " . urlStrRedirect($_GET['module'] . '/' . $_GET['submod'] . '/' . $_GET['previous']));
-    new NotifyWidgetFailure(_T("An error occurred while rescheduling convergence !"));
+    new NotifyWidgetFailure(_T("An error occurred while rescheduling convergence.", "xmppmaster"));
     exit;
 }
