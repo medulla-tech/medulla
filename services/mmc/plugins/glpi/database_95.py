f819dc27d79761377696f4ad06c27860ebbeefd9
botheis <noel.kneib@siveo.net>
2025-02-19 15:35:35 +0100

    (inject inventory) add a function to get glpi plugins states
    This function gives the state of all glpi plugins or specified plugin.

--- services/mmc/plugins/glpi/database_95.py
+++ services/mmc/plugins/glpi/database_95.py
@@ -7172,6 +7172,24 @@ and glpi_computers.id in %s group by glpi_computers.id;""" % (
 
         return result
 
+    @DatabaseHelper._sessionm
+    def get_plugin_inventory_state(self, session, plugin_name=""):
+        where_clause = ""
+        if plugin_name != "":
+            where_clause = "where directory = '%s'"%plugin_name
+        query = session.execute("""select id, directory, name, state from glpi_plugins %s"""%where_clause)
+
+        result = {}
+
+        for element in query:
+            result[element[1]] = {
+                "id": element[0],
+                "directory": element[1],
+                "name": element[2],
+                "state": "enabled" if element[3] == 1 else "disabled"
+            }
+        return result
+
 
 # Class for SQLalchemy mapping
 class Machine(object):
