c245e8c05f7d1f7eabfba92250924b6daf7ba03d
Nicolas Lécureuil <neoclust@mageia.org>
2025-03-19 17:41:34 +0100

    Remove "regenerate menu" button

--- web/modules/imaging/imaging/header.php
+++ web/modules/imaging/imaging/header.php
@@ -29,30 +29,6 @@ global $CUSTOM_MENU;
 global $IN_GROUP;
 
 
-if ($stateid == $SYNCHROSTATE_TODO) {
-    print "<table><tr><td><b>";
-    print _T('You have modified this target\'s boot menu, if you are done please click on "Generate Menu" to update the computer boot menu.', 'imaging');
-    print "</b></td><td>";
-
-    $f = new ValidatingForm();
-    $f->addButton("bsync", _T("Generate Menu", "imaging"));
-    $f->display();
-    print "</td></tr></table>";
-} elseif (isset($_GET['tab']) && ($_GET['tab'] == 'tabbootmenu' || $_GET['tab'] == 'grouptabbootmenu') && isExpertMode()) {
-    print "<table><tr><td>";
-    print _T('Click on "Force Generation" if you want to force the update of the boot menu', 'imaging');
-    print "</td><td>";
-    $f = new ValidatingForm();
-    $f->addButton("bsync", _T("Force Generation", "imaging"));
-    $f->display();
-    print "</td></tr></table>";
-} else {
-    print "<table><tr><td>";
-    print _T('This target\'s boot menu is up-to-date.', 'imaging');
-    print "</td></tr></table>";
-}
-
-
 if ($CUSTOM_MENU == 1) {
     /* This is a machine custom menu, so we propose to restore default location
      * menu */
