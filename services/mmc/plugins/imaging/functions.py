ee437474a574443d7a5d93572ee02bedf3488089
botheis <noel.kneib@siveo.net>
2025-03-25 12:32:45 +0100

    (imaging) Fix the confusion between entity uuid and imaging server uuid
    During the imaging group creation process we use the location uuid as imaging server uuid. Because of that, all the datas related to the group were wrong!

--- services/mmc/plugins/imaging/functions.py
+++ services/mmc/plugins/imaging/functions.py
@@ -4188,6 +4188,10 @@ class ImagingRpcProxy(RpcProxyI):
             entity = entities[0][0].uuid
         return entity
 
+    def getImagingServerByEntityUUID(self, location):
+        result = ImagingDatabase().getImagingServerByEntityUUID(location)
+        return result.id
+
 
 def chooseMacAddress(ctx, uuid, macs):
     # should pass uuids and the list of uuids
