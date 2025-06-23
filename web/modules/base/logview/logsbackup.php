ffed3da97af4e85d05e54696b69c0557b095a974
Adrien T. <a.thaissen@gmail.com>
2025-06-23 16:26:10 +0200

    (HISTORY) Change BackupPC name in local sidebar
    (cherry picked from commit b0561b28970a1bda0f744c8e595ed2795f8ce7f3)

--- web/modules/base/logview/logsbackup.php
+++ web/modules/base/logview/logsbackup.php
@@ -20,7 +20,7 @@
  * You should have received a copy of the GNU General Public License
  * along with MMC; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- * File : logsbackuppc.php
+ * File : logsbackup.php
  */
 
  /*
