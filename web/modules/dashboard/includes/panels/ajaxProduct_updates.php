0126d28ec3600b34a4822c63acacb3f93b33d9bc
Adrien T. <a.thaissen@gmail.com>
2025-07-09 18:34:39 +0200

    (medulla-update-manager) remove simulate data

--- web/modules/dashboard/includes/panels/ajaxProduct_updates.php
+++ web/modules/dashboard/includes/panels/ajaxProduct_updates.php
@@ -8,19 +8,19 @@ require_once("../../../../includes/i18n.inc.php");
 require_once("../../../../modules/medulla_server/includes/xmlrpc.inc.php");
 
 // Simulates data
-$updates = [
-    'success' => true,
-    'data' => [
-        'header' => ['package', 'description', 'version', 'needs_reboot'],
-        'content' => [
-            ['vim', 'Vim text editor', '9.0.1234-1', false],
-            ['curl', 'Command line tool for transferring data', '7.88.1-1', false],
-            ['bash', 'GNU Bourne Again SHell', '5.1.16-2', true]
-        ]
-    ]
-];
+// $updates = [
+//     'success' => true,
+//     'data' => [
+//         'header' => ['package', 'description', 'version', 'needs_reboot'],
+//         'content' => [
+//             ['vim', 'Vim text editor', '9.0.1234-1', false],
+//             ['curl', 'Command line tool for transferring data', '7.88.1-1', false],
+//             ['bash', 'GNU Bourne Again SHell', '5.1.16-2', true]
+//         ]
+//     ]
+// ];
 
-// $updates = getProductUpdates();
+$updates = getProductUpdates();
 
 $update_count = count($updates['data']['content']);
 $updates_b64 = base64_encode(json_encode($updates));
