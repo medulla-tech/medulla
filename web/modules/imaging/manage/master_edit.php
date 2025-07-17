b2ca7d14b3670f68e93b2ede4d6fba974a2b9c54
Adrien T. <a.thaissen@gmail.com>
2025-07-17 14:31:51 +0200

    (IMAGING) Remove post-install table from edit mode (master & images)
    (cherry picked from commit 877a6fc7e6d87bbfe75b73f65d85fd68d252dc80)

--- web/modules/imaging/manage/master_edit.php
+++ web/modules/imaging/manage/master_edit.php
@@ -1,18 +1,18 @@
 <?php
-
 /*
  * (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
- * (c) 2007-2009 Mandriva, http://www.mandriva.com
- * (c) 2017 Siveo, http://http://www.siveo.net
+ * (c) 2007 Mandriva, http://www.mandriva.com
+ * (c) 2016-2023 Siveo, http://www.siveo.net
+ * (c) 2024-2025 Medulla, http://www.medulla-tech.io
  *
  * $Id$
  *
- * This file is part of Mandriva Management Console (MMC).
+ * This file is part of MMC, http://www.medulla-tech.io
  *
  * MMC is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
  *
  * MMC is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -20,8 +20,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MMC; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * along with MMC; If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 require("localSidebar.php");
@@ -62,12 +62,8 @@ if(safeCount($_POST) == 0) {
 
     $f->pop();
 
-    list($count, $post_installs) = xmlrpc_getAllPostInstallScripts($location);
-    if (isset($master['post_install_scripts'])) {
-        $f = get_post_install_scripts($f, $master['post_install_scripts'], $post_installs);
-    } else {
-        $f = get_post_install_scripts($f, array(), $post_installs);
-    }
+    $post_installs = "";
+    $f = get_post_install_scripts($f, array(), $post_installs);
 
     $f->addButton("bvalid", _T("Validate"));
     $f->display();
