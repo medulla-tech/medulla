877a6fc7e6d87bbfe75b73f65d85fd68d252dc80
Adrien T. <a.thaissen@gmail.com>
2025-07-17 14:31:51 +0200

    (IMAGING) Remove post-install table from edit mode (master & images)

--- web/modules/imaging/includes/post_install_script.php
+++ web/modules/imaging/includes/post_install_script.php
@@ -1,17 +1,18 @@
 <?php
-
 /*
  * (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
- * (c) 2007-2010 Mandriva, http://www.mandriva.com
+ * (c) 2007 Mandriva, http://www.mandriva.com
+ * (c) 2016-2023 Siveo, http://www.siveo.net
+ * (c) 2024-2025 Medulla, http://www.medulla-tech.io
  *
  * $Id$
  *
- * This file is part of Mandriva Management Console (MMC).
+ * This file is part of MMC, http://www.medulla-tech.io
  *
  * MMC is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
  *
  * MMC is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -19,8 +20,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MMC; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * along with MMC; If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 function get_post_install_scripts($f, $post_install_scripts, $post_installs)
@@ -68,8 +69,6 @@ function get_post_install_scripts($f, $post_install_scripts, $post_installs)
 
     $l->disableFirstColumnActionLink();
     $l->setRowsPerPage(safeCount($post_installs));
-
-    $f->add($l);
 
     print_exclusive_orders_js($a_pis_id);
     return $f;
