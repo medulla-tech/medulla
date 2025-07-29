ea40f623d5882eb6e3af55bc64a9ae23f89c4580
Adrien T. <a.thaissen@gmail.com>
2025-07-29 11:43:41 +0200

    (USER) Fix prevent illegal offset when editing user groups

--- web/modules/base/includes/publicFunc.php
+++ web/modules/base/includes/publicFunc.php
@@ -1,16 +1,18 @@
 <?php
 /*
  * (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
- * (c) 2007-2011 Mandriva, http://www.mandriva.com
+ * (c) 2007 Mandriva, http://www.mandriva.com
+ * (c) 2016-2023 Siveo, http://www.siveo.net
+ * (c) 2024-2025 Medulla, http://www.medulla-tech.io
  *
  * $Id$
  *
- * This file is part of Mandriva Management Console (MMC).
+ * This file is part of MMC, http://www.medulla-tech.io
  *
  * MMC is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
  *
  * MMC is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -18,7 +20,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MMC.  If not, see <http://www.gnu.org/licenses/>.
+ * along with MMC; If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 require("modules/base/includes/users.inc.php");
@@ -431,7 +434,7 @@ function _base_baseEdit($FH, $mode)
     }
     $member = array();
     foreach($user_groups as $group) {
-        $member[$group] = $group;
+        $member[$group->scalar] = $group->scalar;
     }
     // get all groups
     $available = array();
