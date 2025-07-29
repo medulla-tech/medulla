9f616be626d26c52f7a863053110a78bb7e66c6e
Adrien T. <a.thaissen@gmail.com>
2025-07-29 14:58:52 +0200

    (QuickAction) Fix localSidebar reference after move to audit module

--- web/modules/xmppmaster/xmppmaster/ActionQuickGroup.php
+++ web/modules/xmppmaster/xmppmaster/ActionQuickGroup.php
@@ -1,15 +1,18 @@
 <?php
 /*
- * (c) 2017 Siveo, http://www.siveo.net
+ * (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+ * (c) 2007 Mandriva, http://www.mandriva.com
+ * (c) 2016-2023 Siveo, http://www.siveo.net
+ * (c) 2024-2025 Medulla, http://www.medulla-tech.io
  *
  * $Id$
  *
- * This file is part of MMC, http://www.siveo.net
+ * This file is part of MMC, http://www.medulla-tech.io
  *
  * MMC is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
  *
  * MMC is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,16 +20,11 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MMC; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * along with MMC; If not, see <http://www.gnu.org/licenses/>.
  *
- * file : xmppmaster/ActionQuickGroup.php
  */
-?>
 
-<?php
-
-require("modules/base/computers/localSidebar.php");
+require("modules/xmppmaster/xmppmaster/localSidebarxmpp.php");
 require("graph/navbar.inc.php");
 require_once("modules/xmppmaster/includes/xmlrpc.php");
 require_once("modules/xmppmaster/includes/html.inc.php");
