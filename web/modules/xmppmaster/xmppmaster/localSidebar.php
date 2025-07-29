751a5527390c014d5f6d1dc4f5e119e952c0e1c6
Adrien T. <a.thaissen@gmail.com>
2025-07-29 11:25:05 +0200

    (QuickAction) Move quick action results to localSidebar in Audit module

--- web/modules/xmppmaster/xmppmaster/localSidebar.php
+++ web/modules/xmppmaster/xmppmaster/localSidebar.php
@@ -1,15 +1,16 @@
 <?php
 /*
- *  (c) 2016 - 2020 siveo, http://www.siveo.net
+ * (c) 2016-2023 Siveo, http://www.siveo.net
+ * (c) 2024-2025 Medulla, http://www.medulla-tech.io
  *
  * $Id$
  *
- * This file is part of MMC, http://www.siveo.net
+ * This file is part of MMC, http://www.medulla-tech.io
  *
  * MMC is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
  *
  * MMC is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,8 +18,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MMC; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * along with MMC; If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 if (isExpertMode()) {
@@ -43,7 +44,4 @@ $sidemenu->addSideMenuItem(
 
 $sidemenu->addSideMenuItem(
     new SideMenuItem(_T("Custom Quick Actions", "xmppmaster"), "xmppmaster", "xmppmaster", "customQA")
-);
-$sidemenu->addSideMenuItem(
-    new SideMenuItem(_T("Quick Action results", "xmppmaster"), "xmppmaster", "xmppmaster", "ActionQuickGroup")
 );
--- web/modules/xmppmaster/xmppmaster/localSidebarxmpp.php
+++ web/modules/xmppmaster/xmppmaster/localSidebarxmpp.php
@@ -1,15 +1,16 @@
 <?
 /*
- *  (c) 2016 siveo, http://www.siveo.net
+ * (c) 2016-2023 Siveo, http://www.siveo.net
+ * (c) 2024-2025 Medulla, http://www.medulla-tech.io
  *
  * $Id$
  *
- * This file is part of MMC, http://www.siveo.net
+ * This file is part of MMC, http://www.medulla-tech.io
  *
  * MMC is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
  *
  * MMC is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,9 +18,9 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MMC; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ * along with MMC; If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 $sidemenu= new SideMenu();
 $sidemenu->setClass("xmppmaster");
@@ -32,4 +33,5 @@ $sidemenu->addSideMenuItem(new SideMenuItem(_T("My teams past tasks", 'xmppmaste
 $sidemenu->addSideMenuItem(new SideMenuItem(_T("My Convergence", 'xmppmaster'), "xmppmaster", "xmppmaster","convergence" ));
 $sidemenu->addSideMenuItem(new SideMenuItem(_T("All users convergence", 'xmppmaster'), "xmppmaster", "xmppmaster", "auditconvergence"));
 $sidemenu->addSideMenuItem(new SideMenuItem(_T("My teams convergence", 'xmppmaster'), "xmppmaster", "xmppmaster", "auditteamconvergence"));
+$sidemenu->addSideMenuItem(new SideMenuItem(_T("Quick Action results", "xmppmaster"), "xmppmaster", "xmppmaster", "ActionQuickGroup"));
 ?>
