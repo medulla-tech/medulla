88d05264157a59ce4dfbf5262226934595760a34
SPO <spointu@medulla-tech.io>
2025-08-04 14:03:49 +0200

    Revert "Merge remote-tracking branch 'origin/dev' into ath-notification"
    This reverts commit 8417355f1fb2e62604d8c0352ad917227f27764f.

--- web/modules/admin/admin/localSidebar.php
+++ web/modules/admin/admin/localSidebar.php
@@ -1,16 +1,13 @@
 <?
-/*
- * (c) 2016-2023 Siveo, http://www.siveo.net
- * (c) 2024-2025 Medulla, http://www.medulla-tech.io
+/**
+ * (c) 2020 Siveo, http://siveo.net
  *
- * $Id$
- *
- * This file is part of MMC, http://www.medulla-tech.io
+ * This file is part of Management Console (MMC).
  *
  * MMC is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * any later version.
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  * MMC is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -18,21 +15,19 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MMC; If not, see <http://www.gnu.org/licenses/>.
- *
+ * along with MMC; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-$sidemenu = new SideMenu();
+$sidemenu= new SideMenu();
 $sidemenu->setClass("admin");
 $sidemenu->addSideMenuItem(
-    new SideMenuItem(_T("List of Relays", "admin"), "admin", "admin", "relaysList")
+    new SideMenuItem(_T("List of Relays","admin"), "admin", "admin", "relaysList")
 );
 $sidemenu->addSideMenuItem(new SideMenuItem(_T("Clusters List", 'admin'), "admin", "admin", "clustersList"));
 $sidemenu->addSideMenuItem(new SideMenuItem(_T("New Cluster", 'admin'), "admin", "admin", "newCluster"));
 $sidemenu->addSideMenuItem(
-    new SideMenuItem(_T("Rules", "admin"), "admin", "admin", "rules")
+    new SideMenuItem(_T("Rules","admin"), "admin", "admin", "rules")
 );
 
-$sidemenu->addSideMenuItem(
-    new SideMenuItem(_T("Entities Management", "admin"), "admin", "admin", "entitiesManagement")
-);
+?>
--- web/modules/admin/includes/xmlrpc.php
+++ web/modules/admin/includes/xmlrpc.php
@@ -1,15 +1,13 @@
 <?php
-/*
- * (c) 2024-2025 Medulla, http://www.medulla-tech.io
+/**
+ * (c) 2020 Siveo, http://siveo.net
  *
- * $Id$
- *
- * This file is part of MMC, http://www.medulla-tech.io
+ * This file is part of Management Console (MMC).
  *
  * MMC is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * any later version.
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  * MMC is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,33 +15,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MMC; If not, see <http://www.gnu.org/licenses/>.
- *
+ * along with MMC; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
-
-function xmlrpc_get_CONNECT_API()
-{
-    return xmlCall("admin.get_CONNECT_API", []);
-}
-
-function xmlrpc_create_organization(
-    $parent_entity_id,
-    $name_new_entity,
-    $name_user,
-    $pwd,
-    $profiles_id,
-    $tag_value = "",
-    $realname = null,
-    $firstname = null
-) {
-    return xmlCall("admin.create_organization", [
-        $parent_entity_id,
-        $name_new_entity,
-        $name_user,
-        $pwd,
-        $profiles_id,
-        $tag_value,
-        $realname,
-        $firstname
-    ]);
-}
+?>
