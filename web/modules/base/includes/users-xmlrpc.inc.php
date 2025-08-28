3a1848c8da48512f173d6870b4484c7c23702a3d
Adrien T. <a.thaissen@gmail.com>
2025-08-28 09:38:11 +0200

    (login) store GLPI user info in session

--- web/modules/base/includes/users-xmlrpc.inc.php
+++ web/modules/base/includes/users-xmlrpc.inc.php
@@ -1,14 +1,18 @@
 <?php
 /*
  * (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
- * (c) 2007-2014 Mandriva, http://www.mandriva.com
+ * (c) 2007 Mandriva, http://www.mandriva.com
+ * (c) 2016-2023 Siveo, http://www.siveo.net
+ * (c) 2024-2025 Medulla, http://www.medulla-tech.io
  *
- * This file is part of Mandriva Management Console (MMC).
+ * $Id$
+ *
+ * This file is part of MMC, http://www.medulla-tech.io
  *
  * MMC is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
  *
  * MMC is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,9 +20,10 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MMC; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * along with MMC; If not, see <http://www.gnu.org/licenses/>.
+ *
  */
+
 include_once('common.inc.php');
 
 function check_auth($login, $pass)
@@ -185,6 +190,11 @@ function maxGID()
 function getAcl($uid)
 {
     return xmlCall("base.getUserAcl", array($uid));
+}
+
+function xmlrpc_get_user_by_name($name)
+{
+    return xmlCall("glpi.get_user_by_name", [$name]);
 }
 
 function setAcl($uid, $aclString)
