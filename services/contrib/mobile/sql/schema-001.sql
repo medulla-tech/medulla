1363241553a95411a87cd0710f84e4084665f68f
SPO <spointu@medulla-tech.io>
2025-12-05 09:29:24 +0100

    (packaging) Fix building mobile plugin - mdm

---
+++ services/contrib/mobile/sql/schema-001.sql
@@ -0,0 +1,38 @@
+--
+--  (c) 2024-2025 Medulla, http://www.medulla-tech.io
+--
+--
+-- This file is part of MMC, http://www.medulla-tech.io
+--
+-- MMC is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- MMC is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with MMC; If not, see <http://www.gnu.org/licenses/.>
+
+START TRANSACTION;
+
+-- ----------------------------------------------------------------------
+-- Database version
+-- ----------------------------------------------------------------------
+
+--
+-- Table structure for table `version`
+--
+
+CREATE TABLE if not exists `version` (
+  `Number` tinyint(4) unsigned NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+INSERT INTO `version` VALUES (1);
+
+COMMIT;
+
