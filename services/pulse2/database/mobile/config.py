13ab0579a51e601a436c5af00f1bf3d00bb2afc5
Hba <barghane.hassan.dev@gmail.com>
2026-02-09 16:06:29 +0100

    refactor(mobile): read hmdm credentials from config file

--- services/pulse2/database/mobile/config.py
+++ services/pulse2/database/mobile/config.py
@@ -4,3 +4,18 @@ from mmc.database.config import DatabaseConfig
 class MobileDatabaseConfig(DatabaseConfig):
     dbname = "mobile"
     dbsection = "database"
+
+    def setup(self, config_file):
+        # read the database configuration
+        DatabaseConfig.setup(self, config_file)
+        
+        # hmdm api config
+        if self.cp.has_section("hmdm"):
+            if self.cp.has_option("hmdm", "url"):
+                self.hmdm_url = self.cp.get("hmdm", "url")
+            
+            if self.cp.has_option("hmdm", "login"):
+                self.hmdm_login = self.cp.get("hmdm", "login")
+            
+            if self.cp.has_option("hmdm", "password"):
+                self.hmdm_password = self.cp.get("hmdm", "password")
