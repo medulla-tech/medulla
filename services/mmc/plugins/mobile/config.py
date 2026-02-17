6d71c2a16ac445311d5d52d1474f1dbdb01aea18
theomeloccaro <theo.meloccaro@gmail.com>
2026-02-17 16:08:59 +0100

    (fix) fixing mobile config; used to load mastering conf instead of mobile conf

--- services/mmc/plugins/mobile/config.py
+++ services/mmc/plugins/mobile/config.py
@@ -1,46 +1,44 @@
 # -*- coding:Utf-8; -*
-# SPDX-FileCopyrightText: 2016-2023 Siveo, http://www.siveo.net
-# SPDX-FileCopyrightText: 2024-2025 Medulla, http://www.medulla-tech.io
+# SPDX-FileCopyrightText: 2016-2025 Medulla, http://www.medulla-tech.io
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# file : mmc/plugins/mastering/config.py
-
 from mmc.support.config import PluginConfig
-from pulse2.database.mastering.config import MasteringDatabaseConfig
+from pulse2.database.mobile.config import MobileDatabaseConfig
 
-class MasteringConfig(PluginConfig,MasteringDatabaseConfig):
-    def __init__(self, name = 'mastering', conffile = None, backend = "database"):
+class MobileConfig(PluginConfig, MobileDatabaseConfig):
+    def __init__(self, name='mobile', conffile=None, backend='database'):
         if not hasattr(self, 'initdone'):
-            PluginConfig.__init__(self, name, conffile, backend = backend, db_table = "mastering_conf")
-            MasteringDatabaseConfig.__init__(self)
+            PluginConfig.__init__(self, name, conffile, backend=backend, db_table='mobile_conf')
+            MobileDatabaseConfig.__init__(self)
             self.initdone = True
 
     def setDefault(self):
         PluginConfig.setDefault(self)
-        #self.confOption = "option1"
-        # ...
 
     def readConf(self):
+        """
+        Read the module configuration (from DB backend or INI file).
+        """
         PluginConfig.readConf(self)
-        if self.backend == "database":
+        if self.backend == 'database':
+            # read DB settings from admin DB backend
             self._load_db_settings_from_backend()
-        elif self.backend == "ini" and self.conffile:
-            MasteringDatabaseConfig.setup(self, self.conffile)
-        self.disable = self.getboolean("main", "disable")
-        self.tempdir = self.get("main", "tempdir")
-        
-        self.master_path = ""
-        if self.has_option("master", "path"):
-            self.master_path = self.get("master", "path")
+        elif self.conffile and self.backend == 'ini':
+            # read DB settings from INI file
+            MobileDatabaseConfig.setup(self, self.conffile)
 
-        # ...
+        self.disable = self.getboolean('main', 'disable')
+        self.tempdir = self.get('main', 'tempdir')
+
+        self.hmdm_url = self.get('hmdm', 'url') if self.has_option('hmdm', 'url') else None
+        self.hmdm_login = self.get('hmdm', 'login') if self.has_option('hmdm', 'login') else None
+        self.hmdm_password = self.get('hmdm', 'password') if self.has_option('hmdm', 'password') else None
 
     def check(self):
-        #if not self.confOption: raise ConfigException("Conf error")
         pass
 
     @staticmethod
     def activate():
-        # Get module config from "/etc/mmc/plugins/mastering.ini"
-        MasteringConfig("mastering", None, "database")
+        # Initialize config using database backend by default
+        MobileConfig('mobile', None, 'database')
         return True
