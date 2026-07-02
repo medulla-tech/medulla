157f0e7a376ac44338863eebc604f9b3584bdc33
SPO <spointu@medulla-tech.io>
2026-07-02 15:40:14 +0200

    (acl) Move remote desktop to own section and fix audit details rights

--- services/contrib/admin/sql/schema-016.sql
+++ services/contrib/admin/sql/schema-016.sql
@@ -29,6 +29,20 @@ INSERT INTO acl_feature_definitions (feature_key, label, description, category, 
 UPDATE acl_feature_definitions set install_types = 'onpremise' WHERE feature_key = 'package_deployment_ro' and acl_entry IN ('xmppmaster#xmppmaster#auditmypastdeploysteam', 'xmppmaster#xmppmaster#auditteam', 'xmppmaster#xmppmaster#auditteamconvergence');
 UPDATE acl_feature_definitions set install_types = 'onpremise' WHERE feature_key = 'acl_management';
 
+-- Move remote desktop acl to a separate section
+UPDATE acl_feature_definitions set label = 'Postes - gestion (suppression, scans...)', description = 'Suppression de machines|Configuration|Scans de sécurité|Exclusions CVE|Paramètres sécurité' WHERE feature_key = 'computer_management_rw';
+DELETE from acl_feature_definitions where feature_key = 'computer_management_rw' and acl_entry = 'base#computers#vnc_client';
+INSERT INTO acl_feature_definitions (feature_key, label, description, category, superadmin_only, acl_entry, access_type, install_types) VALUES
+  ('remote_desktop_rw', 'Postes - PMAD', 'VNC|RDP|SSH', 'security', 0, 'base#computers#vnc_client', 'rw', 'onpremise,saas');
+INSERT IGNORE INTO acl_profile_features (profile_name, feature_key, access_level) VALUES
+  ('Super-Admin', 'remote_desktop_rw', 'rw'),
+  ('Admin', 'remote_desktop_rw', 'rw'),
+  ('Technician', 'remote_desktop_rw', 'rw');
+
+-- Missing ACL for package deployment audit details
+INSERT INTO acl_feature_definitions (feature_key, label, description, category, superadmin_only, acl_entry, access_type, install_types) VALUES
+('package_deployment_ro', 'Déploiement - consultation (audits, historique...)', 'Audits personnels|Audits équipe|Convergences|Kiosque|Export CSV', 'deployment', 0, 'xmppmaster#xmppmaster#viewlogs', 'ro', 'onpremise,saas');
+
 UPDATE version SET Number = 16;
 
 COMMIT;
