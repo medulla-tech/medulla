95deba19615b783ab957a4844b6b5174c5c3f503
SPO <spointu@medulla-tech.io>
2026-07-21 11:45:42 +0200

    (updates) Change up_event_move_to_white_list event schedule to every 5 minutes instead of every 1 minute to allow previous event to finish before next one starts

--- services/contrib/xmppmaster/sql/schema-107.sql
+++ services/contrib/xmppmaster/sql/schema-107.sql
@@ -17,6 +17,15 @@ USE `xmppmaster`;
 ALTER TABLE `xmppmaster`.`deploy`
     CHANGE COLUMN `result` `result` MEDIUMTEXT NULL DEFAULT NULL;
 
+
+-- Change event schedule to every 5 minutes instead of every 1 minute to allow previous event to finish before next one starts
+DROP EVENT IF EXISTS xmppmaster.ev_process_pending_events;
+CREATE EVENT IF NOT EXISTS xmppmaster.ev_process_pending_events
+ON SCHEDULE EVERY 5 MINUTE
+DO
+    CALL xmppmaster.up_event_move_to_white_list();
+
+
 -- ----------------------------------------------------------------------
 -- Database version
 -- ----------------------------------------------------------------------
