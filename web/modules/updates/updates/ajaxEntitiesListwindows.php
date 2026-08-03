101525ac3363f92f2ad492f199d669fbd4d9757c
xdri3n <a.thaissen@gmail.com>
2026-08-03 16:37:58 +0200

    fix(updates): add missing ajaxEntitiesListwindows.php

---
+++ web/modules/updates/updates/ajaxEntitiesListwindows.php
@@ -0,0 +1,16 @@
+<?php
+// SPDX-FileCopyrightText: 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+// SPDX-FileCopyrightText: 2007 Mandriva, http://www.mandriva.com
+// SPDX-FileCopyrightText: 2016-2023 Siveo, http://www.siveo.net
+// SPDX-FileCopyrightText: 2024-2025 Medulla, http://www.medulla-tech.io
+// SPDX-License-Identifier: GPL-3.0-or-later
+// file : web/modules/updates/updates/ajaxEntitiesListwindows.php
+
+require_once("modules/updates/includes/xmlrpc.php");
+require_once("modules/updates/includes/html.inc.php");
+require_once("modules/glpi/includes/xmlrpc.php");
+require_once("modules/xmppmaster/includes/xmlrpc.php");
+require_once("modules/xmppmaster/includes/html.inc.php");
+
+require_once("modules/updates/updates/ajaxEntitiesListwindowsfilter.php");
+?>
