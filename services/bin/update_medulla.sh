7079b694c472a9ab4f94b6aaac5f6e05b33d151c
SPO <spointu@medulla-tech.io>
2026-09-21 12:15:20 +0200

    (update script) Setup prerequisites for MacOS agent generation
    (cherry picked from commit ce6e40f457dfde004e1fc0c3d4095d360ba821de)

--- services/bin/update_medulla.sh
+++ services/bin/update_medulla.sh
@@ -1053,6 +1053,7 @@ update_564_to_565() {
     update_medulla
 
     # Harden reversessh connection
+    str="[=] Hardening reversessh connection..."
     usermod -s /usr/sbin/nologin reversessh
     if [[ $? -ne 0 ]]; then
         str="[x] Error setting nologin shell for reversessh user. Aborting."
@@ -1060,6 +1061,38 @@ update_564_to_565() {
         write_to_log "$str"
         exit 1
     fi
+    str="[v] Hardening reversessh connection completed successfully."
+    echo "$str"
+    write_to_log "$str"
+
+    # Install prerequisites for generating Mac agents
+    str="[=] Installing Mac agent generation prerequisites..."
+    echo "$str"
+    write_to_log "$str"
+    apt install -y build-essential autoconf libtool libxml2-dev libssl-dev libbz2-dev zlib1g-dev pkg-config git
+    if ! command -v xar >/dev/null 2>&1; then
+        rm -rf /tmp/xar
+        git clone https://github.com/tpoechtrager/xar.git /tmp/xar
+        (
+            cd /tmp/xar/xar || exit 1
+            ./autogen.sh
+            ./configure
+            make
+            make install
+        )
+    fi
+    if ! command -v mkbom >/dev/null 2>&1; then
+        rm -rf /tmp/bomutils
+        git clone https://github.com/hogliux/bomutils.git /tmp/bomutils
+        (
+            cd /tmp/bomutils || exit 1
+            make
+            make install
+        )
+    fi
+    str="[v] Mac agent generation prerequisites installed successfully."
+    echo "$str"
+    write_to_log "$str"
 
     echo "5.6.5" > /var/lib/mmc/version
     str="[v] Medulla config update from 5.6.4 to 5.6.5 applied successfully."
